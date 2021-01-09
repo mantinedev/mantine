2affe6f2e8748353d137c1cd76f527356207ded4
Vitaly Rtishchev <rtivital@gmail.com>
2021-01-09 13:24:51 +0300

    [@mantine/core] Add new components exports

--- src/mantine-core/src/index.ts
+++ src/mantine-core/src/index.ts
@@ -1,1 +1,3 @@
 export { default as Button } from './Button/Button';
+export { default as ElementsGroup } from './ElementsGroup/ElementsGroup';
+export { default as TextInput } from './TextInput/TextInput';
