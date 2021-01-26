ea07fc3ef1da4891eb7d1ca34233d02751840652
Vitaly Rtishchev <rtivital@gmail.com>
2021-01-26 13:48:20 +0300

    [core] Change storybook development port

--- scripts/storybook-start.ts
+++ scripts/storybook-start.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import storybook from '@storybook/react/standalone';
 
 storybook({
-  port: 9000,
+  port: 7000,
   mode: 'dev',
   configDir: path.join(__dirname, '../configuration/storybook'),
 });
