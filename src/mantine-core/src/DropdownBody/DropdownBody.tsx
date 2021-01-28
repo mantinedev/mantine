7c5a9ba4bdea7b8e7dc68cffd526034fc750b2fe
Vitaly Rtishchev <rtivital@gmail.com>
2021-01-28 12:11:28 +0300

    [@mantine/core] DropdownBody: Add ref forwaeding

--- src/mantine-core/src/DropdownBody/DropdownBody.tsx
+++ src/mantine-core/src/DropdownBody/DropdownBody.tsx
@@ -1,30 +1,28 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import cx from 'clsx';
 import { DefaultProps } from '@mantine/types';
 import classes from './DropdownBody.styles.less';
 
-interface DropdownBodyProps extends DefaultProps {
+interface DropdownBodyProps extends DefaultProps, Omit<React.HTMLProps<HTMLDivElement>, 'ref'> {
   children: React.ReactNode;
-  elementRef?: React.RefObject<HTMLDivElement>;
   noPadding?: boolean;
 }
 
-export default function DropdownBody({
-  className,
-  children,
-  noPadding = false,
-  elementRef,
-  ...others
-}: DropdownBodyProps) {
-  return (
+const DropdownBody = forwardRef(
+  (
+    { className, children, noPadding = false, ...others }: DropdownBodyProps,
+    ref: React.ForwardedRef<HTMLDivElement>
+  ) => (
     <div
       className={cx(classes.dropdownBody, { [classes.withPadding]: !noPadding }, className)}
-      ref={elementRef}
+      ref={ref}
       {...others}
     >
       {children}
     </div>
-  );
-}
+  )
+);
 
 DropdownBody.displayName = '@mantine/core/DropdownBody';
+
+export default DropdownBody;
