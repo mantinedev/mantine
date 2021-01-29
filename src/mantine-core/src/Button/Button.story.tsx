aa7d653a1464f14b4b6f6b429fb115c9af5928e2
Vitaly Rtishchev <rtivital@gmail.com>
2021-01-29 14:52:28 +0300

    [@mantine/core] Button: Add ElementsGroup to story

--- src/mantine-core/src/Button/Button.story.tsx
+++ src/mantine-core/src/Button/Button.story.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { OPEN_COLOR_THEMES } from '@mantine/types';
+import ElementsGroup from '../ElementsGroup/ElementsGroup';
 import Button from './Button';
 
 const themes = OPEN_COLOR_THEMES.map((theme) => (
-  <div key={theme} style={{ marginTop: 10 }}>
-    <Button theme={theme}>{theme}</Button>
-  </div>
+  <Button theme={theme} key={theme}>
+    {theme}
+  </Button>
 ));
 
 storiesOf('@mantine/core/Button', module).add('General usage', () => (
   <div>
-    <div style={{ marginTop: 10 }}>
-      <Button theme="white">White (default)</Button>
-    </div>
-
-    {themes}
-
-    <div style={{ marginTop: 10 }}>
-      <Button disabled>Disabled</Button>
-    </div>
+    <Button theme="white">White (default)</Button>
+    <ElementsGroup style={{ marginTop: 10 }}>{themes}</ElementsGroup>
+    <Button style={{ marginTop: 10 }} disabled>
+      Disabled
+    </Button>
   </div>
 ));
