e981a1a5c993173c4a7ddeec3585086375bf34ed
Vitaly Rtishchev <rtivital@gmail.com>
2021-02-06 16:48:32 +0300

    [@mantine/core] Title: Add @mantine/theme support

--- src/mantine-core/src/Title/Title.styles.ts
+++ src/mantine-core/src/Title/Title.styles.ts
@@ -1,11 +1,15 @@
 import { createUseStyles } from 'react-jss';
+import { theming, MantineTheme } from '@mantine/theme';
 
-export default createUseStyles({
-  title: {
-    fontFamily: 'Roboto, sans-serif',
-    fontWeight: 'bold',
-    margin: 0,
-    WebkitFontSmoothing: 'antialiased',
-    MozOsxFontSmoothing: 'grayscale',
-  },
-});
+export default createUseStyles(
+  (theme: MantineTheme) => ({
+    title: {
+      WebkitFontSmoothing: 'antialiased',
+      MozOsxFontSmoothing: 'grayscale',
+      fontFamily: theme.fontFamily,
+      fontWeight: 'bold',
+      margin: 0,
+    },
+  }),
+  { theming }
+);
--- src/mantine-core/src/Title/Title.tsx
+++ src/mantine-core/src/Title/Title.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
 import cx from 'clsx';
-import { DefaultProps } from '@mantine/types';
+import { DefaultProps, useMantineTheme } from '@mantine/theme';
 import useStyles from './Title.styles';
 
 interface TitleProps extends DefaultProps, React.HTMLProps<HTMLHeadingElement> {
   order?: 1 | 2 | 3 | 4 | 5 | 6;
 }
 
-const TITLE_STYLES: Record<string, [number, number]> = {
-  h1: [38, 1.2],
-  h2: [30, 1.3],
-  h3: [24, 1.35],
-  h4: [20, 1.4],
-  h5: [16, 1.5],
-  h6: [14, 1.5],
-};
-
-export default function Title({ className, order = 1, style, children, ...others }: TitleProps) {
-  const classes = useStyles();
+export default function Title({
+  className,
+  themeOverride,
+  order = 1,
+  style,
+  children,
+  ...others
+}: TitleProps) {
+  const theme = useMantineTheme(themeOverride);
+  const classes = useStyles({ theme });
   const element = `h${order}`;
-  const [fontSize, lineHeight] = TITLE_STYLES[element];
+  const [fontSize, lineHeight] = theme.headings[element];
 
   return React.createElement(
     element,
