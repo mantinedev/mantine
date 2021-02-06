b84086c8c6231342ae8144e464ac7f4e3549187a
Vitaly Rtishchev <rtivital@gmail.com>
2021-02-06 16:45:42 +0300

    [@mantine/theme] Add Headings styles

--- src/mantine-theme/src/default-theme.ts
+++ src/mantine-theme/src/default-theme.ts
@@ -6,4 +6,12 @@ export const DEFAULT_THEME: MantineTheme = {
   colors: OpenColor,
   fontFamily: 'Roboto, sans-serif',
   primaryColor: 'blue',
+  headings: {
+    h1: [38, 1.2],
+    h2: [30, 1.3],
+    h3: [24, 1.35],
+    h4: [20, 1.4],
+    h5: [16, 1.5],
+    h6: [14, 1.5],
+  },
 };
--- src/mantine-theme/src/types.ts
+++ src/mantine-theme/src/types.ts
@@ -3,11 +3,21 @@ import type OpenColor from 'open-color';
 
 export type MantineColor = Exclude<keyof OpenColor, 'white' | 'black'>;
 
+type HeadingStyle = [number, number];
+
 export interface MantineTheme {
   __mantine_theme: boolean;
   colors: typeof OpenColor;
   fontFamily: string;
   primaryColor: MantineColor;
+  headings: {
+    h1: HeadingStyle;
+    h2: HeadingStyle;
+    h3: HeadingStyle;
+    h4: HeadingStyle;
+    h5: HeadingStyle;
+    h6: HeadingStyle;
+  };
 }
 
 export type MantineThemeOverride = Partial<Omit<MantineTheme, '__mantine_theme'>>;
