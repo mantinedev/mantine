1e4891b4135334e72eb0c89295b3cb50ad1dbfbf
Vitaly Rtishchev <rtivital@gmail.com>
2021-02-08 22:17:23 +0300

    [@mantine/core] Loader: Migrate to sv

--- src/mantine-core/src/Loader/Loader.story.tsx
+++ src/mantine-core/src/Loader/Loader.story.tsx
@@ -5,12 +5,6 @@ import Loader from './Loader';
 
 storiesOf('@mantine/core', module).add('Loader', () => (
   <MantineProvider>
-    <Loader color="teal" style={{ marginTop: 20 }} size={60} />
-    <Loader color="indigo" style={{ marginTop: 20 }} size={42} />
-    <Loader color="lime" style={{ marginTop: 20 }} size={36} />
-    <Loader style={{ marginTop: 20 }} size={30} />
-    <Loader color="cyan" style={{ marginTop: 20 }} size={24} />
-    <Loader color="gray" style={{ marginTop: 20 }} size={18} />
-    <Loader color="orange" style={{ marginTop: 20 }} size={12} />
+    <Loader color="orange" style={{ marginTop: 20 }} />
   </MantineProvider>
 ));
--- src/mantine-core/src/Loader/Loader.tsx
+++ src/mantine-core/src/Loader/Loader.tsx
@@ -1,31 +1,127 @@
 import React from 'react';
-import cx from 'clsx';
-import { DefaultProps, MantineColor, useMantineTheme } from '@mantine/theme';
-import useStyles from './Loader.styles';
+import { DefaultProps, MantineColor, MantineSize, useMantineTheme } from '@mantine/theme';
 
-interface LoaderProps extends DefaultProps, React.HTMLProps<HTMLDivElement> {
-  size?: number;
+interface LoaderProps extends DefaultProps, Omit<React.SVGProps<SVGElement>, 'ref'> {
+  size?: MantineSize | number;
   color?: MantineColor;
 }
 
-export default function Loader({ className, size = 36, color, style, ...others }: LoaderProps) {
-  const theme = useMantineTheme();
-  const classes = useStyles();
+const LOADER_SIZES = {
+  xs: 18,
+  sm: 22,
+  md: 36,
+  lg: 44,
+  xl: 58,
+};
+
+export default function Loader({
+  size = 'md',
+  color,
+  style,
+  themeOverride,
+  ...others
+}: LoaderProps) {
+  const theme = useMantineTheme(themeOverride);
 
   return (
-    <div
+    <svg
+      width={typeof size === 'number' ? size : LOADER_SIZES[size]}
+      fill={theme.colors[color || theme.primaryColor][5]}
+      viewBox="0 0 135 140"
+      xmlns="http://www.w3.org/2000/svg"
       {...others}
-      className={cx(classes.loader, className)}
-      style={{
-        ...style,
-        width: size,
-        height: size,
-        borderWidth: Math.max(Math.round(size / 6), 2),
-        borderColor: theme.colors[color || theme.primaryColor][4],
-        borderTopColor: theme.colors[color || theme.primaryColor][0],
-        borderBottomColor: theme.colors[color || theme.primaryColor][0],
-      }}
-    />
+    >
+      <rect y="10" width="15" height="120" rx="6">
+        <animate
+          attributeName="height"
+          begin="0.5s"
+          dur="1s"
+          values="120;110;100;90;80;70;60;50;40;140;120"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+        <animate
+          attributeName="y"
+          begin="0.5s"
+          dur="1s"
+          values="10;15;20;25;30;35;40;45;50;0;10"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+      </rect>
+      <rect x="30" y="10" width="15" height="120" rx="6">
+        <animate
+          attributeName="height"
+          begin="0.25s"
+          dur="1s"
+          values="120;110;100;90;80;70;60;50;40;140;120"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+        <animate
+          attributeName="y"
+          begin="0.25s"
+          dur="1s"
+          values="10;15;20;25;30;35;40;45;50;0;10"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+      </rect>
+      <rect x="60" width="15" height="140" rx="6">
+        <animate
+          attributeName="height"
+          begin="0s"
+          dur="1s"
+          values="120;110;100;90;80;70;60;50;40;140;120"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+        <animate
+          attributeName="y"
+          begin="0s"
+          dur="1s"
+          values="10;15;20;25;30;35;40;45;50;0;10"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+      </rect>
+      <rect x="90" y="10" width="15" height="120" rx="6">
+        <animate
+          attributeName="height"
+          begin="0.25s"
+          dur="1s"
+          values="120;110;100;90;80;70;60;50;40;140;120"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+        <animate
+          attributeName="y"
+          begin="0.25s"
+          dur="1s"
+          values="10;15;20;25;30;35;40;45;50;0;10"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+      </rect>
+      <rect x="120" y="10" width="15" height="120" rx="6">
+        <animate
+          attributeName="height"
+          begin="0.5s"
+          dur="1s"
+          values="120;110;100;90;80;70;60;50;40;140;120"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+        <animate
+          attributeName="y"
+          begin="0.5s"
+          dur="1s"
+          values="10;15;20;25;30;35;40;45;50;0;10"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+      </rect>
+    </svg>
   );
 }
 
