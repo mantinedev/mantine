b640f6d5de08080802b87988ba498b016e851ab1
Vitaly Rtishchev <rtivital@gmail.com>
2021-02-08 22:23:37 +0300

    [@mantine/core] LoadingOverlay: Add opacity support

--- src/mantine-core/src/LoadingOverlay/LoadingOverlay.styles.ts
+++ src/mantine-core/src/LoadingOverlay/LoadingOverlay.styles.ts
@@ -1,17 +1,33 @@
 import { createUseStyles } from 'react-jss';
+import { theming, MantineTheme } from '@mantine/theme';
 
-export default createUseStyles({
-  loadingOverlay: {
-    pointerEvents: 'none',
-    backgroundColor: 'rgba(255, 255, 255, 0.75)',
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    zIndex: 1000,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default createUseStyles(
+  (theme: MantineTheme) => ({
+    loadingOverlay: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      pointerEvents: 'none',
+      zIndex: 1000,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+
+    background: {
+      backgroundColor: theme.colors.white,
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+    },
+
+    loader: {
+      zIndex: 1000,
+    },
+  }),
+  { theming }
+);
--- src/mantine-core/src/LoadingOverlay/LoadingOverlay.tsx
+++ src/mantine-core/src/LoadingOverlay/LoadingOverlay.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import cx from 'clsx';
-import { DefaultProps } from '@mantine/theme';
+import { DefaultProps, useMantineTheme } from '@mantine/theme';
 import Loader from '../Loader/Loader';
 import useStyles from './LoadingOverlay.styles';
 
 interface LoadingOverlayProps extends DefaultProps, React.HTMLProps<HTMLDivElement> {
   loaderSize?: number;
+  opacity?: number;
   visible: boolean;
 }
 
@@ -13,10 +14,11 @@ export default function LoadingOverlay({
   className,
   visible,
   loaderSize,
+  opacity = 0.75,
   themeOverride,
   ...others
 }: LoadingOverlayProps) {
-  const classes = useStyles();
+  const classes = useStyles({ theme: useMantineTheme(themeOverride) });
 
   if (!visible) {
     return null;
@@ -24,7 +26,8 @@ export default function LoadingOverlay({
 
   return (
     <div className={cx(classes.loadingOverlay, className)} {...others}>
-      <Loader size={loaderSize} themeOverride={themeOverride} />
+      <Loader className={classes.loader} size={loaderSize} themeOverride={themeOverride} />
+      <div style={{ opacity }} className={classes.background} />
     </div>
   );
 }
