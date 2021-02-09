fdac5c7e836833087e931576923af50af6da63e6
Vitaly Rtishchev <rtivital@gmail.com>
2021-02-09 20:48:05 +0300

    [@mantine/demos] TransactionsTable: Add basic table data rendering

--- src/mantine-demos/src/TransactionsTable/TransactionsTable.tsx
+++ src/mantine-demos/src/TransactionsTable/TransactionsTable.tsx
@@ -1,10 +1,45 @@
 import React from 'react';
-import { Paper } from '@mantine/core';
+import { TrashIcon } from '@modulz/radix-icons';
+import { Paper, Table, ActionIcon, Title } from '@mantine/core';
+import * as mockdata from './mockdata';
 
 export default function TransactionsTable() {
+  const transactions = mockdata.transactions.map((transaction) => (
+    <tr key={transaction.id}>
+      <td>{transaction.amount}</td>
+      <td>{transaction.category.toString()}</td>
+      <td>
+        {transaction.date.getDate()} {transaction.date.toLocaleDateString('en', { month: 'short' })}
+      </td>
+      <td>{transaction.title}</td>
+      <td>
+        <ActionIcon color="red" title="Remove transaction">
+          <TrashIcon />
+        </ActionIcon>
+      </td>
+    </tr>
+  ));
+
   return (
-    <Paper shadow="xs" padding="lg">
-      Transactions table
-    </Paper>
+    <div>
+      <Title style={{ marginBottom: 15 }} order={3}>
+        February 2021 spendings
+      </Title>
+
+      <Paper shadow="xs" padding="lg">
+        <Table style={{ tableLayout: 'fixed' }}>
+          <thead>
+            <tr>
+              <th style={{ width: 80 }}>Amount, $</th>
+              <th style={{ width: 100 }}>Category</th>
+              <th style={{ width: 60 }}>Date</th>
+              <th>Description</th>
+              <th style={{ width: 40 }} />
+            </tr>
+          </thead>
+          <tbody>{transactions}</tbody>
+        </Table>
+      </Paper>
+    </div>
   );
 }
