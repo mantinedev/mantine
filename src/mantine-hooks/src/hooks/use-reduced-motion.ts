87ae86d01a89f91fe545807b8974cf186b2a429b
Vitaly Rtishchev <rtivital@gmail.com>
2021-02-16 20:31:54 +0300

    [@mantine/hooks] use-reduced-motion: Migrate to use-media-query hook

--- src/mantine-hooks/src/hooks/use-reduced-motion.ts
+++ src/mantine-hooks/src/hooks/use-reduced-motion.ts
@@ -1,18 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { useMediaQuery } from './use-media-query';
 
 export function useReducedMotion() {
   const [shouldReduceMotion, setShouldReduceMotion] = useState(false);
-  const handler = (event: MediaQueryListEvent) => setShouldReduceMotion(event.matches);
-
-  useEffect(() => {
-    const media = window.matchMedia('(prefers-reduced-motion: reduce)');
-    setShouldReduceMotion(media.matches || false);
-    media.addEventListener('change', handler);
-
-    return () => {
-      media.removeEventListener('change', handler);
-    };
-  }, []);
-
+  useMediaQuery('(prefers-reduced-motion: reduce)', (event) =>
+    setShouldReduceMotion(event.matches)
+  );
   return shouldReduceMotion;
 }
