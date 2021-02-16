600d13ef4692206b14c443d522f62b851ffe0ef7
Vitaly Rtishchev <rtivital@gmail.com>
2021-02-16 20:31:26 +0300

    [@mantine/hooks] use-media-query: Init hook

---
+++ src/mantine-hooks/src/hooks/use-media-query.ts
@@ -0,0 +1,27 @@
+import { useEffect, useRef } from 'react';
+
+type MediaQueryCallback = (event: { matches: boolean; media: string }) => void;
+
+/**
+ * Older versions of Safari (shipped withCatalina and before) do not support addEventListener on matchMedia
+ * https://stackoverflow.com/questions/56466261/matchmedia-addlistener-marked-as-deprecated-addeventlistener-equivalent
+ * */
+function attachMediaListener(query: MediaQueryList, callback: MediaQueryCallback) {
+  try {
+    query.addEventListener('change', callback);
+    return () => query.removeEventListener('change', callback);
+  } catch (e) {
+    query.addListener(callback);
+    return () => query.removeListener(callback);
+  }
+}
+
+export function useMediaQuery(query: string, onChange: MediaQueryCallback) {
+  const queryRef = useRef<MediaQueryList>();
+
+  useEffect(() => {
+    queryRef.current = window.matchMedia(query);
+    onChange({ matches: queryRef.current.matches, media: queryRef.current.media });
+    return attachMediaListener(queryRef.current, onChange);
+  }, []);
+}
--- src/mantine-hooks/src/index.ts
+++ src/mantine-hooks/src/index.ts
@@ -2,4 +2,5 @@ export { useClickOutside } from './hooks/use-click-outside';
 export { useForm } from './hooks/use-form';
 export { useId } from './hooks/use-id';
 export { useListState } from './hooks/use-list-state';
+export { useMediaQuery } from './hooks/use-media-query';
 export { useReducedMotion } from './hooks/use-reduced-motion';
