4f7a457b95a964443b5cde10c80c333eea828fbf
Vitaly Rtishchev <rtivital@gmail.com>
2021-02-17 12:23:29 +0300

    [core] Fix jest configuration

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   setupFilesAfterEnv: ['./configuration/jest/enzyme.setup.js'],
   moduleNameMapper: {
-    '.+\\.(css|styl|less|sass|scss|png|jpg|svg|woff|woff2|gql)$': 'identity-obj-proxy',
     '@mantine/(.*)': '<rootDir>/node_modules/@mantine/$1/src',
-  },
-  transform: {
-    '\\.md$': 'jest-raw-loader',
   },
 };
