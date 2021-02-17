31de7f482db10efaa629d6e42e9460f0b38122ec
Vitaly Rtishchev <rtivital@gmail.com>
2021-02-17 12:48:38 +0300

    [@mantine/core] ActionIcon: Add @mantine/test utils to tests

--- src/mantine-core/src/ActionIcon/ActionIcon.test.tsx
+++ src/mantine-core/src/ActionIcon/ActionIcon.test.tsx
@@ -1,7 +1,14 @@
+import React from 'react';
+import { shallow, unwrapForwardedRef } from '@mantine/tests';
 import { ActionIcon } from './ActionIcon';
 
 describe('@mantine/core/ActionIcon', () => {
   it('has correct displayName', () => {
     expect(ActionIcon.displayName).toEqual('@mantine/core/ActionIcon');
+  });
+
+  it('accepts className from props', () => {
+    const element = unwrapForwardedRef(shallow(<ActionIcon className="test-class-name" />));
+    expect(element.hasClass('test-class-name')).toBe(true);
   });
 });
