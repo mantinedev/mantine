09ba7de1d79b5ca1e0570ac6e5256506afd90fce
Vitaly Rtishchev <rtivital@gmail.com>
2021-02-17 12:36:20 +0300

    [core] Fix jest module name mapper

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   setupFilesAfterEnv: ['./configuration/jest/enzyme.setup.js'],
   moduleNameMapper: {
-    '@mantine/(.*)': '<rootDir>/node_modules/@mantine/$1/src',
+    '@mantine/(.*)': '<rootDir>/src/mantine-$1/src',
   },
 };
