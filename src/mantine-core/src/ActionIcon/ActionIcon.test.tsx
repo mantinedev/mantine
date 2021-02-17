fd6ba269a8ccddcd90ce60883759bc15a461154f
Vitaly Rtishchev <rtivital@gmail.com>
2021-02-17 12:22:42 +0300

    [@mantine/core] ActionIcon: Init tests

---
+++ src/mantine-core/src/ActionIcon/ActionIcon.test.tsx
@@ -0,0 +1,7 @@
+import { ActionIcon } from './ActionIcon';
+
+describe('@mantine/core/ActionIcon', () => {
+  it('has correct displayName', () => {
+    expect(ActionIcon.displayName).toEqual('@mantine/core/ActionIcon');
+  });
+});
