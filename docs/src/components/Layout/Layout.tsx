792307182345429dd45f9394fa39cb324d5ef092
Vitaly Rtishchev <rtivital@gmail.com>
2021-02-19 10:58:03 +0300

    [docs] Move Container component to @mantine/core

--- docs/src/components/Layout/Layout.tsx
+++ docs/src/components/Layout/Layout.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { useMantineTheme } from '@mantine/core';
+import { useMantineTheme, Container } from '@mantine/core';
 import Navbar from '../Navbar/Navbar';
-import Container from '../Container/Container';
 import useStyles from './Layout.styles';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const classes = useStyles({ theme: useMantineTheme() });
   return (
     <main className={classes.layout}>
-      <Container className={classes.inner}>
+      <Container className={classes.inner} size="lg">
         <Navbar className={classes.navbar} />
         <div className={classes.main}>{children}</div>
       </Container>
