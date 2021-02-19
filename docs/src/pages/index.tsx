f63a697ecc4fa6fc19b9468a079db2aea76ee8ba
Vitaly Rtishchev <rtivital@gmail.com>
2021-02-19 18:51:11 +0300

    [docs] Remove mdx from index page

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import Layout from '../components/Layout/Layout';
 import AppContainer from '../components/AppContainer/AppContainer';
-import ButtonDocs from '../docs/Button.mdx';
 
 export default function IndexPage() {
   return (
     <AppContainer>
-      <Layout>
-        <ButtonDocs />
-      </Layout>
+      <Layout>IndexPage</Layout>
     </AppContainer>
   );
 }
