4122da7b057f67bc122e8f57a708ba222dd16244
Vitaly Rtishchev <rtivital@gmail.com>
2021-02-21 12:49:00 +0300

    [@mantine/tests] Turn off region rule for axe

--- src/mantine-tests/src/check-accessibility.tsx
+++ src/mantine-tests/src/check-accessibility.tsx
@@ -5,7 +5,11 @@ export function checkAccessibility(elements: ShallowWrapper[]) {
   expect.extend(toHaveNoViolations);
 
   it('has no accessibility violations', () =>
-    Promise.all(elements.map((element) => axe(element.render().html()))).then((results) => {
+    Promise.all(
+      elements.map((element) =>
+        axe(element.render().html(), { rules: { region: { enabled: false } } })
+      )
+    ).then((results) => {
       results.forEach((result) => {
         expect(result).toHaveNoViolations();
       });
