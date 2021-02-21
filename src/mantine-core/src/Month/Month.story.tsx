d386bef90ea4e6829a3849a1182f5f7a1b0496a7
Vitaly Rtishchev <rtivital@gmail.com>
2021-02-21 11:56:35 +0300

    [@mantine/core] Month: Add better story

--- src/mantine-core/src/Month/Month.story.tsx
+++ src/mantine-core/src/Month/Month.story.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
 import { MantineProvider } from '@mantine/theme';
 import { Month } from './Month';
 
+function WrappedMonth(
+  props: Omit<React.ComponentPropsWithoutRef<typeof Month>, 'selected' | 'onDayClick' | 'month'>
+) {
+  const [value, onChange] = useState(new Date());
+  return <Month month={value} selected={value} onDayClick={onChange} {...props} />;
+}
+
 storiesOf('@mantine/core', module).add('Month', () => (
   <MantineProvider>
-    <Month
-      month={new Date(2020, 2)}
-      selected={new Date(2020, 2, 5)}
-      disableOutsideEvents
-      autoFocus
-    />
+    <WrappedMonth disableOutsideEvents autoFocus />
   </MantineProvider>
 ));
