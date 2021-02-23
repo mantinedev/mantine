ed625805d0e22a0fd7b318a31f9103605162954c
Vitaly Rtishchev <rtivital@gmail.com>
2021-02-23 14:23:11 +0300

    [core] Add jsdom matchMedia mock to jest configuration

---
+++ configuration/jest/jsdom.mocks.js
@@ -0,0 +1,13 @@
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
-  setupFilesAfterEnv: ['./configuration/jest/enzyme.setup.js'],
+  setupFilesAfterEnv: [
+    './configuration/jest/enzyme.setup.js',
+    './configuration/jest/jsdom.mocks.js',
+  ],
   moduleNameMapper: {
     '@mantine/(.*)': '<rootDir>/src/mantine-$1/src',
   },
