fad18a032f4372eddf443dc4ff5dd5c394861f47
Vitaly Rtishchev <rtivital@gmail.com>
2021-02-23 17:12:57 +0300

    [@mantine/theme] Add MantineProvider tests

---
+++ src/mantine-theme/src/MantineProvider/MantineProvider.test.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { DEFAULT_THEME } from '../default-theme';
+import { MantineProvider } from './MantineProvider';
+
+describe('@mantine/theme/MantineProvider', () => {
+  it('has correct displayName', () => {
+    expect(MantineProvider.displayName).toEqual('@mantine/Provider');
+  });
+
+  it('provides given theme to ThemeProvider', () => {
+    const theme = { ...DEFAULT_THEME, primaryColor: 'red' };
+    const element = shallow(<MantineProvider theme={theme}>Mantine provider</MantineProvider>);
+    expect(element.prop('theme')).toEqual({ __mantine_theme: true, ...theme });
+  });
+});
