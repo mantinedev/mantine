300b215f8e16fa8fa7399c8898290d8639d8eb59
Vitaly Rtishchev <rtivital@gmail.com>
2021-02-24 20:29:53 +0300

    [@mantine/notifications] Add custom component example

--- src/mantine-notifications/src/Notifications.story.tsx
+++ src/mantine-notifications/src/Notifications.story.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { CheckIcon } from '@modulz/radix-icons';
 import { storiesOf } from '@storybook/react';
-import { Button, ElementsGroup, MantineProvider } from '@mantine/core';
+import { Button, ElementsGroup, MantineProvider, Text } from '@mantine/core';
 import { useNotifications } from './use-notifications/use-notifications';
 import { NotificationsProvider } from './NotificationsProvider/NotificationsProvider';
 
@@ -20,6 +20,18 @@ function NotificationsDemo() {
         }
       >
         Show default notification
+      </Button>
+
+      <Button
+        variant="outline"
+        onClick={() =>
+          notifications.showNotification({
+            title: 'Custom component notification',
+            message: <Text color="red">Custom component</Text>,
+          })
+        }
+      >
+        Custom component
       </Button>
 
       <Button
