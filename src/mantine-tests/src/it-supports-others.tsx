19ada5019c14fbab28d848166946149ea230d38a
Vitaly Rtishchev <rtivital@gmail.com>
2021-02-25 17:39:10 +0300

    [@mantine/tests] Fix typo in test case name

--- src/mantine-tests/src/it-supports-others.tsx
+++ src/mantine-tests/src/it-supports-others.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from './enzyme';
 
 export function itSupportsOthers(Component: React.ElementType, requiredProps: Record<string, any>) {
-  it('support ...others props', () => {
+  it('supports ...others props', () => {
     const element = shallow(<Component {...requiredProps} data-other-attribute="test" />);
     expect(element.render().attr('data-other-attribute')).toBe('test');
   });
