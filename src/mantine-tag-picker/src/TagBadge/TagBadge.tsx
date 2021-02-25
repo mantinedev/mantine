17ccfbade41fe81385b5eada2346685ff315696e
Vitaly Rtishchev <rtivital@gmail.com>
2021-02-25 12:04:37 +0300

    [@mantine/tag-picker] Add DefaultProps support

--- src/mantine-tag-picker/src/TagBadge/TagBadge.tsx
+++ src/mantine-tag-picker/src/TagBadge/TagBadge.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import cx from 'clsx';
-import { useMantineTheme, MantineThemeOverride } from '@mantine/core';
+import { useMantineTheme, DefaultProps } from '@mantine/core';
 import { TagPickerTag } from '../types';
 import isBrightColor from './is-bright-color';
 import useStyles from './TagBadge.styles';
 
-interface TagBadgeProps {
-  className?: string;
+interface TagBadgeProps extends DefaultProps, React.ComponentPropsWithoutRef<'div'> {
   data: TagPickerTag;
-  themeOverride?: MantineThemeOverride;
 }
 
-export default function TagBadge({ className, data, themeOverride }: TagBadgeProps) {
+export default function TagBadge({
+  className,
+  data,
+  themeOverride,
+  style,
+  ...others
+}: TagBadgeProps) {
   const theme = useMantineTheme(themeOverride);
   const classes = useStyles({ theme });
 
@@ -21,7 +25,9 @@ export default function TagBadge({ className, data, themeOverride }: TagBadgePro
       style={{
         color: isBrightColor(data.color) ? theme.black : theme.white,
         backgroundColor: data.color,
+        ...style,
       }}
+      {...others}
     >
       {data.name}
     </div>
