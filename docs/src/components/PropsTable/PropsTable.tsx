4d632413e585907948f2a7df873b7e7f14021fd1
Vitaly Rtishchev <rtivital@gmail.com>
2021-02-26 17:18:25 +0300

    [docs] Add required props handling

--- docs/src/components/PropsTable/PropsTable.tsx
+++ docs/src/components/PropsTable/PropsTable.tsx
@@ -7,23 +7,45 @@ interface PropsTableProps {
 }
 
 export default function PropsTable({ data }: PropsTableProps) {
-  const rows = Object.keys(data.props).map((propKey) => (
-    <tr key={propKey}>
-      <td>{data.props[propKey].name}</td>
-      <td>{(data.props[propKey].defaultValue && data.props[propKey].defaultValue.value) || '–'}</td>
-      <td style={{ verticalAlign: 'middle' }}>
-        <Text
-          color="blue"
-          size="xs"
-          weight={500}
-          style={{ fontFamily: 'IBM Plex Mono, monospace' }}
-        >
-          {data.props[propKey].type.name}
-        </Text>
-      </td>
-      <td>{data.props[propKey].description}</td>
-    </tr>
-  ));
+  const rows = Object.keys(data.props).map((propKey) => {
+    const prop = data.props[propKey];
+    return (
+      <tr key={propKey}>
+        <td>
+          <Text component="span" size="sm">
+            {prop.name}
+          </Text>
+
+          {prop.required && (
+            <Text component="sup" color="red" size="xs">
+              {' '}
+              *
+            </Text>
+          )}
+        </td>
+        <td>
+          <Text size="sm" color={prop.defaultValue && prop.defaultValue.value ? null : 'gray'}>
+            {(prop.defaultValue && prop.defaultValue.value) || '–'}
+          </Text>
+        </td>
+        <td style={{ verticalAlign: 'middle' }}>
+          <Text
+            color="indigo"
+            size="xs"
+            weight={500}
+            style={{ fontFamily: 'IBM Plex Mono, monospace' }}
+          >
+            {prop.type.name}
+          </Text>
+        </td>
+        <td>
+          <Text component="span" size="sm">
+            {prop.description}
+          </Text>
+        </td>
+      </tr>
+    );
+  });
 
   return (
     <div>
@@ -31,7 +53,7 @@ export default function PropsTable({ data }: PropsTableProps) {
         Component props
       </Title>
 
-      <Table style={{ tableLayout: 'fixed' }} highlighOnHover>
+      <Table style={{ tableLayout: 'fixed' }}>
         <thead>
           <tr>
             <th style={{ width: 100 }}>Name</th>
