b0ef83f8a44cbc30784b3759ee22841825bf2c69
Vitaly Rtishchev <rtivital@gmail.com>
2021-02-26 12:34:16 +0300

    [docs] Add docgen resolving

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Layout from '../components/Layout/Layout';
+import docs from '../../.docgen/mantine-core/Hr.json';
+
+console.log(docs);
 
 export default function IndexPage() {
   return <Layout>IndexPage</Layout>;
