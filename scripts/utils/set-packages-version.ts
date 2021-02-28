dc36fd72241e8e77b33e7f9ca948e1e83e400989
Vitaly Rtishchev <rtivital@gmail.com>
2021-02-28 15:59:04 +0300

    [core] Add util to automatically update package.json of each package

---
+++ scripts/utils/set-packages-version.ts
@@ -0,0 +1,22 @@
+import path from 'path';
+import fs from 'fs-extra';
+
+function writeVersionToPackageJson(filePath: string, version: string) {
+  const current = fs.readJSONSync(filePath);
+  current.version = version;
+  fs.writeJSONSync(filePath, current, { spaces: 2 });
+}
+
+export default function setPackagesVersion(version: string) {
+  const src = path.join(__dirname, '../../src');
+
+  const folders = fs
+    .readdirSync(src)
+    .filter((folder) => fs.pathExistsSync(path.join(src, folder, '/package.json')));
+
+  folders.forEach((folder) => {
+    writeVersionToPackageJson(path.join(src, folder, '/package.json'), version);
+  });
+
+  writeVersionToPackageJson(path.join(__dirname, '../../package.json'), version);
+}
