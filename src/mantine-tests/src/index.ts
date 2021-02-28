d9ca507d1beb7c4438bde0cc9fc4c30bded945a7
Vitaly Rtishchev <rtivital@gmail.com>
2021-02-28 20:17:54 +0300

    [@mantine/tests] Add isHasComposableAttribute shared test

--- src/mantine-tests/src/index.ts
+++ src/mantine-tests/src/index.ts
@@ -1,5 +1,6 @@
 export { shallow, mount, unwrapForwardedRef } from './enzyme';
 export { checkAccessibility } from './check-accessibility';
+export { isHasComposableAttribute } from './it-has-composable-attribute';
 export { itRendersChildren } from './it-renders-children';
 export { itSupportsClassName } from './it-supports-classname';
 export { itSupportsOthers } from './it-supports-others';
---
+++ src/mantine-tests/src/it-has-composable-attribute.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from './enzyme';
+
+export function isHasComposableAttribute(
+  Component: React.ElementType,
+  requiredProps: Record<string, any>
+) {
+  it('has data-mantine-composable attribute', () => {
+    const element = shallow(<Component {...requiredProps} />);
+    expect(element.render().attr('data-mantine-composable')).toBe('true');
+  });
+}
