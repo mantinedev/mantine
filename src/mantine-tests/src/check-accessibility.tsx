7890d4b647f5889d952e237d9a93df0b144eb35e
Vitaly Rtishchev <rtivital@gmail.com>
2021-03-01 19:56:24 +0300

    [@mantine/tests] Fix incorrect async handling in check-accessibility loop

--- src/mantine-tests/src/check-accessibility.tsx
+++ src/mantine-tests/src/check-accessibility.tsx
@@ -1,17 +1,24 @@
 import { ReactWrapper } from 'enzyme';
 import { axe, toHaveNoViolations } from 'jest-axe';
 
+const config = {
+  rules: {
+    region: {
+      enabled: false,
+    },
+  },
+};
+
 export function checkAccessibility(elements: ReactWrapper[]) {
   expect.extend(toHaveNoViolations);
 
-  it('has no accessibility violations', () =>
-    Promise.all(
-      elements.map((element) =>
-        axe(element.getDOMNode(), { rules: { region: { enabled: false } } })
-      )
-    ).then((results) => {
-      results.forEach((result) => {
-        expect(result).toHaveNoViolations();
-      });
-    }));
+  it('has no accessibility violations', async () => {
+    // it does not work any other way
+    /* eslint-disable no-restricted-syntax, no-await-in-loop */
+    for (const element of elements) {
+      const result = await axe(element.getDOMNode(), config);
+      expect(result).toHaveNoViolations();
+    }
+    /* eslint-enable no-restricted-syntax, no-await-in-loop */
+  });
 }
