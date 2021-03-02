5dbb0be651e64f1ab926b085e61dfdf3888a3d0d
Vitaly Rtishchev <rtivital@gmail.com>
2021-03-02 17:01:32 +0300

    [@mantine/types] Add Tuple type for generating repeated tuples

--- src/mantine-types/src/index.ts
+++ src/mantine-types/src/index.ts
@@ -8,3 +8,13 @@ export type ComponentPassThrough<
 export type DeepPartial<T> = {
   [P in keyof T]?: DeepPartial<T[P]>;
 };
+
+type _TupleOf<T, N extends number, R extends unknown[]> = R['length'] extends N
+  ? R
+  : _TupleOf<T, N, [T, ...R]>;
+
+export type Tuple<T, N extends number> = N extends N
+  ? number extends N
+    ? T[]
+    : _TupleOf<T, N, []>
+  : never;
