de558d4c440bf4b601bb7c459f10da7c2620c426
Vitaly Rtishchev <rtivital@gmail.com>
2021-03-05 14:58:29 +0300

    [@mantine/core] LoadingOverlay: Add animations

--- src/mantine-core/src/LoadingOverlay/LoadingOverlay.tsx
+++ src/mantine-core/src/LoadingOverlay/LoadingOverlay.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import cx from 'clsx';
+import { Transition } from 'react-transition-group';
 import { DefaultProps } from '@mantine/theme';
+import { useReducedMotion } from '@mantine/hooks';
 import { Overlay } from '../Overlay/Overlay';
 import { Loader } from '../Loader/Loader';
 import useStyles from './LoadingOverlay.styles';
@@ -14,6 +16,9 @@ interface LoadingOverlayProps extends DefaultProps, React.ComponentPropsWithoutR
 
   /** If visible overlay will take 100% width and height of first parent with relative position and overlay all of its content */
   visible: boolean;
+
+  /** Appear and disappear animation duration */
+  transitionDuration: number;
 }
 
 export function LoadingOverlay({
@@ -22,19 +27,35 @@ export function LoadingOverlay({
   loaderSize,
   opacity = 0.75,
   themeOverride,
+  transitionDuration = 200,
   ...others
 }: LoadingOverlayProps) {
   const classes = useStyles();
-
-  if (!visible) {
-    return null;
-  }
+  const reduceMotion = useReducedMotion();
+  const duration = reduceMotion ? 1 : transitionDuration;
 
   return (
-    <div className={cx(classes.loadingOverlay, className)} {...others}>
-      <Loader className={classes.loader} size={loaderSize} themeOverride={themeOverride} />
-      <Overlay opacity={opacity} />
-    </div>
+    <Transition
+      timeout={duration}
+      unmountOnExit
+      mountOnEnter
+      onEnter={(node: any) => node.offsetHeight}
+      in={visible}
+    >
+      {(state) => (
+        <div
+          className={cx(classes.loadingOverlay, className)}
+          style={{
+            transition: `opacity ${duration}ms cubic-bezier(.51,.3,0,1.21)`,
+            opacity: state === 'entering' || state === 'entered' ? 1 : 0,
+          }}
+          {...others}
+        >
+          <Loader className={classes.loader} size={loaderSize} themeOverride={themeOverride} />
+          <Overlay opacity={opacity} />
+        </div>
+      )}
+    </Transition>
   );
 }
 
