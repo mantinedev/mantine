45621234b3a1959a27d3f63a232448597b2f23d4
Vitaly Rtishchev <rtivital@gmail.com>
2021-03-06 16:45:04 +0300

    [@mantine/core] Month: Add tests

---
+++ src/mantine-core/src/Month/Month.test.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import {
+  checkAccessibility,
+  itSupportsClassName,
+  itSupportsOthers,
+  itSupportsStyle,
+} from '@mantine/tests';
+import { Month } from './Month';
+
+describe('@mantine/core/Month', () => {
+  checkAccessibility([
+    mount(<Month month={new Date()} />),
+    mount(<Month month={new Date()} disableOutsideEvents />),
+    mount(<Month month={new Date()} selected={new Date()} />),
+  ]);
+
+  itSupportsStyle(Month, { month: new Date() });
+  itSupportsClassName(Month, { month: new Date() });
+  itSupportsOthers(Month, { month: new Date() });
+
+  it('renders correct amount of weekdays', () => {
+    const element = shallow(<Month month={new Date()} />);
+    expect(element.render().find('thead th')).toHaveLength(7);
+  });
+
+  it('renders correct amount of days', () => {
+    const element = shallow(<Month month={new Date(2021, 1, 2)} />);
+    expect(element.render().find('tbody tr')).toHaveLength(4);
+    expect(element.render().find('tbody td')).toHaveLength(28);
+  });
+});
