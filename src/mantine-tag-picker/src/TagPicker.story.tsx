808ce204c3bca55b2a4bd9cce5e0a5e35992cb7a
Vitaly Rtishchev <rtivital@gmail.com>
2021-03-06 22:22:44 +0300

    [@mantine/tag-picker] Add better story

--- src/mantine-tag-picker/src/TagPicker.story.tsx
+++ src/mantine-tag-picker/src/TagPicker.story.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
 import { nanoid } from 'nanoid';
 import oc from 'open-color';
-import { MantineProvider, DEFAULT_THEME } from '@mantine/theme';
+import { DEFAULT_THEME } from '@mantine/theme';
 import TagPicker from './TagPickerContainer';
 import { TagPickerTag } from './types';
 
@@ -36,38 +36,36 @@ function TagPickerWrapper(props: TagPickerWrapperProps) {
   const [data, setData] = useState(defaultData);
 
   return (
-    <TagPicker
-      enableCreate
-      value={value}
-      onChange={onChange}
-      data={data}
-      searchPlaceholder="Search categories"
-      colors={colors.map((theme) => ({ name: theme, color: oc[theme][0] }))}
-      description="Select category or create new one"
-      createLabel="+ Create new category"
-      deleteLabel="Delete category"
-      noValueLabel="Not selected"
-      onTagCreate={(values) => {
-        const tag = { ...values, id: nanoid() };
-        setData((current) => [...current, { ...values, id: nanoid() }]);
-        return tag;
-      }}
-      onTagDelete={(id) => setData((current) => current.filter((item) => item.id !== id))}
-      onTagUpdate={(id, values) =>
-        setData((current) => {
-          const copy = [...current];
-          const index = current.findIndex((item) => item.id === id);
-          copy[index] = { id, ...values };
-          return copy;
-        })
-      }
-      {...props}
-    />
+    <div style={{ padding: 50 }}>
+      <TagPicker
+        enableCreate
+        value={value}
+        onChange={onChange}
+        data={data}
+        searchPlaceholder="Search categories"
+        colors={colors.map((theme) => ({ name: theme, color: oc[theme][0] }))}
+        description="Select category or create new one"
+        createLabel="+ Create new category"
+        deleteLabel="Delete category"
+        noValueLabel="Not selected"
+        onTagCreate={(values) => {
+          const tag = { ...values, id: nanoid() };
+          setData((current) => [...current, { ...values, id: nanoid() }]);
+          return tag;
+        }}
+        onTagDelete={(id) => setData((current) => current.filter((item) => item.id !== id))}
+        onTagUpdate={(id, values) =>
+          setData((current) => {
+            const copy = [...current];
+            const index = current.findIndex((item) => item.id === id);
+            copy[index] = { id, ...values };
+            return copy;
+          })
+        }
+        {...props}
+      />
+    </div>
   );
 }
 
-storiesOf('@mantine/tag-picker', module).add('General Usage', () => (
-  <MantineProvider>
-    <TagPickerWrapper />
-  </MantineProvider>
-));
+storiesOf('@mantine/tag-picker', module).add('General Usage', () => <TagPickerWrapper />);
