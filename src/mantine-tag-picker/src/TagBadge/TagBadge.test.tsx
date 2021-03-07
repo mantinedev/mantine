9242a0b07cc562b4b9a0b91621293cd952054994
Vitaly Rtishchev <rtivital@gmail.com>
2021-03-07 15:10:16 +0300

    [@mantine/tag-picker] Add new TagBadge tests

--- src/mantine-tag-picker/src/TagBadge/TagBadge.test.tsx
+++ src/mantine-tag-picker/src/TagBadge/TagBadge.test.tsx
@@ -11,7 +11,7 @@ import TagBadge from './TagBadge';
 
 const data = {
   id: 'test-id',
-  color: '#FFFFFF',
+  background: '#FFFFFF',
   name: 'test-tag',
 };
 
@@ -22,15 +22,27 @@ describe('@mantine/tag-picker/TagBadge', () => {
   checkAccessibility([mount(<TagBadge data={data} />)]);
 
   it('has correct displayName', () => {
-    expect(TagBadge.displayName).toEqual('@mantine/tag-picker/TagBadge');
+    expect(TagBadge.displayName).toBe('@mantine/tag-picker/TagBadge');
   });
 
-  it('has correct color contrast', () => {
-    const white = shallow(<TagBadge data={{ ...data, color: '#FFFFFF' }} />);
-    const black = shallow(<TagBadge data={{ ...data, color: '#000000' }} />);
+  it('has correct default color contrast', () => {
+    const white = shallow(<TagBadge data={{ ...data, background: '#FFFFFF' }} />);
+    const black = shallow(<TagBadge data={{ ...data, background: '#000000' }} />);
 
-    expect(white.render().css('color')).toEqual(DEFAULT_THEME.black);
-    expect(black.render().css('color')).toEqual(DEFAULT_THEME.white);
+    expect(white.render().css('color')).toBe(DEFAULT_THEME.black);
+    expect(black.render().css('color')).toBe(DEFAULT_THEME.white);
+  });
+
+  it('sets correct background color', () => {
+    const element = shallow(<TagBadge data={{ ...data, background: '#CECDCE' }} />);
+    expect(element.render().css('background-color')).toBe('#CECDCE');
+  });
+
+  it('allows to force color', () => {
+    const element = shallow(
+      <TagBadge data={{ ...data, background: '#FFFFFF', color: '#FFFFFF' }} />
+    );
+    expect(element.render().css('color')).toBe('#FFFFFF');
   });
 
   it('renders name', () => {
