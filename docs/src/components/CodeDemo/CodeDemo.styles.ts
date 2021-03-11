ed7a5740eb497e0041918600831938580e128565
Vitaly Rtishchev <rtivital@gmail.com>
2021-03-11 12:27:24 +0300

    [docs] Add github link to code demo

---
+++ docs/src/components/CodeDemo/CodeDemo.styles.ts
@@ -0,0 +1,10 @@
+import { createUseStyles } from 'react-jss';
+
+export default createUseStyles({
+  wrapper: {},
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+});
--- docs/src/components/CodeDemo/CodeDemo.tsx
+++ docs/src/components/CodeDemo/CodeDemo.tsx
@@ -1,21 +1,44 @@
 import React from 'react';
 import { Language } from 'prism-react-renderer';
-import { Paper, Title, useMantineTheme } from '@mantine/core';
+import { Paper, Title, Text } from '@mantine/core';
 import CodeHighlight from '../CodeHighlight/CodeHighlight';
+import useStyles from './CodeDemo.styles';
 
 interface CodeDemoProps {
   code: string;
   language: Language;
   title?: React.ReactNode;
+  demoLink?: string;
   children: React.ReactNode;
 }
 
-export default function CodeDemo({ code, language, children, title }: CodeDemoProps) {
-  const theme = useMantineTheme();
+const URL_BASE = 'https://github.com/mantinedev/mantine/tree/master/docs/src/demos';
+
+export default function CodeDemo({
+  code,
+  language,
+  children,
+  title = 'Demo',
+  demoLink,
+}: CodeDemoProps) {
+  const classes = useStyles();
 
   return (
-    <div style={{ marginBottom: theme.spacing.xl }}>
-      {title && <Title order={2}>{title}</Title>}
+    <div className={classes.wrapper}>
+      <div className={classes.header}>
+        <Title order={2}>{title}</Title>
+        {demoLink && (
+          <Text<'a'>
+            component="a"
+            target="_blank"
+            size="sm"
+            variant="link"
+            href={`${URL_BASE}${demoLink}`}
+          >
+            Full code
+          </Text>
+        )}
+      </div>
       <CodeHighlight code={code} language={language} />
       <Paper padding="md" shadow="xs">
         {children}
