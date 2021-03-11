4b1ecaf6f02b4fe3c82f623ebce9acd86238f8d0
Vitaly Rtishchev <rtivital@gmail.com>
2021-03-11 10:44:55 +0300

    [docs] Remove Roboto font

--- docs/src/components/AppContainer/AppContainer.tsx
+++ docs/src/components/AppContainer/AppContainer.tsx
@@ -1,9 +1,6 @@
 import 'normalize.css';
 import '@fontsource/ibm-plex-mono/500.css';
 import '@fontsource/ibm-plex-mono/700.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 
 import React from 'react';
 import useStyles from './AppContainer.styles';
