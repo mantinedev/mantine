893567ef3a9f5ad48b9ed225b5f6d81aef37b15c
Vitaly Rtishchev <rtivital@gmail.com>
2021-03-15 21:38:32 +0300

    [@mantine/tests] Add option to specify ref property to is-supports-ref shared test

--- src/mantine-tests/src/it-supports-ref.tsx
+++ src/mantine-tests/src/it-supports-ref.tsx
@@ -4,11 +4,12 @@ import { mount } from 'enzyme';
 export function itSupportsRef(
   Component: React.ElementType,
   requiredProps: Record<string, any>,
-  refType: any
+  refType: any,
+  refProp: string = 'ref'
 ) {
   it('supports ref', () => {
     const ref = React.createRef<typeof refType>();
-    mount(<Component {...requiredProps} ref={ref} />);
+    mount(<Component {...requiredProps} {...{ [refProp]: ref }} />);
     expect(ref.current instanceof refType).toBe(true);
   });
 }
