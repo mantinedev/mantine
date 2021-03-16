d8d019514c46d8e47ca00ab2ef0afc254a27b6ec
Vitaly Rtishchev <rtivital@gmail.com>
2021-03-16 12:02:25 +0300

    [@mantine/core] Tabs: Add props descriptions

--- src/mantine-core/src/Tabs/Tabs.tsx
+++ src/mantine-core/src/Tabs/Tabs.tsx
@@ -13,12 +13,25 @@ function clamp(value: number, min: number, max: number) {
 }
 
 interface TabsProps extends DefaultProps, React.ComponentPropsWithoutRef<'div'> {
+  /** <Tab /> components only */
   children: React.ReactNode;
+
+  /** index of initial tab */
   initialTab?: number;
+
+  /** index of active tab, overrides internal state */
   active?: number;
+
+  /** active tab color from theme */
   color?: string;
+
+  /** true if tabs should take all available space */
   grow?: boolean;
+
+  /** tab controls position */
   position?: ElementsGroupPosition;
+
+  /** called when tab control is clicked with tab index */
   onTabChange?(tabIndex: number): void;
 }
 
