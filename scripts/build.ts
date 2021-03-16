aec82538899d4d0378ee9c5319080c9b20e9fbab
Vitaly Rtishchev <rtivital@gmail.com>
2021-03-16 09:27:13 +0300

    [core] Fix typo in build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,5 +3,5 @@ import { argv } from 'yargs';
 import { buildPackage } from './utils/build-package';
 
 (async () => {
-  await buildPackage(argv._[0] as string, argv._[1] === 'analyse');
+  await buildPackage(argv._[0] as string, argv._[1] === 'analyze');
 })();
