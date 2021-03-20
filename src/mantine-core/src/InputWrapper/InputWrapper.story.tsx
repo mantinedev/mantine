612dc31c9a0919544046559803ec198adc8aabd9
Vitaly Rtishchev <rtivital@gmail.com>
2021-03-20 13:53:08 +0300

    [@mantine/core] InputWrapper: Add overflow handling

--- src/mantine-core/src/InputWrapper/InputWrapper.story.tsx
+++ src/mantine-core/src/InputWrapper/InputWrapper.story.tsx
@@ -3,16 +3,62 @@ import { storiesOf } from '@storybook/react';
 import { InputWrapper } from './InputWrapper';
 import { Text } from '../Text/Text';
 
-storiesOf('@mantine/core/InputWrapper', module).add('InputWrapper', () => (
-  <>
-    <InputWrapper
-      label="Input label"
-      required
-      error="Validation error"
-      id="input-wrapper-1"
-      description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis totam harum, a, cumque sed quia asperiores amet libero error atque mollitia sit obcaecati repudiandae? Neque molestiae ipsum rem excepturi corporis."
-    >
-      <Text>Input is located here</Text>
-    </InputWrapper>
-  </>
-));
+const lorem =
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet consequatur vitae commodi ipsa ipsam vel voluptatem eligendi placeat. Deleniti nulla atque officiis mollitia quasi numquam explicabo animi earum quas labore.';
+
+storiesOf('@mantine/core/InputWrapper', module)
+  .add('All parts', () => (
+    <div style={{ padding: 15, maxWidth: 400 }}>
+      <InputWrapper
+        label="Input label"
+        required
+        error="Validation error"
+        id="input-wrapper-1"
+        description="Provide additional information about input here, can be multiline. Dimmed and smaller compared to label."
+      >
+        <Text>Input is located here</Text>
+      </InputWrapper>
+    </div>
+  ))
+  .add('Label only', () => (
+    <div style={{ padding: 15, maxWidth: 400 }}>
+      <InputWrapper label="Input label" required id="input-wrapper-1">
+        <Text>Input is located here</Text>
+      </InputWrapper>
+    </div>
+  ))
+  .add('Error overflow', () => (
+    <div style={{ padding: 15, maxWidth: 300, backgroundColor: '#f3f3f3' }}>
+      <InputWrapper label="Input label" required id="input-wrapper-1" error={lorem}>
+        <Text>Input is located here</Text>
+      </InputWrapper>
+      <InputWrapper
+        label="Input label"
+        required
+        id="input-wrapper-1"
+        error={lorem.replace(/\s/g, '')}
+      >
+        <Text>Input is located here</Text>
+      </InputWrapper>
+    </div>
+  ))
+
+  .add('Label overflow', () => (
+    <div style={{ padding: 15, maxWidth: 300, backgroundColor: '#f3f3f3' }}>
+      <InputWrapper
+        label="This input has so large label that it should get to the next line"
+        required
+        id="input-wrapper-1"
+      >
+        <Text>Input is located here</Text>
+      </InputWrapper>
+
+      <InputWrapper
+        label="ThisInputHasLabelThatIsSoLargeThatItWillOverflowAndBreakLayout"
+        required
+        id="input-wrapper-1"
+      >
+        <Text>Input is located here</Text>
+      </InputWrapper>
+    </div>
+  ));
