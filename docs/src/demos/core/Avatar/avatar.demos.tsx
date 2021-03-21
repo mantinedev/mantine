f98011e73fd650e6ba62c801a945212bf43c9c8c
Vitaly Rtishchev <rtivital@gmail.com>
2021-03-21 18:48:20 +0300

    [docs] Add Avatar letters and icons placeholders docs

--- docs/src/demos/core/Avatar/avatar.demos.tsx
+++ docs/src/demos/core/Avatar/avatar.demos.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Avatar, ElementsGroup } from '@mantine/core';
+import { StarIcon } from '@modulz/radix-icons';
 import CodeDemo from '../../../components/CodeDemo/CodeDemo';
 
 const code = `import React from 'react';
@@ -35,6 +36,70 @@ export function AvatarPlaceholderDemo() {
     <CodeDemo code={placeholderCode} language="tsx">
       <ElementsGroup position="center">
         <Avatar src={null} alt="no image here" />
+      </ElementsGroup>
+    </CodeDemo>
+  );
+}
+
+const iconPlaceholderCode = `import React from 'react';
+import { Avatar } from '@mantine/core';
+import { StarIcon } from '@modulz/radix-icons';
+
+export function AvatarDemo() {
+  return (
+    <ElementsGroup position="center">
+      <Avatar>
+        <StarIcon />
+      </Avatar>
+      <Avatar color="blue">
+        <StarIcon />
+      </Avatar>
+      <Avatar color="red">
+        <StarIcon />
+      </Avatar>
+    </ElemensGroup>
+  );
+}`;
+
+export function AvatarIconPlaceholderDemo() {
+  return (
+    <CodeDemo code={iconPlaceholderCode} language="tsx">
+      <ElementsGroup position="center">
+        <Avatar>
+          <StarIcon style={{ width: 24, height: 24 }} />
+        </Avatar>
+        <Avatar color="blue">
+          <StarIcon style={{ width: 24, height: 24 }} />
+        </Avatar>
+        <Avatar color="red">
+          <StarIcon style={{ width: 24, height: 24 }} />
+        </Avatar>
+      </ElementsGroup>
+    </CodeDemo>
+  );
+}
+
+const lettersPlaceholderCode = `import React from 'react';
+import { Avatar } from '@mantine/core';
+import { StarIcon } from '@modulz/radix-icons';
+
+export function AvatarDemo() {
+  return (
+    <ElementsGroup position="center">
+      <Avatar>MK</Avatar>
+      <Avatar color="green">YT</Avatar>
+      <Avatar color="indigo">VR</Avatar>
+    </ElemensGroup>
+  );
+}`;
+
+export function AvatarLettersPlaceholderDemo() {
+  return (
+    <CodeDemo code={lettersPlaceholderCode} language="tsx">
+      <ElementsGroup position="center">
+        <Avatar>MK</Avatar>
+        <Avatar color="green">YT</Avatar>
+        <Avatar color="indigo">VR</Avatar>
       </ElementsGroup>
     </CodeDemo>
   );
