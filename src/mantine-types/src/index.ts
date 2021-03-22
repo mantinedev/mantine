ce9238fe960b258269857ed04009a1ee5c51915e
Vitaly Rtishchev <rtivital@gmail.com>
2021-03-22 18:52:31 +0300

    [@mantine/types] Remove Subtract type as it is not longer used in components

--- src/mantine-types/src/index.ts
+++ src/mantine-types/src/index.ts
@@ -18,7 +18,3 @@ export type Tuple<T, N extends number> = N extends N
     ? T[]
     : _TupleOf<T, N, []>
   : never;
-
-export type SetDifference<A, B> = A extends B ? never : A;
-export type SetComplement<A, A1 extends A> = SetDifference<A, A1>;
-export type Subtract<T extends T1, T1 extends object> = Pick<T, SetComplement<keyof T, keyof T1>>;
