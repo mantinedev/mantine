51bb335f5c2e577caa3230b69857e5c2d844dea0
Vitaly Rtishchev <rtivital@gmail.com>
2021-03-23 12:04:31 +0300

    [docs] Add badge docs

---
+++ docs/src/demos/core/Badge/badge.demos.tsx
@@ -0,0 +1,124 @@
+import React from 'react';
+import { Badge, Button, ActionIcon, ElementsGroup } from '@mantine/core';
+import { GearIcon } from '@modulz/radix-icons';
+import { DEFAULT_THEME } from '@mantine/theme';
+import CodeDemo from '../../../components/CodeDemo/CodeDemo';
+
+const code = `import React from 'react';
+import { Badge, ElementsGroup } from '@mantine/core';
+
+export function BadgeDemo() {
+  return (
+    <ElementsGroup position="center">
+      <Badge>Light</Badge>
+      <Badge variant="outline">Outline</Badge>
+      <Badge variant="filled">Filled</Badge>
+    </ElementsGroup>
+  );
+}`;
+
+export function BadgeBaseDemo() {
+  return (
+    <CodeDemo code={code} language="tsx">
+      <ElementsGroup position="center">
+        <Badge>Light</Badge>
+        <Badge variant="outline">Outline</Badge>
+        <Badge variant="filled">Filled</Badge>
+      </ElementsGroup>
+    </CodeDemo>
+  );
+}
+
+const fullWidth = `import React from 'react';
+import { Badge, ElementsGroup } from '@mantine/core';
+
+export function BadgeDemo() {
+  return (
+    <>
+      <div style={{ width: 200 }}>
+        <Badge fullWidth>Full width badge</Badge>
+      </div>
+
+      <div style={{ width: 120 }}>
+        <Badge fullWidth>Badge with overflow</Badge>
+      </div>
+    </>
+  );
+}`;
+
+export function BadgeFullWidthDemo() {
+  return (
+    <CodeDemo code={fullWidth} language="tsx">
+      <div style={{ width: 200 }}>
+        <Badge fullWidth>Full width badge</Badge>
+      </div>
+
+      <div style={{ width: 130, marginTop: 20 }}>
+        <Badge fullWidth>Badge with overflow</Badge>
+      </div>
+    </CodeDemo>
+  );
+}
+
+const composeCode = `import React from 'react';
+import { Badge, Button, ActionIcon, ElementsGroup } from '@mantine/core';
+import { GearIcon } from '@modulz/radix-icons';
+
+export function BadgeDemo() {
+  return (
+    <ElementsGroup position="center">
+      <Badge>badge 1</Badge>
+      <Badge>badge 2</Badge>
+      <ActionIcon title="Setting">
+        <GearIcon />
+      </ActionIcon>
+      <Button>Button</Button>
+    </ElementsGroup>
+  );
+}`;
+
+export function BadgeComposeDemo() {
+  return (
+    <CodeDemo code={composeCode} language="tsx">
+      <ElementsGroup position="center">
+        <Badge>badge 1</Badge>
+        <Badge>badge 2</Badge>
+        <ActionIcon title="Setting">
+          <GearIcon />
+        </ActionIcon>
+        <Button>Button</Button>
+      </ElementsGroup>
+    </CodeDemo>
+  );
+}
+
+const getThemes = (props?: any) =>
+  Object.keys(DEFAULT_THEME.colors).map((color) => (
+    <Badge key={color} color={color} {...props}>
+      {color}
+    </Badge>
+  ));
+
+export function BadgeLightVariantDemo() {
+  return (
+    <CodeDemo>
+      <ElementsGroup position="center">{getThemes()}</ElementsGroup>
+    </CodeDemo>
+  );
+}
+
+export function BadgeOutlineVariantDemo() {
+  return (
+    <CodeDemo>
+      <ElementsGroup position="center">{getThemes({ variant: 'outline' })}</ElementsGroup>
+    </CodeDemo>
+  );
+}
+
+export function BadgeFilledVariantDemo() {
+  return (
+    <CodeDemo>
+      <ElementsGroup position="center">{getThemes({ variant: 'filled' })}</ElementsGroup>
+    </CodeDemo>
+  );
+}
