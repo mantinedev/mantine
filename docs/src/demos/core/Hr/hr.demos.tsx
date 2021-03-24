4e1f69cebc2e6797cd1dc0783f12ec7a518d978b
Vitaly Rtishchev <rtivital@gmail.com>
2021-03-24 11:27:55 +0300

    [docs] Add Hr component sizes and colors docs

--- docs/src/demos/core/Hr/hr.demos.tsx
+++ docs/src/demos/core/Hr/hr.demos.tsx
@@ -24,3 +24,55 @@ export function HrBaseDemo() {
     </CodeDemo>
   );
 }
+
+const colorCode = `import React from 'react';
+import { Hr } from '@mantine/core';
+
+export function HrDemo() {
+  return (
+    <>
+      <Hr color="blue" />
+      <Hr color="red" />
+      <Hr color="green" />
+    </>
+  );
+}`;
+
+export function HrColorDemo() {
+  return (
+    <CodeDemo code={colorCode} language="tsx">
+      <Hr color="blue" />
+      <Hr color="red" />
+      <Hr color="green" />
+    </CodeDemo>
+  );
+}
+
+const sizesCode = `import React from 'react';
+import { Hr } from '@mantine/core';
+
+export function HrDemo() {
+  return (
+    <>
+      <Hr size="xs" />
+      <Hr size="sm" />
+      <Hr size="md" />
+      <Hr size="lg" />
+      <Hr size="xl" />
+      <Hr size={10} />
+    </>
+  );
+}`;
+
+export function HrSizesDemo() {
+  return (
+    <CodeDemo code={sizesCode} language="tsx">
+      <Hr size="xs" />
+      <Hr size="sm" />
+      <Hr size="md" />
+      <Hr size="lg" />
+      <Hr size="xl" />
+      <Hr size={10} />
+    </CodeDemo>
+  );
+}
