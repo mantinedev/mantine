4dd84cd287fe2d46d5fc6ac6a6911a296f622c26
Vitaly Rtishchev <rtivital@gmail.com>
2021-03-24 12:58:29 +0300

    [docs] Add base Button component props docs

---
+++ docs/src/demos/core/Button/button-colors.demo.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Button, ElementsGroup } from '@mantine/core';
+import { DEFAULT_THEME } from '@mantine/theme';
+import CodeDemo from '../../../components/CodeDemo/CodeDemo';
+
+const getThemes = (props?: any) =>
+  Object.keys(DEFAULT_THEME.colors).map((color) => (
+    <Button key={color} color={color} {...props}>
+      {color}
+    </Button>
+  ));
+
+export function ButtonFilledVariantDemo() {
+  return (
+    <CodeDemo>
+      <ElementsGroup position="center">{getThemes()}</ElementsGroup>
+    </CodeDemo>
+  );
+}
+
+export function ButtonOutlineVariantDemo() {
+  return (
+    <CodeDemo>
+      <ElementsGroup position="center">{getThemes({ variant: 'outline' })}</ElementsGroup>
+    </CodeDemo>
+  );
+}
+
+export function ButtonLinkVariantDemo() {
+  return (
+    <CodeDemo>
+      <ElementsGroup position="center">{getThemes({ variant: 'link' })}</ElementsGroup>
+    </CodeDemo>
+  );
+}
