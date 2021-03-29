25a4411c64f1d4ba4d7e29841025a46cefe88f59
Vitaly Rtishchev <rtivital@gmail.com>
2021-03-29 12:24:33 +0300

    [docs] Add more demos to use-form hook

---
+++ docs/src/demos/hooks/use-form/use-form.blur-validate-demo.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { TextInput } from '@mantine/core';
+import { useForm } from '@mantine/hooks';
+import CodeDemo from '../../../components/CodeDemo/CodeDemo';
+
+const code = `import React from 'react';
+import { TextInput } from '@mantine/core';
+import { useForm } from '@mantine/hooks';
+
+export function UseFormDemo() {
+  const form = useForm({
+    initialValues: { email: '' },
+    validationRules: {
+      email: (value) => /^\\S+@\\S+$/.test(value),
+    },
+  });
+
+  return (
+    <TextInput
+      required
+      label="Email"
+      error={form.errors.email && 'Please specify valid email'}
+      value={form.values.email}
+      onChange={(event) => form.setFieldValue('email', event.currentTarget.value)}
+    />
+  );
+}
+`;
+
+export function UseFormBlurValidateDemo() {
+  const form = useForm({
+    initialValues: { email: '' },
+    validationRules: {
+      email: (value) => /^\S+@\S+$/.test(value),
+    },
+  });
+
+  return (
+    <CodeDemo code={code} language="tsx">
+      <div style={{ maxWidth: 360, margin: 'auto' }}>
+        <TextInput
+          required
+          label="Email"
+          placeholder="your@email.com"
+          error={form.errors.email && 'Please specify valid email'}
+          value={form.values.email}
+          onChange={(event) => form.setFieldValue('email', event.currentTarget.value)}
+          onBlur={() => form.validateField('email')}
+        />
+      </div>
+    </CodeDemo>
+  );
+}
---
+++ docs/src/demos/hooks/use-form/use-form.server-validation-demo.tsx
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import { TextInput, ElementsGroup, Button, LoadingOverlay } from '@mantine/core';
+import { useForm } from '@mantine/hooks';
+import CodeDemo from '../../../components/CodeDemo/CodeDemo';
+
+const code = `import React from 'react';
+import { TextInput, Button, Checkbox } from '@mantine/core';
+import { useForm } from '@mantine/hooks';
+
+export function UseFormDemo() {
+  const [loading, setLoading] = useState(false);
+  const [serverError, setServerError] = useState<string>(null);
+  const form = useForm({
+    initialValues: { email: 'test@mantine.dev' },
+    validationRules: {
+      email: (value) => /^\\S+@\\S+$/.test(value),
+    },
+  });
+
+  const handleSubmit = (values: typeof form['values']) => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      if (values.email === 'test@mantine.dev') {
+        setServerError('Email already taken');
+        form.setFieldError('email', true);
+      }
+    }, 1500);
+  };
+
+  return (
+    <form onSubmit={form.onSubmit(handleSubmit)} style={{ position: 'relative' }}>
+      <LoadingOverlay visible={loading} />
+      <TextInput
+        required
+        label="Email"
+        placeholder="your@email.com"
+        error={form.errors.email && (serverError || 'Please specify valid email')}
+        value={form.values.email}
+        onChange={(event) => form.setFieldValue('email', event.currentTarget.value)}
+        onFocus={() => {
+          setServerError(null);
+          form.setFieldError('email', false);
+        }}
+      />
+
+      <Button type="submit">Register</Button>
+    </form>
+  );
+}
+`;
+
+export function UseFormServerValidationDemo() {
+  const [loading, setLoading] = useState(false);
+  const [serverError, setServerError] = useState<string>(null);
+
+  const form = useForm({
+    initialValues: { email: 'test@mantine.dev' },
+    validationRules: {
+      email: (value) => /^\S+@\S+$/.test(value),
+    },
+  });
+
+  const handleSubmit = (values: typeof form['values']) => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      if (values.email === 'test@mantine.dev') {
+        setServerError('Email already taken');
+        form.setFieldError('email', true);
+      }
+    }, 1500);
+  };
+
+  return (
+    <CodeDemo code={code} language="tsx">
+      <div style={{ maxWidth: 360, margin: 'auto' }}>
+        <form onSubmit={form.onSubmit(handleSubmit)} style={{ position: 'relative' }}>
+          <LoadingOverlay visible={loading} />
+          <TextInput
+            required
+            label="Email"
+            placeholder="your@email.com"
+            error={form.errors.email && (serverError || 'Please specify valid email')}
+            value={form.values.email}
+            onChange={(event) => form.setFieldValue('email', event.currentTarget.value)}
+            onFocus={() => {
+              setServerError(null);
+              form.setFieldError('email', false);
+            }}
+          />
+
+          <ElementsGroup position="right" style={{ marginTop: 15 }}>
+            <Button type="submit">Register</Button>
+          </ElementsGroup>
+        </form>
+      </div>
+    </CodeDemo>
+  );
+}
