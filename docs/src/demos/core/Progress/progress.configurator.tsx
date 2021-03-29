647680041e2282b048b0f92ddeaa679034b0e25f
Vitaly Rtishchev <rtivital@gmail.com>
2021-03-29 19:08:59 +0300

    [docs] Add Progress component docs

---
+++ docs/src/demos/core/Progress/progress.configurator.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Progress } from '@mantine/core';
+import Configurator from '../../../components/Configurator/Configurator';
+
+function ProgressWrapper(props: React.ComponentPropsWithoutRef<typeof Progress>) {
+  return (
+    <div style={{ width: 400, margin: 'auto' }}>
+      <Progress {...props} />
+    </div>
+  );
+}
+
+const codeTemplate = (props: string) => `<Progress${props} />`;
+
+export function ProgressConfigurator() {
+  return (
+    <Configurator
+      component={ProgressWrapper}
+      codeTemplate={codeTemplate}
+      props={[
+        { name: 'striped', type: 'boolean', initialValue: false, defaultValue: false },
+        { name: 'color', type: 'color', initialValue: 'blue', defaultValue: 'blue' },
+        { name: 'size', type: 'size', initialValue: 'md', defaultValue: 'md' },
+        { name: 'radius', type: 'size', initialValue: 'sm', defaultValue: 'sm' },
+        { name: 'value', type: 'number', initialValue: 50 },
+      ]}
+    />
+  );
+}
