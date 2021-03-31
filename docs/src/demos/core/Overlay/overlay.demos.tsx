b3e105b70350cef7faf99af68f03c57878413bb7
Vitaly Rtishchev <rtivital@gmail.com>
2021-03-31 11:55:00 +0300

    [docs] Add custom component section to Overlay component

--- docs/src/demos/core/Overlay/overlay.demos.tsx
+++ docs/src/demos/core/Overlay/overlay.demos.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Button, ElementsGroup, Overlay, Text } from '@mantine/core';
+import { useMantineTheme } from '@mantine/theme';
 import CodeDemo from '../../../components/CodeDemo/CodeDemo';
 
 const code = `import React, { useState } from 'react';
@@ -45,6 +46,66 @@ export function OverlayBaseDemo() {
       <ElementsGroup position="center" style={{ marginTop: 20 }}>
         <Button onClick={() => setVisible((v) => !v)}>Toggle overlay</Button>
       </ElementsGroup>
+    </CodeDemo>
+  );
+}
+
+const linkCode = `import React from 'react';
+import { Overlay, ElementsGroup, Button } from '@mantine/core';
+import { useMantineTheme } from '@mantine/theme';
+
+export function OverlayDemo() {
+  const theme = useMantineTheme();
+  
+  return (
+    <div style={{
+      position: 'relative',
+      padding: theme.spacing.xl,
+      backgroundColor: theme.colors.gray[0],
+    }}>
+      <Overlay
+        component="a"
+        href="https://mantine.dev"
+        target="_blank"
+        opacity={0}
+      />
+
+      <Text>
+        You cannon interact with content inside this div as it is overlaid with Overlay component.
+        If you click something new tab will be opened
+      </Text>
+
+      <ElementsGroup position="center">
+        <Button>You will not click me, sorry</Button>
+      </ElementsGroup>
+    </div>
+  );
+}`;
+
+export function OverlayLinkDemo() {
+  const theme = useMantineTheme();
+  return (
+    <CodeDemo code={linkCode} language="tsx">
+      <div
+        style={{
+          position: 'relative',
+          padding: theme.spacing.xl,
+          maxWidth: 400,
+          margin: 'auto',
+          backgroundColor: theme.colors.gray[0],
+        }}
+      >
+        <Overlay opacity={0} component="a" href="https://mantine.dev" target="_blank" />
+
+        <Text align="center">
+          You cannon interact with content inside this div as it is overlaid with Overlay component.
+          If you click something new tab will be opened
+        </Text>
+
+        <ElementsGroup position="center" style={{ marginTop: 15 }}>
+          <Button>You will not click me, sorry</Button>
+        </ElementsGroup>
+      </div>
     </CodeDemo>
   );
 }
