c41c30055ec6ab489bf43e0eb5c5003bd3cff421
Vitaly Rtishchev <rtivital@gmail.com>
2021-04-02 17:00:07 +0300

    [docs] Add Text component docs

---
+++ docs/src/demos/core/Text/text.demos.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Text } from '@mantine/core';
+import CodeDemo from '../../../components/CodeDemo/CodeDemo';
+
+const code = `import React from 'react';
+import { Text } from '@mantine/core';
+
+export function TextDemo() {
+  return (
+    <>
+      <Text size="xs">Extra small text</Text>
+      <Text size="sm">Small text</Text>
+      <Text size="md">Default text</Text>
+      <Text size="lg">Large text</Text>
+      <Text size="xl">Extra large text</Text>
+      <Text weight={500}>Semibold</Text>
+      <Text weight={700}>Bold</Text>
+      <Text variant="link" component="a" href="https://mantine.dev">Link variant</Text>
+      <Text color="red">Red text</Text>
+      <Text color="blue">Blue text</Text>
+      <Text color="gray">Gray text</Text>
+      <Text transform="uppercase">Uppercase</Text>
+      <Text transform="capitalize">capitalized text</Text>
+      <Text align="center">Aligned to center</Text>
+      <Text align="right">Aligned to right</Text>
+    </>
+  );
+}`;
+
+export function TextBaseDemo() {
+  return (
+    <CodeDemo code={code} language="tsx">
+      <Text size="xs">Extra small text</Text>
+      <Text size="sm">Small text</Text>
+      <Text size="md">Default text</Text>
+      <Text size="lg">Large text</Text>
+      <Text size="xl">Extra large text</Text>
+      <Text weight={500}>Semibold</Text>
+      <Text weight={700}>Bold</Text>
+      <Text variant="link" component="a" href="https://mantine.dev">
+        Link variant
+      </Text>
+      <Text color="red">Red text</Text>
+      <Text color="blue">Blue text</Text>
+      <Text color="gray">Gray text</Text>
+      <Text transform="uppercase">Uppercase</Text>
+      <Text transform="capitalize">capitalized text</Text>
+      <Text align="center">Aligned to center</Text>
+      <Text align="right">Aligned to right</Text>
+    </CodeDemo>
+  );
+}
