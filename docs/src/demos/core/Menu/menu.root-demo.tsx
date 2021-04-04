5c61f4485cddbda89a4fd5bee89e27a306f83b3b
Vitaly Rtishchev <rtivital@gmail.com>
2021-04-04 17:10:47 +0300

    [docs] Add new Menu component root demo

--- docs/src/demos/core/Menu/menu.root-demo.tsx
+++ docs/src/demos/core/Menu/menu.root-demo.tsx
@@ -1,24 +1,48 @@
 import React from 'react';
-import { MenuItem, Hr } from '@mantine/core';
-import { GearIcon, ChatBubbleIcon, Cross1Icon } from '@modulz/radix-icons';
+import { Menu, MenuItem, Hr } from '@mantine/core';
+import { GearIcon, ChatBubbleIcon, ImageIcon, MagnifyingGlassIcon } from '@modulz/radix-icons';
 import CodeDemo from '../../../components/CodeDemo/CodeDemo';
-import MenuWrapper from './MenuWrapper';
+
+const code = `import React from 'react';
+import { Menu, MenuItem, Hr } from '@mantine/core';
+import {
+  GearIcon,
+  ChatBubbleIcon,
+  ImageIcon,
+  MagnifyingGlassIcon,
+} from '@modulz/radix-icons';
+
+export function MenuDemo() {
+  return (
+    <Menu>
+      <MenuItem icon={<GearIcon />}>Settings</MenuItem>
+      <MenuItem icon={<ChatBubbleIcon />}>Messages</MenuItem>
+      <MenuItem icon={<ImageIcon />}>Gallery</MenuItem>
+      <MenuItem cmd="⌘K" icon={<MagnifyingGlassIcon />}>Search</MenuItem>
+      <Hr />
+      <MenuItem disabled>Delete my data</MenuItem>
+      <MenuItem color="red">Delete account</MenuItem>
+    </Menu>
+  );
+}`;
+
+const iconSize = { width: 14, height: 14 };
 
 export function MenuBaseDemo() {
   return (
-    <CodeDemo>
+    <CodeDemo code={code} language="tsx">
       <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <MenuWrapper style={{ left: 0, top: 0 }}>
-          <MenuItem icon={<GearIcon style={{ width: 12, height: 12 }} />}>Settings</MenuItem>
-          <MenuItem disabled icon={<Cross1Icon style={{ width: 12, height: 12 }} />}>
-            Disabled
+        <Menu>
+          <MenuItem icon={<GearIcon style={iconSize} />}>Settings</MenuItem>
+          <MenuItem icon={<ChatBubbleIcon style={iconSize} />}>Messages</MenuItem>
+          <MenuItem icon={<ImageIcon style={iconSize} />}>Gallery</MenuItem>
+          <MenuItem cmd="⌘K" icon={<MagnifyingGlassIcon style={iconSize} />}>
+            Search
           </MenuItem>
           <Hr />
-          <MenuItem icon={<ChatBubbleIcon style={{ width: 12, height: 12 }} />}>Messages</MenuItem>
-          <MenuItem color="red" icon={<Cross1Icon style={{ width: 12, height: 12 }} />}>
-            Delete
-          </MenuItem>
-        </MenuWrapper>
+          <MenuItem disabled>Delete my data</MenuItem>
+          <MenuItem color="red">Delete account</MenuItem>
+        </Menu>
       </div>
     </CodeDemo>
   );
