5be77ed9ae6c6fa6a525ef98753929f36665bbef
Vitaly Rtishchev <rtivital@gmail.com>
2021-04-05 16:05:09 +0300

    [@mantine/hooks] use-scroll-lock: Unlock scroll on component unmount

--- src/mantine-hooks/src/hooks/use-scroll-lock/use-scroll-lock.ts
+++ src/mantine-hooks/src/hooks/use-scroll-lock/use-scroll-lock.ts
@@ -4,14 +4,22 @@ export function useScrollLock(lock: boolean) {
   const bodyOverflow = useRef<React.CSSProperties['overflow']>(null);
   const locked = useRef(false);
 
+  const unlockScroll = () => {
+    if (locked.current) {
+      locked.current = false;
+      document.body.style.overflow = bodyOverflow.current || '';
+    }
+  };
+
   useEffect(() => {
     if (lock) {
       locked.current = true;
       bodyOverflow.current = document.body.style.overflow;
       document.body.style.overflow = 'hidden';
-    } else if (locked.current) {
-      locked.current = false;
-      document.body.style.overflow = bodyOverflow.current || '';
+    } else {
+      unlockScroll();
     }
+
+    return unlockScroll;
   }, [lock]);
 }
