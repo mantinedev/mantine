905f12d6bc3fc4d1a0e6d812d167f638d5637842
Vitaly Rtishchev <rtivital@gmail.com>
2021-04-05 15:33:54 +0300

    [@mantine/hooks] useDocumentTitle: Add tests

---
+++ src/mantine-hooks/src/hooks/use-document-title/use-document-title.test.ts
@@ -0,0 +1,22 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { useDocumentTitle } from './use-document-title';
+
+describe('@mantine/hooks/use-scroll-lock', () => {
+  it('sets given value as document.title', () => {
+    renderHook(() => useDocumentTitle('test-title'));
+    expect(document.title).toBe('test-title');
+  });
+
+  it('does not change document.title if called with empty string', () => {
+    document.title = 'test-title';
+    renderHook(() => useDocumentTitle(''));
+    expect(document.title).toBe('test-title');
+    renderHook(() => useDocumentTitle('  \t\n'));
+    expect(document.title).toBe('test-title');
+  });
+
+  it('trims value before setting to document.title', () => {
+    renderHook(() => useDocumentTitle('  test-title\t\n   '));
+    expect(document.title).toBe('test-title');
+  });
+});
