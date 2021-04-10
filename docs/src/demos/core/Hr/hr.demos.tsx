2c0d4970655e2cd8739ac01ebed273cef4094daa
Vitaly Rtishchev <rtivital@gmail.com>
2021-04-10 12:23:33 +0300

    [docs] Fix Hr component demo margins

--- docs/src/demos/core/Hr/hr.demos.tsx
+++ docs/src/demos/core/Hr/hr.demos.tsx
@@ -19,8 +19,8 @@ export function HrBaseDemo() {
   return (
     <CodeDemo code={code} language="tsx">
       <Hr />
-      <Hr variant="solid" />
-      <Hr variant="dotted" />
+      <Hr variant="solid" style={{ marginTop: 10 }} />
+      <Hr variant="dotted" style={{ marginTop: 10 }} />
     </CodeDemo>
   );
 }
@@ -42,8 +42,8 @@ export function HrColorDemo() {
   return (
     <CodeDemo code={colorCode} language="tsx">
       <Hr color="blue" />
-      <Hr color="red" />
-      <Hr color="green" />
+      <Hr color="red" style={{ marginTop: 10 }} />
+      <Hr color="green" style={{ marginTop: 10 }} />
     </CodeDemo>
   );
 }
@@ -68,11 +68,11 @@ export function HrSizesDemo() {
   return (
     <CodeDemo code={sizesCode} language="tsx">
       <Hr size="xs" />
-      <Hr size="sm" />
-      <Hr size="md" />
-      <Hr size="lg" />
-      <Hr size="xl" />
-      <Hr size={10} />
+      <Hr size="sm" style={{ marginTop: 10 }} />
+      <Hr size="md" style={{ marginTop: 10 }} />
+      <Hr size="lg" style={{ marginTop: 10 }} />
+      <Hr size="xl" style={{ marginTop: 10 }} />
+      <Hr size={10} style={{ marginTop: 10 }} />
     </CodeDemo>
   );
 }
