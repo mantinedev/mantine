80fc3a535983a7f402e6d6aa52362d297edbe701
Vitaly Rtishchev <rtivital@gmail.com>
2021-04-11 12:39:31 +0300

    [core] Add option to build all packages with build npm script

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,7 +1,12 @@
 import { argv } from 'yargs';
 
 import { buildPackage } from './utils/build-package';
+import { buildAllPackages } from './utils/build-all-packages';
 
 (async () => {
-  await buildPackage(argv._[0] as string, argv._[1] === 'analyze');
+  if (argv._[0] === 'all') {
+    await buildAllPackages();
+  } else {
+    await buildPackage(argv._[0] as string, argv._[1] === 'analyze');
+  }
 })();
