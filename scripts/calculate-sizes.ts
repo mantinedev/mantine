e06718347c5590c59cbbf2c89b4a19c0dc901dbd
Vitaly Rtishchev <rtivital@gmail.com>
2021-04-11 10:23:16 +0300

    [core] Add script for packages sizes calculation

---
+++ scripts/calculate-sizes.ts
@@ -0,0 +1,40 @@
+import path from 'path';
+import fs from 'fs-extra';
+import gzipSize from 'gzip-size';
+
+const REPO_ROOT = path.join(__dirname, '../');
+
+const PACKAGES = [
+  'mantine-theme',
+  'mantine-hooks',
+  'mantine-core',
+  'mantine-notifications',
+  'mantine-tag-picker',
+];
+
+function truncateNumber(number: number) {
+  return parseFloat(number.toFixed(2));
+}
+
+function getPackageSize(packageName: string) {
+  const libPath = path.join(REPO_ROOT, 'src', packageName, 'dist/lib.js');
+
+  if (!fs.existsSync(libPath)) {
+    return { size: 0, gzip: 0 };
+  }
+
+  const file = fs.readFileSync(libPath).toString('utf-8');
+  const size = fs.statSync(libPath).size / 1024;
+
+  return {
+    size: truncateNumber(size),
+    gzip: truncateNumber(gzipSize.sync(file) / 1024),
+  };
+}
+
+const results = PACKAGES.reduce((acc, p) => {
+  acc[p] = getPackageSize(p);
+  return acc;
+}, {});
+
+fs.writeJSONSync(path.join(REPO_ROOT, 'docs/.docgen/sizes.json'), results, { spaces: 2 });
