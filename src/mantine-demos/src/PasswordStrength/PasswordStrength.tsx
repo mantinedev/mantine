698302f2c2e45152be04909991359be6333d8cf7
Vitaly Rtishchev <rtivital@gmail.com>
2021-04-13 17:18:19 +0300

    [@mantine/demos] PasswordStrength: Init component

---
+++ src/mantine-demos/src/PasswordStrength/PasswordStrength.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import { CheckIcon, Cross1Icon } from '@modulz/radix-icons';
+import { PasswordInput, Progress, Transition, Text } from '@mantine/core';
+
+function PasswordRequirement({ meets, label }: { meets: boolean; label: string }) {
+  return (
+    <Text
+      color={meets ? 'teal' : 'red'}
+      style={{ display: 'flex', alignItems: 'center', marginTop: 7 }}
+      size="sm"
+    >
+      {meets ? <CheckIcon /> : <Cross1Icon />} <span style={{ marginLeft: 10 }}>{label}</span>
+    </Text>
+  );
+}
+
+const requirements = [
+  { re: /[0-9]/, label: 'Includes number' },
+  { re: /[a-z]/, label: 'Includes lowercase letter' },
+  { re: /[A-Z]/, label: 'Includes uppercase letter' },
+  { re: /[$&+,:;=?@#|'<>.^*()%!-]/, label: 'Includes special symbol' },
+];
+
+function getStrength(password: string) {
+  let multiplier = password.length > 5 ? 0 : 1;
+
+  requirements.forEach((requirement) => {
+    if (!requirement.re.test(password)) {
+      multiplier += 1;
+    }
+  });
+
+  return Math.max(100 - (100 / (requirements.length + 1)) * multiplier, 10);
+}
+
+export function PasswordStrength() {
+  const [value, setValue] = useState('');
+  const [showInfo, setShowInfo] = useState(false);
+  const checks = requirements.map((requirement, index) => (
+    <PasswordRequirement key={index} label={requirement.label} meets={requirement.re.test(value)} />
+  ));
+
+  const strength = getStrength(value);
+  const color = strength === 100 ? 'teal' : strength > 50 ? 'yellow' : 'red';
+
+  return (
+    <div>
+      <PasswordInput
+        label="Your password"
+        placeholder="Your password"
+        description="Strong password should include letters in lower and uppercase, at least 1 number, at least 1 special symbol"
+        value={value}
+        onChange={(event) => setValue(event.currentTarget.value)}
+        onFocus={() => setShowInfo(true)}
+        onBlur={() => setShowInfo(false)}
+      />
+
+      <div style={{ position: 'relative', height: 120 }}>
+        <Transition transition="fade" mounted={showInfo}>
+          {(styles) => (
+            <div
+              style={{
+                marginTop: 10,
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                right: 0,
+                ...styles,
+              }}
+            >
+              <Progress color={color} value={getStrength(value)} size={3} />
+              <PasswordRequirement
+                label="Includes at least 6 characters"
+                meets={value.length > 5}
+              />
+              {checks}
+            </div>
+          )}
+        </Transition>
+      </div>
+    </div>
+  );
+}
--- src/mantine-demos/src/index.ts
+++ src/mantine-demos/src/index.ts
@@ -1,3 +1,4 @@
 export { AuthenticationForm } from './AuthenticationForm/AuthenticationForm';
+export { PasswordStrength } from './PasswordStrength/PasswordStrength';
 export { TodoList } from './TodoList/TodoList';
 export { TransactionsTable } from './TransactionsTable/TransactionsTable';
