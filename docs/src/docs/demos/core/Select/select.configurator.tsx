f3fa4cce912d71660ef3a25469ff937fef9c181b
Vitaly Rtishchev <rtivital@gmail.com>
2021-04-14 10:55:11 +0300

    [docs] Add Select component docs

---
+++ docs/src/docs/demos/core/Select/select.configurator.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Select } from '@mantine/core';
+import Configurator from '../../../../components/Configurator/Configurator';
+
+const data = [
+  { value: 'react', label: 'React' },
+  { value: 'vue', label: 'Vue' },
+  { value: 'ng', label: 'Angular', disabled: true },
+  { value: 'svelte', label: 'Svelte' },
+];
+
+const codeTemplate = (props: string) => `<Select
+  data={[
+    { value: 'react', label: 'React' },
+    { value: 'vue', label: 'Vue' },
+    { value: 'ng', label: 'Angular', disabled: true },
+    { value: 'svelte', label: 'Svelte' },
+  ]}
+ ${props}
+/>`;
+
+export function SelectConfigurator() {
+  return (
+    <Configurator
+      component={(props: any) => <Select {...props} data={data} />}
+      codeTemplate={codeTemplate}
+      multiline
+      props={[
+        { name: 'placeholder', type: 'string', initialValue: 'Pick one' },
+        {
+          name: 'label',
+          type: 'string',
+          initialValue: 'Select your favorite framework/library',
+        },
+        {
+          name: 'description',
+          type: 'string',
+          initialValue: 'This is anonymous, you can safely pick Angular',
+        },
+        {
+          name: 'error',
+          type: 'string',
+          initialValue: '',
+        },
+        {
+          name: 'variant',
+          type: 'select',
+          data: [
+            { label: 'default', value: 'default' },
+            { label: 'filled', value: 'filled' },
+            { label: 'unstyled', value: 'unstyled' },
+          ],
+          initialValue: 'default',
+          defaultValue: 'default',
+        },
+        { name: 'radius', type: 'size', initialValue: 'sm', defaultValue: 'sm' },
+        { name: 'disabled', type: 'boolean', initialValue: false, defaultValue: false },
+        {
+          name: 'required',
+          type: 'boolean',
+          initialValue: true,
+          defaultValue: false,
+        },
+      ]}
+    />
+  );
+}
