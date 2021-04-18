a25c4bbaf7ad3989047d4f262247fab2c29ac9bb
Vitaly Rtishchev <rtivital@gmail.com>
2021-04-18 19:36:13 +0300

    [@mantine/theme] Add dark theme support to get-focus-styles

--- src/mantine-theme/src/utils/get-focus-styles/get-focus-styles.ts
+++ src/mantine-theme/src/utils/get-focus-styles/get-focus-styles.ts
@@ -5,7 +5,9 @@ export function getFocusStyles(theme: MantineTheme) {
     outline: 0,
 
     '&:focus': {
-      boxShadow: `0 0 0 2px ${theme.white}, 0 0 0 4px ${theme.colors[theme.primaryColor][5]}`,
+      boxShadow: `0 0 0 2px ${
+        theme.colorScheme === 'dark' ? theme.colors.dark[9] : theme.white
+      }, 0 0 0 4px ${theme.colors[theme.primaryColor][5]}`,
     },
 
     '&:focus:not(:focus-visible)': {
