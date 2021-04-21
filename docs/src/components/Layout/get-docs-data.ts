37d66865bb416087314bdd1c6ebf288ad9c3f05e
Vitaly Rtishchev <rtivital@gmail.com>
2021-04-21 19:37:45 +0300

    [docs] Init tag picker page

--- docs/src/components/Layout/get-docs-data.ts
+++ docs/src/components/Layout/get-docs-data.ts
@@ -15,7 +15,7 @@ export interface DocItem {
 
 export type DocsData = Record<string, DocItem[]>;
 
-const order = ['guides', 'mantine-hooks', 'mantine-core'];
+const order = ['guides', 'mantine-hooks', 'mantine-core', 'mantine-tag-picker'];
 
 export default function getDocsData(query: DocsQuery): DocsData {
   const results = query.allMdx.edges.reduce((acc: DocsData, item) => {
