83ecd2e5fc73f8c287bfa2ad06976f85fe7de36c
Vitaly Rtishchev <rtivital@gmail.com>
2021-04-23 15:07:40 +0300

    [docs] Add Pacakges table to index page

---
+++ docs/src/home-page/PackagesTable/data.ts
@@ -0,0 +1,27 @@
+export default [
+  {
+    package: '@mantine/core',
+    description: 'Components library',
+    link: '/core/getting-started/',
+  },
+  {
+    package: '@mantine/hooks',
+    description: 'Hooks collection for state and UI management',
+    link: '/hooks/getting-started/',
+  },
+  {
+    package: '@mantine/theme',
+    description: 'Customize and extends theme: colors, fonts, spacing and other properties',
+    link: '/pages/theming/',
+  },
+  {
+    package: '@mantine/notifications',
+    description: 'Notifications system',
+    link: '/others/notifications/',
+  },
+  {
+    package: '@mantine/tag-picker',
+    description: 'Notion like tag picker component',
+    link: '/others/tag-picker/',
+  },
+];
