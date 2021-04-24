17b9381a096af1d60977b0763efda89d08cafde9
Vitaly Rtishchev <rtivital@gmail.com>
2021-04-24 13:27:57 +0300

    [@mantine/demos] JsonInput: Init component

---
+++ src/mantine-demos/src/JsonInput/JsonInput.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { Textarea } from '@mantine/core';
+import { useMantineTheme } from '@mantine/theme';
+
+interface JsonInputProps
+  extends Omit<React.ComponentPropsWithoutRef<typeof Textarea>, 'error' | 'onChange'> {
+  value: string;
+  onChange(value: string): void;
+}
+
+export function JsonInput({
+  value,
+  onChange,
+  onBlur,
+  onFocus,
+  inputStyle,
+  ...others
+}: JsonInputProps) {
+  const [valid, setValid] = useState(true);
+  const theme = useMantineTheme();
+
+  return (
+    <Textarea
+      autosize
+      value={value}
+      minRows={2}
+      error={!valid && 'Invalid json'}
+      inputStyle={{
+        fontFamily: theme.fontFamilyMonospace,
+        fontSize: theme.fontSizes.xs,
+        ...inputStyle,
+      }}
+      onChange={(event) => onChange(event.currentTarget.value)}
+      onFocus={(event) => {
+        setValid(true);
+        typeof onFocus === 'function' && onFocus(event);
+      }}
+      onBlur={(event) => {
+        typeof onBlur === 'function' && onBlur(event);
+        try {
+          onChange(JSON.stringify(JSON.parse(event.currentTarget.value), null, 2));
+        } catch (e) {
+          setValid(false);
+        }
+      }}
+      {...others}
+    />
+  );
+}
--- src/mantine-demos/src/index.ts
+++ src/mantine-demos/src/index.ts
@@ -1,5 +1,6 @@
 export { AuthenticationForm } from './AuthenticationForm/AuthenticationForm';
 export { IntermediateCheckbox } from './IntermediateCheckbox/IntermediateCheckbox';
+export { JsonInput } from './JsonInput/JsonInput';
 export { PasswordStrength } from './PasswordStrength/PasswordStrength';
 export { TodoList } from './TodoList/TodoList';
 export { TransactionsTable } from './TransactionsTable/TransactionsTable';
