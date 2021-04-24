b417730c53c2c1664fff610e770d2c0c5ead289d
Vitaly Rtishchev <rtivital@gmail.com>
2021-04-24 18:04:02 +0300

    [@mantine/theme] Fix focus styles in Firefox

--- src/mantine-theme/src/utils/get-focus-styles/get-focus-styles.ts
+++ src/mantine-theme/src/utils/get-focus-styles/get-focus-styles.ts
@@ -2,9 +2,8 @@ import { MantineTheme } from '../../types';
 
 export function getFocusStyles(theme: MantineTheme) {
   return {
-    outline: 0,
-
     '&:focus': {
+      outline: 'none',
       boxShadow: `0 0 0 2px ${
         theme.colorScheme === 'dark' ? theme.colors.dark[9] : theme.white
       }, 0 0 0 4px ${theme.colors[theme.primaryColor][5]}`,
