f62746f24fd9caba9116397b5dc765189acbb222
Vitaly Rtishchev <rtivital@gmail.com>
2021-04-29 13:27:35 +0300

    [docs] Add positions and controlled demos to tooltip

---
+++ docs/src/docs/demos/core/Tooltip/tooltip-positions.demo.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Tooltip, Button, ElementsGroup } from '@mantine/core';
+import CodeDemo from '../../../../components/CodeDemo/CodeDemo';
+
+const code = `import React from 'react';
+import { Tooltip, Button, ElementsGroup } from '@mantine/core';
+
+function Demo() {
+  const tooltips = ['top', 'right', 'bottom', 'left'].map((position) => {
+    const placements = ['start', 'center', 'end'].map((placement) => (
+      <Tooltip
+        data-mantine-composable
+        key={placement}
+        placement={placement}
+        position={position}
+        label={\`\${position}-\${placement}\`}
+        withArrow
+        gutter={10}
+      >
+        <Button variant="outline" color="gray" size="xl">{\`\${position}-\${placement}\`}</Button>
+      </Tooltip>
+    ));
+
+    return (
+      <ElementsGroup key={position} position="center">
+        {placements}
+      </ElementsGroup>
+    );
+  });
+
+  return <ElementsGroup position="center">{transitions}</ElementsGroup>;
+}`;
+
+export function TooltipPositionsDemo() {
+  const tooltips = (['top', 'right', 'bottom', 'left'] as const).map((position, index) => {
+    const placements = (['start', 'center', 'end'] as const).map((placement) => (
+      <Tooltip
+        data-mantine-composable
+        key={placement}
+        placement={placement}
+        position={position}
+        label={`${position}-${placement}`}
+        withArrow
+        gutter={10}
+      >
+        <Button variant="outline" color="gray" size="xl">{`${position}-${placement}`}</Button>
+      </Tooltip>
+    ));
+
+    return (
+      <ElementsGroup key={position} position="center" style={{ marginTop: index !== 0 && 10 }}>
+        {placements}
+      </ElementsGroup>
+    );
+  });
+
+  return (
+    <CodeDemo code={code} toggle language="tsx">
+      {tooltips}
+    </CodeDemo>
+  );
+}
