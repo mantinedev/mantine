6d04bdc940b07bac6a1140d7d7a603b2d7e3a35f
Vitaly Rtishchev <rtivital@gmail.com>
2021-05-14 20:56:09 +0300

    [docs] Add Popover ford demo from @mantine/demos

--- docs/src/docs/demos/core/Popover/popover-form.demo.tsx
+++ docs/src/docs/demos/core/Popover/popover-form.demo.tsx
@@ -1,173 +1,26 @@
-import React, { useState } from 'react';
-import {
-  Popover,
-  Button,
-  Group,
-  TextInput,
-  Avatar,
-  Text,
-  ActionIcon,
-  useMantineTheme,
-} from '@mantine/core';
-import { useForm, useMediaQuery } from '@mantine/hooks';
-import { GearIcon } from '@modulz/radix-icons';
+import React from 'react';
+import { EditUserPopover } from '@mantine/demos';
+import { useMantineTheme } from '@mantine/core';
 import CodeDemo from '../../../../components/CodeDemo/CodeDemo';
 
-const code = `import React, { useState } from 'react';
-import { Popover, ActionIcon, Group } from '@mantine/core';
-import { GearIcon } from '@modulz/radix-icons';
+// @ts-ignore
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import code from '!!raw-loader!../../../../../src/mantine-demos/src/EditUserPopover/EditUserPopover';
 
-function Demo() {
-  const [opened, setOpened] = useState(false);
-  const [values, setValues] = useState({
-    name: 'Bob Handsome',
-    email: 'bob@handsome.inc',
-  });
-
-  return (
-    <Group>
-      <User name={values.name} email={values.email} />
-
-      <Popover
-        opened={opened}
-        onClose={() => setOpened(false)}
-        position="bottom"
-        placement="end"
-        withArrow
-        withCloseButton
-        target={
-          <ActionIcon variant="light" onClick={() => setOpened(true)}>
-            <GearIcon />
-          </ActionIcon>
-        }
-      >
-        <UserEditForm
-          initialValues={values}
-          onCancel={() => setOpened(false)}
-          onSubmit={(data) => {
-            setValues(data);
-            setOpened(false);
-          }}
-        />
-      </Popover>
-    </Group>
-  );
-}`;
-
-function UserEditForm({
-  initialValues,
-  onSubmit,
-  onCancel,
-}: {
-  initialValues: { name: string; email: string };
-  onSubmit(values: { name: string; email: string }): void;
-  onCancel(): void;
-}) {
-  const isMobile = useMediaQuery('(max-width: 755px');
-  const form = useForm({
-    initialValues,
-    validationRules: {
-      name: (value) => value.trim().length > 2,
-      email: (value) => value.trim().length > 2,
-    },
-  });
-
-  return (
-    <form onSubmit={form.onSubmit(onSubmit)}>
-      <TextInput
-        required
-        label="Name"
-        placeholder="Name"
-        style={{ minWidth: isMobile ? 220 : 300 }}
-        value={form.values.name}
-        onChange={(event) => form.setFieldValue('name', event.currentTarget.value)}
-        error={form.errors.name}
-      />
-
-      <TextInput
-        required
-        label="Email"
-        placeholder="Email"
-        style={{ minWidth: isMobile ? 220 : 300, marginTop: 15 }}
-        value={form.values.email}
-        onChange={(event) => form.setFieldValue('email', event.currentTarget.value)}
-        error={form.errors.email}
-      />
-
-      <Group position="apart" style={{ marginTop: 15 }}>
-        <Button variant="link" color="gray" onClick={onCancel}>
-          Cancel
-        </Button>
-        <Button type="submit">Save</Button>
-      </Group>
-    </form>
-  );
-}
-
-function User({ name, email, className }: { name: string; email: string; className?: string }) {
-  return (
-    <div className={className} style={{ display: 'flex' }}>
-      <Avatar style={{ marginRight: 15 }} color="blue">
-        {name
-          .split(' ')
-          .map((part) => part.charAt(0).toUpperCase())
-          .slice(0, 2)
-          .join('')}
-      </Avatar>
-
-      <div style={{ marginRight: 15 }}>
-        <Text>{name}</Text>
-        <Text size="xs" color="gray">
-          {email}
-        </Text>
-      </div>
-    </div>
-  );
-}
-
-function Demo() {
-  const [values, setValues] = useState({ name: 'Bob Handsome', email: 'bob@handsome.inc' });
-  const [opened, setOpened] = useState(false);
+export function PopoverFormDemo() {
   const theme = useMantineTheme();
 
   return (
-    <Group>
-      <User name={values.name} email={values.email} />
-
-      <Popover
-        opened={opened}
-        onClose={() => setOpened(false)}
-        position="bottom"
-        placement="end"
-        withArrow
-        withCloseButton
-        target={
-          <ActionIcon
-            variant={theme.colorScheme === 'dark' ? 'hover' : 'light'}
-            onClick={() => setOpened(true)}
-          >
-            <GearIcon />
-          </ActionIcon>
-        }
-      >
-        <UserEditForm
-          initialValues={values}
-          onCancel={() => setOpened(false)}
-          onSubmit={(data) => {
-            setValues(data);
-            setOpened(false);
-          }}
-        />
-      </Popover>
-    </Group>
-  );
-}
-
-export function PopoverFormDemo() {
-  return (
-    <CodeDemo code={code} language="tsx">
+    <CodeDemo
+      code={code}
+      language="tsx"
+      toggle
+      githubLink="https://github.com/mantinedev/mantine/blob/master/src/mantine-demos/src/EditUserPopover/EditUserPopover.tsx"
+      demoBackground={theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white}
+      zIndex={4}
+    >
       <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <Demo />
+        <EditUserPopover />
       </div>
     </CodeDemo>
   );
