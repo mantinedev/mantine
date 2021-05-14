078b8c1f31fe323232309bf14c907168daac67ea
Vitaly Rtishchev <rtivital@gmail.com>
2021-05-14 10:23:53 +0300

    [@mantine/tag-picker] Add types export

--- src/mantine-tag-picker/src/index.ts
+++ src/mantine-tag-picker/src/index.ts
@@ -1,2 +1,3 @@
 export type { TagPickerColor, TagPickerTag } from './types';
+export type { TagPickerProps } from './TagPickerContainer';
 export { TagPickerContainer as TagPicker } from './TagPickerContainer';
