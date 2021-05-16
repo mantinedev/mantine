26401e38e61f4bd5b789fb57443a569064b51104
Vitaly Rtishchev <rtivital@gmail.com>
2021-05-16 20:21:24 +0300

    [@mantine/theme] Add correct typings to create-memo-styles

--- src/mantine-theme/src/create-memo-styles.ts
+++ src/mantine-theme/src/create-memo-styles.ts
@@ -2,10 +2,10 @@ import { useMemo } from 'react';
 import { createUseStyles, Styles } from 'react-jss';
 import { MantineTheme } from './types';
 
-export function createMemoStyles<C extends string = string, Props = unknown, Theme = MantineTheme>(
-  styles: Styles<string, Props, Theme>
+export function createMemoStyles<C extends string = string, Props = unknown>(
+  styles: Styles<C, Props, MantineTheme>
 ) {
-  const useStyles = createUseStyles<C, Props, Theme>(styles);
+  const useStyles = createUseStyles<C, Props, MantineTheme>(styles);
 
   return function useMemoStyles(args: Props) {
     const dependencies =
