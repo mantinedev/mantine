04c8fe9b2a13e0aed0c41418c29643aa3a068972
Vitaly Rtishchev <rtivital@gmail.com>
2021-05-18 17:38:45 +0300

    [@mantine/core] LoadingOverlay: Add custom loader support

--- src/mantine-core/src/LoadingOverlay/LoadingOverlay.story.tsx
+++ src/mantine-core/src/LoadingOverlay/LoadingOverlay.story.tsx
@@ -7,7 +7,7 @@ import { Group } from '../Group/Group';
 import { LoadingOverlay } from './LoadingOverlay';
 
 function LoadingOverlayDemo(props?: any) {
-  const [visible, setVisible] = useState(false);
+  const [visible, setVisible] = useState(true);
 
   return (
     <div style={{ width: 400, padding: 20 }}>
@@ -23,12 +23,39 @@ function LoadingOverlayDemo(props?: any) {
   );
 }
 
+const customLoader = (
+  <svg
+    width="54"
+    height="54"
+    viewBox="0 0 38 38"
+    xmlns="http://www.w3.org/2000/svg"
+    stroke={DEFAULT_THEME.colors.blue[6]}
+  >
+    <g fill="none" fillRule="evenodd">
+      <g transform="translate(1 1)" strokeWidth="2">
+        <circle strokeOpacity=".5" cx="18" cy="18" r="18" />
+        <path d="M36 18c0-9.94-8.06-18-18-18">
+          <animateTransform
+            attributeName="transform"
+            type="rotate"
+            from="0 18 18"
+            to="360 18 18"
+            dur="1s"
+            repeatCount="indefinite"
+          />
+        </path>
+      </g>
+    </g>
+  </svg>
+);
+
 storiesOf('@mantine/core/LoadingOverlay', module)
   .add('General usage', () => <LoadingOverlayDemo />)
   .add('Custom Loader props', () => (
     <LoadingOverlayDemo loaderProps={{ size: 'lg', color: 'grape' }} />
   ))
   .add('Custom Overlay props', () => <LoadingOverlayDemo overlayOpacity={0.6} overlayColor="red" />)
+  .add('Custom loader', () => <LoadingOverlayDemo loader={customLoader} />)
   .add('Dark theme', () => (
     <div style={{ background: DEFAULT_THEME.colors.dark[7], minHeight: '100vh', padding: 50 }}>
       <LoadingOverlayDemo themeOverride={{ colorScheme: 'dark' }} />
