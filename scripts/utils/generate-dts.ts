1a869e044bd923290ce9c1af10a3dc52ce787df6
Elon Tang <blackcater2015@gmail.com>
2021-05-20 19:32:01 +0800

    feat: add .npmignore files

--- scripts/utils/generate-dts.ts
+++ scripts/utils/generate-dts.ts
@@ -1,7 +1,14 @@
 import execa from 'execa';
+import fg from 'fast-glob';
+import path from 'path';
+import fs from 'fs-extra';
 
 export default async function generateDts(packagePath: string) {
   await execa('yarn', ['tsc', '--build'], {
     cwd: packagePath,
   });
+
+  const files = await fg(['lib/**/*.js'], { cwd: packagePath });
+
+  return Promise.all(files.map((file) => fs.remove(path.join(packagePath, file))));
 }
