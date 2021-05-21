cbb203350f795fecb57ff06e1d82326b81a59b18
blackcater <blackcater2015@gmail.com>
2021-05-21 13:56:52 +0800

    feat: add default options

--- scripts/utils/build-all-packages.ts
+++ scripts/utils/build-all-packages.ts
@@ -6,6 +6,14 @@ import { getPackagesBuildOrder } from './get-packages-build-order';
 export async function buildAllPackages(options?: BuildOptions) {
   const packages = await getPackagesBuildOrder();
 
+  // eslint-disable-next-line no-param-reassign
+  options = options || {
+    analyze: false,
+    sourcemap: true,
+    minify: false,
+    formats: ['es', 'cjs', 'umd'],
+  };
+
   for (const item of packages) {
     await buildPackage(item.packageJson.name, options);
   }
