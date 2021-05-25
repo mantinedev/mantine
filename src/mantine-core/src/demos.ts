11916e2ebdb7f23597663ffeba43f479ee8be9b4
Vitaly Rtishchev <rtivital@gmail.com>
2021-05-25 21:24:57 +0300

    [docs] Move Breadcrumbs demos to @mantine/core

--- src/mantine-core/src/demos.ts
+++ src/mantine-core/src/demos.ts
@@ -3,3 +3,4 @@ export * as ActionIconDemos from './components/ActionIcon/demos';
 export * as AvatarDemos from './components/Avatar/demos';
 export * as BadgeDemos from './components/Badge/demos';
 export * as BlockquoteDemos from './components/Blockquote/demos';
+export * as BreadcrumbsDemos from './components/Breadcrumbs/demos';
