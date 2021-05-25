bf79e36cd4ca12049c63649c6efacdcaedaf3793
Vitaly Rtishchev <rtivital@gmail.com>
2021-05-25 21:18:02 +0300

    [docs] Simplify blockquote demos

--- src/mantine-core/src/demos.ts
+++ src/mantine-core/src/demos.ts
@@ -2,3 +2,4 @@ export * as AlertDemos from './components/Alert/demos';
 export * as ActionIconDemos from './components/ActionIcon/demos';
 export * as AvatarDemos from './components/Avatar/demos';
 export * as BadgeDemos from './components/Badge/demos';
+export * as BlockquoteDemos from './components/Blockquote/demos';
