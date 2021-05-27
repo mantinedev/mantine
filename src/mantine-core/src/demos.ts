79726f0f30d3c370ab8d953f31e16680072d2d20
Vitaly Rtishchev <rtivital@gmail.com>
2021-05-27 17:51:57 +0300

    [docs] Migrate overlay component demos to @mantine/core

--- src/mantine-core/src/demos.ts
+++ src/mantine-core/src/demos.ts
@@ -26,3 +26,4 @@ export * as MenuDemos from './components/Menu/demos';
 export * as ModalDemos from './components/Modal/demos';
 export * as NotificationDemos from './components/Notification/demos';
 export * as NumberInputDemos from './components/NumberInput/demos';
+export * as OverlayDemos from './components/Overlay/demos';
