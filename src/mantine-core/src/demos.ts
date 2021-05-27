264297f06a42d40faa129ec4e12aab63dfc9e643
Vitaly Rtishchev <rtivital@gmail.com>
2021-05-27 11:44:28 +0300

    [docs] Migrate Kbd component demos to @mantine/core

--- src/mantine-core/src/demos.ts
+++ src/mantine-core/src/demos.ts
@@ -17,3 +17,5 @@ export * as GridDemos from './components/Grid/demos';
 export * as GroupDemos from './components/Group/demos';
 export * as HighlightDemos from './components/Highlight/demos';
 export * as ImageDemos from './components/Image/demos';
+
+export * as KbdDemos from './components/Kbd/demos';
