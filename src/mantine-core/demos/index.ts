a2f9d5a54e721ef640be1d1177e18cd280ab0473
Vitaly Rtishchev <rtivital@gmail.com>
2021-05-27 17:31:57 +0300

    [docs] Add indeterminate demo to Checkbox

--- src/mantine-core/demos/index.ts
+++ src/mantine-core/demos/index.ts
@@ -1,7 +1,6 @@
 export { AuthenticationForm } from './AuthenticationForm/AuthenticationForm';
 export { ColorInput } from './ColorInput/ColorInput';
 export { EditUserPopover } from './EditUserPopover/EditUserPopover';
-export { IndeterminateCheckbox } from './IndeterminateCheckbox/IndeterminateCheckbox';
 export { JsonInput } from './JsonInput/JsonInput';
 export { PasswordStrength } from './PasswordStrength/PasswordStrength';
 export { TodoList } from './TodoList/TodoList';
--- src/mantine-core/src/components/Checkbox/demos/index.ts
+++ src/mantine-core/src/components/Checkbox/demos/index.ts
@@ -1,2 +1,3 @@
 export { sizes } from './sizes';
 export { configurator } from './configurator';
+export { indeterminate } from './indeterminate';
