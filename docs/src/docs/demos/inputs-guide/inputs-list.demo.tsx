c786ac4272bfac728ce699e752e85532a7d997ac
Vitaly Rtishchev <rtivital@gmail.com>
2021-05-28 13:14:06 +0300

    [docs] Remove old demos

--- docs/src/docs/demos/inputs-guide/inputs-list.demo.tsx
+++ docs/src/docs/demos/inputs-guide/inputs-list.demo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   TextInput,
   PasswordInput,
@@ -9,10 +9,25 @@ import {
   Textarea,
   Checkbox,
   Switch,
+  SegmentedControl,
+  SegmentedControlProps,
   useMantineTheme,
 } from '@mantine/core';
-import { SegmentedControlWrapper } from '../core/SegmentedControl/segmented-control-wrapper';
 import CodeDemo from '../../../components/CodeDemo/CodeDemo';
+
+const data = [
+  { label: 'React', value: 'react' },
+  { label: 'Angular', value: 'ng' },
+  { label: 'Vue', value: 'vue' },
+  { label: 'Svelte', value: 'svelte' },
+];
+
+export function SegmentedControlWrapper(
+  props: Omit<SegmentedControlProps, 'value' | 'onChange' | 'data'>
+) {
+  const [value, onChange] = useState<string>('react');
+  return <SegmentedControl {...props} data={data} value={value} onChange={onChange} />;
+}
 
 const selectData = [
   { value: 'react', label: 'React' },
