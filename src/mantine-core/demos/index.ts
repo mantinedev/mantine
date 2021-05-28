966ce75d98a3b649f0baafa8d1442a225c972679
Vitaly Rtishchev <rtivital@gmail.com>
2021-05-28 12:53:31 +0300

    [@mantine/core] Cleanup needless demos

--- src/mantine-core/demos/index.ts
+++ src/mantine-core/demos/index.ts
@@ -1,4 +1,1 @@
 export { AuthenticationForm } from './AuthenticationForm/AuthenticationForm';
-export { ColorInput } from './ColorInput/ColorInput';
-export { JsonInput } from './JsonInput/JsonInput';
-export { TodoList } from './TodoList/TodoList';
