95012ea32d8321fa93dd51c132eb82ec91df2edb
Vitaly Rtishchev <rtivital@gmail.com>
2021-06-01 11:27:33 +0300

    [@mantine/notifications] Fix incorrect import in demos

--- src/mantine-notifications/src/demos/limit.tsx
+++ src/mantine-notifications/src/demos/limit.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Group, Button } from '@mantine/core';
-import { useNotifications } from '@mantine/notifications';
+import { useNotifications } from '../index';
 
 const code = `import React from 'react';
 import { Group, Button } from '@mantine/core';
