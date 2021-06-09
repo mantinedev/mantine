9d70aacbca7488fe358d8d27af55c6ea47d74b07
Vitaly Rtishchev <rtivital@gmail.com>
2021-06-09 17:30:39 +0300

    [@mantine/core] Slider: Add utility functions tests

---
+++ src/mantine-core/src/components/Slider/utils/get-change-value/get-change-value.test.ts
@@ -0,0 +1,14 @@
+import { getChangeValue } from './get-change-value';
+
+describe('@mantine/core/Slider/get-change-value', () => {
+  it('returns correct change value', () => {
+    expect(getChangeValue({ value: 20, containerWidth: 200, min: 0, max: 100, step: 1 })).toBe(10);
+    expect(getChangeValue({ value: 20, containerWidth: 800, min: 0, max: 100, step: 1 })).toBe(3);
+    expect(getChangeValue({ value: 123, containerWidth: 800, min: 0, max: 100, step: 10 })).toBe(
+      20
+    );
+    expect(getChangeValue({ value: 574, containerWidth: 800, min: 0, max: 100, step: 40 })).toBe(
+      80
+    );
+  });
+});
