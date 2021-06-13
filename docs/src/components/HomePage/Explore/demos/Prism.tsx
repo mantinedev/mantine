3387965c814a1da808db2263445f96fb7c2714f6
Vitaly Rtishchev <rtivital@gmail.com>
2021-06-13 16:42:17 +0300

    [docs] Add prism demos to home page

--- docs/src/components/HomePage/Explore/demos/Prism.tsx
+++ docs/src/components/HomePage/Explore/demos/Prism.tsx
@@ -1,5 +1,39 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import { ArrowRightIcon } from '@modulz/radix-icons';
+import { Group, Title, Anchor } from '@mantine/core';
+import { Prism } from '@mantine/prism';
+
+const code = `
+import React from 'react';
+import { Prism } from '@mantine/prism';
+
+function Demo() {
+  return (
+    <>
+      <Prism language="tsx">{code}</Prism>
+      <Prism language="tsx" themeOverride={{ colorScheme: 'dark' }}>{code}</Prism>
+    </>
+  );
+}
+`.trim();
 
 export function PrismDemo() {
-  return <div>Prism</div>;
+  return (
+    <>
+      <Group position="apart" spacing={0}>
+        <Title order={3}>Highlight code with your theme colors</Title>
+        <Anchor component={Link} to="/others/prism/">
+          Get started <ArrowRightIcon style={{ width: 12, height: 12 }} />
+        </Anchor>
+      </Group>
+
+      <Prism language="tsx" style={{ marginTop: 25 }}>
+        {code}
+      </Prism>
+      <Prism language="tsx" themeOverride={{ colorScheme: 'dark' }} style={{ marginTop: 25 }}>
+        {code}
+      </Prism>
+    </>
+  );
 }
