2e63d66c05b21e4315f0bf8ef97c2cee396a260f
Vitaly Rtishchev <rtivital@gmail.com>
2021-06-18 20:02:23 +0300

    [docs] Add new ways to contribute section

--- docs/src/components/Contribute/Contribute.styles.ts
+++ docs/src/components/Contribute/Contribute.styles.ts
@@ -1,12 +1,19 @@
 import { createUseStyles } from 'react-jss';
 import { theming } from '@mantine/core';
 
+const BREAKPOINT = '@media (max-width: 1100px)';
+
 export default createUseStyles(
   (theme) => ({
     wrapper: {
       paddingTop: 54,
       paddingLeft: theme.spacing.xl * 2,
       paddingRight: theme.spacing.xl * 2,
+
+      [BREAKPOINT]: {
+        paddingLeft: theme.spacing.xl,
+        paddingRight: theme.spacing.xl,
+      },
     },
 
     title: {
@@ -17,6 +24,35 @@ export default createUseStyles(
 
     subtitle: {
       marginTop: theme.spacing.xl,
+    },
+
+    header: {
+      display: 'flex',
+      alignItems: 'center',
+      marginBottom: theme.spacing.xs,
+    },
+
+    description: {
+      minHeight: 80,
+    },
+
+    cards: {
+      marginTop: theme.spacing.xs,
+    },
+
+    card: {
+      boxSizing: 'border-box',
+      flex: `0 0 calc(33.333333% - ${theme.spacing.md}px)`,
+      border: `1px solid ${theme.colors.gray[2]}`,
+      padding: theme.spacing.md,
+
+      [BREAKPOINT]: {
+        flex: 1,
+      },
+    },
+
+    control: {
+      marginTop: theme.spacing.xs,
     },
   }),
   { theming }
--- docs/src/components/Contribute/Contribute.tsx
+++ docs/src/components/Contribute/Contribute.tsx
@@ -1,20 +1,53 @@
 import React from 'react';
-import { Title, Text } from '@mantine/core';
+import { ArrowRightIcon } from '@modulz/radix-icons';
+import { Title, Text, ThemeIcon, Group, Button } from '@mantine/core';
+import { CONTRIBUTE_DATA } from './data';
 import useStyles from './Contribute.styles';
 
 export function Contribute() {
   const classes = useStyles();
+  const items = CONTRIBUTE_DATA.map((item) => (
+    <div className={classes.card} key={item.title}>
+      <div className={classes.header}>
+        <ThemeIcon color={item.color} size="lg">
+          <item.icon width={20} height={20} />
+        </ThemeIcon>
+        <Text weight={500} style={{ marginLeft: 15 }}>
+          {item.title}
+        </Text>
+      </div>
+      <Text size="sm" className={classes.description}>
+        {item.description}
+      </Text>
+      <Button
+        className={classes.control}
+        component="a"
+        data-size="large"
+        href={item.link}
+        variant="outline"
+        color="gray"
+        size="sm"
+        rightIcon={<ArrowRightIcon width={10} height={10} />}
+      >
+        {item.linkLabel}
+      </Button>
+    </div>
+  ));
   return (
     <div className={classes.wrapper}>
       <Title className={classes.title}>Contribute to Mantine</Title>
       <Text style={{ maxWidth: 600 }}>
         First of all, thank you for showing interest in contributing to Mantine, all your
-        contributions are extremely valuable to the project.
+        contributions are extremely valuable to the project!
       </Text>
 
       <Title order={2} className={classes.subtitle}>
         Ways to contribute
       </Title>
+
+      <Group className={classes.cards} align="stretch">
+        {items}
+      </Group>
 
       <Title order={2} className={classes.subtitle}>
         Become a maintainer
