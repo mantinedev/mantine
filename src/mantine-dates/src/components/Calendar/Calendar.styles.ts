f17c367fad8464f3fc5a08cac81e21b36b30add4
Vitaly Rtishchev <rtivital@gmail.com>
2021-06-20 18:04:54 +0300

    [@mantine/dates] Calendar: Add header

--- src/mantine-dates/src/components/Calendar/Calendar.styles.ts
+++ src/mantine-dates/src/components/Calendar/Calendar.styles.ts
@@ -6,4 +6,5 @@ interface CalendarStyles {
 
 export default createMemoStyles({
   root: {},
+  header: {},
 });
--- src/mantine-dates/src/components/Calendar/Calendar.tsx
+++ src/mantine-dates/src/components/Calendar/Calendar.tsx
@@ -1,20 +1,65 @@
 import React from 'react';
 import cx from 'clsx';
-import { DefaultProps, useMantineTheme, mergeStyles } from '@mantine/core';
+import dayjs from 'dayjs';
+import { DefaultProps, useMantineTheme, mergeStyles, Text, Group, ActionIcon } from '@mantine/core';
+import { useUncontrolled } from '@mantine/hooks';
+import { ArrowIcon } from './ArrowIcon';
 import useStyles from './Calendar.styles';
 
 interface CalendarProps
   extends DefaultProps<typeof useStyles>,
-    React.ComponentPropsWithoutRef<'div'> {}
+    React.ComponentPropsWithoutRef<'div'> {
+  initialMonth?: Date;
+  month?: Date;
+  nextMonthLabel?: string;
+  previousMonthLabel?: string;
+  locale?: string;
+  onMonthChange?(value: Date): void;
+}
 
-export function Calendar({ className, classNames, styles, style, themeOverride }: CalendarProps) {
+export function Calendar({
+  className,
+  classNames,
+  styles,
+  style,
+  locale = 'en',
+  themeOverride,
+  nextMonthLabel,
+  previousMonthLabel,
+  initialMonth,
+  month,
+  onMonthChange,
+}: CalendarProps) {
   const theme = useMantineTheme(themeOverride);
   const classes = useStyles({ theme }, classNames, 'calendar');
   const _styles = mergeStyles(classes, styles);
+  const [_month, setMonth] = useUncontrolled({
+    value: month,
+    defaultValue: initialMonth,
+    finalValue: new Date(),
+    onChange: onMonthChange,
+    rule: (value) => value instanceof Date,
+  });
 
   return (
     <div className={cx(classes.root, className)} style={{ ...style, ..._styles.root }}>
-      Calendar
+      <Group className={classes.header}>
+        <ActionIcon
+          aria-label={nextMonthLabel}
+          onClick={() => setMonth(dayjs(_month).subtract(1, 'month').toDate())}
+        >
+          <ArrowIcon direction="left" width={14} height={14} />
+        </ActionIcon>
+
+        <Text>{dayjs(_month).locale(locale).format('MMMM')}</Text>
+
+        <ActionIcon
+          aria-label={previousMonthLabel}
+          onClick={() => setMonth(dayjs(_month).add(1, 'month').toDate())}
+        >
+          <ArrowIcon direction="right" width={14} height={14} />
+        </ActionIcon>
+      </Group>
     </div>
   );
 }
