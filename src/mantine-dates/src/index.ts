ece63fa0a9dee255fc8b857c39a42c2c440cf2fa
Vitaly Rtishchev <rtivital@gmail.com>
2021-06-21 10:28:48 +0300

    [@mantine/dates] Add all current exports

--- src/mantine-dates/src/index.ts
+++ src/mantine-dates/src/index.ts
@@ -1,7 +1,5 @@
-export { getEndOfWeek } from './utils/get-end-of-week/get-end-of-week';
-export { getMonthDays } from './utils/get-month-days/get-month-days';
-export { getStartOfWeek } from './utils/get-start-of-week/get-start-of-week';
-export { getWeekdaysNames } from './utils/get-weekdays-names/get-weekdays-names';
-export { isSameMonth } from './utils/is-same-month/is-same-month';
+export * from './utils';
 
+export { Calendar } from './components/Calendar/Calendar';
+export { DatePicker } from './components/DatePicker/DatePicker';
 export { Month } from './components/Month/Month';
