d15d799702b27ff5d4954e1eb0e6927d4ef7b246
Vitaly Rtishchev <rtivital@gmail.com>
2021-06-21 20:53:29 +0300

    [@mantine/dates] DatePicker: Add better styles to story

--- src/mantine-dates/src/components/DatePicker/DatePicker.story.tsx
+++ src/mantine-dates/src/components/DatePicker/DatePicker.story.tsx
@@ -3,7 +3,7 @@ import { storiesOf } from '@storybook/react';
 import { DatePicker } from './DatePicker';
 
 storiesOf('@mantine/dates/DatePicker', module).add('Uncontrolled', () => (
-  <div style={{ padding: 40 }}>
+  <div style={{ padding: 40, maxWidth: 300 }}>
     <DatePicker placeholder="Pick date" label="Date picker" />
   </div>
 ));
