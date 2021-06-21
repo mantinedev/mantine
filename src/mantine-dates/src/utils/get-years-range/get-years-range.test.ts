e3530be80970512732ecb9d4c286b0e46ce8f781
Vitaly Rtishchev <rtivital@gmail.com>
2021-06-21 17:38:23 +0300

    [@mantine/dates] Add utils tests

---
+++ src/mantine-dates/src/utils/get-years-range/get-years-range.test.ts
@@ -0,0 +1,9 @@
+import { getYearsRange } from './get-years-range';
+
+describe('@mantine/dates/get-years-range', () => {
+  it('returns correct years range', () => {
+    expect(getYearsRange({ from: 2000, to: 2010 })).toEqual([
+      2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
+    ]);
+  });
+});
