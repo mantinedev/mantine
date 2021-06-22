3efb65714af49e6fdfb6007a897a3c9bc9be6b78
Vitaly Rtishchev <rtivital@gmail.com>
2021-06-22 10:01:34 +0300

    [@mantine/dates] DatePicker: Add controlled story

--- src/mantine-dates/src/components/DatePicker/DatePicker.story.tsx
+++ src/mantine-dates/src/components/DatePicker/DatePicker.story.tsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
 import { DatePicker } from './DatePicker';
 
-storiesOf('@mantine/dates/DatePicker', module).add('Uncontrolled', () => (
-  <div style={{ padding: 40, maxWidth: 300 }}>
-    <DatePicker placeholder="Pick date" label="Date picker" />
+function Controlled() {
+  const [value, onChange] = useState(new Date());
+
+  return (
     <DatePicker
+      value={value}
+      onChange={onChange}
       placeholder="With month and year"
       label="Date picker"
       withSelect
-      style={{ marginTop: 20 }}
     />
-  </div>
-));
+  );
+}
+
+storiesOf('@mantine/dates/DatePicker', module)
+  .add('Uncontrolled', () => (
+    <div style={{ padding: 40, maxWidth: 300 }}>
+      <DatePicker placeholder="Pick date" label="Date picker" />
+      <DatePicker
+        placeholder="With month and year"
+        label="Date picker"
+        withSelect
+        style={{ marginTop: 20 }}
+      />
+    </div>
+  ))
+  .add('Controlled', () => (
+    <div style={{ padding: 40, maxWidth: 300 }}>
+      <Controlled />
+    </div>
+  ));
