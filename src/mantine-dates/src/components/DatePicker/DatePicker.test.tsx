71bdf0859b247611c65d524efd3e85a2788e7b41
Vitaly Rtishchev <rtivital@gmail.com>
2021-06-22 14:54:53 +0300

    [@mantine/dates] DatePicker: Add input and input-wrapper styles api tests

--- src/mantine-dates/src/components/DatePicker/DatePicker.test.tsx
+++ src/mantine-dates/src/components/DatePicker/DatePicker.test.tsx
@@ -1,10 +1,36 @@
-import { itSupportsClassName, itSupportsRef, itSupportsStyle } from '@mantine/tests';
+import {
+  itSupportsClassName,
+  itSupportsRef,
+  itSupportsStyle,
+  itSupportsStylesApi,
+} from '@mantine/tests';
 import { DatePicker } from './DatePicker';
 
 describe('@mantine/dates/DatePicker', () => {
   itSupportsClassName(DatePicker, {});
   itSupportsStyle(DatePicker, {});
   itSupportsRef(DatePicker, {}, HTMLButtonElement, 'elementRef');
+
+  itSupportsStylesApi(
+    DatePicker,
+    { icon: '$', rightSection: '$' },
+    ['icon', 'withIcon', 'input', 'rightSection'],
+    'input',
+    'input'
+  );
+
+  itSupportsStylesApi(
+    DatePicker,
+    {
+      label: 'test-label',
+      error: 'test-error',
+      description: 'test-description',
+      required: true,
+    },
+    ['root', 'label', 'error', 'description', 'required'],
+    'input-wrapper',
+    'input-wrapper'
+  );
 
   it('has correct displayName', () => {
     expect(DatePicker.displayName).toEqual('@mantine/dates/DatePicker');
