d053910662cc44eef75d7addd93a0ee8b3645469
Vitaly Rtishchev <rtivital@gmail.com>
2021-06-23 21:23:44 +0300

    [docs] Add vite and gatsby guides to getting started page

--- docs/src/components/GettingStarted/Guides/guides/ViteGuide.tsx
+++ docs/src/components/GettingStarted/Guides/guides/ViteGuide.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text } from '@mantine/core';
 import { Prism } from '@mantine/prism';
 import { Installation } from './Installation';
+import { Done } from './Done';
 
 interface CraGuideProps {
   dependencies: string;
@@ -13,11 +14,12 @@ export function ViteGuide({ dependencies }: CraGuideProps) {
       <Text weight={700} style={{ marginBottom: 15 }}>
         Init application
       </Text>
-      <Prism language="bash">npx create-react-app my-app --template typescript</Prism>
+      <Prism language="bash">yarn create @vitejs/app mantine-vite --template react-ts</Prism>
       <Text weight={700} style={{ marginTop: 30 }}>
         Install dependencies
       </Text>
       <Installation dependencies={dependencies} />
+      <Done />
     </div>
   );
 }
