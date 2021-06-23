6a011375b7135c9d903a4650225cedd237df9530
Vitaly Rtishchev <rtivital@gmail.com>
2021-06-23 19:40:54 +0300

    [docs] Add all content to next step section on getting started page

--- docs/src/components/GettingStarted/NextSteps/NextSteps.styles.ts
+++ docs/src/components/GettingStarted/NextSteps/NextSteps.styles.ts
@@ -1,9 +1,33 @@
 import { createUseStyles } from 'react-jss';
-import { theming } from '@mantine/core';
+import { getFocusStyles, theming } from '@mantine/core';
 
 export default createUseStyles(
   (theme) => ({
-    links: {},
+    links: {
+      display: 'flex',
+      marginTop: theme.spacing.md,
+    },
+
+    link: {
+      ...getFocusStyles(theme),
+      padding: theme.spacing.md,
+      borderRadius: theme.radius.sm,
+      border: `1px solid ${
+        theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[2]
+      }`,
+      textDecoration: 'none',
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
+
+      '&:hover': {
+        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[9] : theme.colors.gray[0],
+      },
+    },
+
+    description: {
+      marginTop: theme.spacing.xs,
+      color: theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.gray[6],
+    },
+
     wrapper: {
       paddingTop: theme.spacing.xl * 2,
       paddingBottom: theme.spacing.xl * 4,
--- docs/src/components/GettingStarted/NextSteps/NextSteps.tsx
+++ docs/src/components/GettingStarted/NextSteps/NextSteps.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import { Title } from '@mantine/core';
+import { Title, ThemeIcon, Text, Group } from '@mantine/core';
 import { NEXT_STEPS_DATA } from './data';
 import useStyles from './NextSteps.styles';
 
@@ -11,12 +11,28 @@ export function NextSteps() {
     const LinkComponent = item.type === 'gatsby' ? Link : (props: any) => <a {...props} />;
     const linkProps = item.type === 'gatsby' ? { to: item.link } : { href: item.link };
 
-    return <LinkComponent {...linkProps}>Link</LinkComponent>;
+    return (
+      <LinkComponent className={classes.link} {...linkProps}>
+        <Group spacing="lg">
+          <ThemeIcon size={40} style={{ backgroundColor: item.color }}>
+            <item.icon width={26} height={26} size={26} />
+          </ThemeIcon>
+          <Text weight={500} size="lg">
+            {item.title}
+          </Text>
+        </Group>
+        <Text size="sm" className={classes.description}>
+          {item.description}
+        </Text>
+      </LinkComponent>
+    );
   });
   return (
     <div className={classes.wrapper}>
       <Title>Next steps</Title>
-      <div className={classes.links}>{items}</div>
+      <Group className={classes.links} grow align="stretch">
+        {items}
+      </Group>
     </div>
   );
 }
