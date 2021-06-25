36bb0610208f1033faa0ae09b06a47541c576bc4
Vitaly Rtishchev <rtivital@gmail.com>
2021-06-25 19:25:37 +0300

    [@mantine/dates] Calendar: Add CalendarWrapper component tests

---
+++ src/mantine-dates/src/components/Calendar/CalendarWrapper/CalendarWrapper.test.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import {
+  itRendersChildren,
+  itSupportsStyle,
+  itSupportsClassName,
+  itSupportsOthers,
+} from '@mantine/tests';
+import { sizes as DAY_SIZES } from '../../Month/Day/Day.styles';
+import { CalendarWrapper } from './CalendarWrapper';
+
+const defaultProps = {
+  size: 'sm',
+  fullWidth: false,
+};
+
+describe('@mantine/dates/CalendarWrapper', () => {
+  itRendersChildren(CalendarWrapper, defaultProps);
+  itSupportsStyle(CalendarWrapper, defaultProps);
+  itSupportsClassName(CalendarWrapper, defaultProps);
+  itSupportsOthers(CalendarWrapper, defaultProps);
+
+  it('sets max-width based on size value', () => {
+    const xl = shallow(<CalendarWrapper size="xl" fullWidth={false} />);
+    const sm = shallow(<CalendarWrapper size="sm" fullWidth={false} />);
+
+    expect(xl.render().css('max-width')).toBe(`${DAY_SIZES.xl * 7}px`);
+    expect(sm.render().css('max-width')).toBe(`${DAY_SIZES.sm * 7}px`);
+  });
+
+  it('ignores size if fullWidth is set to true', () => {
+    const element = shallow(<CalendarWrapper size="xl" fullWidth />);
+    expect(element.render().css('max-width')).toBe('100%');
+  });
+
+  it('has correct displayName', () => {
+    expect(CalendarWrapper.displayName).toEqual('@mantine/dates/CalendarWrapper');
+  });
+});
