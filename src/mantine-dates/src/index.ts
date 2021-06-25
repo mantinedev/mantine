91ca4dc5c1969a65dfea19b2ecae1a22b0c57711
Vitaly Rtishchev <rtivital@gmail.com>
2021-06-25 11:56:52 +0300

    [@mantine/dates] Add RangeCalendar exports

--- src/mantine-dates/src/index.ts
+++ src/mantine-dates/src/index.ts
@@ -3,3 +3,4 @@ export * from './utils';
 export { Calendar } from './components/Calendar/Calendar';
 export { DatePicker } from './components/DatePicker/DatePicker';
 export { Month } from './components/Month/Month';
+export { RangeCalendar } from './components/RangeCalendar/RangeCalendar';
