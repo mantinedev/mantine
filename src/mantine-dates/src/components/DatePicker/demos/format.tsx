c59ae2cef5959ac1f61b24f4acf15af9adab7c63
Vitaly Rtishchev <rtivital@gmail.com>
2021-06-28 13:41:42 +0300

    [docs] Add all docs to DatePicker component

--- src/mantine-dates/src/components/DatePicker/demos/format.tsx
+++ src/mantine-dates/src/components/DatePicker/demos/format.tsx
@@ -3,12 +3,12 @@ import { DatePicker } from '../DatePicker';
 
 const code = `
 <DatePicker
-    placeholder="Pick date"
-    label="Event date"
-    inputFormat="MM/DD/YYYY"
-    labelFormat="MM/YYYY"
-    defaultValue={new Date()}
-  />
+  placeholder="Pick date"
+  label="Event date"
+  inputFormat="MM/DD/YYYY"
+  labelFormat="MM/YYYY"
+  defaultValue={new Date()}
+/>
 `;
 
 function Demo() {
