7328d8b17a43009d6c92dc2a1f615313283f079c
Vitaly Rtishchev <rtivital@gmail.com>
2021-06-28 22:03:59 +0300

    [@mantine/dates] TimeInput: Add styles API descriptions

---
+++ src/mantine-dates/src/components/TimeInput/styles.api.ts
@@ -0,0 +1,9 @@
+import { Input, InputWrapper } from '@mantine/core/src/styles.api';
+import { TimeInputStylesNames } from './TimeInput';
+
+export const TimeInput: Record<TimeInputStylesNames, string> = {
+  controls: 'Wrapper around hours, minutes and seconds inputs',
+  timeInput: 'Hours, minutes and seconds inputs',
+  ...Input,
+  ...InputWrapper,
+};
--- src/mantine-dates/src/styles.api.ts
+++ src/mantine-dates/src/styles.api.ts
@@ -1,3 +1,4 @@
 export * from './components/Month/styles.api';
 export * from './components/Calendar/styles.api';
 export * from './components/DatePickerBase/styles.api';
+export * from './components/TimeInput/styles.api';
