0b0e9137a1642ec7956dce246093bf63cf321bc4
Vitaly Rtishchev <rtivital@gmail.com>
2021-06-28 22:13:12 +0300

    [@mantine/dates] TimeInput: Add usage demo

--- src/mantine-dates/src/demos.ts
+++ src/mantine-dates/src/demos.ts
@@ -2,3 +2,4 @@ export * as CalendarDemos from './components/Calendar/demos';
 export * as DatePickerDemos from './components/DatePicker/demos';
 export * as RangeCalendarDemos from './components/RangeCalendar/demos';
 export * as MonthDemos from './components/Month/demos';
+export * as TimeInputDemos from './components/TimeInput/demos';
