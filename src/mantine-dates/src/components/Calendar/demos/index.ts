e463ccf139ec9f78234bd04930222c57d8939235
Vitaly Rtishchev <rtivital@gmail.com>
2021-06-28 15:38:03 +0300

    [docs] Add all docs to Calendar component

--- src/mantine-dates/src/components/Calendar/demos/index.ts
+++ src/mantine-dates/src/components/Calendar/demos/index.ts
@@ -6,3 +6,4 @@ export { localization } from './localization';
 export { styles } from './styles';
 export { modifiers } from './modifiers';
 export { stylesApi } from './stylesApi';
+export { format } from './format';
