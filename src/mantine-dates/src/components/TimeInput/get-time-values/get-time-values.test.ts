a8526752a7a5b52483d62d2dec795682a2644fa8
Vitaly Rtishchev <rtivital@gmail.com>
2021-06-29 08:13:02 +0300

    [@mantine/dates] TimeInput: Add utility functions tests

---
+++ src/mantine-dates/src/components/TimeInput/get-time-values/get-time-values.test.ts
@@ -0,0 +1,11 @@
+import { getTimeValues } from './get-time-value';
+
+describe('@mantine/dates/get-time-value', () => {
+  it('returns correct time values', () => {
+    expect(getTimeValues(new Date(2021, 7, 21, 1, 23, 44))).toEqual({
+      hours: '01',
+      minutes: '23',
+      seconds: '44',
+    });
+  });
+});
