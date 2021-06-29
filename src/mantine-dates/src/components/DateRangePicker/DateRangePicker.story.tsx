bbde4828e4c00d140d41f62a4bcf5fe2dd84440c
Vitaly Rtishchev <rtivital@gmail.com>
2021-06-29 09:29:17 +0300

    [@mantine/dates] DateRangePicker: Init component

---
+++ src/mantine-dates/src/components/DateRangePicker/DateRangePicker.story.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { DateRangePicker } from './DateRangePicker';
+
+storiesOf('@mantine/dates/DateRangePicker', module).add('General usage', () => (
+  <div style={{ padding: 40 }}>
+    <DateRangePicker placeholder="Dates range" />
+  </div>
+));
--- src/mantine-dates/src/index.ts
+++ src/mantine-dates/src/index.ts
@@ -2,12 +2,14 @@ export * from './utils';
 
 export { Calendar } from './components/Calendar/Calendar';
 export { DatePicker } from './components/DatePicker/DatePicker';
+export { DateRangePicker } from './components/DateRangePicker/DateRangePicker';
 export { Month } from './components/Month/Month';
 export { RangeCalendar } from './components/RangeCalendar/RangeCalendar';
 export { TimeInput } from './components/TimeInput/TimeInput';
 
 export type { CalendarStylesNames, CalendarProps } from './components/Calendar/Calendar';
 export type { DatePickerProps } from './components/DatePicker/DatePicker';
+export type { DateRangePickerProps } from './components/DateRangePicker/DateRangePicker';
 export type { DatePickerStylesNames } from './components/DatePickerBase/DatePickerBase';
 export type { MonthProps, MonthStylesNames } from './components/Month/Month';
 export type { RangeCalendarProps } from './components/RangeCalendar/RangeCalendar';
