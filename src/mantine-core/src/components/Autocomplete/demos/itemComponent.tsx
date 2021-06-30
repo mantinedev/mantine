158d1ce9803344568b80ef567543c690a95d46e4
Vitaly Rtishchev <rtivital@gmail.com>
2021-06-30 17:42:10 +0300

    [docs] Add autocomplete component docs

--- src/mantine-core/src/components/Autocomplete/demos/itemComponent.tsx
+++ src/mantine-core/src/components/Autocomplete/demos/itemComponent.tsx
@@ -5,7 +5,13 @@ import { Autocomplete } from '../Autocomplete';
 const code = `
 import { Group, Avatar, Text, UnstyledButton, Autocomplete } from '@mantine/core';
 
-function AutoCompleteItem({ color, email, name, ...others }: ItemProps) {
+const data = [
+  { value: 'bob@handsome.inc', color: 'red', email: 'bob@handsome.inc', name: 'Bob Handsome' },
+  { value: 'bill@outlook.com', color: 'teal', email: 'bill@outlook.com', name: 'Bill Gates' },
+  { value: 'amy@wong.cn', color: 'blue', email: 'amy@wong.cn', name: 'Amy Wong' },
+];
+
+function AutoCompleteItem({ color, email, name, ...others }) {
   return (
     <UnstyledButton {...others}>
       <Group>
