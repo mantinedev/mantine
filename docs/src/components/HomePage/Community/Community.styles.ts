10e3f57bfa8ed782f8f3b43eaf1fe13eedc4c4c8
Vitaly Rtishchev <rtivital@gmail.com>
2021-07-01 22:38:17 +0300

    [docs] Remove old home page components

--- docs/src/components/HomePage/Community/Community.styles.ts
+++ docs/src/components/HomePage/Community/Community.styles.ts
@@ -8,7 +8,7 @@ export default createUseStyles(
     wrapper: {
       position: 'relative',
       paddingTop: 80,
-      paddingBottom: 120,
+      paddingBottom: 160,
       backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
     },
   }),
