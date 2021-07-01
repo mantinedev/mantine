8c9f40e918cb9d594685bca9311cd0acd2d7500b
Vitaly Rtishchev <rtivital@gmail.com>
2021-07-01 12:11:28 +0300

    [@mantine/core] Select: Add custom item component demos

--- src/mantine-core/src/components/Select/demos/index.ts
+++ src/mantine-core/src/components/Select/demos/index.ts
@@ -2,3 +2,4 @@ export { usage } from './usage';
 export { searchable } from './searchable';
 export { large } from './large';
 export { configurator } from './configurator';
+export { itemComponent } from './itemComponent';
