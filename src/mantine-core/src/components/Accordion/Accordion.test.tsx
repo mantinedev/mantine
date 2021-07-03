0e512e5fd4d392be057b292420ed267f82f36207
Vitaly Rtishchev <rtivital@gmail.com>
2021-07-03 12:48:00 +0300

    [@mantine/core] Accordion: Add all tests

--- src/mantine-core/src/components/Accordion/Accordion.test.tsx
+++ src/mantine-core/src/components/Accordion/Accordion.test.tsx
@@ -9,6 +9,7 @@ import {
 } from '@mantine/tests';
 import { Accordion, AccordionItem } from './Accordion';
 import { Accordion as AccordionStylesApi } from './styles.api';
+import { AccordionControl } from './AccordionControl/AccordionControl';
 
 const defaultProps = {
   children: [
@@ -24,6 +25,11 @@ describe('@mantine/core/Accordion', () => {
   itSupportsStyle(Accordion, defaultProps);
   checkAccessibility([mount(<Accordion {...defaultProps} />)]);
   itSupportsStylesApi(Accordion, defaultProps, Object.keys(AccordionStylesApi), 'accordion');
+
+  it('renders correct amount of items', () => {
+    const element = shallow(<Accordion {...defaultProps} />);
+    expect(element.find(AccordionControl)).toHaveLength(defaultProps.children.length);
+  });
 
   it('has correct displayName', () => {
     expect(Accordion.displayName).toEqual('@mantine/core/Accordion');
