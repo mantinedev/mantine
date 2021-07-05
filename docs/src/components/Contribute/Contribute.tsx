965eb47eed431b08dfc1ce8721e72816ca63cc98
Vitaly Rtishchev <rtivital@gmail.com>
2021-07-05 20:19:19 +0300

    [docs] Wrap contribute page into container

--- docs/src/components/Contribute/Contribute.tsx
+++ docs/src/components/Contribute/Contribute.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ArrowRightIcon } from '@modulz/radix-icons';
 import { useClipboard } from '@mantine/hooks';
-import { Title, Text, Anchor, ThemeIcon, Group, Button } from '@mantine/core';
+import { Title, Text, Anchor, ThemeIcon, Group, Button, Container } from '@mantine/core';
 import Head from '../Head/Head';
 import { Footer } from '../Footer/Footer';
 import { SocialButton } from '../SocialButton/SocialButton';
@@ -45,64 +45,66 @@ export function Contribute() {
       <div className={classes.wrapper}>
         <Head title="Contribute to Mantine" disableTitleTemplate />
 
-        <Title className={classes.title}>Contribute to Mantine</Title>
-        <Text style={{ maxWidth: 600 }}>
-          First of all, thank you for showing interest in contributing to Mantine, all your
-          contributions are extremely valuable to the project!
-        </Text>
+        <Container size={1080}>
+          <Title className={classes.title}>Contribute to Mantine</Title>
+          <Text style={{ maxWidth: 600 }}>
+            First of all, thank you for showing interest in contributing to Mantine, all your
+            contributions are extremely valuable to the project!
+          </Text>
 
-        <Title order={2} className={classes.subtitle}>
-          Ways to contribute
-        </Title>
+          <Title order={2} className={classes.subtitle}>
+            Ways to contribute
+          </Title>
 
-        <Group className={classes.cards} align="stretch">
-          {items}
-        </Group>
+          <Group className={classes.cards} align="stretch">
+            {items}
+          </Group>
 
-        <Title order={2} className={classes.subtitle}>
-          Become a maintainer
-        </Title>
+          <Title order={2} className={classes.subtitle}>
+            Become a maintainer
+          </Title>
 
-        <Text style={{ maxWidth: 600, marginBottom: 15 }}>
-          Mantine is a new fast growing React components and hooks library, we are always looking
-          forward extending core team. If you want to be a part of open source project we will be
-          happy to have you onboard.
-        </Text>
+          <Text style={{ maxWidth: 600, marginBottom: 15 }}>
+            Mantine is a new fast growing React components and hooks library, we are always looking
+            forward extending core team. If you want to be a part of open source project we will be
+            happy to have you onboard.
+          </Text>
 
-        <Text weight={700}>What you will able to do as a maintainer:</Text>
+          <Text weight={700}>What you will able to do as a maintainer:</Text>
 
-        <ul style={{ maxWidth: 600, marginBottom: 15 }}>
-          <li>
-            Build new components and hooks for @mantine/core, @mantine/hooks and other packages
-          </li>
-          <li>Work on improvements of existing components</li>
-          <li>Work on Mantine documentation website to improve UX</li>
-          <li>
-            Participate in{' '}
-            <Anchor href="https://github.com/mantinedev/mantine/discussions">
-              community discussions
-            </Anchor>
-          </li>
-        </ul>
+          <ul style={{ maxWidth: 600, marginBottom: 15 }}>
+            <li>
+              Build new components and hooks for @mantine/core, @mantine/hooks and other packages
+            </li>
+            <li>Work on improvements of existing components</li>
+            <li>Work on Mantine documentation website to improve UX</li>
+            <li>
+              Participate in{' '}
+              <Anchor href="https://github.com/mantinedev/mantine/discussions">
+                community discussions
+              </Anchor>
+            </li>
+          </ul>
 
-        <Text weight={700}>What you will need:</Text>
-        <ul>
-          <li>Good React knowledge (with hooks only, Mantine does not have class components)</li>
-          <li>Basic understanding of TypeScript</li>
-          <li>2-3 hours per week to devote to open source work</li>
-        </ul>
+          <Text weight={700}>What you will need:</Text>
+          <ul>
+            <li>Good React knowledge (with hooks only, Mantine does not have class components)</li>
+            <li>Basic understanding of TypeScript</li>
+            <li>2-3 hours per week to devote to open source work</li>
+          </ul>
 
-        <Text>Sounds interesting? Let&apos;s contact and discuss all details:</Text>
-        <Group style={{ marginTop: 10 }}>
-          <SocialButton type="large" service="tg" />
-          <Button
-            style={{ height: 42, paddingLeft: 20, paddingRight: 20 }}
-            onClick={() => clipboard.copy('rtivital@gmail.com')}
-            color={clipboard.copied ? 'teal' : 'blue'}
-          >
-            {clipboard.copied ? 'Email copied to clipboard' : 'Contact via email'}
-          </Button>
-        </Group>
+          <Text>Sounds interesting? Let&apos;s contact and discuss all details:</Text>
+          <Group style={{ marginTop: 10 }}>
+            <SocialButton type="large" service="tg" />
+            <Button
+              style={{ height: 42, paddingLeft: 20, paddingRight: 20 }}
+              onClick={() => clipboard.copy('rtivital@gmail.com')}
+              color={clipboard.copied ? 'teal' : 'blue'}
+            >
+              {clipboard.copied ? 'Email copied to clipboard' : 'Contact via email'}
+            </Button>
+          </Group>
+        </Container>
       </div>
 
       <Footer withNavbar />
