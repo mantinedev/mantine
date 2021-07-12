e227c355cd59be7ae55d9fb85445124455d163f1
kosciolek <45483493+kosciolek@users.noreply.github.com>
2021-07-12 14:08:10 +0200

    use-intersection implementation

---
+++ src/mantine-hooks/src/use-intersection/use-intersection.ts
@@ -0,0 +1,25 @@
+import { useCallback, useState } from 'react';
+
+export function useIntersection(
+  options: ConstructorParameters<typeof IntersectionObserver>[1]
+) {
+  const [latestEntry, setLatestEntry] = useState<null | IntersectionObserverEntry>(null);
+
+  const refCallback = useCallback(
+    (element: HTMLElement | null) => {
+      if (element !== null) return () => {};
+
+      const io = new IntersectionObserver(([entry]) => {
+        /* The hook supports observing just one element */
+        setLatestEntry(entry);
+      }, options);
+
+      io.observe(element);
+
+      return () => io.disconnect();
+    },
+    [options.rootMargin, options.root, options.threshold]
+  );
+
+  return [refCallback, latestEntry];
+}
