726c9fc28d62b9138307ebeabb3d4ee692822292
Chindukuri <ghana.chindukuri@gmail.com>
2021-07-13 14:55:38 +0530

    Adding useHash hook

---
+++ src/mantine-hooks/src/use-hash/use-hash.ts
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import { useWindowEvent } from 'src/use-window-event/use-window-event';
+
+export function useHash() {
+  const isBrowser = typeof window !== 'undefined';
+
+  const [hash, setHashValue] = useState<null | string>(null);
+  const handleHashChange = (e: HashChangeEvent) => {
+    e.preventDefault();
+    setHashValue(window.location.hash);
+  };
+  function setHash(value: string) {
+    if (isBrowser) {
+      window.location.hash = value;
+      setHashValue(value);
+    }
+  }
+
+  useWindowEvent('hashchange', handleHashChange);
+
+  return [hash, setHash];
+}
