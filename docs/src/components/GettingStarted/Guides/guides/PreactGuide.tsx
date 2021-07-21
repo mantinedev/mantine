44f341e843f4dd853436514df398e728f6cc1480
Vitaly Rtishchev <rtivital@gmail.com>
2021-07-21 09:48:25 +0300

    [docs] Update preact guide to work with new cli version

--- docs/src/components/GettingStarted/Guides/guides/PreactGuide.tsx
+++ docs/src/components/GettingStarted/Guides/guides/PreactGuide.tsx
@@ -8,22 +8,6 @@ interface CraGuideProps {
   dependencies: string;
 }
 
-const code = `export default {
-  webpack(config, env) {
-    config.resolve = {
-      ...config.resolve,
-      alias: {
-        ...config.resolve.alias,
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      },
-    };
-
-    return config;
-  },
-};`;
-
 export function PreactGuide({ dependencies }: CraGuideProps) {
   return (
     <div>
@@ -35,10 +19,6 @@ export function PreactGuide({ dependencies }: CraGuideProps) {
         Install dependencies
       </Text>
       <Installation dependencies={dependencies} />
-      <Text weight={700} style={{ marginBottom: 15, marginTop: 30 }}>
-        Create preact.config.js
-      </Text>
-      <Prism language="tsx">{code}</Prism>
       <Done />
     </div>
   );
