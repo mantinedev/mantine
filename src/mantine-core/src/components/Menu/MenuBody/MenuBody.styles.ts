0bef361fc482f0d6fa9ecf00f7b3fb572f2891ce
Vitaly Rtishchev <rtivital@gmail.com>
2021-07-23 23:50:04 +0300

    [@mantine/core] Menu: Fix dark theme for menu label

--- src/mantine-core/src/components/Menu/MenuBody/MenuBody.styles.ts
+++ src/mantine-core/src/components/Menu/MenuBody/MenuBody.styles.ts
@@ -33,7 +33,7 @@ export default createMemoStyles({
   }),
 
   label: ({ theme }: MenuBodyStyles) => ({
-    color: theme.colors.gray[6],
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.gray[6],
     fontWeight: 500,
     fontSize: theme.fontSizes.xs,
     padding: [theme.spacing.xs / 2, theme.spacing.sm],
