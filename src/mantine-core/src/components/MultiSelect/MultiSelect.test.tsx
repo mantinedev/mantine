dd286cc1dd2d76e8f3b3bc3a976a7d6985108ea5
Vitaly Rtishchev <rtivital@gmail.com>
2021-07-28 16:47:22 +0300

    [@mantine/core] MultiSelect: Add accesibility tests

--- src/mantine-core/src/components/MultiSelect/MultiSelect.test.tsx
+++ src/mantine-core/src/components/MultiSelect/MultiSelect.test.tsx
@@ -1,5 +1,11 @@
-// import React from 'react';
-import { itSupportsClassName, itSupportsRef, itSupportsStyle } from '@mantine/tests';
+import React from 'react';
+import { mount } from 'enzyme';
+import {
+  itSupportsClassName,
+  itSupportsRef,
+  itSupportsStyle,
+  checkAccessibility,
+} from '@mantine/tests';
 import { MultiSelect } from './MultiSelect';
 
 const defaultProps = {
@@ -11,6 +17,19 @@ describe('@mantine/core/MultiSelect', () => {
   itSupportsClassName(MultiSelect, defaultProps);
   itSupportsStyle(MultiSelect, defaultProps);
   itSupportsRef(MultiSelect, defaultProps, HTMLInputElement, 'elementRef');
+  checkAccessibility([
+    mount(<MultiSelect {...defaultProps} label="test-label" />),
+    mount(<MultiSelect {...defaultProps} label="test-label" defaultValue={[]} />),
+    mount(<MultiSelect {...defaultProps} label="test-label" initiallyOpened />),
+    mount(
+      <MultiSelect
+        {...defaultProps}
+        aria-label="test-label"
+        clearable
+        clearButtonLabel="Clear field"
+      />
+    ),
+  ]);
 
   it('has correct displayName', () => {
     expect(MultiSelect.displayName).toEqual('@mantine/core/MultiSelect');
