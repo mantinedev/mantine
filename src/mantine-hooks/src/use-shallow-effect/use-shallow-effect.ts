9d88d47d36d8474f3d24ab9f517b002f1b5ac172
achmurali <achmurali@github.com>
2021-07-30 19:57:09 +0530

    minor changes and useShallowEffect doc

--- src/mantine-hooks/src/use-shallow-effect/use-shallow-effect.ts
+++ src/mantine-hooks/src/use-shallow-effect/use-shallow-effect.ts
@@ -1,40 +1,7 @@
 import { useEffect, useRef } from 'react';
+import { shallowCompare as compare } from '../utils/shallow-compare/shallow-compare';
 
 type useEffectReturnType = ReturnType<typeof useEffect>;
-
-function typeOf(object: any): string {
-    return object.constructor.name.toLowerCase();
-}
-
-const keyList = Object.keys;
-
-function compare(a:any, b:any) {
-    const source = a;
-    const target = b;
-    //for primitive types
-    if (Object.is(source, target)) {
-        return true;
-    }
-    //to check for undefined
-    if (!(source instanceof Object) || !(target instanceof Object)) {
-        return false;
-    }
-
-    if (typeOf(source) !== typeOf(target)) {
-        return false;
-    }
-
-    const keys = keyList(a);
-    const { length } = keys;
-    for (let i = 0; i < length; i += 1) {
-      if (!(keys[i] in b)) return false;
-    }
-    for (let i = 0; i < length; i += 1) {
-      if (a[keys[i]] !== b[keys[i]]) return false;
-    }
-
-    return length === keyList(b).length;
-}
 
 function shallowCompare(prevValue: React.DependencyList, currValue: React.DependencyList) {
     if (!prevValue || !currValue) {
