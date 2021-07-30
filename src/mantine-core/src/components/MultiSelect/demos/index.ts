91472d1a3ed2a1e23858ee1563a8b19051d80d35
Vitaly Rtishchev <rtivital@gmail.com>
2021-07-30 12:44:50 +0300

    [@mantine/core] MultiSelect: Add transitions demo

--- src/mantine-core/src/components/MultiSelect/demos/index.ts
+++ src/mantine-core/src/components/MultiSelect/demos/index.ts
@@ -5,3 +5,4 @@ export { clearable } from './clearable';
 export { configurator } from './configurator';
 export { large } from './large';
 export { users } from './users';
+export { transitions } from './transitions';
