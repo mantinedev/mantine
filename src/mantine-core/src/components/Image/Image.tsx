c8d69f32bae84dd13512ff2a8040ecfb5c884152
Vitaly Rtishchev <rtivital@gmail.com>
2021-07-30 09:24:15 +0300

    [@mantine/core] Image: Add figcaption support

--- src/mantine-core/src/components/Image/Image.tsx
+++ src/mantine-core/src/components/Image/Image.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import cx from 'clsx';
 import { DefaultProps, useMantineTheme, MantineNumberSize, mergeStyles } from '../../theme';
+import { Text } from '../Text/Text';
 import { ImageIcon } from './ImageIcon';
 import useStyles from './Image.styles';
 
@@ -41,6 +42,9 @@ export interface ImageProps
 
   /** Get image element ref */
   imageRef?: React.ForwardedRef<HTMLImageElement>;
+
+  /** Image figcaption, displayed bellow image */
+  caption?: React.ReactNode;
 }
 
 export function Image({
@@ -60,6 +64,7 @@ export function Image({
   elementRef,
   classNames,
   styles,
+  caption,
   ...others
 }: ImageProps) {
   const theme = useMantineTheme(themeOverride);
@@ -93,22 +98,36 @@ export function Image({
         </div>
       )}
 
-      <img
-        className={classes.image}
-        src={src}
-        alt={alt}
-        style={{ ..._styles.image, objectFit: fit }}
-        ref={imageRef}
-        onLoad={(event) => {
-          setLoaded(true);
-          typeof imageProps?.onLoad === 'function' && imageProps.onLoad(event);
-        }}
-        onError={(event) => {
-          setError(true);
-          typeof imageProps?.onError === 'function' && imageProps.onError(event);
-        }}
-        {...imageProps}
-      />
+      <figure className={classes.figure}>
+        <img
+          className={classes.image}
+          src={src}
+          alt={alt}
+          style={{ ..._styles.image, objectFit: fit }}
+          ref={imageRef}
+          onLoad={(event) => {
+            setLoaded(true);
+            typeof imageProps?.onLoad === 'function' && imageProps.onLoad(event);
+          }}
+          onError={(event) => {
+            setError(true);
+            typeof imageProps?.onError === 'function' && imageProps.onError(event);
+          }}
+          {...imageProps}
+        />
+
+        {!!caption && (
+          <Text
+            component="figcaption"
+            size="sm"
+            align="center"
+            className={classes.caption}
+            style={_styles.caption}
+          >
+            {caption}
+          </Text>
+        )}
+      </figure>
     </div>
   );
 }
--- src/mantine-core/src/components/Image/demos/caption.tsx
+++ src/mantine-core/src/components/Image/demos/caption.tsx
@@ -7,6 +7,7 @@ const code = `
     radius="md"
     src="https://images.unsplash.com/long-image-url-was-here.jpg"
     alt="Random unsplash image"
+    caption="My dog begging for treats"
   />
 </div>
 `;
@@ -16,15 +17,15 @@ function Demo() {
     <div style={{ width: 240, marginLeft: 'auto', marginRight: 'auto' }}>
       <Image
         radius="md"
-        src="https://images.unsplash.com/photo-1511216335778-7cb8f49fa7a3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+        src="https://images.unsplash.com/photo-1627552245715-77d79bbf6fe2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=640&q=80"
         alt="Random unsplash image"
-        caption="Random unsplash image"
+        caption="My dog begging for treats"
       />
     </div>
   );
 }
 
-export const usage: MantineDemo = {
+export const caption: MantineDemo = {
   type: 'demo',
   code,
   component: Demo,
