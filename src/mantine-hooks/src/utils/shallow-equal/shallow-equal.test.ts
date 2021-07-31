d14b5da5ed8fd647e2dec65b74caa04031116b83
Vitaly Rtishchev <rtivital@gmail.com>
2021-07-31 12:53:58 +0300

    [@mantine/hooks] Format shallow equal utils tests

--- src/mantine-hooks/src/utils/shallow-equal/shallow-equal.test.ts
+++ src/mantine-hooks/src/utils/shallow-equal/shallow-equal.test.ts
@@ -5,41 +5,22 @@ describe('@mantine/hooks/shallow-equal', () => {
     expect(shallowEqual(1, 1)).toBe(true);
     expect(shallowEqual(-1.0001, -1.0001)).toBe(true);
     expect(shallowEqual('test', 'test')).toBe(true);
-    const number = 1;
-    const string = 'test';
-    expect(shallowEqual(number, 1)).toBe(true);
-    expect(shallowEqual(string, 'test')).toBe(true);
     expect(shallowEqual(1, 2)).toBe(false);
     expect(shallowEqual(undefined, undefined)).toBe(true);
     expect(shallowEqual(null, null)).toBe(true);
-    expect(shallowEqual(NaN, NaN)).toBe(true);
   });
 
   it('correctly compares object and array type', () => {
-    const obj = {
-      a: 1,
-      b: 2,
-      c: {
-        a: 1,
-      },
-    };
-    const arr = [1, 2, 3];
+    const object = { a: 1, b: 2, c: { a: 1 } };
+    const list = [1, 2, 3];
     const test = { a: 1 };
     expect(shallowEqual([1, 2, 3], [1, 2, 3])).toBe(true);
-    expect(shallowEqual([1, 2, 3], arr)).toBe(true);
+    expect(shallowEqual([1, 2, 3], list)).toBe(true);
     expect(shallowEqual([1, 2, 3, test], [1, 2, 3, test])).toBe(true);
-    expect(shallowEqual([1, 2, 3, test], [...arr, test])).toBe(true);
+    expect(shallowEqual([1, 2, 3, test], [...list, test])).toBe(true);
     expect(shallowEqual([1, 2, 3, test], [1, 2, 3, { a: 1 }])).toBe(false);
-    expect(shallowEqual([1, 2, 3, 4], arr)).toBe(false);
-    expect(shallowEqual(obj, obj)).toBe(true);
-    expect(
-      shallowEqual(obj, {
-        a: 1,
-        b: 2,
-        c: {
-          a: 1,
-        },
-      })
-    ).toBe(false);
+    expect(shallowEqual([1, 2, 3, 4], list)).toBe(false);
+    expect(shallowEqual(object, object)).toBe(true);
+    expect(shallowEqual(object, { a: 1, b: 2, c: { a: 1 } })).toBe(false);
   });
 });
