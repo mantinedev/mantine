9583a2a66c4223429c3f77c78861fdc63d37b753
Vitaly Rtishchev <rtivital@gmail.com>
2021-08-06 16:41:31 +0300

    [@mantine/core] Image: Remove incorrect caption from usage demo

--- src/mantine-core/src/components/Image/demos/usage.tsx
+++ src/mantine-core/src/components/Image/demos/usage.tsx
@@ -18,7 +18,6 @@ function Demo() {
         radius="md"
         src="https://images.unsplash.com/photo-1511216335778-7cb8f49fa7a3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
         alt="Random unsplash image"
-        caption="Random unsplash image"
       />
     </div>
   );
