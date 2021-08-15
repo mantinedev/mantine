e53f719ee0ad4df467650a1f8f051f4b03ba37b9
Vitaly Rtishchev <rtivital@gmail.com>
2021-08-15 18:15:13 +0300

    [@mantine/core] Menu: Update docs for position

---
+++ src/mantine-core/src/components/Menu/demos/positionConfigurator.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Menu, MenuProps } from '../Menu';
+import { Group } from '../../Group/Group';
+import { menuItems } from './_menu-items';
+
+function Wrapper(props: MenuProps) {
+  return (
+    <Group position="center">
+      <Menu {...props}>{menuItems}</Menu>
+    </Group>
+  );
+}
+
+const codeTemplate = (props: string) => `<Menu${props}>
+  {/* Menu items */}
+</Menu>`;
+
+export const positionConfigurator: MantineDemo = {
+  type: 'configurator',
+  component: Wrapper,
+  codeTemplate,
+  configurator: [
+    {
+      name: 'position',
+      type: 'select',
+      initialValue: 'bottom',
+      defaultValue: 'bottom',
+      data: [
+        { label: 'Bottom', value: 'bottom' },
+        { label: 'Top', value: 'top' },
+        { label: 'Right', value: 'right' },
+        { label: 'Left', value: 'left' },
+      ],
+    },
+    {
+      name: 'placement',
+      type: 'select',
+      initialValue: 'start',
+      defaultValue: 'start',
+      data: [
+        { label: 'Start', value: 'start' },
+        { label: 'Center', value: 'center' },
+        { label: 'End', value: 'end' },
+      ],
+    },
+    {
+      name: 'gutter',
+      type: 'number',
+      initialValue: 5,
+      defaultValue: 5,
+      max: 20,
+      min: -20,
+    },
+    {
+      name: 'withArrow',
+      type: 'boolean',
+      initialValue: false,
+      defaultValue: false,
+    },
+  ],
+};
