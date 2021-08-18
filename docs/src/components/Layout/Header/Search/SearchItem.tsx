ed8ebf85d8b87a06567087f0afc147bbdd9bcf60
achmurali <achmurali@github.com>
2021-08-18 21:00:54 +0530

    Migrating Search Functionality in docs to Autocomplete

---
+++ docs/src/components/Layout/Header/Search/SearchItem.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable no-console */
+import React from 'react';
+import { Text, Highlight } from '@mantine/core';
+import { upperFirst } from '@mantine/hooks';
+import useStyles from './Search.styles';
+
+interface SearchItemProps {
+  slug: string;
+  title: string;
+  mantinePackage: string;
+  group: string;
+  query: string;
+  elementRef?:any;
+}
+
+export default function SearchItem({
+  slug,
+  title,
+  mantinePackage,
+  group,
+  query,
+  elementRef,
+  ...others
+  }: SearchItemProps) {
+  const classes = useStyles();
+  return (
+    <button
+      type="button"
+      key={slug}
+      tabIndex={-1}
+      ref={elementRef}
+      {...others}
+    >
+      <Highlight highlight={query}>{title}</Highlight>
+      <Text color="gray" size="sm" className={classes.package}>
+        {mantinePackage ? mantinePackage.replace('mantine-', '@mantine/') : upperFirst(group)}
+      </Text>
+    </button>
+  );
+}
