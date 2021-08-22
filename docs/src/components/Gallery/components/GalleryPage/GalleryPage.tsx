97d0a46de4352f59eaafbfdf9843b7816470f6cd
Vitaly Rtishchev <rtivital@gmail.com>
2021-08-22 14:38:18 +0300

    [docs] Add footer to all gallery pages

--- docs/src/components/Gallery/components/GalleryPage/GalleryPage.tsx
+++ docs/src/components/Gallery/components/GalleryPage/GalleryPage.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Footer } from '../../../Footer/Footer';
+import useStyles from './GalleryPage.styles';
 
 interface GalleryPageProps {
   children: React.ReactNode;
 }
 
 export function GalleryPage({ children }: GalleryPageProps) {
-  return <div style={{ paddingTop: 22 }}>{children}</div>;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.content}>{children}</div>
+      <Footer />
+    </div>
+  );
 }
