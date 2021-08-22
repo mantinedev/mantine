3e1b5e3c2bf725f330e9a78a3ebbe24200b798a4
Vitaly Rtishchev <rtivital@gmail.com>
2021-08-22 14:47:28 +0300

    [docs] Add separate gallery components previews

--- docs/src/components/Gallery/GalleryComponentPage.tsx
+++ docs/src/components/Gallery/GalleryComponentPage.tsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import * as GalleryComponents from '../../gallery';
+import { GalleryComponent } from './types';
 
-export default function GalleryComponentPage() {
-  return <div>GalleryComponentPage</div>;
+interface GalleryComponentPageProps {
+  pageContext: GalleryComponent;
+}
+
+export default function GalleryComponentPage({ pageContext }: GalleryComponentPageProps) {
+  const Component = GalleryComponents[pageContext._component];
+  return (
+    <div style={{ paddingTop: 22 }}>
+      <div
+        style={{
+          paddingTop: pageContext.attributes.canvas?.maxWidth ? 40 : 0,
+          maxWidth: pageContext.attributes.canvas?.maxWidth || '100%',
+          marginLeft: pageContext.attributes.canvas?.center ? 'auto' : 'unset',
+          marginRight: pageContext.attributes.canvas?.center ? 'auto' : 'unset',
+        }}
+      >
+        <Component />
+      </div>
+    </div>
+  );
 }
