954fd038d8e2feb6139a9ef82f26fdff050b38fa
Vitaly Rtishchev <rtivital@gmail.com>
2021-08-23 13:38:12 +0300

    [@mantine/core] Text: Add dimmed color support

--- src/mantine-core/src/components/Text/Text.styles.ts
+++ src/mantine-core/src/components/Text/Text.styles.ts
@@ -13,18 +13,25 @@ interface TextStyles {
   size: MantineSize;
 }
 
+function getTextColor({ theme, color, variant }: Partial<TextStyles>) {
+  if (color === 'dimmed') {
+    return theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.gray[6];
+  }
+
+  return color in theme.colors
+    ? theme.colors[color][theme.colorScheme === 'dark' ? 5 : 7]
+    : variant === 'link'
+    ? theme.colors[theme.primaryColor][theme.colorScheme === 'dark' ? 5 : 7]
+    : theme.colorScheme === 'dark'
+    ? theme.colors.dark[0]
+    : theme.black;
+}
+
 export default createMemoStyles({
   root: ({ theme, color, variant, size }: TextStyles) => ({
     ...getFontStyles(theme),
     ...getFocusStyles(theme),
-    color:
-      color in theme.colors
-        ? theme.colors[color][theme.colorScheme === 'dark' ? 5 : 7]
-        : variant === 'link'
-        ? theme.colors[theme.primaryColor][theme.colorScheme === 'dark' ? 5 : 7]
-        : theme.colorScheme === 'dark'
-        ? theme.colors.dark[0]
-        : theme.black,
+    color: getTextColor({ color, theme, variant }),
     fontSize: theme.fontSizes[size],
     lineHeight: theme.lineHeight,
     textDecoration: 'none',
--- src/mantine-core/src/components/Text/demos/index.ts
+++ src/mantine-core/src/components/Text/demos/index.ts
@@ -1,1 +1,2 @@
 export { usage } from './usage';
+export { dimmed } from './dimmed';
