bac07751f99c30abf65e967ab83f81df26e68b11
Vitaly Rtishchev <rtivital@gmail.com>
2021-08-25 22:17:17 +0300

    [@mantine/core] Checkbox: Add better icons

--- src/mantine-core/src/components/Checkbox/CheckboxIcon.tsx
+++ src/mantine-core/src/components/Checkbox/CheckboxIcon.tsx
@@ -5,21 +5,18 @@ interface CheckboxIconProps extends React.ComponentPropsWithoutRef<'svg'> {
 }
 
 export function CheckboxIcon({ indeterminate, ...others }: CheckboxIconProps) {
+  if (indeterminate) {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 32 6" {...others}>
+        <rect width="32" height="6" fill="currentColor" rx="3" />
+      </svg>
+    );
+  }
+
   return (
-    <svg
-      width="15"
-      height="15"
-      viewBox="0 0 15 15"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      {...others}
-    >
+    <svg viewBox="0 0 10 7" fill="none" xmlns="http://www.w3.org/2000/svg" {...others}>
       <path
-        d={
-          indeterminate
-            ? 'M2 7.5C2 7.22386 2.22386 7 2.5 7H12.5C12.7761 7 13 7.22386 13 7.5C13 7.77614 12.7761 8 12.5 8H2.5C2.22386 8 2 7.77614 2 7.5Z'
-            : 'M11.4669 3.72684C11.7558 3.91574 11.8369 4.30308 11.648 4.59198L7.39799 11.092C7.29783 11.2452 7.13556 11.3467 6.95402 11.3699C6.77247 11.3931 6.58989 11.3355 6.45446 11.2124L3.70446 8.71241C3.44905 8.48022 3.43023 8.08494 3.66242 7.82953C3.89461 7.57412 4.28989 7.55529 4.5453 7.78749L6.75292 9.79441L10.6018 3.90792C10.7907 3.61902 11.178 3.53795 11.4669 3.72684Z'
-        }
+        d="M4 4.586L1.707 2.293A1 1 0 1 0 .293 3.707l3 3a.997.997 0 0 0 1.414 0l5-5A1 1 0 1 0 8.293.293L4 4.586z"
         fill="currentColor"
         fillRule="evenodd"
         clipRule="evenodd"
