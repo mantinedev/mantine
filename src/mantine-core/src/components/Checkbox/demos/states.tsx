f5e4b5f1ebd96e4a00d4189634586db483d2c714
Vitaly Rtishchev <rtivital@gmail.com>
2021-08-25 22:23:54 +0300

    [@mantine/core] Checkbox: Add all states demo

---
+++ src/mantine-core/src/components/Checkbox/demos/states.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Checkbox } from '../Checkbox';
+import { Group } from '../../Group/Group';
+
+const code = `
+<Checkbox checked={false} label="Default checkbox" />
+<Checkbox checked={false} indeterminate label="Indeterminate checkbox" />
+<Checkbox checked label="Checked checkbox" />
+<Checkbox disabled label="Disabled checkbox" />
+<Checkbox disabled checked label="Disabled checked checkbox" />
+<Checkbox disabled indeterminate label="Disabled indeterminate checkbox" />
+`;
+
+function Demo() {
+  return (
+    <Group direction="column">
+      <Checkbox checked={false} label="Default checkbox" />
+      <Checkbox checked={false} indeterminate label="Indeterminate checkbox" />
+      <Checkbox checked label="Checked checkbox" />
+      <Checkbox disabled label="Disabled checkbox" />
+      <Checkbox disabled checked label="Disabled checked checkbox" />
+      <Checkbox disabled indeterminate label="Disabled indeterminate checkbox" />
+    </Group>
+  );
+}
+
+export const states: MantineDemo = {
+  type: 'demo',
+  component: Demo,
+  code,
+};
