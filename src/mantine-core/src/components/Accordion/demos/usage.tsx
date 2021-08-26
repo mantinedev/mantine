a386cbffba60451f60fdbc03aa98a4aa698f6da4
Vitaly Rtishchev <rtivital@gmail.com>
2021-08-26 10:08:59 +0300

    [@mantine/core] Accordion: Add simpler nested components api

--- src/mantine-core/src/components/Accordion/demos/usage.tsx
+++ src/mantine-core/src/components/Accordion/demos/usage.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import { Accordion, AccordionItem } from '../Accordion';
+import { Accordion } from '../Accordion';
 
 const code = `
 <Accordion>
-  <AccordionItem label="Customization">
+  <Accordion.Item label="Customization">
     Colors, fonts, shadows and many other parts are customizable to fit your design needs
-  </AccordionItem>
+  </Accordion.Item>
 
-  <AccordionItem label="Flexibility">
+  <Accordion.Item label="Flexibility">
     Configure components appearance and behavior with vast amount of settings or overwrite any
     part of component styles
-  </AccordionItem>
+  </Accordion.Item>
 
-  <AccordionItem label="No annoying focus ring">
+  <Accordion.Item label="No annoying focus ring">
     With new :focus-visible pseudo-class focus ring appears only when user navigates with
     keyboard
-  </AccordionItem>
+  </Accordion.Item>
 </Accordion>
 `;
 
@@ -23,19 +23,19 @@ function Demo() {
   return (
     <div style={{ maxWidth: 400, marginLeft: 'auto', marginRight: 'auto' }}>
       <Accordion>
-        <AccordionItem label="Customization">
+        <Accordion.Item label="Customization">
           Colors, fonts, shadows and many other parts are customizable to fit your design needs
-        </AccordionItem>
+        </Accordion.Item>
 
-        <AccordionItem label="Flexibility">
+        <Accordion.Item label="Flexibility">
           Configure components appearance and behavior with vast amount of settings or overwrite any
           part of component styles
-        </AccordionItem>
+        </Accordion.Item>
 
-        <AccordionItem label="No annoying focus ring">
+        <Accordion.Item label="No annoying focus ring">
           With new :focus-visible pseudo-class focus ring appears only when user navigates with
           keyboard
-        </AccordionItem>
+        </Accordion.Item>
       </Accordion>
     </div>
   );
