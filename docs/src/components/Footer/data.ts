896cbc80ebc894cc3ddc6e6a0b41bb1f8d2b4a92
Vitaly Rtishchev <rtivital@gmail.com>
2021-08-26 16:44:13 +0300

    [docs] Remove maintainer section from Contribute page

--- docs/src/components/Footer/data.ts
+++ docs/src/components/Footer/data.ts
@@ -5,7 +5,6 @@ export const FOOTER_LINKS_DATA: LinksGroupProps[] = [
     title: 'Project',
     data: [
       { type: 'gatsby', label: 'Contribute to Mantine', link: '/contribute/' },
-      { type: 'gatsby', label: 'Become a maintainer', link: '/contribute/' },
       { type: 'gatsby', label: 'Changelog', link: '/pages/changelog/' },
       { type: 'link', label: 'Releases', link: 'https://github.com/mantinedev/mantine/releases' },
     ],
