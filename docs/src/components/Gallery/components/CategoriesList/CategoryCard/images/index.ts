6a63c0ef30c7c40f9af800fe189a23030fa02faa
Vitaly Rtishchev <rtivital@gmail.com>
2021-08-28 15:21:31 +0300

    [docs] Add hero headers gallery images

--- docs/src/components/Gallery/components/CategoriesList/CategoryCard/images/index.ts
+++ docs/src/components/Gallery/components/CategoriesList/CategoryCard/images/index.ts
@@ -4,8 +4,11 @@ import formsLight from './forms-light.svg';
 import cardsLight from './cards-light.svg';
 import cardsDark from './cards-dark.svg';
 
+import heroLight from './hero-light.svg';
+import heroDark from './hero-dark.svg';
+
 export default {
   forms: { light: formsLight, dark: formsDark },
   cards: { light: cardsLight, dark: cardsDark },
-  hero: {},
+  hero: { light: heroLight, dark: heroDark },
 };
