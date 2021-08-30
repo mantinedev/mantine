641d07157dc7e6b8ba81ef2bf83fd8b3e8f6044d
Leo Rotzler <leo.rotzler@bkb.ch>
2021-08-30 21:44:45 +0200

    Updated Grid docs with new responsive Grid

--- src/mantine-core/src/components/Grid/demos/responsive.tsx
+++ src/mantine-core/src/components/Grid/demos/responsive.tsx
@@ -1,36 +1,30 @@
 import React from 'react';
-import { useMediaQuery } from '@mantine/hooks';
 import { Grid } from '../Grid';
 import { ColWrapper as Col } from './ColWrapper';
 
 const code = `
 import React from 'react';
 import { Grid, Col } from '@mantine/core';
-import { useMediaQuery } from '@mantine/hooks';
 
 function Demo() {
-  const isMobile = useMediaQuery('(max-width: 755px)');
-  const span = isMobile ? 12 : 4;
-
   return (
     <Grid>
-      <Col span={span}>1</Col>
-      <Col span={span}>2</Col>
-      <Col span={span}>3</Col>
+      <Col span={12} md={6} lg={3}>1</Col>
+      <Col span={12} md={6} lg={3}>2</Col>
+      <Col span={12} md={6} lg={3}>3</Col>
+      <Col span={12} md={6} lg={3}>4</Col>
     </Grid>
   );
 }
 `;
 
 function Demo() {
-  const isMobile = useMediaQuery('(max-width: 755px)');
-  const span = isMobile ? 12 : 4;
-
   return (
     <Grid>
-      <Col span={span}>1</Col>
-      <Col span={span}>2</Col>
-      <Col span={span}>3</Col>
+      <Col span={12} md={6} lg={3}>1</Col>
+      <Col span={12} md={6} lg={3}>2</Col>
+      <Col span={12} md={6} lg={3}>3</Col>
+      <Col span={12} md={6} lg={3}>4</Col>
     </Grid>
   );
 }
