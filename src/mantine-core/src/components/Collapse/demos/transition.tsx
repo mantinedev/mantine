46dfaf6ae859a729e780709317cb190b08ff80a3
Vitaly Rtishchev <rtivital@gmail.com>
2021-08-31 10:08:06 +0300

    [@mantine/core] Collapse: Fix dark theme demos

--- src/mantine-core/src/components/Collapse/demos/transition.tsx
+++ src/mantine-core/src/components/Collapse/demos/transition.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Button } from '../../Button/Button';
 import { Group } from '../../Group/Group';
+import { Text } from '../../Text/Text';
 import { Collapse } from '../Collapse';
 
 const code = `
@@ -34,14 +35,16 @@ function Demo() {
       </Group>
 
       <Collapse in={opened} transitionDuration={1000} transitionTimingFunction="linear">
-        From Bulbapedia: Bulbasaur is a small, quadrupedal Pokémon that has blue-green skin with
-        darker patches. It has red eyes with white pupils, pointed, ear-like structures on top of
-        its head, and a short, blunt snout with a wide mouth. A pair of small, pointed teeth are
-        visible in the upper jaw when its mouth is open. Each of its thick legs ends with three
-        sharp claws. On Bulbasaur&apos;s back is a green plant bulb, which is grown from a seed
-        planted there at birth. The bulb also conceals two slender, tentacle-like vines and provides
-        it with energy through photosynthesis as well as from the nutrient-rich seeds contained
-        within.
+        <Text>
+          From Bulbapedia: Bulbasaur is a small, quadrupedal Pokémon that has blue-green skin with
+          darker patches. It has red eyes with white pupils, pointed, ear-like structures on top of
+          its head, and a short, blunt snout with a wide mouth. A pair of small, pointed teeth are
+          visible in the upper jaw when its mouth is open. Each of its thick legs ends with three
+          sharp claws. On Bulbasaur&apos;s back is a green plant bulb, which is grown from a seed
+          planted there at birth. The bulb also conceals two slender, tentacle-like vines and
+          provides it with energy through photosynthesis as well as from the nutrient-rich seeds
+          contained within.
+        </Text>
       </Collapse>
     </div>
   );
