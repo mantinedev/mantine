d10754a4ff77563505fda2f75aeedfd1f39619ff
Vitaly Rtishchev <rtivital@gmail.com>
2021-09-06 10:23:57 +0300

    [@mantine/core] Chips: Add tests for Chip component

--- src/mantine-core/src/components/Chips/Chip/Chip.test.tsx
+++ src/mantine-core/src/components/Chips/Chip/Chip.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import {
   itSupportsClassName,
   itSupportsOthers,
@@ -6,6 +7,7 @@ import {
   itRendersChildren,
 } from '@mantine/tests';
 import { Chip } from './Chip';
+import { CheckboxIcon } from '../../Checkbox/CheckboxIcon';
 
 const defaultProps = {
   children: 'test-chip',
@@ -16,6 +18,20 @@ describe('@mantine/core/Chip', () => {
   itSupportsOthers(Chip, defaultProps);
   itRendersChildren(Chip, defaultProps);
   itSupportsRef(Chip, defaultProps, HTMLInputElement, 'elementRef');
+
+  it('displays checked icon based on checked prop', () => {
+    const checked = shallow(<Chip checked {...defaultProps} />);
+    const notChecked = shallow(<Chip checked={false} {...defaultProps} />);
+
+    expect(checked.find(CheckboxIcon)).toHaveLength(1);
+    expect(notChecked.find(CheckboxIcon)).toHaveLength(0);
+  });
+
+  it('connects label with input with given id', () => {
+    const element = shallow(<Chip id="test-chip" {...defaultProps} />);
+    expect(element.find('label').prop('htmlFor')).toBe('test-chip');
+    expect(element.find('input').prop('id')).toBe('test-chip');
+  });
 
   it('has correct displayName', () => {
     expect(Chip.displayName).toEqual('@mantine/core/Chip');
