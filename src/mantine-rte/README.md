44397a8dec5bc7ec15a77494e276440aadefb881
Vitaly Rtishchev <rtivital@gmail.com>
2021-09-09 17:48:24 +0300

    [@mantine/rte] Init package

---
+++ src/mantine-rte/README.md
@@ -0,0 +1,1 @@
+# Mantine Rich Text Editor
---
+++ src/mantine-rte/src/components/RichTextEditor/RichTextEditor.story.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { RichTextEditor } from './RichTextEditor';
+
+storiesOf('@mantine/rte/RichTextEditor', module).add('General usage', () => <RichTextEditor />);
