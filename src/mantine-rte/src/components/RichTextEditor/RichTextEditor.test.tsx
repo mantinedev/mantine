4d125b7d69d4c7c766296e0f071e116db2611d7c
Vitaly Rtishchev <rtivital@gmail.com>
2021-09-12 21:53:40 +0300

    [@mantine/rte] Add styles api descriptions and tests

--- src/mantine-rte/src/components/RichTextEditor/RichTextEditor.test.tsx
+++ src/mantine-rte/src/components/RichTextEditor/RichTextEditor.test.tsx
@@ -1,5 +1,11 @@
-import { itSupportsClassName, itSupportsOthers, itSupportsStyle } from '@mantine/tests';
+import {
+  itSupportsClassName,
+  itSupportsOthers,
+  itSupportsStyle,
+  itSupportsStylesApi,
+} from '@mantine/tests';
 import { RichTextEditor } from './RichTextEditor';
+import { RichTextEditor as RichTextEditorStylesApi } from '../../styles.api';
 
 const defaultProps = {
   value: '',
@@ -10,6 +16,7 @@ describe('@mantine/core/RichTextEditor', () => {
   itSupportsClassName(RichTextEditor, defaultProps);
   itSupportsOthers(RichTextEditor, defaultProps);
   itSupportsStyle(RichTextEditor, defaultProps);
+  itSupportsStylesApi(RichTextEditor, defaultProps, Object.keys(RichTextEditorStylesApi), 'rte');
 
   it('has correct displayName', () => {
     expect(RichTextEditor.displayName).toEqual('@mantine/rte/RichTextEditor');
