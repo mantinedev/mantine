45275f56c4d7aeb36f523a198ede300afc65b977
Vitaly Rtishchev <rtivital@gmail.com>
2021-09-12 20:38:49 +0300

    [@mantine/rte] Add ssr component

--- src/mantine-rte/src/index.ts
+++ src/mantine-rte/src/index.ts
@@ -1,3 +1,4 @@
 export { RichTextEditor } from './components/RichTextEditor/RichTextEditor';
+export { SSRRichTextEditor } from './components/SSRRichTextEditor/SSRRichTextEditor';
 
 export type { RichTextEditorProps } from './components/RichTextEditor/RichTextEditor';
