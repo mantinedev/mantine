e541ae0b6ce307eb7c53040fdb27dbd527ca6c81
Vitaly Rtishchev <rtivital@gmail.com>
2021-09-13 19:29:50 +0300

    [@mantine/core] Pagination: Decrease default icons size

--- src/mantine-core/src/components/Pagination/icons/PrevIcon.tsx
+++ src/mantine-core/src/components/Pagination/icons/PrevIcon.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 export function PrevIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
   return (
     <svg
-      width={20}
-      height={20}
+      width={16}
+      height={16}
       viewBox="0 0 16 16"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
