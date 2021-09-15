8d3d7735d7fafee983b2c354e66670eaf9b81539
achmurali <achmurali@github.com>
2021-09-15 23:42:22 +0530

    AvatarsGroup: styles api

---
+++ src/mantine-core/src/components/AvatarGroup/styles.api.ts
@@ -0,0 +1,7 @@
+import { AvatarsGroupStylesNames } from './AvatarsGroup';
+
+export const Avatar: Record<AvatarsGroupStylesNames, string> = {
+  root: 'Root element',
+  child: 'Styles added to every child inside AvatarsGroup',
+  number: 'Truncated avatars count',
+};
