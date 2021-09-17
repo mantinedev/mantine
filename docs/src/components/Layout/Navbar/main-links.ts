040ed02c9b189d64b3b844ad7e00f1323c7c16b3
Vitaly Rtishchev <rtivital@gmail.com>
2021-09-17 11:01:54 +0300

    [docs] Init theme builder

--- docs/src/components/Layout/Navbar/main-links.ts
+++ docs/src/components/Layout/Navbar/main-links.ts
@@ -1,4 +1,4 @@
-import { RocketIcon, GitHubLogoIcon } from '@modulz/radix-icons';
+import { RocketIcon, GitHubLogoIcon, BlendingModeIcon } from '@modulz/radix-icons';
 import { DEFAULT_THEME } from '@mantine/core';
 
 export default [
@@ -7,6 +7,12 @@ export default [
     label: 'Getting Started',
     theme: DEFAULT_THEME.colors.blue[6],
     icon: RocketIcon,
+  },
+  {
+    to: '/theme-builder/',
+    label: 'Theme Builder',
+    theme: DEFAULT_THEME.colors.orange[6],
+    icon: BlendingModeIcon,
   },
   { to: '/contribute/', label: 'Contribute', theme: '#000', icon: GitHubLogoIcon },
 ];
