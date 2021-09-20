03fc65707ad9ca3360b9c2a31fc3c5837a3391e8
Vitaly Rtishchev <rtivital@gmail.com>
2021-09-20 11:01:33 +0300

    [docs] Remove react-jss ssr logic

--- docs/gatsby-ssr.js
+++ docs/gatsby-ssr.js
@@ -1,42 +1,10 @@
 /* eslint-disable react/jsx-filename-extension */
-import React from 'react';
 import { renderToString } from 'react-dom/server';
 import htmlReactParser from 'html-react-parser';
-import { JssProvider, SheetsRegistry, createGenerateId } from 'react-jss';
 import createEmotionServer from '@emotion/server/create-instance';
 import { getCache } from '@mantine/styles';
 
 const emotionServer = createEmotionServer(getCache());
-
-const sheetsRegistryManager = new Map();
-
-// eslint-disable-next-line react/prop-types, react/display-name
-export const wrapRootElement = ({ element, pathname }) => {
-  const sheets = new SheetsRegistry();
-  sheetsRegistryManager.set(pathname, sheets);
-  const generateId = createGenerateId({ minify: true });
-
-  return (
-    <JssProvider registry={sheets} generateId={generateId}>
-      {element}
-    </JssProvider>
-  );
-};
-
-export const onRenderBody = ({ setHeadComponents, pathname }) => {
-  const sheets = sheetsRegistryManager.get(pathname);
-  if (sheets) {
-    setHeadComponents([
-      <style
-        type="text/css"
-        id="server-side-jss"
-        key="server-side-jss"
-        dangerouslySetInnerHTML={{ __html: sheets.toString() }}
-      />,
-    ]);
-    sheetsRegistryManager.delete(pathname);
-  }
-};
 
 export const replaceRenderer = ({ bodyComponent, replaceBodyHTMLString, setHeadComponents }) => {
   const html = renderToString(bodyComponent);
@@ -46,6 +14,5 @@ export const replaceRenderer = ({ bodyComponent, replaceBodyHTMLString, setHeadC
   );
 
   setHeadComponents([htmlReactParser(styles)]);
-
   replaceBodyHTMLString(html);
 };
