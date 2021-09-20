45f6330658a11a0de56b43864028cadc7bac4fa7
Vitaly Rtishchev <rtivital@gmail.com>
2021-09-20 16:59:49 +0300

    [@mantine/dates] Month: Fix incorrect styles after migration to tss

--- src/mantine-dates/src/components/Month/Day/Day.styles.ts
+++ src/mantine-dates/src/components/Month/Day/Day.styles.ts
@@ -20,72 +20,80 @@ export const sizes = {
   xl: 66,
 };
 
-// @XXX: STYLES
-export default createStyles((theme, { size, fullWidth }: DayStyles) => ({
-  weekend: {},
-  outside: {},
-  selected: {},
-  inRange: {},
-  firstInRange: {},
-  lastInRange: {},
+export default createStyles((theme, { size, fullWidth }: DayStyles, getRef) => {
+  const weekend = { ref: getRef() } as const;
+  const outside = { ref: getRef() } as const;
+  const selected = { ref: getRef() } as const;
+  const inRange = { ref: getRef() } as const;
+  const firstInRange = { ref: getRef() } as const;
+  const lastInRange = { ref: getRef() } as const;
 
-  day: {
-    ...getFontStyles(theme),
-    ...getFocusStyles(theme),
-    position: 'relative',
-    WebkitTapHighlightColor: 'transparent',
-    backgroundColor: 'transparent',
-    width: fullWidth ? '100%' : getSizeValue({ size, sizes }),
-    height: getSizeValue({ size, sizes }),
-    lineHeight: `${getSizeValue({ size, sizes })}px`,
-    fontSize: getSizeValue({ size, sizes: theme.fontSizes }),
-    padding: 0,
-    borderRadius: theme.radius.sm,
-    border: '1px dotted transparent',
-    cursor: 'pointer',
-    userSelect: 'none',
-    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
+  return {
+    weekend,
+    outside,
+    selected,
+    inRange,
+    firstInRange,
+    lastInRange,
 
-    '&:disabled': {
-      pointerEvents: 'none',
-      color: theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.gray[4],
+    day: {
+      ...getFontStyles(theme),
+      ...getFocusStyles(theme),
+      position: 'relative',
+      WebkitTapHighlightColor: 'transparent',
+      backgroundColor: 'transparent',
+      width: fullWidth ? '100%' : getSizeValue({ size, sizes }),
+      height: getSizeValue({ size, sizes }),
+      lineHeight: `${getSizeValue({ size, sizes })}px`,
+      fontSize: getSizeValue({ size, sizes: theme.fontSizes }),
+      padding: 0,
+      borderRadius: theme.radius.sm,
+      border: '1px dotted transparent',
+      cursor: 'pointer',
+      userSelect: 'none',
+      color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
+
+      '&:disabled': {
+        pointerEvents: 'none',
+        color: theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.gray[4],
+      },
+
+      '&:hover': {
+        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[0],
+      },
+
+      [`&.${weekend.ref}:not(:disabled)`]: {
+        color: theme.colorScheme === 'dark' ? theme.colors.red[5] : theme.colors.red[7],
+      },
+
+      [`&.${outside.ref}`]: {
+        color: `${
+          theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.gray[4]
+        } !important`,
+      },
+
+      [`&.${inRange.ref}:not(:disabled)`]: {
+        backgroundColor:
+          theme.colorScheme === 'dark'
+            ? hexToRgba(theme.colors[theme.primaryColor][9], 0.3)
+            : theme.colors[theme.primaryColor][0],
+        borderRadius: 0,
+      },
+
+      [`&.${selected.ref}:not(:disabled)`]: {
+        backgroundColor: theme.colors[theme.primaryColor][6],
+        color: theme.white,
+      },
+
+      [`&.${firstInRange.ref}:not(:disabled)`]: {
+        borderTopLeftRadius: theme.radius.sm,
+        borderBottomLeftRadius: theme.radius.sm,
+      },
+
+      [`&.${lastInRange.ref}:not(:disabled)`]: {
+        borderTopRightRadius: theme.radius.sm,
+        borderBottomRightRadius: theme.radius.sm,
+      },
     },
-
-    '&:hover': {
-      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[0],
-    },
-
-    '&$weekend:not(:disabled)': {
-      color: theme.colorScheme === 'dark' ? theme.colors.red[5] : theme.colors.red[7],
-    },
-
-    '&$outside': {
-      color: `${
-        theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.gray[4]
-      } !important`,
-    },
-
-    '&$inRange:not(:disabled)': {
-      backgroundColor:
-        theme.colorScheme === 'dark'
-          ? hexToRgba(theme.colors[theme.primaryColor][9], 0.3)
-          : theme.colors[theme.primaryColor][0],
-      borderRadius: 0,
-    },
-
-    '&$selected:not(:disabled)': {
-      backgroundColor: theme.colors[theme.primaryColor][6],
-      color: theme.white,
-    },
-
-    '&$firstInRange:not(:disabled)': {
-      borderTopLeftRadius: theme.radius.sm,
-      borderBottomLeftRadius: theme.radius.sm,
-    },
-
-    '&$lastInRange:not(:disabled)': {
-      borderTopRightRadius: theme.radius.sm,
-      borderBottomRightRadius: theme.radius.sm,
-    },
-  },
-}));
+  };
+});
