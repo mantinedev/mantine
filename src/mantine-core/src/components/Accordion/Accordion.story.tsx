97793a072535f7e84d348e756de94f3bdc665bd4
Vitaly Rtishchev <rtivital@gmail.com>
2021-09-21 12:10:51 +0300

    [@mantine/core] Accordion: Add better stories structure

--- src/mantine-core/src/components/Accordion/Accordion.story.tsx
+++ src/mantine-core/src/components/Accordion/Accordion.story.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Accordion } from './Accordion';
+import { Accordion, AccordionProps } from './Accordion';
 import { BASE_DATA } from './demos/_mockdata';
 import { DarkStory } from '../../../demos';
 
-const base = (
-  <Accordion>
-    {BASE_DATA.map((item) => (
-      <Accordion.Item label={item.label} key={item.label}>
-        {item.content}
-      </Accordion.Item>
-    ))}
-  </Accordion>
-);
+function Base(props: Partial<AccordionProps>) {
+  return (
+    <div style={{ padding: 40, maxWidth: 540 }}>
+      <Accordion {...props}>
+        {BASE_DATA.map((item) => (
+          <Accordion.Item label={item.label} key={item.label}>
+            {item.content}
+          </Accordion.Item>
+        ))}
+      </Accordion>
+    </div>
+  );
+}
 
 storiesOf('@mantine/core/Accordion', module)
-  .add('General usage', () => <div style={{ padding: 40, maxWidth: 540 }}>{base}</div>)
+  .add('General usage', () => <Base />)
+  .add('Multiple selected items', () => <Base multiple />)
   .add('Dark theme', () => (
     <DarkStory>
-      <div style={{ padding: 40, maxWidth: 540 }}>{base}</div>
+      <Base />
     </DarkStory>
   ));
