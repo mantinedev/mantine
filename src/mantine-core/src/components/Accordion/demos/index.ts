362e3afc557fecf4325e6fad2f8119f81e206282
Vitaly Rtishchev <rtivital@gmail.com>
2021-09-21 15:31:40 +0300

    [@mantine/core] Accordion: Add all demos exports

--- src/mantine-core/src/components/Accordion/demos/index.ts
+++ src/mantine-core/src/components/Accordion/demos/index.ts
@@ -2,3 +2,5 @@ export { usage } from './usage';
 export { label } from './label';
 export { multiple } from './multiple';
 export { stylesApi } from './stylesApi';
+export { icon } from './icon';
+export { icons } from './icons';
