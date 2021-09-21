08fd322a17a9033efc3948b11492aefbaa0bee38
Vitaly Rtishchev <rtivital@gmail.com>
2021-09-21 14:43:02 +0300

    [@mantine/core] Accordion: Add better styles api demo

--- src/mantine-core/src/components/Accordion/demos/stylesApi.tsx
+++ src/mantine-core/src/components/Accordion/demos/stylesApi.tsx
@@ -1,111 +1,112 @@
 import React from 'react';
-import { createStyles, hexToRgba } from '@mantine/styles';
+import { PlusIcon } from '@modulz/radix-icons';
+import { createStyles } from '@mantine/styles';
 import { Accordion, AccordionProps } from '../Accordion';
 import { baseDemoItems } from './_base';
 
 const code = `
 import { Accordion, AccordionProps, createStyles } from '@mantine/core';
+import { PlusIcon } from '@modulz/radix-icons';
 
-const useStyles = createStyles((theme) => ({
-  control: {
-    fontSize: theme.fontSizes.lg,
-  },
+const useStyles = createStyles((theme, _params, getRef) => {
+  const controlRef = getRef('control');
+  const iconRef = getRef('icon');
 
-  item: {
-    border: \`1px solid \${theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[4]}\`,
-
-    '&:first-of-type': {
-      borderTopRightRadius: theme.radius.sm,
-      borderTopLeftRadius: theme.radius.sm,
+  return {
+    icon: {
+      ref: iconRef,
     },
 
-    '&:last-of-type': {
-      borderColor: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[4],
-      borderBottomRightRadius: theme.radius.sm,
-      borderBottomLeftRadius: theme.radius.sm,
+    control: {
+      ref: controlRef,
+      border: 0,
+      opacity: 0.6,
+
+      '&:hover': {
+        backgroundColor: 'transparent',
+        opacity: 1,
+      },
     },
 
-    '& + &': {
-      borderTop: 'none',
+    item: {
+      borderBottom: 0,
+      overflow: 'hidden',
+      transition: \`box-shadow 150ms \${theme.transitionTimingFunction}\`,
+      border: '1px solid transparent',
+      borderRadius: theme.radius.sm,
     },
-  },
 
-  itemOpened: {
-    '& $control': {
-      backgroundColor: theme.colors.blue[theme.colorScheme === 'dark' ? 9 : 0],
-      color: theme.colorScheme === 'dark' ? theme.white : theme.colors.blue[9],
+    itemOpened: {
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.white,
+      borderColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[3],
+
+      [\`& .\${controlRef}\`]: {
+        opacity: 1,
+      },
+
+      [\`& .\${iconRef}\`]: {
+        transform: 'rotate(45deg)',
+      },
     },
-  },
 
-  contentInner: {
-    paddingTop: theme.spacing.md,
-  },
-}));
+    content: {
+      paddingLeft: 0,
+    },
+  };
+});
 
 function StyledAccordion(props: AccordionProps) {
   const { classes } = useStyles();
-  return <Accordion classNames={classes} {...props} />;
+  return <Accordion classNames={classes} icon={<PlusIcon />} {...props} />;
 }
 
 function Demo() {
   return (
     <StyledAccordion>
-      <Accordion.Item label="Customization">
-        Colors, fonts, shadows and many other parts are customizable to fit your design needs
-      </Accordion.Item>
-
-      <Accordion.Item label="Flexibility">
-        Configure components appearance and behavior with vast amount of settings or overwrite any
-        part of component styles
-      </Accordion.Item>
-
-      <Accordion.Item label="No annoying focus ring">
-        With new :focus-visible pseudo-class focus ring appears only when user navigates with
-        keyboard
-      </Accordion.Item>
+      {/* <Accordion.Item /> components */}
     </StyledAccordion>
   );
 }
 `;
 
 const useStyles = createStyles((theme, _params, getRef) => {
-  const control = {
-    ref: getRef('control'),
-    fontSize: theme.fontSizes.lg,
-    border: 0,
-  } as const;
+  const controlRef = getRef('control');
+  const iconRef = getRef('icon');
 
   return {
-    control,
+    icon: {
+      ref: iconRef,
+    },
 
-    item: {
-      border: `1px solid ${
-        theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[3]
-      }`,
-      borderBottom: 0,
-      overflow: 'hidden',
+    control: {
+      ref: controlRef,
+      border: 0,
+      opacity: 0.6,
 
-      '&:first-of-type': {
-        borderTopRightRadius: theme.radius.sm,
-        borderTopLeftRadius: theme.radius.sm,
-      },
-
-      '&:last-of-type': {
-        borderBottomRightRadius: theme.radius.sm,
-        borderBottomLeftRadius: theme.radius.sm,
-        borderBottom: `1px solid ${
-          theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[3]
-        }`,
+      '&:hover': {
+        backgroundColor: 'transparent',
+        opacity: 1,
       },
     },
 
+    item: {
+      borderBottom: 0,
+      overflow: 'hidden',
+      transition: `box-shadow 150ms ${theme.transitionTimingFunction}`,
+      border: '1px solid transparent',
+      borderRadius: theme.radius.sm,
+    },
+
     itemOpened: {
-      [`& .${control.ref}, & .${control.ref}:hover`]: {
-        backgroundColor:
-          theme.colorScheme === 'dark'
-            ? hexToRgba(theme.colors.blue[9], 0.45)
-            : theme.colors.blue[0],
-        color: theme.colorScheme === 'dark' ? theme.colors.blue[1] : theme.colors.blue[9],
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.white,
+      borderColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[3],
+
+      [`& .${controlRef}`]: {
+        opacity: 1,
+      },
+
+      [`& .${iconRef}`]: {
+        transform: 'rotate(45deg)',
       },
     },
 
@@ -117,7 +118,7 @@ const useStyles = createStyles((theme, _params, getRef) => {
 
 export function StyledAccordion(props: AccordionProps) {
   const { classes } = useStyles();
-  return <Accordion classNames={classes} {...props} />;
+  return <Accordion classNames={classes} icon={<PlusIcon />} {...props} />;
 }
 
 function Demo() {
