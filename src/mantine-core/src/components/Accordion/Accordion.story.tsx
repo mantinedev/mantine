a8cef86075d1c1125be3d626cb3900fb6a2f2a07
Vitaly Rtishchev <rtivital@gmail.com>
2021-09-21 11:59:34 +0300

    [@mantine/core] Accordion: Add shared mockdata

--- src/mantine-core/src/components/Accordion/Accordion.story.tsx
+++ src/mantine-core/src/components/Accordion/Accordion.story.tsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Accordion } from './Accordion';
+import { BASE_DATA } from './demos/_mockdata';
 import { DarkStory } from '../../../demos';
 
 const base = (
   <Accordion>
-    <Accordion.Item label="Customization">
-      Colors, fonts, shadows and many other parts are customizable to fit your design needs
-    </Accordion.Item>
-
-    <Accordion.Item label="Flexibility">
-      Configure components appearance and behavior with vast amount of settings or overwrite any
-      part of component styles
-    </Accordion.Item>
-
-    <Accordion.Item label="No annoying focus ring">
-      With new :focus-visible pseudo-class focus ring appears only when user navigates with keyboard
-    </Accordion.Item>
+    {BASE_DATA.map((item) => (
+      <Accordion.Item label={item.label} key={item.label}>
+        {item.content}
+      </Accordion.Item>
+    ))}
   </Accordion>
 );
 
---
+++ src/mantine-core/src/components/Accordion/demos/_mockdata.ts
@@ -0,0 +1,17 @@
+export const BASE_DATA = [
+  {
+    label: 'Customization',
+    content:
+      'Colors, fonts, shadows and many other parts are customizable to fit your design needs',
+  },
+  {
+    label: 'Flexibility',
+    content:
+      'Configure components appearance and behavior with vast amount of settings or overwrite any part of component styles',
+  },
+  {
+    label: 'No annoying focus ring',
+    content:
+      'With new :focus-visible pseudo-class focus ring appears only when user navigates with keyboard',
+  },
+];
