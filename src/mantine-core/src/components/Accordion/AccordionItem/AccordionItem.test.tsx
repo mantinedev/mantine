815dffd3b4fe288aa9e3e203d32ac9f2aa221ea3
Vitaly Rtishchev <rtivital@gmail.com>
2021-09-21 18:25:40 +0300

    [@mantine/core] Accordion: Remove old tests

--- src/mantine-core/src/components/Accordion/AccordionItem/AccordionItem.test.tsx
+++ src/mantine-core/src/components/Accordion/AccordionItem/AccordionItem.test.tsx
@@ -37,17 +37,6 @@ describe('@mantine/core/AccordionItem', () => {
     expect(element.find('.mantine-accordion-label').text()).toBe('test-label');
   });
 
-  it('sets animation properties based on opened prop', () => {
-    const opened = shallow(<AccordionItem {...defaultProps} opened />);
-    const closed = shallow(<AccordionItem {...defaultProps} opened={false} />);
-
-    expect(opened.find('.mantine-accordion-contentInner').prop('style').opacity).toBe(1);
-    expect(opened.find('.mantine-accordion-content').prop('style').height).toBe('auto');
-
-    expect(closed.find('.mantine-accordion-contentInner').prop('style').opacity).toBe(0);
-    expect(closed.find('.mantine-accordion-content').prop('style').height).toBe(0);
-  });
-
   it('has correct displayName', () => {
     expect(AccordionItem.displayName).toEqual('@mantine/core/AccordionItem');
   });
