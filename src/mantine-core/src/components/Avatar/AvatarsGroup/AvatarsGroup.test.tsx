cce7d6305343b229e28998c58091dc74298db0d0
Vitaly Rtishchev <rtivital@gmail.com>
2021-09-22 13:07:12 +0300

    [@mantine/core] Avatar: Add AvatarsGroup limit test

--- src/mantine-core/src/components/Avatar/AvatarsGroup/AvatarsGroup.test.tsx
+++ src/mantine-core/src/components/Avatar/AvatarsGroup/AvatarsGroup.test.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import {
   itSupportsClassName,
   itSupportsStyle,
   itSupportsOthers,
   itSupportsStylesApi,
-  checkAccessibility,
 } from '@mantine/tests';
 import { AvatarsGroup } from './AvatarsGroup';
 import { Avatar } from '../Avatar';
 import { AvatarsGroup as AvatarsGroupStylesApi } from './styles.api';
 
 const defaultProps = {
-  children: [<Avatar>M</Avatar>, <Avatar>K</Avatar>, <Avatar>L</Avatar>],
+  children: [<Avatar>M</Avatar>, <Avatar>K</Avatar>, <Avatar>L</Avatar>, <Avatar>L</Avatar>],
 };
 
 describe('@mantine/core/AvatarsGroup', () => {
@@ -25,7 +24,16 @@ describe('@mantine/core/AvatarsGroup', () => {
     Object.keys(AvatarsGroupStylesApi),
     'avatars-group'
   );
-  checkAccessibility([mount(<AvatarsGroup {...defaultProps} />)]);
+
+  it('renders avatars based on limit prop', () => {
+    const limit2 = shallow(<AvatarsGroup {...defaultProps} limit={2} />);
+    const limit3 = shallow(<AvatarsGroup {...defaultProps} limit={3} />);
+    const limit10 = shallow(<AvatarsGroup {...defaultProps} limit={10} />);
+
+    expect(limit2.find(Avatar)).toHaveLength(3);
+    expect(limit3.find(Avatar)).toHaveLength(4);
+    expect(limit10.find(Avatar)).toHaveLength(4);
+  });
 
   it('has correct displayName', () => {
     expect(AvatarsGroup.displayName).toEqual('@mantine/core/AvatarsGroup');
