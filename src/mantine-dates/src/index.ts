5f5389dde3fef747685b097b9799085972af4502
Vitaly Rtishchev <rtivital@gmail.com>
2021-09-23 10:52:20 +0300

    [@mantine/dates] Migrate all components to single file exports

--- src/mantine-dates/src/index.ts
+++ src/mantine-dates/src/index.ts
@@ -1,18 +1,9 @@
 export * from './utils';
 
-export { Calendar } from './components/Calendar/Calendar';
-export { DatePicker } from './components/DatePicker/DatePicker';
-export { DateRangePicker } from './components/DateRangePicker/DateRangePicker';
-export { Month } from './components/Month/Month';
-export { RangeCalendar } from './components/RangeCalendar/RangeCalendar';
-export { TimeInput } from './components/TimeInput/TimeInput';
-export { TimeRangeInput } from './components/TimeRangeInput/TimeRangeInput';
-
-export type { CalendarStylesNames, CalendarProps } from './components/Calendar/Calendar';
-export type { DatePickerProps } from './components/DatePicker/DatePicker';
-export type { DateRangePickerProps } from './components/DateRangePicker/DateRangePicker';
-export type { DatePickerStylesNames } from './components/DatePickerBase/DatePickerBase';
-export type { MonthProps, MonthStylesNames } from './components/Month/Month';
-export type { RangeCalendarProps } from './components/RangeCalendar/RangeCalendar';
-export type { TimeInputProps, TimeInputStylesNames } from './components/TimeInput/TimeInput';
-export type { TimeRangeInputStylesNames } from './components/TimeRangeInput/TimeRangeInput';
+export * from './components/Calendar';
+export * from './components/DatePicker';
+export * from './components/DateRangePicker';
+export * from './components/Month';
+export * from './components/RangeCalendar';
+export * from './components/TimeInput';
+export * from './components/TimeRangeInput';
