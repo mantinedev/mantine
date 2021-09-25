4d2a02a84cd942043cd17f32df9305f4daa6029e
Ashwin Shenoy <ashwinshenoy05@gmail.com>
2021-09-25 13:22:30 +0530

    [Feat][Pagination] Add some storybook demos.
    This commit adds some storybook demos to see these in action.

--- src/mantine-core/src/components/Pagination/Pagination.story.tsx
+++ src/mantine-core/src/components/Pagination/Pagination.story.tsx
@@ -16,4 +16,11 @@ storiesOf('@mantine/core/Pagination', module)
   .add('With 2 boundaries', () => <Pagination total={20} boundaries={2} initialPage={12} />)
   .add('With 2 boundaries and 2 siblings', () => (
     <Pagination siblings={2} boundaries={2} total={20} initialPage={12} />
+  ))
+  .add('With jump to first/last buttons', () => (
+      <div style={{ padding: 40 }}>
+        <Pagination total={20} boundaries={2} initialPage={12} showFirstButton />
+        <Pagination total={20} boundaries={2} initialPage={12} showLastButton />
+        <Pagination total={20} boundaries={2} initialPage={12} showFirstButton showLastButton />
+      </div>
   ));
