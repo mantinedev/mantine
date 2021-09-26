7e4c5bbdc201b468fa4e32e63c9721229484edd9
Vitaly Rtishchev <rtivital@gmail.com>
2021-09-26 13:40:50 +0300

    [@mantine/styles] Add separate ColorSchemeProvider component

--- src/mantine-styles/src/theme/index.ts
+++ src/mantine-styles/src/theme/index.ts
@@ -3,6 +3,7 @@ export * from './utils';
 
 export { DEFAULT_THEME, MANTINE_COLORS, MANTINE_SIZES } from './default-theme';
 
-export { MantineProvider, useMantineColorScheme, useMantineTheme } from './MantineProvider';
+export { MantineProvider, useMantineTheme } from './MantineProvider';
+export { ColorSchemeProvider, useMantineColorScheme } from './ColorSchemeProvider';
 export { GlobalStyles } from './GlobalStyles';
 export { NormalizeCSS } from './NormalizeCSS';
