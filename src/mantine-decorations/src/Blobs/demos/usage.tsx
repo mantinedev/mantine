258a101690f0582980649f50c4ac94faa2bc6153
Vitaly Rtishchev <rtivital@gmail.com>
2021-09-26 10:47:43 +0300

    [@mantine/decorations] Blobs: Add demos

---
+++ src/mantine-decorations/src/Blobs/demos/usage.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Blobs } from '../Blobs';
+
+const code = `
+<div
+  style={{
+    backgroundImage: 'linear-gradient(90deg, #6741d9 0%, #22b8cf 100%)',
+    height: 800,
+    position: 'relative',
+  }}
+>
+  <Blobs />
+</div>
+`;
+
+function Demo() {
+  return (
+    <div
+      style={{
+        backgroundImage: 'linear-gradient(90deg, #6741d9 0%, #22b8cf 100%)',
+        height: 800,
+        position: 'relative',
+      }}
+    >
+      <Blobs />
+    </div>
+  );
+}
+
+export const usage: MantineDemo = {
+  type: 'demo',
+  component: Demo,
+  code,
+};
