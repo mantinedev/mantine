4a22356ccb537d0f00f66fa336531f1265e94c4e
Vitaly Rtishchev <rtivital@gmail.com>
2021-09-28 18:15:30 +0300

    [gatsby-plugin-mantine] Init package

---
+++ packages/gatsby-plugin-mantine/gatsby-ssr.js
@@ -0,0 +1,11 @@
+const React = require('react');
+const { renderToString } = require('react-dom/server');
+const { createStylesServer, ServerStyles } = require('@mantine/ssr');
+
+const stylesServer = createStylesServer();
+
+export const replaceRenderer = ({ bodyComponent, replaceBodyHTMLString, setHeadComponents }) => {
+  const html = renderToString(bodyComponent);
+  setHeadComponents([React.createElement(ServerStyles, { html: html, server: stylesServer })]);
+  replaceBodyHTMLString(html);
+};
