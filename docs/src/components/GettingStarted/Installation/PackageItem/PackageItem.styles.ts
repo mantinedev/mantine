ee1c5ff4313956cc76c68c4f1c47211af68f7327
Vitaly Rtishchev <rtivital@gmail.com>
2021-09-28 21:04:16 +0300

    [docs] Update Getting Started guide with gallery ui

--- docs/src/components/GettingStarted/Installation/PackageItem/PackageItem.styles.ts
+++ docs/src/components/GettingStarted/Installation/PackageItem/PackageItem.styles.ts
@@ -1,41 +1,13 @@
-import { getFontStyles, createStyles } from '@mantine/core';
+import { createStyles } from '@mantine/core';
 
 export default createStyles((theme) => ({
   wrapper: {
-    ...getFontStyles(theme),
-    display: 'flex',
-    alignItems: 'center',
+    minHeight: 90,
     flex: `0 0 calc(50% - ${theme.spacing.md}px)`,
-    border: `2px solid ${
-      theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[2]
-    }`,
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
-    textAlign: 'left',
-    padding: theme.spacing.xl,
-    borderRadius: theme.radius.sm,
     margin: theme.spacing.md / 2,
-    cursor: 'pointer',
-
-    '&:hover': {
-      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[9] : theme.colors.gray[0],
-    },
 
     '@media (max-width: 1000px)': {
       flex: `0 0 calc(100% - ${theme.spacing.md}px)`,
     },
-  },
-
-  body: {
-    marginLeft: theme.spacing.xl,
-  },
-
-  title: {
-    fontFamily: theme.fontFamilyMonospace,
-    marginBottom: 3,
-  },
-
-  description: {
-    color: theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.gray[6],
-    fontSize: theme.fontSizes.sm,
   },
 }));
--- docs/src/components/GettingStarted/Installation/PackageItem/PackageItem.tsx
+++ docs/src/components/GettingStarted/Installation/PackageItem/PackageItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Checkbox, Text } from '@mantine/core';
+import { CheckboxCard } from '../../../../gallery/CheckboxCard/CheckboxCard';
 import useStyles from './PackageItem.styles';
 
 interface PackageItemProps {
@@ -12,12 +12,12 @@ interface PackageItemProps {
 export function PackageItem({ title, description, checked, onChange }: PackageItemProps) {
   const { classes } = useStyles();
   return (
-    <button className={classes.wrapper} type="button" onClick={() => onChange(!checked)}>
-      <Checkbox checked={checked} onChange={() => {}} />
-      <div className={classes.body}>
-        <Text className={classes.title}>{title}</Text>
-        <Text className={classes.description}>{description}</Text>
-      </div>
-    </button>
+    <CheckboxCard
+      checked={checked}
+      title={title}
+      description={description}
+      onChange={onChange}
+      className={classes.wrapper}
+    />
   );
 }
