2ecbfdd0cf69834767de1cc495fbfc454fed1bbc
achmurali <achmurali@github.com>
2021-10-01 00:09:10 +0530

    Add Creatable Functionality for SelectItems

--- src/mantine-core/src/components/Select/SelectItems/SelectItems.tsx
+++ src/mantine-core/src/components/Select/SelectItems/SelectItems.tsx
@@ -19,6 +19,8 @@ interface SelectItemsProps extends DefaultProps<SelectItemsStylesNames> {
   size: MantineSize;
   itemComponent: React.FC<any>;
   nothingFound?: React.ReactNode;
+  creatable?: boolean;
+  createLabel?: React.ReactNode;
 }
 
 export function SelectItems({
@@ -35,58 +37,117 @@ export function SelectItems({
   itemComponent: Item,
   size,
   nothingFound,
+  creatable,
+  createLabel,
 }: SelectItemsProps) {
   const { classes, cx } = useStyles({ size }, classNames, __staticSelector);
   const _styles = mergeStyles(classes, styles);
-  const items = data.map((item, index) => {
-    const selected = typeof isItemSelected === 'function' ? isItemSelected(item.value) : false;
+  const ungroupedItems: React.ReactElement<any>[] = [];
+  const groupedItems: React.ReactElement<any>[] = [];
+  let creatableDataIndex = null;
 
-    return !item.seperator ? (<Item
-      key={item.value}
-      className={cx(classes.item, {
-        [classes.hovered]: !item.disabled && hovered === index,
-        [classes.selected]: !item.disabled && selected,
-        [classes.disabled]: item.disabled,
-      })}
-      style={{
-        ..._styles.item,
-        ...(hovered === index && !item.disabled ? _styles.hovered : null),
-        ...(selected && !item.disabled ? _styles.selected : null),
-        ...(item.disabled ? _styles.disabled : null),
-      }}
-      onMouseEnter={() => onItemHover(index)}
-      id={`${uuid}-${index}`}
-      role="option"
-      tabIndex={-1}
-      aria-selected={hovered === index}
-      elementRef={(node: HTMLDivElement) => {
-        if (itemsRefs && itemsRefs.current) {
-          // eslint-disable-next-line no-param-reassign
-          itemsRefs.current[item.value] = node;
-        }
-      }}
-      onMouseDown={!item.disabled ? (event: React.MouseEvent<HTMLDivElement>) => {
-        event.preventDefault();
-        onItemSelect(item);
-      } : null}
-      {...item}
-    />) : (
-      <div className={classes.seperator} style={_styles.seperator}>
-        <Divider
-          classNames={{
-            label: classes.seperatorLabel,
-          }}
-          styles={{
-            label: { ..._styles.seperatorLabel },
-          }}
-          label={item.label}
-        />
-      </div>
-    );
+  const constructItemComponent = (item: SelectItem, index: number) => {
+    const selected = typeof isItemSelected === 'function' ? isItemSelected(item.value) : false;
+    return (
+      <Item
+        key={item.value}
+        label={item.label}
+        value={item.value}
+        className={cx(classes.item, {
+          [classes.hovered]: !item.disabled && hovered === index,
+          [classes.selected]: !item.disabled && selected,
+          [classes.disabled]: item.disabled,
+        })}
+        style={{
+          ..._styles.item,
+          ...(hovered === index && !item.disabled ? _styles.hovered : null),
+          ...(selected && !item.disabled ? _styles.selected : null),
+          ...(item.disabled ? _styles.disabled : null),
+        }}
+        onMouseEnter={() => onItemHover(index)}
+        id={`${uuid}-${index}`}
+        role="option"
+        tabIndex={-1}
+        aria-selected={hovered === index}
+        elementRef={(node: HTMLDivElement) => {
+          if (itemsRefs && itemsRefs.current) {
+            // eslint-disable-next-line no-param-reassign
+            itemsRefs.current[item.value] = node;
+          }
+        }}
+        onMouseDown={!item.disabled ? (event: React.MouseEvent<HTMLDivElement>) => {
+          event.preventDefault();
+          onItemSelect(item);
+        } : null}
+        disabled={item.disabled}
+      />
+      );
+  };
+
+  const createSeperator = (label?: string) => (
+    <div className={classes.seperator} style={_styles.seperator}>
+      <Divider
+        classNames={{
+          label: classes.seperatorLabel,
+        }}
+        styles={{
+          label: { ..._styles.seperatorLabel },
+        }}
+        label={label}
+      />
+    </div>
+  );
+
+  let groupName = null;
+  data.forEach((item, index) => {
+    if (item.creatable) creatableDataIndex = index;
+    else if (!item.group) ungroupedItems.push(constructItemComponent(item, index));
+    else {
+      if (groupName !== item.group) {
+        groupName = item.group;
+        groupedItems.push(createSeperator(groupName));
+      }
+      groupedItems.push(constructItemComponent(item, index));
+    }
   });
 
-  return items.length > 0 ? (
-    <>{items}</>
+  if (creatable) {
+    const creatableDataItem = data[creatableDataIndex];
+    const selected = typeof isItemSelected === 'function' ? isItemSelected(data[creatableDataIndex].value) : false;
+    ungroupedItems.push(
+      <div
+        key={creatableDataItem.value}
+        className={cx(classes.item, {
+          [classes.hovered]: hovered === creatableDataIndex,
+          [classes.selected]: selected,
+        })}
+        style={{
+          ..._styles.item,
+          ...(hovered === creatableDataIndex ? _styles.hovered : null),
+          ...(selected ? _styles.selected : null),
+        }}
+        tabIndex={-1}
+        ref={(node: HTMLDivElement) => {
+          if (itemsRefs && itemsRefs.current) {
+            // eslint-disable-next-line no-param-reassign
+            itemsRefs.current[creatableDataItem.value] = node;
+          }
+        }}
+      >
+        {createLabel}
+      </div>
+    );
+  }
+
+  if (groupedItems.length > 0 && ungroupedItems.length > 0) {
+    ungroupedItems.unshift(createSeperator());
+  }
+
+  return groupedItems.length > 0 || ungroupedItems.length > 0 ? (
+    <>
+      {groupedItems}
+      {ungroupedItems}
+    </>
   ) : (
     <Text size={size} className={classes.nothingFound} style={_styles.nothingFound}>
       {nothingFound}
