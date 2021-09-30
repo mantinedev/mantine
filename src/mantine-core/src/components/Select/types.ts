5a4baefaa0ad4f06a2731ebb53208f2ee951ed4d
achmurali <achmurali@github.com>
2021-10-01 00:07:46 +0530

    Add support for group type

--- src/mantine-core/src/components/Select/types.ts
+++ src/mantine-core/src/components/Select/types.ts
@@ -4,9 +4,10 @@ import type { SelectDropdownStylesNames } from './SelectDropdown/SelectDropdown'
 import type { SelectItemsStylesNames } from './SelectItems/SelectItems';
 
 export interface SelectItem {
-  value?: string;
+  value: string;
   label?: string;
   disabled?: boolean;
+  group?: string;
   [key: string]: any;
 }
 
