f1f701c7c85fdf611bd58ee4cdf9f431b7de7a79
Vitaly Rtishchev <rtivital@gmail.com>
2021-10-01 16:47:20 +0300

    [docs] Add use-hover hook to 3.0.0 changelog

--- docs/src/demos/hooks/use-hover.tsx
+++ docs/src/demos/hooks/use-hover.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useHover } from '@mantine/hooks';
-import { useMantineTheme } from '@mantine/core';
+import { useMantineTheme, Text } from '@mantine/core';
 
 const code = `
 function Demo() {
@@ -27,7 +27,7 @@ function Demo() {
         justifyContent: 'center',
       }}
     >
-      {hovered ? 'I am hovered' : 'Put mouse over me please'}
+      <Text>{hovered ? 'I am hovered' : 'Put mouse over me please'}</Text>
     </div>
   );
 }
