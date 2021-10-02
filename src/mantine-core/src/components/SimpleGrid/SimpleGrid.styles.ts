0c35e529c9cddf75f1ae3e573aed1515d48fffbf
Vitaly Rtishchev <rtivital@gmail.com>
2021-10-02 08:51:34 +0300

    [@mantine/core] SimpleGrid: Fix incorrect spacing between columns within partial rows

--- src/mantine-core/src/components/SimpleGrid/SimpleGrid.styles.ts
+++ src/mantine-core/src/components/SimpleGrid/SimpleGrid.styles.ts
@@ -16,7 +16,7 @@ export default createStyles((theme, { spacing, breakpoints, cols }: SimpleGridSt
   const gridBreakpoints = breakpoints.reduce((acc, breakpoint) => {
     const breakpointSize = getSizeValue({ size: breakpoint.maxWidth, sizes: theme.breakpoints });
     acc[`@media (max-width: ${breakpointSize}px)`] = {
-      gridTemplateColumns: `repeat(${breakpoint.cols}, minmax(auto, ${100 / breakpoint.cols}%))`,
+      gridTemplateColumns: `repeat(${breakpoint.cols}, minmax(0, 1fr))`,
       gap: getSizeValue({
         size: breakpoint.spacing || spacing,
         sizes: theme.spacing,
@@ -30,7 +30,7 @@ export default createStyles((theme, { spacing, breakpoints, cols }: SimpleGridSt
     grid: {
       boxSizing: 'border-box',
       display: 'grid',
-      gridTemplateColumns: `repeat(${cols}, minmax(auto, ${100 / cols}%))`,
+      gridTemplateColumns: `repeat(${cols}, minmax(0, 1fr))`,
       gap: getSizeValue({ size: spacing, sizes: theme.spacing }),
       ...gridBreakpoints,
     },
