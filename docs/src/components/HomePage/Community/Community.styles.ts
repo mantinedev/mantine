f62ac597f6b4fd2fbcf8a82c20c16865d0075bea
Vitaly Rtishchev <rtivital@gmail.com>
2021-10-02 09:39:01 +0300

    [docs] Add new community section to home page

--- docs/src/components/HomePage/Community/Community.styles.ts
+++ docs/src/components/HomePage/Community/Community.styles.ts
@@ -9,4 +9,22 @@ export default createStyles((theme) => ({
     paddingBottom: 160,
     backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
   },
+
+  inner: {
+    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[9] : theme.colors.dark[7],
+    padding: 30,
+    borderRadius: theme.radius.md,
+    position: 'relative',
+    overflow: 'hidden',
+  },
+
+  title: {
+    color: theme.white,
+    fontSize: 44,
+  },
+
+  description: {
+    color: theme.white,
+    lineHeight: 1.6,
+  },
 }));
--- docs/src/components/HomePage/Community/Community.tsx
+++ docs/src/components/HomePage/Community/Community.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
-import { Container, Group, Text } from '@mantine/core';
+import { Container, Group, Text, Title, useMantineTheme } from '@mantine/core';
+import { Blobs } from '@mantine/decorations';
 import { SocialButton } from '../../SocialButton/SocialButton';
-import { SectionTitle } from '../SectionTitle/SectionTitle';
 import useStyles from './Community.styles';
 
 export function Community() {
   const { classes } = useStyles();
+  const theme = useMantineTheme();
 
   return (
     <div className={classes.wrapper}>
       <Container size={1100}>
-        <SectionTitle>Join the community</SectionTitle>
-        <Text style={{ maxWidth: 600 }}>
-          Mantine is a new React components and hooks library, join Mantine Discord server to ask
-          questions and discuss new features or follow Mantine on Twitter to learn about new
-          features and releases
-        </Text>
-        <Group style={{ marginTop: 30 }}>
-          <SocialButton service="discord" />
-          <SocialButton service="twitter" />
-          <SocialButton service="email" />
-        </Group>
+        <div className={classes.inner}>
+          <Blobs color={theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.white} />
+          <div style={{ position: 'relative', zIndex: 1 }}>
+            <Title order={2} className={classes.title}>
+              Join the community
+            </Title>
+
+            <Text style={{ maxWidth: 600 }} className={classes.description}>
+              Mantine has a very friendly community, we are always open to new ideas and feedback.
+              Join us on Discord or Github Discussions to get any kind of help or on Twitter to get
+              notified about releases.
+            </Text>
+
+            <Group mt={30}>
+              <SocialButton service="github" />
+              <SocialButton service="discord" />
+              <SocialButton service="twitter" />
+            </Group>
+          </div>
+        </div>
       </Container>
     </div>
   );
