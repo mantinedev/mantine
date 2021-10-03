05593c6580e159ebfe3e341c6e4625124083fb1d
Vitaly Rtishchev <rtivital@gmail.com>
2021-10-03 20:32:54 +0300

    [@mantine/core] Overlay: Migrate to forwardRef

--- src/mantine-core/src/components/Overlay/Overlay.tsx
+++ src/mantine-core/src/components/Overlay/Overlay.tsx
@@ -1,13 +1,8 @@
-import React from 'react';
-import cx from 'clsx';
+import React, { forwardRef } from 'react';
+import clsx from 'clsx';
+import { PolymorphicComponentProps, PolymorphicRef } from '@mantine/styles';
 
-interface _OverlayProps<C extends React.ElementType, R extends HTMLElement> {
-  /** Root element or custom component */
-  component?: C;
-
-  /** Get element ref */
-  elementRef?: React.ForwardedRef<R>;
-
+interface _OverlayProps {
   /** Overlay opacity */
   opacity?: React.CSSProperties['opacity'];
 
@@ -21,47 +16,49 @@ interface _OverlayProps<C extends React.ElementType, R extends HTMLElement> {
   zIndex?: React.CSSProperties['zIndex'];
 }
 
-export type OverlayProps<
-  C extends React.ElementType = 'div',
-  R extends HTMLElement = HTMLDivElement
-> = _OverlayProps<C, R> & Omit<React.ComponentPropsWithoutRef<C>, keyof _OverlayProps<C, R>>;
+export type OverlayProps<C extends React.ElementType> = PolymorphicComponentProps<C, _OverlayProps>;
 
-export function Overlay<
-  C extends React.ElementType = 'div',
-  R extends HTMLElement = HTMLDivElement
->({
-  className,
-  style,
-  opacity = 0.6,
-  color = '#fff',
-  gradient,
-  zIndex = 1000,
-  component,
-  elementRef,
-  ...others
-}: OverlayProps<C, R>) {
-  const Element = component || 'div';
+type OverlayComponent = <C extends React.ElementType = 'div'>(
+  props: OverlayProps<C>
+) => React.ReactElement;
 
-  const background = gradient ? { backgroundImage: gradient } : { backgroundColor: color };
+export const Overlay: OverlayComponent & { displayName?: string } = forwardRef(
+  <C extends React.ElementType = 'div'>(
+    {
+      className,
+      style,
+      opacity = 0.6,
+      color = '#fff',
+      gradient,
+      zIndex = 1000,
+      component,
+      ...others
+    }: OverlayProps<C>,
+    ref: PolymorphicRef<C>
+  ) => {
+    const Element = component || 'div';
 
-  return (
-    <Element
-      className={cx('mantine-overlay', className)}
-      ref={elementRef as any}
-      style={{
-        ...background,
-        opacity,
-        position: 'absolute',
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0,
-        zIndex,
-        ...style,
-      }}
-      {...others}
-    />
-  );
-}
+    const background = gradient ? { backgroundImage: gradient } : { backgroundColor: color };
+
+    return (
+      <Element
+        className={clsx('mantine-overlay', className)}
+        ref={ref}
+        style={{
+          ...background,
+          opacity,
+          position: 'absolute',
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+          zIndex,
+          ...style,
+        }}
+        {...others}
+      />
+    );
+  }
+);
 
 Overlay.displayName = '@mantine/core/Overlay';
