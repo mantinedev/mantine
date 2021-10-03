fcd85df8e2c1b07bdd9d5989cdc72c64b2b16bd3
Vitaly Rtishchev <rtivital@gmail.com>
2021-10-03 16:25:19 +0300

    [@mantine/styles] Add prop description to polymorphic component prop

--- src/mantine-styles/src/theme/types/Polymorphic.ts
+++ src/mantine-styles/src/theme/types/Polymorphic.ts
@@ -7,6 +7,7 @@ type PropsOf<C extends keyof JSX.IntrinsicElements | React.JSXElementConstructor
   JSX.LibraryManagedAttributes<C, React.ComponentPropsWithoutRef<C>>;
 
 type ComponentProp<C extends React.ElementType> = {
+  /** Tag or component that should be used as root element */
   component?: C;
 };
 
