6a92b35cc5e3bfba4464506611b52cbaf372b9ba
Vitaly Rtishchev <rtivital@gmail.com>
2021-10-03 08:48:28 +0300

    [docs] Init basic layout for theme builder

--- docs/src/components/ThemeBuilder/ThemeBuilder.tsx
+++ docs/src/components/ThemeBuilder/ThemeBuilder.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { Container, Title } from '@mantine/core';
+import { Footer } from '../Footer/Footer';
+import useStyles from './ThemeBuilder.styles';
 
 export function ThemeBuilder() {
-  return <div>ThemeBuilder</div>;
+  const { classes } = useStyles();
+  return (
+    <>
+      <div className={classes.wrapper}>
+        <Container className={classes.container} size={1100}>
+          <Title className={classes.title}>Mantine theme builder</Title>
+        </Container>
+      </div>
+      <Footer withNavbar />
+    </>
+  );
 }
