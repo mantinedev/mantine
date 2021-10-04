dc34c29283831095582452322129a2b02b7a2cd0
Vitaly Rtishchev <rtivital@gmail.com>
2021-10-04 17:17:50 +0300

    [docs] Add more consistent dark theme colors to Guides

--- docs/src/components/GettingStarted/Guides/Guides.styles.ts
+++ docs/src/components/GettingStarted/Guides/Guides.styles.ts
@@ -18,14 +18,14 @@ export default createStyles((theme) => ({
     flexDirection: 'column',
     alignItems: 'center',
     border: `1px solid ${
-      theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[4]
+      theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[4]
     }`,
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.white,
+    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
     padding: theme.spacing.xl,
     borderRadius: theme.radius.md,
 
     '&:hover': {
-      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[9] : theme.colors.gray[0],
     },
   },
 
