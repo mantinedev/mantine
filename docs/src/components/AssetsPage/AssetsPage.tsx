ad5e8f91dd95ad8dce61afc9850d2ba5c167c906
Vitaly Rtishchev <rtivital@gmail.com>
2021-10-06 14:25:12 +0300

    [docs] Add logo section to assets page

--- docs/src/components/AssetsPage/AssetsPage.tsx
+++ docs/src/components/AssetsPage/AssetsPage.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Container } from '@mantine/core';
+import { Container, Title } from '@mantine/core';
 import Head from '../Head/Head';
 import { Footer } from '../Footer/Footer';
+import { Logos } from './Logos';
 import useStyles from './AssetsPage.styles';
 
 export function AssetsPage() {
@@ -13,7 +14,10 @@ export function AssetsPage() {
 
       <div className={classes.wrapper}>
         <Container className={classes.container} size={1100}>
-          Assets
+          <Title order={2} mb="md">
+            Mantine logo
+          </Title>
+          <Logos />
         </Container>
       </div>
 
