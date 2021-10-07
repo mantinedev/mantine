0e63ac75a34ad3e41fe8333b356b9a0de64279c3
Vitaly Rtishchev <rtivital@gmail.com>
2021-10-07 17:16:36 +0300

    [@mantine/dropzone] Dropzone: Add dark theme support

--- src/mantine-dropzone/src/Dropzone/Dropzone.styles.ts
+++ src/mantine-dropzone/src/Dropzone/Dropzone.styles.ts
@@ -4,6 +4,7 @@ import {
   getSizeValue,
   getFontStyles,
   getFocusStyles,
+  getSharedColorScheme,
 } from '@mantine/core';
 
 interface DropzoneStyles {
@@ -11,36 +12,45 @@ interface DropzoneStyles {
   radius: MantineNumberSize;
 }
 
-export default createStyles((theme, { padding, radius }: DropzoneStyles) => ({
-  root: {
-    ...getFontStyles(theme),
-    ...getFocusStyles(theme),
-    boxSizing: 'border-box',
-    backgroundColor: theme.white,
-    border: `1px dashed ${theme.colors.gray[4]}`,
-    padding: getSizeValue({ size: padding, sizes: theme.spacing }),
-    borderRadius: getSizeValue({ size: radius, sizes: theme.radius }),
-    cursor: 'pointer',
-    userSelect: 'none',
-    transition: 'background-color 150ms ease',
-    position: 'relative',
+export default createStyles((theme, { padding, radius }: DropzoneStyles) => {
+  const rejected = getSharedColorScheme({ color: 'red', theme, variant: 'light' });
+  const accepted = getSharedColorScheme({ color: theme.primaryColor, theme, variant: 'light' });
 
-    '&:hover': {
-      backgroundColor: theme.colors.gray[0],
+  return {
+    root: {
+      ...getFontStyles(theme),
+      ...getFocusStyles(theme),
+      boxSizing: 'border-box',
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
+      border: `2px dashed ${
+        theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.gray[4]
+      }`,
+      padding: getSizeValue({ size: padding, sizes: theme.spacing }),
+      borderRadius: getSizeValue({ size: radius, sizes: theme.radius }),
+      cursor: 'pointer',
+      userSelect: 'none',
+      transition: 'background-color 150ms ease',
+      position: 'relative',
+
+      '&:hover': {
+        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+      },
     },
-  },
 
-  active: {
-    backgroundColor: theme.colors[theme.primaryColor][0],
-    borderColor: theme.colors[theme.primaryColor][4],
+    active: {
+      backgroundColor:
+        theme.colorScheme === 'dark' ? accepted.background : theme.colors[theme.primaryColor][0],
+      borderColor:
+        theme.colorScheme === 'dark' ? accepted.border : theme.colors[theme.primaryColor][4],
 
-    '&:hover': {
-      backgroundColor: theme.colors[theme.primaryColor][0],
+      '&:hover': {
+        backgroundColor: theme.colors[theme.primaryColor][0],
+      },
     },
-  },
 
-  reject: {
-    backgroundColor: theme.colors.red[0],
-    borderColor: theme.colors.red[4],
-  },
-}));
+    reject: {
+      backgroundColor: theme.colorScheme === 'dark' ? rejected.background : theme.colors.red[0],
+      borderColor: theme.colorScheme === 'dark' ? rejected.border : theme.colors.red[4],
+    },
+  };
+});
--- src/mantine-dropzone/src/Dropzone/demos/usage.tsx
+++ src/mantine-dropzone/src/Dropzone/demos/usage.tsx
@@ -1,14 +1,27 @@
 /* eslint-disable no-console */
 import React from 'react';
 import { Group, Text, useMantineTheme, MantineTheme } from '@mantine/core';
-import { ImageIcon } from '@modulz/radix-icons';
+import { ImageIcon, UploadIcon, CrossCircledIcon } from '@modulz/radix-icons';
 import { Dropzone, DropzoneStatus } from '../Dropzone';
 import { IMAGE_MIME_TYPE } from '../../mime-types';
 
 const code = `
-import { Group, Text, useMantineTheme, MantineTheme } from '@mantine/core';
-import { Dropzone, IMAGE_MIME_TYPE } from '@mantine/dropzone';
+import { Group, Text, useMantineTheme } from '@mantine/core';
+import { ImageIcon, UploadIcon, CrossCircledIcon } from '@modulz/radix-icons';
 import { ImageIcon } from '@modulz/radix-icons';
+
+function ImageUploadIcon({ status, ...props }) {
+  if (status.accepted) {
+    return <UploadIcon {...props} />;
+  }
+
+  if (status.rejected) {
+    return <CrossCircledIcon {...props} />;
+  }
+
+  return <ImageIcon {...props} />;
+}
+
 
 function getIconColor(status, theme) {
   return status.accepted
@@ -27,11 +40,16 @@ function Demo() {
     <Dropzone onDrop={console.log} maxSize={3 * 1024 ** 2} accept={IMAGE_MIME_TYPE}>
       {(status) => (
         <Group position="center" spacing="xl" style={{ minHeight: 220, pointerEvents: 'none' }}>
-          <ImageIcon style={{ width: 60, height: 60, color: getIconColor(status, theme) }} />
+          <ImageUploadIcon
+            status={status}
+            style={{ width: 80, height: 80, color: getIconColor(status, theme) }}
+          />
 
           <div>
-            <Text size="xl">Drag images here or click to select files</Text>
-            <Text size="sm" color="dimmed">
+            <Text size="xl" inline>
+              Drag images here or click to select files
+            </Text>
+            <Text size="sm" color="dimmed" inline mt={7}>
               Attach as many files as you like, each file should not exceed 5mb
             </Text>
           </div>
@@ -44,12 +62,27 @@ function Demo() {
 
 function getIconColor(status: DropzoneStatus, theme: MantineTheme) {
   return status.accepted
-    ? theme.colors[theme.primaryColor][6]
+    ? theme.colors[theme.primaryColor][theme.colorScheme === 'dark' ? 4 : 6]
     : status.rejected
-    ? theme.colors.red[6]
+    ? theme.colors.red[theme.colorScheme === 'dark' ? 4 : 6]
     : theme.colorScheme === 'dark'
     ? theme.colors.dark[0]
-    : theme.black;
+    : theme.colors.gray[7];
+}
+
+function ImageUploadIcon({
+  status,
+  ...props
+}: React.ComponentPropsWithoutRef<typeof ImageIcon> & { status: DropzoneStatus }) {
+  if (status.accepted) {
+    return <UploadIcon {...props} />;
+  }
+
+  if (status.rejected) {
+    return <CrossCircledIcon {...props} />;
+  }
+
+  return <ImageIcon {...props} />;
 }
 
 function Demo() {
@@ -59,11 +92,16 @@ function Demo() {
     <Dropzone onDrop={console.log} maxSize={3 * 1024 ** 2} accept={IMAGE_MIME_TYPE}>
       {(status) => (
         <Group position="center" spacing="xl" style={{ minHeight: 220, pointerEvents: 'none' }}>
-          <ImageIcon style={{ width: 60, height: 60, color: getIconColor(status, theme) }} />
+          <ImageUploadIcon
+            status={status}
+            style={{ width: 80, height: 80, color: getIconColor(status, theme) }}
+          />
 
           <div>
-            <Text size="xl">Drag images here or click to select files</Text>
-            <Text size="sm" color="dimmed">
+            <Text size="xl" inline>
+              Drag images here or click to select files
+            </Text>
+            <Text size="sm" color="dimmed" inline mt={7}>
               Attach as many files as you like, each file should not exceed 5mb
             </Text>
           </div>
