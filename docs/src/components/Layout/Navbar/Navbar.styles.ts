2a799b8e51f8059f6570f639e731e943b6c5394b
Vitaly Rtishchev <rtivital@gmail.com>
2021-10-08 13:01:53 +0300

    [docs] Add better navbar light color scheme styles

--- docs/src/components/Layout/Navbar/Navbar.styles.ts
+++ docs/src/components/Layout/Navbar/Navbar.styles.ts
@@ -12,7 +12,7 @@ export default createStyles((theme: MantineTheme) => ({
     borderRight: `1px solid ${
       theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[2]
     }`,
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
+    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
     position: 'fixed',
     zIndex: 5,
     top: 0,
