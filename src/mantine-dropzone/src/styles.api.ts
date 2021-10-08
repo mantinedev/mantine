0e5dd819351ca37c285059611d32e5fff47f4d69
Vitaly Rtishchev <rtivital@gmail.com>
2021-10-08 10:40:21 +0300

    [@mantine/dropzone] FullScreenDropzone: Add styles api descriptions and props documentation

--- src/mantine-dropzone/src/styles.api.ts
+++ src/mantine-dropzone/src/styles.api.ts
@@ -1,4 +1,5 @@
 import type { DropzoneStylesNames } from './Dropzone';
+import type { FullScreenDropzoneStylesNames } from './FullScreenDropzone';
 
 export const Dropzone: Record<DropzoneStylesNames, string> = {
   root: 'Root element',
@@ -7,4 +8,13 @@ export const Dropzone: Record<DropzoneStylesNames, string> = {
     'Active state modifier, style is added to root element when valid files are dragged over dropzone',
   reject:
     'Active state modifier, style is added to root element when invalid files are dragged over dropzone',
+};
+
+export const FullScreenDropzone: Record<FullScreenDropzoneStylesNames, string> = {
+  wrapper: 'Wraps dropzone, root element',
+  dropzone: 'Dropzone itself',
+  active:
+    'Active state modifier, style is added to dropzone when valid files are dragged over browser window',
+  reject:
+    'Active state modifier, style is added to dropzone when invalid files are dragged over browser window',
 };
