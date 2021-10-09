bdf252ba9278b92569ca3d0beecec3cb9fc8eea4
achmurali <achmurali@github.com>
2021-10-10 00:00:44 +0530

    Fix linting errors

--- src/mantine-core/src/components/Slider/utils/get-change-value/get-change-value.ts
+++ src/mantine-core/src/components/Slider/utils/get-change-value/get-change-value.ts
@@ -7,8 +7,9 @@ interface GetChangeValue {
 }
 
 export function getChangeValue({ value, containerWidth, min, max, step }: GetChangeValue) {
-  const left = !containerWidth ?
-    value : (Math.min(Math.max(value, 0), containerWidth) / containerWidth);
+  const left = !containerWidth
+    ? value
+    : Math.min(Math.max(value, 0), containerWidth) / containerWidth;
   const dx = left * (max - min);
   return (dx !== 0 ? Math.round(dx / step) * step : 0) + min;
 }
