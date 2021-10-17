9067c60fdfd2571155b9fada0b796032d9d529f3
Vitaly Rtishchev <rtivital@gmail.com>
2021-10-17 18:07:13 +0300

    [@mantine/core] Button: Reorder classnames for better styles api composition

--- src/mantine-core/src/components/Button/Button.tsx
+++ src/mantine-core/src/components/Button/Button.tsx
@@ -132,7 +132,7 @@ export const Button: ButtonComponent & { displayName?: string } = forwardRef(
     return (
       <Element
         {...rest}
-        className={cx(classes.root, classes[variant], { [classes.loading]: loading }, className)}
+        className={cx(classes[variant], classes.root, { [classes.loading]: loading }, className)}
         type={type}
         disabled={disabled || loading}
         ref={ref}
