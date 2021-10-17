2769d2e873eb3afca84f05936aef14e9ccd19947
Vitaly Rtishchev <rtivital@gmail.com>
2021-10-17 18:44:53 +0300

    [docs] Fix incorrect colors extend demos code

--- docs/src/demos/theme/theme-colors-extend.tsx
+++ docs/src/demos/theme/theme-colors-extend.tsx
@@ -9,7 +9,7 @@ function Demo() {
     <MantineProvider theme={{
       colors: {
         'ocean-blue': ['#7AD1DD', '#5FCCDB', '#44CADC', '#2AC9DE', '#1AC2D9', '#11B7CD', '#09ADC3', '#0E99AC', '#128797', '#147885'],
-        'bright-pink': ['#F0BBDD', '#ED9BCF', '#EC7CC3', '#ED5DB8', '#F13EAF', '#F71FA7', '#FF00A1', '#E00890', '#C50E82', '#AD1374]',
+        'bright-pink': ['#F0BBDD', '#ED9BCF', '#EC7CC3', '#ED5DB8', '#F13EAF', '#F71FA7', '#FF00A1', '#E00890', '#C50E82', '#AD1374'],
       },
     }}>
       <Button color="ocean-blue">Ocean blue button</Button>
