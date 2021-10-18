c1fb5eb8cbdb52165a692e6018c767a52f5c48f2
Vitaly Rtishchev <rtivital@gmail.com>
2021-10-18 10:59:19 +0300

    [@mantine/styles] Add use-css hook export

--- src/mantine-styles/src/tss/index.ts
+++ src/mantine-styles/src/tss/index.ts
@@ -4,3 +4,4 @@ export { keyframes } from '@emotion/react';
 export { createStyles } from './create-styles';
 export { Global } from './Global';
 export { getCache } from './CacheProvider';
+export { useCss } from './use-css';
