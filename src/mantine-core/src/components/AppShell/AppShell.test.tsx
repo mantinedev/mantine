2fef40cb99a9e5b177eaee01677910e708671a51
Vitaly Rtishchev <rtivital@gmail.com>
2021-10-23 16:56:16 +0300

    [@mantine/core] AppShell: Add tests

--- src/mantine-core/src/components/AppShell/AppShell.test.tsx
+++ src/mantine-core/src/components/AppShell/AppShell.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import {
   itRendersChildren,
   itSupportsClassName,
@@ -7,6 +8,8 @@ import {
   itSupportsRef,
   itSupportsStylesApi,
 } from '@mantine/tests';
+import { Header } from './Header/Header';
+import { Navbar } from './Navbar/Navbar';
 import { AppShell } from './AppShell';
 import { AppShell as AppShellStylesAPi } from './styles.api';
 
@@ -19,6 +22,56 @@ describe('@mantine/core/AppShell', () => {
   itSupportsOthers(AppShell, defaultProps);
   itSupportsRef(AppShell, defaultProps, HTMLDivElement);
   itSupportsStylesApi(AppShell, defaultProps, Object.keys(AppShellStylesAPi), 'AppShell');
+
+  it('passes fixed and zIndex props to Header', () => {
+    const fixed = shallow(
+      <AppShell fixed zIndex={476} header={<Header height={60}>test-header</Header>}>
+        test-shell
+      </AppShell>
+    );
+
+    const _static = shallow(
+      <AppShell fixed={false} header={<Header height={60}>test-header</Header>}>
+        test-shell
+      </AppShell>
+    );
+
+    expect(fixed.find(Header).prop('zIndex')).toBe(476);
+    expect(fixed.find(Header).prop('fixed')).toBe(true);
+    expect(_static.find(Header).prop('fixed')).toBe(false);
+  });
+
+  it('passes fixed and zIndex props to Navbar', () => {
+    const fixed = shallow(
+      <AppShell fixed zIndex={476} navbar={<Navbar>test-navbar</Navbar>}>
+        test-shell
+      </AppShell>
+    );
+
+    const _static = shallow(
+      <AppShell fixed={false} navbar={<Navbar>test-navbar</Navbar>}>
+        test-shell
+      </AppShell>
+    );
+
+    expect(fixed.find(Navbar).prop('zIndex')).toBe(476);
+    expect(fixed.find(Navbar).prop('fixed')).toBe(true);
+    expect(_static.find(Navbar).prop('fixed')).toBe(false);
+  });
+
+  it('sets Navbar height and position based on Header height', () => {
+    const element = shallow(
+      <AppShell
+        header={<Header height={87}>test-header</Header>}
+        navbar={<Navbar width={{ base: 200 }}>test-navbar</Navbar>}
+      >
+        test-shell
+      </AppShell>
+    );
+
+    expect(element.find(Navbar).prop('height')).toBe('calc(100vh - 87px)');
+    expect(element.find(Navbar).prop('position')).toEqual({ top: '87px', left: 0 });
+  });
 
   it('has correct displayName', () => {
     expect(AppShell.displayName).toEqual('@mantine/core/AppShell');
