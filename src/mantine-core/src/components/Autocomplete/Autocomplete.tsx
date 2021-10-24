2671022e12a305fc488e963b77cadb3fb885c386
Vitaly Rtishchev <rtivital@gmail.com>
2021-10-24 22:15:40 +0300

    [@mantine/core] Autocomplete: Turn off browser autocomplete

--- src/mantine-core/src/components/Autocomplete/Autocomplete.tsx
+++ src/mantine-core/src/components/Autocomplete/Autocomplete.tsx
@@ -247,6 +247,7 @@ export const Autocomplete = forwardRef<HTMLInputElement, AutocompleteProps>(
             onFocus={handleInputFocus}
             onBlur={handleInputBlur}
             onClick={handleInputClick}
+            autoComplete="off"
             aria-autocomplete="list"
             aria-controls={shouldRenderDropdown ? `${uuid}-items` : null}
             aria-activedescendant={hovered !== -1 ? `${uuid}-${hovered}` : null}
