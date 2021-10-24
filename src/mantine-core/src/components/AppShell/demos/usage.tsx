16a45fedc23e61ed56fa3d149194ec015ebc33e8
Vitaly Rtishchev <rtivital@gmail.com>
2021-10-24 14:08:40 +0300

    [@mantine/core] AppShell: Fix spacing in usage demo

--- src/mantine-core/src/components/AppShell/demos/usage.tsx
+++ src/mantine-core/src/components/AppShell/demos/usage.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { SunIcon, MoonIcon } from '@modulz/radix-icons';
+import { useMantineColorScheme } from '@mantine/styles';
 import { AppShell, Navbar, Header } from '../index';
+import { Group, Image, ActionIcon } from '../../../index';
 import { MainLinks } from './_mainLinks';
 import { User } from './_user';
-import { Brand } from './_brand';
+import logo from './logo.svg';
+import logoWhite from './logo-white.svg';
 
 const code = `
 import { AppShell, Navbar, Header } from '@mantine/core';
@@ -11,25 +15,27 @@ function Demo() {
   return (
     <AppShell
       padding="md"
-      navbar={<Navbar width={{ base: 300 }} height={300} padding="md">Navbar</Navbar>}
-      header={<Header height={60} padding="md">Header</Header>}
+      navbar={<Navbar width={{ base: 300 }} height={500} padding="xs">{/* Navbar content */}</Navbar>}
+      header={<Header height={60} padding="xs">{/* Header content */}</Header>}
       styles={(theme) => ({
         main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
       })}
     >
-      Your application goes here
+      {/* Your application here */}
     </AppShell>
   );
 }
 `;
 
 function Demo() {
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+
   return (
     <AppShell
       padding="md"
       navbar={
         <Navbar width={{ base: 300 }} height={500} padding="xs">
-          <Navbar.Section grow>
+          <Navbar.Section grow mt="xs">
             <MainLinks />
           </Navbar.Section>
           <Navbar.Section>
@@ -38,8 +44,22 @@ function Demo() {
         </Navbar>
       }
       header={
-        <Header height={60} padding="xs">
-          <Brand />
+        <Header height={60}>
+          <Group
+            style={{
+              height: '100%',
+              marginTop: 0,
+              marginBottom: 0,
+              paddingLeft: 20,
+              paddingRight: 20,
+            }}
+            position="apart"
+          >
+            <Image src={colorScheme === 'dark' ? logoWhite : logo} width={100} />
+            <ActionIcon variant="default" onClick={() => toggleColorScheme()} size={30}>
+              {colorScheme === 'dark' ? <SunIcon /> : <MoonIcon />}
+            </ActionIcon>
+          </Group>
         </Header>
       }
       styles={(theme) => ({
