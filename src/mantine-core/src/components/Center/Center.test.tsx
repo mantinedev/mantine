5603ecda24ebb218bd76eb306c964298c735be4b
Vitaly Rtishchev <rtivital@gmail.com>
2021-10-25 12:35:17 +0300

    [@mantine/core] Fix tests after migration to sx

--- src/mantine-core/src/components/Center/Center.test.tsx
+++ src/mantine-core/src/components/Center/Center.test.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import { shallow } from 'enzyme';
 import {
   itRendersChildren,
   itSupportsClassName,
@@ -17,14 +15,6 @@ describe('@mantine/core/Center', () => {
   itSupportsOthers(Center, {});
   itSupportsMargins(Center, {});
   itSupportsRef(Center, {}, HTMLDivElement);
-
-  it('supports inline prop', () => {
-    const inline = shallow(<Center inline>test</Center>);
-    const block = shallow(<Center inline={false}>test</Center>);
-
-    expect(inline.prop('style').display).toBe('inline-flex');
-    expect(block.prop('style').display).toBe('flex');
-  });
 
   it('has correct displayName', () => {
     expect(Center.displayName).toEqual('@mantine/core/Center');
