3b31f0b6cb5fec139035136924eecc4092e9185b
Emil Małańczak <53233583+EmilMalanczak@users.noreply.github.com>
2021-10-28 11:58:42 +0200

    [mantine/styles] Fix ssr uuid generation (#347)

--- src/mantine-styles/src/theme/SsrProvider.tsx
+++ src/mantine-styles/src/theme/SsrProvider.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 
 function randomId() {
   return `mantine-${Math.random().toString(36).substr(2, 9)}`;
@@ -8,7 +8,14 @@ const UuidContext = createContext<() => string>(() => randomId());
 
 export const useUuid = (staticId?: string) => {
   const generateId = useContext(UuidContext);
-  return staticId || generateId();
+  const [uid, setUid] = useState('');
+  const _useEffect = typeof window !== 'undefined' ? React.useLayoutEffect : React.useEffect;
+
+  _useEffect(() => {
+    setUid(generateId());
+  }, []);
+
+  return staticId || uid;
 };
 
 interface SsrProviderProps {
