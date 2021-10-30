9bcdcf8288b839a8c749b55dcba6bd6be96f1e97
Vitaly Rtishchev <rtivital@gmail.com>
2021-10-30 15:33:27 +0300

    [gallery] Move getDependencyInfo function to separate function

--- gallery/src/components/Gallery/components/ComponentCanvas/CanvasHeader/CanvasHeader.tsx
+++ gallery/src/components/Gallery/components/ComponentCanvas/CanvasHeader/CanvasHeader.tsx
@@ -16,49 +16,10 @@ import {
   Center,
   Anchor,
 } from '@mantine/core';
-import { upperFirst } from '@mantine/hooks';
-import { MantineIcon } from '../icons/MantineIcon';
-import { NpmIcon } from '../icons/NpmIcon';
 import { ColorControl } from '../ColorControl/ColorControl';
+import { getDependencyInfo } from './get-dependency-info';
 import { GalleryComponent } from '../../../types';
 import useStyles from './CanvasHeader.styles';
-
-function getDependencyInfo(url: string) {
-  if (url.startsWith('/core') || url.startsWith('/hooks')) {
-    const _name = url.split('/')[2];
-    const name = url.startsWith('/hooks') ? _name : _name.split('-').map(upperFirst).join('');
-
-    return {
-      name,
-      url: `https://mantine.dev${url}`,
-      icon: <MantineIcon color="blue" />,
-    };
-  }
-
-  if (url.startsWith('/component')) {
-    return {
-      name: url.split('/')[1].split('-').map(upperFirst).join(''),
-      url,
-      icon: <MantineIcon color="blue" />,
-    };
-  }
-
-  if (url.startsWith('https://')) {
-    const _name = url.replace('https://', '').replace('www.', '');
-    const name = _name.endsWith('/') ? _name.slice(0, -1) : _name;
-    return {
-      name,
-      url,
-      icon: <ExternalLinkIcon />,
-    };
-  }
-
-  return {
-    name: url,
-    url: `https://www.npmjs.com/package/${url}`,
-    icon: <NpmIcon />,
-  };
-}
 
 export interface CanvasHeaderProps extends GalleryComponent, React.ComponentPropsWithoutRef<'div'> {
   state: string;
