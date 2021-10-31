b397693fb866ac737e06cde965299860f7ee4628
Vitaly Rtishchev <rtivital@gmail.com>
2021-10-31 22:58:57 +0300

    [gallery] FeaturesCard: Init component

--- gallery/src/gallery/index.ts
+++ gallery/src/gallery/index.ts
@@ -10,6 +10,7 @@ export { ImageBanner } from './ImageBanner/ImageBanner';
 // Cards
 export { ArticleCard } from './ArticleCard/ArticleCard';
 export { BadgeCard } from './BadgeCard/BadgeCard';
+export { FeaturesCard } from './FeaturesCard/FeaturesCard';
 export { ImageActionCard } from './ImageActionCard/ImageActionCard';
 export { ImageButtonCards } from './ImageButtonCards/ImageButtonCards';
 export { ImageCard } from './ImageCard/ImageCard';
