ed8a9d609deee75c5ccf99c00f22880423e098ba
Vitaly Rtishchev <rtivital@gmail.com>
2021-11-02 20:33:17 +0300

    [gallery] NavbarNested: Replace LinksGroup with gallery component

--- gallery/src/gallery/NavbarNested/NavbarNested.tsx
+++ gallery/src/gallery/NavbarNested/NavbarNested.tsx
@@ -1,17 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Navbar, Group, Code, createStyles } from '@mantine/core';
 import {
-  Navbar,
-  Group,
-  Code,
-  Box,
-  Collapse,
-  ThemeIcon,
-  Text,
-  UnstyledButton,
-  createStyles,
-} from '@mantine/core';
-import {
-  Icon as TablerIcon,
   Notes,
   CalendarStats,
   Gauge,
@@ -21,95 +10,9 @@ import {
   Lock,
 } from 'tabler-icons-react';
 import { Scrollbars } from 'react-custom-scrollbars';
-import { ChevronRightIcon } from '@modulz/radix-icons';
 import { UserButton } from '../UserButton/UserButton';
+import { LinksGroup } from '../NavbarLinksGroup/NavbarLinksGroup';
 import { Logo } from './Logo';
-
-interface LinksGroupProps {
-  icon: TablerIcon;
-  label: string;
-  initiallyOpened?: boolean;
-  links?: { label: string; link: string }[];
-}
-
-const useLinksGroupStyles = createStyles((theme) => ({
-  control: {
-    fontWeight: 500,
-    display: 'block',
-    width: '100%',
-    padding: `${theme.spacing.xs}px ${theme.spacing.md}px`,
-    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
-
-    '&:hover': {
-      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[0],
-      color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-    },
-  },
-
-  link: {
-    fontWeight: 500,
-    display: 'block',
-    textDecoration: 'none',
-    padding: `${theme.spacing.xs}px ${theme.spacing.md}px`,
-    paddingLeft: 31,
-    marginLeft: 30,
-    fontSize: theme.fontSizes.sm,
-    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
-    borderLeft: `1px solid ${
-      theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[3]
-    }`,
-
-    '&:hover': {
-      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[0],
-      color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-    },
-  },
-
-  chevron: {
-    transition: 'transform 200ms ease',
-  },
-}));
-
-function LinksGroup({ icon: Icon, label, initiallyOpened, links }: LinksGroupProps) {
-  const { classes } = useLinksGroupStyles();
-  const hasLinks = Array.isArray(links);
-  const [opened, setOpened] = useState(initiallyOpened || false);
-  const items = (hasLinks ? links : []).map((link) => (
-    <Box<'a'>
-      component="a"
-      className={classes.link}
-      href={link.link}
-      key={link.label}
-      onClick={(event) => event.preventDefault()}
-    >
-      {link.label}
-    </Box>
-  ));
-
-  return (
-    <>
-      <UnstyledButton onClick={() => setOpened((o) => !o)} className={classes.control}>
-        <Group position="apart" spacing={0}>
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <ThemeIcon variant="light" size={30}>
-              <Icon size={18} />
-            </ThemeIcon>
-            <Text ml="md" size="sm">
-              {label}
-            </Text>
-          </Box>
-          {hasLinks && (
-            <ChevronRightIcon
-              className={classes.chevron}
-              style={{ transform: opened ? 'rotate(90deg)' : 'none' }}
-            />
-          )}
-        </Group>
-      </UnstyledButton>
-      {hasLinks ? <Collapse in={opened}>{items}</Collapse> : null}
-    </>
-  );
-}
 
 const mockdata = [
   { label: 'Dashboard', icon: Gauge },
