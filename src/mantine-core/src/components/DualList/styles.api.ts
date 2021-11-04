a2271df7a000da5dfed20d0a487ef97eb5b383bd
Justin Smid <justin250899@hotmail.com>
2021-11-04 14:15:41 +0100

    [@mantine/core] DualList: add styles api

--- src/mantine-core/src/components/DualList/styles.api.ts
+++ src/mantine-core/src/components/DualList/styles.api.ts
@@ -1,1 +1,16 @@
-// TODO
+import { DualListStylesNames } from './DualList';
+
+export const DualList: Record<DualListStylesNames, string> = {
+  root: 'Root DualList element',
+  action: 'Action button',
+  actions: 'Action button container',
+  disabled: 'Disabled list item',
+  item: 'List item',
+  empty: 'empty list placeholder',
+  list: 'List element',
+  listRoot: 'List root element',
+  listContainer: 'List container - child of listRoot, parent of list',
+  listTitle: 'List title',
+  searchBar: 'List search bar',
+  selectedItem: 'Item when clicked',
+};
