01f410d45195578936f75b9c21b3afcd1bbc826a
Justin Smid <justin250899@hotmail.com>
2021-11-04 10:47:39 +0100

    [@mantine/core] DualList: Add tests

--- src/mantine-core/src/components/DualList/DualList.test.tsx
+++ src/mantine-core/src/components/DualList/DualList.test.tsx
@@ -1,1 +1,46 @@
-// TODO
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import {
+  checkAccessibility,
+  itSupportsClassName,
+  itSupportsRef,
+  itSupportsStyle,
+  itSupportsOthers,
+  itSupportsStylesApi,
+  itSupportsMargins,
+} from '@mantine/tests';
+import { DualList } from './DualList';
+
+const requiredProps = { available: [], selected: [] };
+const Wrapper = () => <DualList {...requiredProps} styles={{}} />;
+
+describe('@mantine/core/DualList', () => {
+  checkAccessibility([mount(<Wrapper />)]);
+  itSupportsOthers(DualList, requiredProps);
+  itSupportsStyle(DualList, requiredProps);
+  itSupportsRef(DualList, requiredProps, HTMLDivElement, 'leftListRef');
+  itSupportsRef(DualList, requiredProps, HTMLDivElement, 'rightListRef');
+  itSupportsClassName(DualList, requiredProps);
+  itSupportsMargins(DualList, requiredProps);
+
+  itSupportsStylesApi(
+    DualList,
+    requiredProps,
+    [
+      'root',
+      'actions',
+      'listRoot',
+      'listContainer',
+      'listTitle',
+      'list',
+      'actions',
+      'item',
+      'searchBar',
+    ],
+    'DualList'
+  );
+
+  it('has correct displayName', () => {
+    expect(DualList.displayName).toEqual('@mantine/core/DualList');
+  });
+});
