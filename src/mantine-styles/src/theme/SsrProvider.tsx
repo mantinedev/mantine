47fc04365208e9d82c88461a09d730abb304deee
Vitaly Rtishchev <rtivital@gmail.com>
2021-11-05 21:44:51 +0300

    [@mantine/styles] Fix incorrect ids generated for elements after hydration

--- src/mantine-styles/src/theme/SsrProvider.tsx
+++ src/mantine-styles/src/theme/SsrProvider.tsx
@@ -1,34 +1,16 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { useState } from 'react';
 
 function randomId() {
   return `mantine-${Math.random().toString(36).substr(2, 9)}`;
 }
 
-const UuidContext = createContext<() => string>(() => randomId());
-
 export const useUuid = (staticId?: string) => {
-  const generateId = useContext(UuidContext);
   const [uid, setUid] = useState('');
   const _useEffect = typeof window !== 'undefined' ? React.useLayoutEffect : React.useEffect;
 
   _useEffect(() => {
-    setUid(generateId());
+    setUid(randomId());
   }, []);
 
   return staticId || uid;
 };
-
-interface SsrProviderProps {
-  children: React.ReactNode;
-}
-
-export function SsrProvider({ children }: SsrProviderProps) {
-  let id = 0;
-
-  const generateId = () => {
-    id += 1;
-    return `mantine-${id}`;
-  };
-
-  return <UuidContext.Provider value={generateId}>{children}</UuidContext.Provider>;
-}
