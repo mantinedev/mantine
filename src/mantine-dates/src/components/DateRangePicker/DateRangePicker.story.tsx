b18ac055dfa22ede0e72caa8e54a987a0e00dd8d
achmurali <achmurali@github.com>
2021-11-05 12:21:07 +0530

    Add firstdayofweek stories

--- src/mantine-dates/src/components/DateRangePicker/DateRangePicker.story.tsx
+++ src/mantine-dates/src/components/DateRangePicker/DateRangePicker.story.tsx
@@ -6,7 +6,18 @@ import { DateRangePicker } from './DateRangePicker';
 storiesOf('@mantine/dates/DateRangePicker', module)
   .add('General usage', () => (
     <div style={{ padding: 40 }}>
-      <DateRangePicker placeholder="Dates range" defaultValue={[new Date(), new Date()]} />
+      <DateRangePicker
+        label="Pick dates"
+        placeholder="Dates range"
+        defaultValue={[new Date(), new Date()]}
+      />
+      <DateRangePicker
+        label="First day of the week - sunday"
+        placeholder="Dates range"
+        defaultValue={[new Date(), new Date()]}
+        firstDayOfWeek="sunday"
+        style={{ paddingTop: '10px' }}
+      />
     </div>
   ))
   .add('Within Group', () => (
