dbebc09f8adb73d8396c03e4db3e24dede413818
Vitaly Rtishchev <rtivital@gmail.com>
2021-11-07 18:13:27 +0300

    [@mantine/dates] DatePicker: Add accessibility tests

--- src/mantine-dates/src/components/DatePicker/DatePicker.test.tsx
+++ src/mantine-dates/src/components/DatePicker/DatePicker.test.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import {
   itSupportsClassName,
   itSupportsRef,
   itSupportsStyle,
   itSupportsMargins,
+  checkAccessibility,
 } from '@mantine/tests';
 import { DatePicker } from './DatePicker';
 import { DatePickerBase } from '../DatePickerBase/DatePickerBase';
@@ -15,6 +16,7 @@ describe('@mantine/dates/DatePicker', () => {
   itSupportsStyle(DatePicker, {});
   itSupportsMargins(DatePicker, {});
   itSupportsRef(DatePicker, {}, HTMLInputElement);
+  checkAccessibility([mount(<DatePicker label="date picker" />)]);
 
   it('passes correct __staticSelector to Calendar and DatePickerBase components', () => {
     const element = shallow(<DatePicker initiallyOpened />);
