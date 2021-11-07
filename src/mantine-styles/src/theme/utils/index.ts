f4d33d87a425e7bf8fb973c959f5301eeaa7ae6d
Vitaly Rtishchev <rtivital@gmail.com>
2021-11-07 18:45:45 +0300

    [@mantine/styles] Remove old get-theme-color function

--- src/mantine-styles/src/theme/utils/index.ts
+++ src/mantine-styles/src/theme/utils/index.ts
@@ -1,5 +1,4 @@
 export { getSharedColorScheme } from './get-shared-color-scheme/get-shared-color-scheme';
 export { getSizeValue } from './get-size-value/get-size-value';
-export { getThemeColor } from './get-theme-color/get-theme-color';
 export { useExtractedMargins } from './use-extracted-margins/use-extracted-margins';
 export { useSx } from './use-sx/use-sx';
