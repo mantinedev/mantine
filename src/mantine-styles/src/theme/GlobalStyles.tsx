b1a7438398365decfaf2b6f91c01a6733d1857da
Darren Labithiotis <labithiotis@users.noreply.github.com>
2021-11-10 14:24:34 +0100

    [@mantine/styles] Set body font-size in GlobalStyles (#403)

--- src/mantine-styles/src/theme/GlobalStyles.tsx
+++ src/mantine-styles/src/theme/GlobalStyles.tsx
@@ -14,6 +14,7 @@ export function GlobalStyles() {
           backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
           color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
           lineHeight: theme.lineHeight,
+          fontSizes: theme.fontSizes.md,
         } as any,
       })}
     />
