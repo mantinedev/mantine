5ad41bcdfa29f19cba8e17a67e0bad780a3ada20
Vitaly Rtishchev <rtivital@gmail.com>
2021-11-11 21:22:45 +0300

    [@mantine/dates] RangeCalendar: Fix multiple months demo producing horizontal scrollbars

--- src/mantine-dates/src/components/RangeCalendar/demos/multiMonth.tsx
+++ src/mantine-dates/src/components/RangeCalendar/demos/multiMonth.tsx
@@ -1,23 +1,10 @@
 import React, { useState } from 'react';
 import dayjs from 'dayjs';
-import { Group } from '@mantine/core';
+import { Group, MediaQuery, Text } from '@mantine/core';
 import { RangeCalendar } from '../RangeCalendar';
 
 const code = `
-import { useState } from 'react';
-import dayjs from 'dayjs';
-import { RangeCalendar } from '@mantine/dates';
-function Demo() {
-  const [value, setValue] = useState<[Date, Date]>([
-    dayjs(new Date()).startOf('month').toDate(),
-    dayjs(new Date()).startOf('month').add(45, 'days').toDate(),
-  ]);
-  return (
-    <Group position="center">
-      <RangeCalendar amountOfMonths={2} value={value} onChange={setValue} />
-    </Group>
-  );
-}
+<RangeCalendar amountOfMonths={2} />
 `;
 
 function Demo() {
@@ -27,9 +14,16 @@ function Demo() {
   ]);
 
   return (
-    <Group position="center">
-      <RangeCalendar amountOfMonths={2} value={value} onChange={setValue} />
-    </Group>
+    <>
+      <MediaQuery largerThan="md" styles={{ display: 'none' }}>
+        <Group position="center">
+          <RangeCalendar amountOfMonths={2} value={value} onChange={setValue} />
+        </Group>
+      </MediaQuery>
+      <MediaQuery smallerThan="md" styles={{ display: 'none' }}>
+        <Text align="center">Demo is not available, increase viewport size to see it</Text>
+      </MediaQuery>
+    </>
   );
 }
 
