8ea9f671dc0f16f6c07d8198ecb996398a07d963
Vitaly Rtishchev <rtivital@gmail.com>
2021-11-12 20:08:04 +0300

    [docs] Fix incorrect font name

--- docs/src/components/Layout/Layout.tsx
+++ docs/src/components/Layout/Layout.tsx
@@ -8,7 +8,7 @@ import {
 } from '@mantine/core';
 import { useHotkeys, useLocalStorageValue } from '@mantine/hooks';
 import { LayoutInner, LayoutProps } from './LayoutInner';
-import '../../fonts/GreycfifCF/styles.css';
+import '../../fonts/GreycliffCF/styles.css';
 
 const THEME_KEY = 'mantine-color-scheme';
 
