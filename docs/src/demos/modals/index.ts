77c7cfde45ead329a76ea48bab752d8f54da796f
Vitaly Rtishchev <rtivital@gmail.com>
2021-11-13 20:03:47 +0300

    [docs] Add content @mantine/modals docs

--- docs/src/demos/modals/index.ts
+++ docs/src/demos/modals/index.ts
@@ -2,3 +2,4 @@ export { confirm } from './confirm';
 export { context } from './context';
 export { confirmCustomize } from './confirmCustomize';
 export { multipleSteps } from './multipleSteps';
+export { content } from './content';
