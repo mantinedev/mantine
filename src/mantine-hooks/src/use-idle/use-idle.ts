8cc7ab23b3ac2e21f74766c9b8eadf6c8217fc7f
Lucas Lemanowicz <lucas.github@protonmail.com>
2021-11-15 14:27:41 +0000

    [@mantine/hooks] use-idle: Add initialState option support (#419)
    * Add initialState option to use-idle hook
    
    * Update documentation
    
    * npm run prettier:write

--- src/mantine-hooks/src/use-idle/use-idle.ts
+++ src/mantine-hooks/src/use-idle/use-idle.ts
@@ -1,9 +1,17 @@
 import { useState, useEffect, useRef } from 'react';
 
 const DEFAULT_EVENTS = ['keypress', 'mousemove', 'touchmove', 'click', 'scroll'];
+const DEFAULT_OPTIONS = {
+  events: DEFAULT_EVENTS,
+  initialState: true,
+};
 
-export function useIdle(timeout: number, events: string[] = DEFAULT_EVENTS) {
-  const [idle, setIdle] = useState<boolean>(true);
+export function useIdle(
+  timeout: number,
+  options?: Partial<{ events: string[]; initialState: boolean }>
+) {
+  const { events, initialState } = { ...DEFAULT_OPTIONS, ...options };
+  const [idle, setIdle] = useState<boolean>(initialState);
   const timer = useRef<number>();
 
   useEffect(() => {
