c13239fcb43b730af80a3773d4fe03d4151c2900
Vitaly Rtishchev <rtivital@gmail.com>
2021-11-15 15:23:50 +0300

    [@mantine/core] Stepper: Add configurator demo

---
+++ src/mantine-core/src/components/Stepper/demos/configurator.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Stepper, StepperProps } from '../Stepper';
+
+function Wrapper(props: Partial<StepperProps>) {
+  return (
+    <Stepper {...props} active={1}>
+      <Stepper.Step label="Step 1" description="Create an account" />
+      <Stepper.Step label="Step 2" description="Verify email" />
+    </Stepper>
+  );
+}
+
+const codeTemplate = (props: string) => `<Stepper${props} active={1}>
+  <Stepper.Step label="Step 1" description="Create an account" />
+  <Stepper.Step label="Step 2" description="Verify email" />
+</Stepper>`;
+
+export const configurator: MantineDemo = {
+  type: 'configurator',
+  component: Wrapper,
+  codeTemplate,
+  configurator: [
+    { name: 'color', type: 'color', initialValue: 'blue', defaultValue: 'blue' },
+    { name: 'size', type: 'size', initialValue: 'md' },
+  ],
+};
--- src/mantine-core/src/components/Stepper/demos/index.ts
+++ src/mantine-core/src/components/Stepper/demos/index.ts
@@ -1,1 +1,2 @@
 export { usage } from './usage';
+export { configurator } from './configurator';
