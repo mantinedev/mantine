f0dcbc8bd400a621d37d2bb760127e1d5693f83a
Vitaly Rtishchev <rtivital@gmail.com>
2021-11-18 13:40:28 +0300

    [gallery] FloatingLabelInput: Add more features

--- gallery/src/gallery/FloatingLabelInput/FloatingLabelInput.tsx
+++ gallery/src/gallery/FloatingLabelInput/FloatingLabelInput.tsx
@@ -12,7 +12,6 @@ const useStyles = createStyles((theme, { floating }: { floating: boolean }) => (
     top: 7,
     left: theme.spacing.sm,
     pointerEvents: 'none',
-    fontWeight: 400,
     color: floating
       ? theme.colorScheme === 'dark'
         ? theme.white
@@ -20,8 +19,22 @@ const useStyles = createStyles((theme, { floating }: { floating: boolean }) => (
       : theme.colorScheme === 'dark'
       ? theme.colors.dark[3]
       : theme.colors.gray[5],
-    transition: 'transform 150ms ease, color 150ms ease',
-    transform: floating ? `translate(-${theme.spacing.sm}px, -30px)` : 'none',
+    transition: 'transform 150ms ease, color 150ms ease, font-size 150ms ease',
+    transform: floating ? `translate(-${theme.spacing.sm}px, -28px)` : 'none',
+    fontSize: floating ? theme.fontSizes.xs : theme.fontSizes.sm,
+    fontWeight: floating ? 500 : 400,
+  },
+
+  required: {
+    transition: 'opacity 150ms ease',
+    opacity: floating ? 1 : 0,
+  },
+
+  input: {
+    '&::placeholder': {
+      transition: 'color 150ms ease',
+      color: !floating ? 'transparent' : undefined,
+    },
   },
 }));
 
@@ -33,6 +46,8 @@ export function FloatingLabelInput() {
   return (
     <TextInput
       label="Floating label"
+      placeholder="OMG, it also has a placeholder"
+      required
       classNames={classes}
       value={value}
       onChange={(event) => setValue(event.currentTarget.value)}
