44f74e02a9a939cdc4fef48f9f6754d7a90dc686
Vitaly Rtishchev <rtivital@gmail.com>
2021-11-21 17:09:08 +0300

    [@mantine/core] ScrollArea: Add usage demo

--- src/mantine-core/src/components/ScrollArea/demos/usage.tsx
+++ src/mantine-core/src/components/ScrollArea/demos/usage.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { ScrollArea } from '../ScrollArea';
+import { Group } from '../../Group';
+import { Content } from './_content';
 
 const code = `
+<ScrollArea style={{ width: 400, height: 250 }}>
+  {/* content */}
+</ScrollArea>
 `;
 
 function Demo() {
-  return <ScrollArea>Demo</ScrollArea>;
+  return (
+    <Group position="center">
+      <ScrollArea style={{ width: 400, height: 250 }}>
+        <Content />
+      </ScrollArea>
+    </Group>
+  );
 }
 
 export const usage: MantineDemo = {
