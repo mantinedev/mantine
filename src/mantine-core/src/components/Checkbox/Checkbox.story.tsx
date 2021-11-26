b321ebd7283b83803f6221727eab0a9f13fe030e
Vitaly Rtishchev <rtivital@gmail.com>
2021-11-26 17:34:29 +0300

    [@mantine/core] Checkbox: Cleanup stories and docs

--- src/mantine-core/src/components/Checkbox/Checkbox.story.tsx
+++ src/mantine-core/src/components/Checkbox/Checkbox.story.tsx
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
-import { DEFAULT_THEME } from '@mantine/styles';
-import { Checkbox } from './Checkbox';
+import { MANTINE_COLORS } from '@mantine/styles';
+import { Checkbox, CheckboxProps } from './Checkbox';
 
-function CheckboxWrapper(
-  props: Omit<React.ComponentPropsWithoutRef<typeof Checkbox>, 'value' | 'onChange'>
-) {
+function Controlled(props: Partial<CheckboxProps>) {
   const [value, onChange] = useState(true);
   return (
     <Checkbox
@@ -16,35 +14,11 @@ function CheckboxWrapper(
   );
 }
 
-function States() {
-  return (
-    <div style={{ padding: 40 }}>
-      <Checkbox label="default" />
-      <Checkbox style={{ marginTop: 15 }} label="checked" checked />
-      <Checkbox style={{ marginTop: 15 }} label="indeterminate" indeterminate />
-      <Checkbox style={{ marginTop: 15 }} label="indeterminate disabled" indeterminate disabled />
-      <Checkbox style={{ marginTop: 15 }} label="checked disabled" checked disabled />
-    </div>
-  );
-}
-
-const sizes = (['xs', 'sm', 'md', 'lg', 'xl'] as const).map((size) => (
-  <Checkbox
-    color="blue"
-    key={size}
-    size={size}
-    label={`Checkbox ${size}`}
-    style={{ marginTop: 15 }}
-  />
-));
-
-const getThemes = (props?: any) =>
-  Object.keys(DEFAULT_THEME.colors).map((color) => (
-    <Checkbox key={color} color={color} {...props} label={color} style={{ marginTop: 15 }} />
+const getThemes = (props?: Partial<CheckboxProps>) =>
+  MANTINE_COLORS.map((color) => (
+    <Checkbox key={color} color={color} {...props} label={color} mt={15} />
   ));
 
-storiesOf('@mantine/core/Checkbox', module)
-  .add('Themes', () => <div style={{ padding: 15 }}>{getThemes({ checked: true })}</div>)
-  .add('Sizes', () => <div style={{ padding: 15 }}>{sizes}</div>)
-  .add('Controlled', () => <CheckboxWrapper label="Controlled" style={{ padding: 15 }} />)
-  .add('States', () => <States />);
+storiesOf('@mantine/core/Checkbox/stories', module)
+  .add('Colors', () => <div style={{ padding: 15 }}>{getThemes({ checked: true })}</div>)
+  .add('Controlled', () => <Controlled label="Controlled" style={{ padding: 15 }} />);
