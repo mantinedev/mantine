d1056390e3dcab8e86732bae98541b51d84aa3e5
Vitaly Rtishchev <rtivital@gmail.com>
2021-11-26 11:30:07 +0300

    [@mantine/core] Badge: Add styles api stories

--- src/mantine-core/src/components/Badge/stories/Badge.styles-api.story.tsx
+++ src/mantine-core/src/components/Badge/stories/Badge.styles-api.story.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { createStyles, MantineProvider } from '@mantine/styles';
 import { generateBorderStyles } from '@mantine/ds/src';
 import { Group } from '../../Group';
 import { Badge, BadgeProps } from '../Badge';
 import { Badge as BadgeStylesApi } from '../styles.api';
 
 const styles = generateBorderStyles(BadgeStylesApi);
+const useStyles = createStyles(() => styles);
 
 const variants = ['light', 'filled', 'outline', 'dot', 'gradient'] as const;
 
@@ -22,6 +24,17 @@ function Wrapper(props: Partial<BadgeProps<'div'>>) {
   );
 }
 
-storiesOf('@mantine/core/Badge/styles-api', module).add('General usage', () => (
-  <Wrapper sx={{ border: '1px solid red', backgroundColor: 'blue' }} />
-));
+function WithClassNames() {
+  return <Wrapper classNames={useStyles().classes} />;
+}
+
+storiesOf('@mantine/core/Badge/styles-api', module)
+  .add('General usage', () => <Wrapper sx={{ border: '1px solid red', backgroundColor: 'blue' }} />)
+  .add('With styles as object', () => <Wrapper styles={styles} />)
+  .add('With styles as function', () => <Wrapper styles={() => styles} />)
+  .add('With styles as classNames', () => <WithClassNames />)
+  .add('Styles API on MantineProvider', () => (
+    <MantineProvider styles={{ Badge: () => styles }}>
+      <Wrapper />
+    </MantineProvider>
+  ));
