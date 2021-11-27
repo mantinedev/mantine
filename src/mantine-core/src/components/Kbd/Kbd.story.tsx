72eb256b02fcf92cd4e19094414b0c309d0fe980
Vitaly Rtishchev <rtivital@gmail.com>
2021-11-27 22:36:02 +0300

    [@mantine/core] Kbd: Cleanup docs and stories

--- src/mantine-core/src/components/Kbd/Kbd.story.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { Kbd } from './Kbd';
-
-storiesOf('@mantine/core/Kbd', module).add('General usage', () => (
-  <div style={{ padding: 50 }}>
-    <Kbd>shift</Kbd> + <Kbd>H</Kbd>
-  </div>
-));
