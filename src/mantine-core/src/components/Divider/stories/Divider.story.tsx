b945a651a7fa54a037aa5fc0820ddf8b77119706
Vitaly Rtishchev <rtivital@gmail.com>
2021-11-27 10:08:19 +0300

    [@mantine/core] Divider: Cleanup stories

---
+++ src/mantine-core/src/components/Divider/stories/Divider.story.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { DEFAULT_THEME } from '@mantine/styles';
+import { Divider } from '../Divider';
+
+const getColors = (props?: any) =>
+  Object.keys(DEFAULT_THEME.colors).map((color) => (
+    <Divider key={color} color={color} style={{ marginTop: 15 }} {...props} />
+  ));
+
+storiesOf('@mantine/core/Divider/stories', module).add('Colors', () => (
+  <div style={{ padding: 20 }}>{getColors()}</div>
+));
