e164ac88ca0cf39857f3a83e881376c3e878ae72
Vitaly Rtishchev <rtivital@gmail.com>
2021-11-28 12:08:51 +0300

    [@mantine/core] MediaQuery: Cleanup stories

---
+++ src/mantine-core/src/components/MediaQuery/stories/MediaQuery.demos.story.tsx
@@ -0,0 +1,7 @@
+import { storiesOf } from '@storybook/react';
+import { attachDemos } from '@mantine/ds/src';
+import * as demos from '../demos';
+
+const stories = storiesOf('@mantine/core/MediaQuery/demos', module);
+
+attachDemos(stories, demos);
