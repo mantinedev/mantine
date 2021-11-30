b8c536c3315bb697a0f73c04be28675cec83996d
Vitaly Rtishchev <rtivital@gmail.com>
2021-11-30 11:46:34 +0300

    [@mantine/core] Switch: Cleanup stories and docs

--- src/mantine-core/src/components/Switch/stories/Switch.story.tsx
+++ src/mantine-core/src/components/Switch/stories/Switch.story.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
 import { DEFAULT_THEME, MANTINE_SIZES } from '@mantine/styles';
-import { Switch, SwitchProps } from './Switch';
+import { Switch, SwitchProps } from '../Switch';
 
 function SwitchWrapper(props: Omit<SwitchProps, 'value' | 'onChange'>) {
   const [value, onChange] = useState(true);
@@ -18,19 +18,12 @@ const sizes = MANTINE_SIZES.map((size) => (
   <Switch color="blue" key={size} size={size} label={`Switch ${size}`} style={{ marginTop: 15 }} />
 ));
 
-const getThemes = (props?: any) =>
+const getThemes = (props?: Partial<SwitchProps>) =>
   Object.keys(DEFAULT_THEME.colors).map((color) => (
     <Switch key={color} color={color} {...props} label={color} style={{ marginTop: 15 }} />
   ));
 
-storiesOf('@mantine/core/Switch', module)
+storiesOf('@mantine/core/Switch/stories', module)
   .add('Themes', () => <div style={{ padding: 15 }}>{getThemes({ checked: true })}</div>)
   .add('Sizes', () => <div style={{ padding: 15 }}>{sizes}</div>)
-  .add('Controlled', () => <SwitchWrapper label="Controlled" style={{ padding: 15 }} />)
-  .add('Autofocus', () => <SwitchWrapper label="Autofocus" style={{ padding: 15 }} autoFocus />)
-  .add('Disabled', () => (
-    <div style={{ padding: 15 }}>
-      <Switch label="Disabled" disabled />
-      <Switch checked label="Disabled checked" disabled style={{ marginTop: 15 }} />
-    </div>
-  ));
+  .add('Controlled', () => <SwitchWrapper label="Controlled" style={{ padding: 15 }} />);
