a3b544d33f8637450b58aa5a2f47ec1eb2c09089
Vitaly Rtishchev <rtivital@gmail.com>
2021-12-01 10:53:29 +0300

    [@mantine/hooks] use-input-state: Add core logic and tests

--- src/mantine-hooks/src/use-input-state/use-input-state.test.ts
+++ src/mantine-hooks/src/use-input-state/use-input-state.test.ts
@@ -1,12 +1,12 @@
 import { act, renderHook } from '@testing-library/react-hooks';
 import { useInputState } from './use-input-state';
 
-const reactChangeEventText = {
+const changeEventText = {
   currentTarget: { type: 'text', value: 'test-text-value' },
   nativeEvent: {},
 } as any;
 
-const reactChangeEventBoolean = {
+const changeEventBoolean = {
   currentTarget: { type: 'checkbox', checked: true },
   nativeEvent: {},
 } as any;
@@ -18,17 +18,30 @@ describe('@mantine/hooks/use-input-state', () => {
     expect(value).toBe('test-value');
   });
 
-  it('sets value with react change event on text input', () => {
+  it('sets value with change event on text input', () => {
     const hook = renderHook(() => useInputState(''));
-    act(() => hook.result.current[1](reactChangeEventText));
+    act(() => hook.result.current[1](changeEventText));
     const [value] = hook.result.current;
-    expect(value).toBe(reactChangeEventText.currentTarget.value);
+    expect(value).toBe(changeEventText.currentTarget.value);
   });
 
-  it('sets value with react change event on boolean input', () => {
+  it('sets value with change event on boolean input', () => {
     const hook = renderHook(() => useInputState(false));
-    act(() => hook.result.current[1](reactChangeEventBoolean));
+    act(() => hook.result.current[1](changeEventBoolean));
     const [value] = hook.result.current;
     expect(value).toBe(true);
+  });
+
+  it('sets value when onChange is called with actual value', () => {
+    const boolean = renderHook(() => useInputState(false));
+    const string = renderHook(() => useInputState(''));
+
+    act(() => {
+      boolean.result.current[1](true);
+      string.result.current[1]('test');
+    });
+
+    expect(boolean.result.current[0]).toBe(true);
+    expect(string.result.current[0]).toBe('test');
   });
 });
--- src/mantine-hooks/src/use-input-state/use-input-state.ts
+++ src/mantine-hooks/src/use-input-state/use-input-state.ts
@@ -3,26 +3,16 @@ import React, { useState } from 'react';
 export function useInputState<T>(initialState: T) {
   const [value, setValue] = useState(initialState);
 
-  const handleChange = (
-    val: T | React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement> | Event
-  ) => {
+  const handleChange = (val: T | React.ChangeEvent<any>) => {
     if (!val) {
       setValue(val as T);
-    } else if ('nativeEvent' in val) {
+    } else if (typeof val === 'object' && 'nativeEvent' in val) {
       const { currentTarget } = val;
 
-      if (currentTarget.type === 'checkbox' || currentTarget.type === 'radio') {
+      if (currentTarget.type === 'checkbox') {
         setValue((currentTarget as any).checked as any);
       } else {
         setValue(currentTarget.value as any);
-      }
-    } else if (val instanceof Event) {
-      const target = val.target as EventTarget & HTMLInputElement;
-
-      if (target.type === 'checkbox' || target.type === 'radio') {
-        setValue(target.checked as any);
-      } else {
-        setValue(target.value as any);
       }
     } else {
       setValue(val);
