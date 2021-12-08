f04d47eb7c9531f4da27cc6c008f1d3777e6899d
Vitaly Rtishchev <rtivital@gmail.com>
2021-12-08 13:01:03 +0300

    [@mantine/core] Select: Add ChevronIcon exports

--- src/mantine-core/src/components/Select/index.ts
+++ src/mantine-core/src/components/Select/index.ts
@@ -1,4 +1,5 @@
 export { Select } from './Select';
+export { ChevronIcon as SelectChevronIcon } from './SelectRightSection/ChevronIcon';
 export type { SelectProps } from './Select';
 export type { SelectItemProps } from './DefaultItem/DefaultItem';
 export type { BaseSelectStylesNames as SelectStylesNames, SelectItem } from './types';
