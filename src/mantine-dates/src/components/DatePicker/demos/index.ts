df14a850b15c97f86e317a827dcfb23c9f7cc518
Vitaly Rtishchev <rtivital@gmail.com>
2021-12-09 11:44:10 +0300

    [@mantine/dates] Remove old withSelect demos

--- src/mantine-dates/src/components/DatePicker/demos/index.ts
+++ src/mantine-dates/src/components/DatePicker/demos/index.ts
@@ -1,7 +1,6 @@
 export { usage } from './usage';
 export { modal } from './modal';
 export { configurator } from './configurator';
-export { withSelect } from './withSelect';
 export { boundaries } from './boundaries';
 export { exclude } from './exclude';
 export { styles } from './styles';
