8aabcac1a6f4d9b0cba9d884410829f81d4c0412
Vitaly Rtishchev <rtivital@gmail.com>
2021-12-11 11:48:56 +0300

    [@mantine/core] Box: Add array format to sx support

--- src/mantine-core/src/components/Box/Box.tsx
+++ src/mantine-core/src/components/Box/Box.tsx
@@ -3,25 +3,49 @@ import {
   DefaultProps,
   PolymorphicComponentProps,
   PolymorphicRef,
-  useSx,
+  useCss,
+  useMantineTheme,
   useExtractedMargins,
+  CSSObject,
+  MantineTheme,
 } from '@mantine/styles';
 
-interface _BoxProps extends DefaultProps {}
+type Sx = CSSObject | ((theme: MantineTheme) => CSSObject);
+type BoxSx = Sx | Sx[];
+
+interface _BoxProps extends Omit<DefaultProps, 'sx'> {
+  sx?: BoxSx;
+}
 
 export type BoxProps<C extends React.ElementType> = PolymorphicComponentProps<C, _BoxProps>;
 
 type BoxComponent = <C extends React.ElementType = 'div'>(props: BoxProps<C>) => React.ReactElement;
+
+function extractSx(sx: Sx, theme: MantineTheme) {
+  return typeof sx === 'function' ? sx(theme) : sx;
+}
+
+function useBoxSx(sx: BoxSx, className: string) {
+  const { css, cx } = useCss();
+  const theme = useMantineTheme();
+  if (Array.isArray(sx)) {
+    return cx(
+      className,
+      sx.map((partial) => css(extractSx(partial, theme)))
+    );
+  }
+  return cx(className, css(extractSx(sx, theme)));
+}
 
 export const Box: BoxComponent & { displayName?: string } = forwardRef(
   <C extends React.ElementType = 'div'>(
     { className, component, style, sx, ...others }: BoxProps<C>,
     ref: PolymorphicRef<C>
   ) => {
-    const { sxClassName } = useSx({ sx, className });
     const { mergedStyles, rest } = useExtractedMargins({ others, style });
     const Element = component || 'div';
-    return <Element ref={ref} className={sxClassName} style={mergedStyles} {...rest} />;
+
+    return <Element ref={ref} className={useBoxSx(sx, className)} style={mergedStyles} {...rest} />;
   }
 );
 
