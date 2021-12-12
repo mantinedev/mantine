8766dc415b73f091913c1926edc28d351c5585e3
Vitaly Rtishchev <rtivital@gmail.com>
2021-12-12 15:23:44 +0300

    [@mantine/core] Space: Migrate to Box

--- src/mantine-core/src/components/Space/Space.test.tsx
+++ src/mantine-core/src/components/Space/Space.test.tsx
@@ -4,15 +4,19 @@ import {
   itSupportsOthers,
   itSupportsStyle,
   itSupportsRef,
+  itSupportsSx,
 } from '@mantine/tests';
 import { Space } from './Space';
 
+const defaultProps = {};
+
 describe('@mantine/core/Space', () => {
-  itSupportsClassName(Space, {});
-  itSupportsMargins(Space, {});
-  itSupportsOthers(Space, {});
-  itSupportsStyle(Space, {});
-  itSupportsRef(Space, {}, HTMLDivElement);
+  itSupportsClassName(Space, defaultProps);
+  itSupportsSx(Space, defaultProps);
+  itSupportsMargins(Space, defaultProps);
+  itSupportsOthers(Space, defaultProps);
+  itSupportsStyle(Space, defaultProps);
+  itSupportsRef(Space, defaultProps, HTMLDivElement);
 
   it('has correct displayName', () => {
     expect(Space.displayName).toEqual('@mantine/core/Space');
