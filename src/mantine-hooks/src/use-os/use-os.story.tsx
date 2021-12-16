8827ddaf5e2f12893e776719d06fdfbabc950cd8
Jerebtw <49494752+jerebtw@users.noreply.github.com>
2021-12-16 11:55:53 +0100

    [@mantine/core] NumberInput: Fix incorrect onChange value with high precision (#518)
    * Fix for #516
    
    * Fix for #519
    
    * Add use-os story
    
    * Formatting error corrected
    
    * Added a check for the precision on blur

---
+++ src/mantine-hooks/src/use-os/use-os.story.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { useOs } from './use-os';
+
+const Demo = () => {
+  const os = useOs();
+
+  return (
+    <>
+      Your os is <b>{os}</b>
+    </>
+  );
+};
+
+storiesOf('@mantine/hooks/use-os', module).add('Usage', () => (
+  <div style={{ padding: 40 }}>
+    <Demo />
+  </div>
+));
