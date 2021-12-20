8bd21b76c9773f8c0a02a8185a8ac0bf5804c136
Vitaly Rtishchev <rtivital@gmail.com>
2021-12-20 11:26:27 +0300

    [@mantine/core] MediaQuery: Fix incorrect min-width/max-width calculations

--- src/mantine-core/src/components/MediaQuery/demos/usage.tsx
+++ src/mantine-core/src/components/MediaQuery/demos/usage.tsx
@@ -1,44 +1,80 @@
 import React from 'react';
+import { useMantineTheme } from '@mantine/styles';
 import { MediaQuery } from '../MediaQuery';
-import { Badge } from '../../Badge';
 import { Group } from '../../Group';
+import { Box } from '../../Box';
 
 const code = `
-const hide = { display: 'none' };
+import { Group, Box, MediaQuery, useMantineTheme } from '@mantine/core';
 
-<MediaQuery largerThan="lg" styles={hide}>
-  <Badge>Larger than lg</Badge>
-</MediaQuery>
+function Demo() {
+  const theme = useMantineTheme();
+  const highlight = {
+    backgroundColor:
+      theme.colorScheme === 'dark'
+        ? theme.fn.rgba(theme.colors.blue[7], 0.25)
+        : theme.colors.blue[0],
+    border: \`1px solid \${
+      theme.colorScheme === 'dark' ? theme.colors.blue[6] : theme.colors.blue[3]
+    }\`,
+  };
 
-<MediaQuery smallerThan="lg" styles={hide}>
-  <Badge color="red">Smaller than lg</Badge>
-</MediaQuery>
+  const boxStyles = {
+    borderRadius: 3,
+    padding: '3px 5px',
+    border: '1px solid transparent',
+  };
 
-<MediaQuery smallerThan="xl" largerThan="sm" styles={hide}>
-  <Badge color="pink">Smaller than xl, larger than sm</Badge>
-</MediaQuery>
-
-<MediaQuery smallerThan={1500} largerThan={800} styles={hide}>
-  <Badge color="cyan">Smaller than 1500px, larger than 800px</Badge>
-</MediaQuery>
+  return (
+    <Group direction="column" spacing={5}>
+      <MediaQuery largerThan="lg" styles={highlight}>
+        <Box sx={boxStyles}>- larger than lg</Box>
+      </MediaQuery>
+      <MediaQuery smallerThan="lg" styles={highlight}>
+        <Box sx={boxStyles}>- Smaller than lg</Box>
+      </MediaQuery>
+      <MediaQuery smallerThan="xl" largerThan="sm" styles={highlight}>
+        <Box sx={boxStyles}>- Smaller than xl, larger than sm</Box>
+      </MediaQuery>
+      <MediaQuery smallerThan={1500} largerThan={800} styles={highlight}>
+        <Box sx={boxStyles}>- Smaller than 1500px, larger than 800px</Box>
+      </MediaQuery>
+    </Group>
+  );
+}
 `;
 
 function Demo() {
-  const hide = { display: 'none' };
+  const theme = useMantineTheme();
+  const highlight = {
+    backgroundColor:
+      theme.colorScheme === 'dark'
+        ? theme.fn.rgba(theme.colors.blue[7], 0.25)
+        : theme.colors.blue[0],
+    border: `1px solid ${
+      theme.colorScheme === 'dark' ? theme.colors.blue[6] : theme.colors.blue[3]
+    }`,
+  };
+
+  const boxStyles = {
+    borderRadius: 3,
+    padding: '3px 5px',
+    border: '1px solid transparent',
+  };
 
   return (
-    <Group position="center">
-      <MediaQuery largerThan="lg" styles={hide}>
-        <Badge>Larger than lg</Badge>
+    <Group direction="column" spacing={5}>
+      <MediaQuery largerThan="lg" styles={highlight}>
+        <Box sx={boxStyles}>- larger than lg</Box>
       </MediaQuery>
-      <MediaQuery smallerThan="lg" styles={hide}>
-        <Badge color="red">Smaller than lg</Badge>
+      <MediaQuery smallerThan="lg" styles={highlight}>
+        <Box sx={boxStyles}>- Smaller than lg</Box>
       </MediaQuery>
-      <MediaQuery smallerThan="xl" largerThan="sm" styles={hide}>
-        <Badge color="pink">Smaller than xl, larger than sm</Badge>
+      <MediaQuery smallerThan="xl" largerThan="sm" styles={highlight}>
+        <Box sx={boxStyles}>- Smaller than xl, larger than sm</Box>
       </MediaQuery>
-      <MediaQuery smallerThan={1500} largerThan={800} styles={hide}>
-        <Badge color="cyan">Smaller than 1500px, larger than 800px</Badge>
+      <MediaQuery smallerThan={1500} largerThan={800} styles={highlight}>
+        <Box sx={boxStyles}>- Smaller than 1500px, larger than 800px</Box>
       </MediaQuery>
     </Group>
   );
