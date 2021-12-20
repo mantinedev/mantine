259d654877f966403b8402abc30d63d09f572f1e
Vitaly Rtishchev <rtivital@gmail.com>
2021-12-20 13:41:58 +0300

    [@mantine/core] Card: Cleanup CardComponent type

--- src/mantine-core/src/components/Card/Card.tsx
+++ src/mantine-core/src/components/Card/Card.tsx
@@ -11,54 +11,53 @@ interface _CardProps extends SharedPaperProps {
 
 export type CardProps<C extends React.ElementType> = PolymorphicComponentProps<C, _CardProps>;
 
-type CardComponent = <C extends React.ElementType = 'div'>(
+type CardComponent = (<C extends React.ElementType = 'div'>(
   props: CardProps<C>
-) => React.ReactElement;
+) => React.ReactElement) & { displayName?: string; Section: typeof CardSection };
 
-export const Card: CardComponent & { displayName?: string; Section: typeof CardSection } =
-  forwardRef(
-    <C extends React.ElementType = 'div'>(
-      {
-        component,
-        className,
-        padding = 'md',
-        radius = 'sm',
-        children,
-        classNames,
-        styles,
-        ...others
-      }: CardProps<C>,
-      ref: PolymorphicRef<C>
-    ) => {
-      const { classes, cx } = useStyles(null, { name: 'Card', classNames, styles });
-      const _children = Children.toArray(children);
+export const Card: CardComponent = forwardRef(
+  <C extends React.ElementType = 'div'>(
+    {
+      component,
+      className,
+      padding = 'md',
+      radius = 'sm',
+      children,
+      classNames,
+      styles,
+      ...others
+    }: CardProps<C>,
+    ref: PolymorphicRef<C>
+  ) => {
+    const { classes, cx } = useStyles(null, { name: 'Card', classNames, styles });
+    const _children = Children.toArray(children);
 
-      const content = _children.map((child, index) => {
-        if (typeof child === 'object' && child && 'type' in child && child.type === CardSection) {
-          return cloneElement(child, {
-            padding,
-            first: index === 0,
-            last: index === _children.length - 1,
-          });
-        }
+    const content = _children.map((child, index) => {
+      if (typeof child === 'object' && child && 'type' in child && child.type === CardSection) {
+        return cloneElement(child, {
+          padding,
+          first: index === 0,
+          last: index === _children.length - 1,
+        });
+      }
 
-        return child;
-      });
+      return child;
+    });
 
-      return (
-        <Paper
-          className={cx(classes.root, className)}
-          radius={radius}
-          padding={padding}
-          component={component as any}
-          ref={ref}
-          {...others}
-        >
-          {content}
-        </Paper>
-      );
-    }
-  ) as any;
+    return (
+      <Paper
+        className={cx(classes.root, className)}
+        radius={radius}
+        padding={padding}
+        component={component as any}
+        ref={ref}
+        {...others}
+      >
+        {content}
+      </Paper>
+    );
+  }
+) as any;
 
 Card.Section = CardSection;
 Card.displayName = '@mantine/core/Card';
