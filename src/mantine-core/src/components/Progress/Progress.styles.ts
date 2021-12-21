47adf7bc8f0e932dfa91d93528e68a481d00e168
Ashwin Shenoy <ashwinshenoy05@gmail.com>
2021-12-21 18:25:55 +0530

    [@mantine/core] Progress: Fix prettier issues.

--- src/mantine-core/src/components/Progress/Progress.styles.ts
+++ src/mantine-core/src/components/Progress/Progress.styles.ts
@@ -20,41 +20,43 @@ export const move = keyframes({
   '100%': { backgroundPosition: '40px 100%' },
 });
 
-export default createStyles((theme, { color, radius, size, striped, animated }: ProgressStyles) => ({
-  root: {
-    position: 'relative',
-    height: theme.fn.size({ size, sizes }),
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2],
-    borderRadius: theme.fn.size({ size: radius, sizes: theme.radius }),
-    overflow: 'hidden',
-  },
-
-  bar: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    height: '100%',
-    backgroundColor: theme.fn.themeColor(color, theme.colorScheme === 'dark' ? 4 : 6),
-    transition: `width 200ms ${theme.transitionTimingFunction}`,
-    backgroundSize: `${theme.spacing.md}px ${theme.spacing.md}px`,
-    animation: animated ? `${move} 1500ms linear infinite` : 'none',
-    backgroundImage: striped
-      ? 'linear-gradient(45deg, rgba(255, 255, 255, 0.15) 25%, transparent 25%, transparent 50%, rgba(255, 255, 255, 0.15) 50%, rgba(255, 255, 255, 0.15) 75%, transparent 75%, transparent)'
-      : 'none',
-
-    '&:last-of-type': {
-      borderTopRightRadius: theme.fn.size({ size: radius, sizes: theme.radius }),
-      borderBottomRightRadius: theme.fn.size({ size: radius, sizes: theme.radius }),
+export default createStyles(
+  (theme, { color, radius, size, striped, animated }: ProgressStyles) => ({
+    root: {
+      position: 'relative',
+      height: theme.fn.size({ size, sizes }),
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2],
+      borderRadius: theme.fn.size({ size: radius, sizes: theme.radius }),
+      overflow: 'hidden',
     },
 
-    '&:first-of-type': {
-      borderTopLeftRadius: theme.fn.size({ size: radius, sizes: theme.radius }),
-      borderBottomLeftRadius: theme.fn.size({ size: radius, sizes: theme.radius }),
-    },
+    bar: {
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      height: '100%',
+      backgroundColor: theme.fn.themeColor(color, theme.colorScheme === 'dark' ? 4 : 6),
+      transition: `width 200ms ${theme.transitionTimingFunction}`,
+      backgroundSize: `${theme.spacing.md}px ${theme.spacing.md}px`,
+      animation: animated ? `${move} 1500ms linear infinite` : 'none',
+      backgroundImage: striped
+        ? 'linear-gradient(45deg, rgba(255, 255, 255, 0.15) 25%, transparent 25%, transparent 50%, rgba(255, 255, 255, 0.15) 50%, rgba(255, 255, 255, 0.15) 75%, transparent 75%, transparent)'
+        : 'none',
 
-    '@media (prefers-reduced-motion)': {
-      transitionDuration: '0ms',
+      '&:last-of-type': {
+        borderTopRightRadius: theme.fn.size({ size: radius, sizes: theme.radius }),
+        borderBottomRightRadius: theme.fn.size({ size: radius, sizes: theme.radius }),
+      },
+
+      '&:first-of-type': {
+        borderTopLeftRadius: theme.fn.size({ size: radius, sizes: theme.radius }),
+        borderBottomLeftRadius: theme.fn.size({ size: radius, sizes: theme.radius }),
+      },
+
+      '@media (prefers-reduced-motion)': {
+        transitionDuration: '0ms',
+      },
     },
-  },
-}));
+  })
+);
