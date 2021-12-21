4144c5a37b5e5d9920e43c4c95c09ab5f57b1de2
Vitaly Rtishchev <rtivital@gmail.com>
2021-12-21 14:22:40 +0300

    [@mantine/core] Add array of types support to filterChildrenByType function

--- src/mantine-core/src/utils/filter-children-by-type/filter-children-by-type.test.tsx
+++ src/mantine-core/src/utils/filter-children-by-type/filter-children-by-type.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { filterChildrenByType } from './filter-children-by-type';
 
 const TestComponent = () => <div>test</div>;
+const TestComponent2 = () => <div>test</div>;
 
 describe('@mantine/core/utils/filter-children-by-type', () => {
   it('filters nodes by given type', () => {
@@ -17,6 +18,24 @@ describe('@mantine/core/utils/filter-children-by-type', () => {
     );
 
     expect(result).toHaveLength(3);
-    expect(result.every((item) => (item as any).type === TestComponent)).toBe(true);
+    expect(result.every((item) => item.type === TestComponent)).toBe(true);
+  });
+
+  it('filters nodes by given types array', () => {
+    const result = filterChildrenByType(
+      [
+        <div key="0" />,
+        <TestComponent2 key="1" />,
+        <TestComponent key="2" />,
+        <div key="3" />,
+        <TestComponent2 key="4" />,
+      ],
+      [TestComponent, TestComponent2]
+    );
+
+    expect(result).toHaveLength(3);
+    expect(
+      result.every((item) => item.type === TestComponent || item.type === TestComponent2)
+    ).toBe(true);
   });
 });
--- src/mantine-core/src/utils/filter-children-by-type/filter-children-by-type.ts
+++ src/mantine-core/src/utils/filter-children-by-type/filter-children-by-type.ts
@@ -1,8 +1,10 @@
-import React, { Children } from 'react';
+import { Children, JSXElementConstructor, ReactNode, ReactElement } from 'react';
 
-export function filterChildrenByType<T = React.ReactElement>(
-  children: React.ReactNode,
-  type: React.JSXElementConstructor<any>
+export function filterChildrenByType(
+  children: ReactNode,
+  type: JSXElementConstructor<any> | JSXElementConstructor<any>[]
 ) {
-  return (Children.toArray(children) as T[]).filter((item) => (item as any).type === type);
+  return (Children.toArray(children) as ReactElement[]).filter((item) =>
+    Array.isArray(type) ? type.some((component) => component === item.type) : item.type === type
+  );
 }
