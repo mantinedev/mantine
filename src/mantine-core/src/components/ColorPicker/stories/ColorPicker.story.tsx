a3051aa76397b86487e90fc159c75b3c7a18fbd1
Vitaly Rtishchev <rtivital@gmail.com>
2021-12-21 12:25:26 +0300

    [@mantine/core] ColorPicker: Add default size to HueSlider and AlphaSlider components

--- src/mantine-core/src/components/ColorPicker/stories/ColorPicker.story.tsx
+++ src/mantine-core/src/components/ColorPicker/stories/ColorPicker.story.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
 import { DEFAULT_THEME, MANTINE_SIZES } from '@mantine/styles';
 import { RtlProvider } from '@mantine/ds/src';
 import { ColorPicker } from '../ColorPicker';
+import { HueSlider, HueSliderProps } from '../HueSlider/HueSlider';
+import { AlphaSlider, AlphaSliderProps } from '../AlphaSlider/AlphaSlider';
 
 const sizes = MANTINE_SIZES.map((size) => (
   <div style={{ marginTop: 40 }}>
@@ -15,8 +17,30 @@ const sizes = MANTINE_SIZES.map((size) => (
   </div>
 ));
 
+function ControlledHueSlider(props: Partial<HueSliderProps>) {
+  const [value, setValue] = useState(50);
+  return (
+    <>
+      <HueSlider value={value} onChange={setValue} {...props} />
+      {value}
+    </>
+  );
+}
+
+function ControlledAlphaSlider(props: Partial<AlphaSliderProps>) {
+  const [value, setValue] = useState(0.4);
+  return (
+    <>
+      <AlphaSlider color="#00ff00" value={value} onChange={setValue} {...props} />
+      {value}
+    </>
+  );
+}
+
 storiesOf('@mantine/core/ColorPicker/stories', module)
   .add('Sizes', () => <div style={{ padding: 40 }}>{sizes}</div>)
+  .add('Controlled HueSlider', () => <ControlledHueSlider />)
+  .add('Controlled AlphaSlider', () => <ControlledAlphaSlider />)
   .add('RTL', () => (
     <RtlProvider>
       <div style={{ padding: 40 }}>
