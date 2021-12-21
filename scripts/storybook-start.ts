19591dc234deef60a315b29196bfce9fb4fcd646
Vitaly Rtishchev <rtivital@gmail.com>
2021-12-21 12:15:57 +0300

    [core] Setup storybook build script

--- scripts/storybook-start.ts
+++ scripts/storybook-start.ts
@@ -2,8 +2,10 @@ import path from 'path';
 import { argv } from 'yargs';
 import storybook from '@storybook/react/standalone';
 
+const _argv = argv as any;
+
 storybook({
-  port: (argv as any).gallery ? 7523 : 7520,
+  port: _argv.gallery ? 7523 : 7520,
   mode: 'dev',
   configDir: path.join(__dirname, '../configuration/storybook'),
 });
