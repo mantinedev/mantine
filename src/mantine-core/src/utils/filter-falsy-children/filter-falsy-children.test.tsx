0f1eba8b9942d2f72fa1f068dfda0361b5ab6577
Jerebtw <jeremialp41@gmail.com>
2021-12-23 18:36:29 +0100

    [@mantine/core] Utils: Fix test name

--- src/mantine-core/src/utils/filter-falsy-children/filter-falsy-children.test.tsx
+++ src/mantine-core/src/utils/filter-falsy-children/filter-falsy-children.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { filterFalsyChildren } from './filter-falsy-children';
 
-describe('@mantine/core/utils/find-child-by-type', () => {
+describe('@mantine/core/utils/filter-falsy-children', () => {
   it('remove falsy children', () => {
     expect(filterFalsyChildren([undefined, null, false, '', <div key="1" />]).length).toBe(1);
   });
