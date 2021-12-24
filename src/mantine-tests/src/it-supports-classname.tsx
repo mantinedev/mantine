c233f85d41bbb78cf7ae8b61c592554cc6299ec6
Vitaly Rtishchev <rtivital@gmail.com>
2021-12-24 18:12:31 +0300

    [@mantine/tests] Migrate it-supports-margins to react-testing-library

--- src/mantine-tests/src/it-supports-classname.tsx
+++ src/mantine-tests/src/it-supports-classname.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-// import { shallow } from 'enzyme';
 
 export function itSupportsClassName(
   Component: React.ElementType,
--- src/mantine-tests/src/it-supports-margins.tsx
+++ src/mantine-tests/src/it-supports-margins.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render } from '@testing-library/react';
 import { DEFAULT_THEME } from '@mantine/styles';
 
 export function itSupportsMargins(
@@ -7,29 +7,22 @@ export function itSupportsMargins(
   requiredProps: Record<string, any>
 ) {
   it('supports m, mx, my, mt, mb, mr and ml props', () => {
-    const m = shallow(<Component {...requiredProps} m={45} />).render();
-    const theme = shallow(<Component {...requiredProps} m="xl" />).render();
+    const { container: m } = render(<Component {...requiredProps} m={45} />);
+    const { container: theme } = render(<Component {...requiredProps} m="xl" />);
+    const { container: mx } = render(<Component {...requiredProps} mx={34} />);
+    const { container: my } = render(<Component {...requiredProps} my={22} />);
+    const { container: mt } = render(<Component {...requiredProps} mt={13} />);
+    const { container: mb } = render(<Component {...requiredProps} mb={43} />);
+    const { container: mr } = render(<Component {...requiredProps} mr={98} />);
+    const { container: ml } = render(<Component {...requiredProps} ml={11} />);
 
-    const mx = shallow(<Component {...requiredProps} mx={34} />).render();
-    const my = shallow(<Component {...requiredProps} my={22} />).render();
-
-    const mt = shallow(<Component {...requiredProps} mt={13} />).render();
-    const mb = shallow(<Component {...requiredProps} mb={43} />).render();
-    const mr = shallow(<Component {...requiredProps} mr={98} />).render();
-    const ml = shallow(<Component {...requiredProps} ml={11} />).render();
-
-    expect(m.css('margin')).toBe('45px');
-    expect(theme.css('margin')).toBe(`${DEFAULT_THEME.spacing.xl}px`);
-
-    expect(mx.css('margin-left')).toBe('34px');
-    expect(mx.css('margin-right')).toBe('34px');
-
-    expect(my.css('margin-top')).toBe('22px');
-    expect(my.css('margin-bottom')).toBe('22px');
-
-    expect(mt.css('margin-top')).toBe('13px');
-    expect(mb.css('margin-bottom')).toBe('43px');
-    expect(mr.css('margin-right')).toBe('98px');
-    expect(ml.css('margin-left')).toBe('11px');
+    expect(m.firstChild).toHaveStyle({ margin: '45px' });
+    expect(theme.firstChild).toHaveStyle({ margin: `${DEFAULT_THEME.spacing.xl}px` });
+    expect(mx.firstChild).toHaveStyle({ marginLeft: '34px', marginRight: '34px' });
+    expect(my.firstChild).toHaveStyle({ marginTop: '22px', marginBottom: '22px' });
+    expect(mt.firstChild).toHaveStyle({ marginTop: '13px' });
+    expect(mb.firstChild).toHaveStyle({ marginBottom: '43px' });
+    expect(mr.firstChild).toHaveStyle({ marginRight: '98px' });
+    expect(ml.firstChild).toHaveStyle({ marginLeft: '11px' });
   });
 }
