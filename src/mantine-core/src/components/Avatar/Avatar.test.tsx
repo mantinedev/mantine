6ffddb9db6002dbef5ffe34e288327ffb4ba1a1c
Vitaly Rtishchev <rtivital@gmail.com>
2021-12-26 21:40:43 +0300

    [@mantine/core] Avatar: Migrate tests to react testing library

--- src/mantine-core/src/components/Avatar/Avatar.test.tsx
+++ src/mantine-core/src/components/Avatar/Avatar.test.tsx
@@ -1,63 +1,44 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import { render } from '@testing-library/react';
-import {
-  checkAccessibility,
-  itSupportsClassName,
-  itSupportsOthers,
-  itSupportsStyle,
-  itSupportsRef,
-  itSupportsMargins,
-  itIsPolymorphic,
-  itSupportsSx,
-} from '@mantine/tests';
-import { Avatar } from './Avatar';
+import { render, screen } from '@testing-library/react';
+import { checkAccessibility, itIsPolymorphic, itSupportsSystemProps } from '@mantine/tests';
+import { Avatar, AvatarProps } from './Avatar';
 
-const defaultProps = { src: './test-image' };
+const defaultProps: AvatarProps<'div'> = {
+  src: './test-image',
+};
 
 describe('@mantine/core/Avatar', () => {
+  itIsPolymorphic(Avatar, defaultProps);
   checkAccessibility([
     render(<Avatar {...defaultProps} alt="It's me!" />),
     render(<Avatar src={null} alt="It's me!" />),
   ]);
 
-  itSupportsClassName(Avatar, defaultProps);
-  itSupportsMargins(Avatar, defaultProps);
-  itSupportsOthers(Avatar, defaultProps);
-  itSupportsStyle(Avatar, defaultProps);
-  itSupportsSx(Avatar, defaultProps);
-  itIsPolymorphic(Avatar, defaultProps);
-  itSupportsRef(Avatar, defaultProps, HTMLDivElement);
+  itSupportsSystemProps({
+    component: Avatar,
+    props: defaultProps,
+    displayName: '@mantine/core/Avatar',
+    refType: HTMLDivElement,
+  });
 
   it('passes src and alt to image', () => {
-    const element = shallow(<Avatar {...defaultProps} alt="test-alt" />)
-      .render()
-      .find('img');
-    expect(element.attr('src')).toBe(defaultProps.src);
-    expect(element.attr('alt')).toBe('test-alt');
+    render(<Avatar {...defaultProps} alt="test-alt" />);
+    expect(screen.getByAltText('test-alt')).toBeInTheDocument();
+    expect(screen.getByAltText('test-alt')).toHaveAttribute('src', './test-image');
   });
 
   it('renders placeholder if src was not provided', () => {
-    const element = shallow(<Avatar src={null} alt="no-image-test" />);
-    expect(element.render().find('.mantine-Avatar-placeholder')).toHaveLength(1);
-    expect(element.render().find('.mantine-Avatar-placeholder').attr('title')).toBe(
-      'no-image-test'
-    );
+    const { container } = render(<Avatar src={null} alt="no-image-test" />);
+    expect(container.querySelector('.mantine-Avatar-placeholder')).toBeInTheDocument();
   });
 
   it('renders children if src was not given', () => {
-    const element = shallow(
+    render(
       <Avatar src={null} alt="no-image-test">
-        <span className="test">test-placeholder</span>
+        <span className="test-placeholder">test-placeholder</span>
       </Avatar>
     );
 
-    expect(element.render().find('.mantine-Avatar-placeholder').find('.test').text()).toBe(
-      'test-placeholder'
-    );
-  });
-
-  it('has correct displayName', () => {
-    expect(Avatar.displayName).toEqual('@mantine/core/Avatar');
+    expect(screen.getByText('test-placeholder')).toBeInTheDocument();
   });
 });
