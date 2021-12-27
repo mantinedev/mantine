ddc2366342e4b0cb5953998c129fb587e2035f13
Vitaly Rtishchev <rtivital@gmail.com>
2021-12-27 15:39:56 +0300

    [@mantine/core] Center: Migrate tests to react testing library

--- src/mantine-core/src/components/Center/Center.test.tsx
+++ src/mantine-core/src/components/Center/Center.test.tsx
@@ -1,28 +1,17 @@
-import {
-  itRendersChildren,
-  itSupportsClassName,
-  itSupportsStyle,
-  itSupportsOthers,
-  itSupportsMargins,
-  itSupportsRef,
-  itSupportsSx,
-  itIsPolymorphic,
-} from '@mantine/tests';
-import { Center } from './Center';
+import { itRendersChildren, itIsPolymorphic, itSupportsSystemProps } from '@mantine/tests';
+import { Center, CenterProps } from './Center';
 
-const defaultProps = {};
+const defaultProps: CenterProps<'div'> = {
+  children: 'test-center',
+};
 
 describe('@mantine/core/Center', () => {
   itRendersChildren(Center, defaultProps);
-  itSupportsClassName(Center, defaultProps);
-  itSupportsStyle(Center, defaultProps);
-  itSupportsSx(Center, defaultProps);
-  itSupportsOthers(Center, defaultProps);
-  itSupportsMargins(Center, defaultProps);
   itIsPolymorphic(Center, defaultProps);
-  itSupportsRef(Center, defaultProps, HTMLDivElement);
-
-  it('has correct displayName', () => {
-    expect(Center.displayName).toEqual('@mantine/core/Center');
+  itSupportsSystemProps({
+    component: Center,
+    props: defaultProps,
+    displayName: '@mantine/core/Center',
+    refType: HTMLDivElement,
   });
 });
