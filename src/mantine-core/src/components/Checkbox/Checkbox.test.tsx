f83dc75f605a832e013bf08bafaee44231ae5420
Vitaly Rtishchev <rtivital@gmail.com>
2021-12-27 15:56:46 +0300

    [@mantine/core] Checkbox: Migrate tests to react testing library

--- src/mantine-core/src/components/Checkbox/Checkbox.test.tsx
+++ src/mantine-core/src/components/Checkbox/Checkbox.test.tsx
@@ -1,95 +1,70 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import {
   checkAccessibility,
-  itSupportsRef,
-  itSupportsClassName,
-  itSupportsStyle,
-  itSupportsMargins,
-  itSupportsSx,
+  itSupportsSystemProps,
+  itSupportsWrapperProps,
+  itConnectsLabelAndInput,
+  itSupportsFocusEvents,
 } from '@mantine/tests';
-import { Checkbox } from './Checkbox';
+import { Checkbox, CheckboxProps } from './Checkbox';
 
-const defaultProps = {
+const defaultProps: CheckboxProps = {
   checked: true,
   onChange: () => {},
   label: 'test-label',
 };
 
 describe('@mantine/core/Checkbox', () => {
-  itSupportsStyle(Checkbox, defaultProps);
-  itSupportsSx(Checkbox, defaultProps);
-  itSupportsClassName(Checkbox, defaultProps);
-  itSupportsMargins(Checkbox, defaultProps);
-  itSupportsRef(Checkbox, defaultProps, HTMLInputElement);
+  itSupportsWrapperProps(Checkbox, defaultProps);
+  itConnectsLabelAndInput(Checkbox, defaultProps);
+  itSupportsFocusEvents(Checkbox, defaultProps, 'input');
   checkAccessibility([
     render(<Checkbox aria-label="Checkbox without label" />),
     render(<Checkbox label="With label" />),
     render(<Checkbox id="with-id" label="With id" />),
   ]);
 
-  it('renders label based on label prop', () => {
-    const withLabel = shallow(<Checkbox label="test-label" />);
-    const withoutLabel = shallow(<Checkbox />);
-
-    expect(withLabel.render().find('label').text()).toBe('test-label');
-    expect(withoutLabel.render().find('label')).toHaveLength(0);
+  itSupportsSystemProps({
+    component: Checkbox,
+    props: defaultProps,
+    displayName: '@mantine/core/Checkbox',
+    refType: HTMLInputElement,
+    excludeOthers: true,
   });
 
-  it('passes id from props to input and label', () => {
-    const withLabel = shallow(<Checkbox label="test-label" id="test-id-1" />);
-    const withoutLabel = shallow(<Checkbox id="test-id-2" />);
-
-    expect(withLabel.render().find('label').attr('for')).toBe('test-id-1');
-    expect(withLabel.render().find('input').attr('id')).toBe('test-id-1');
-    expect(withoutLabel.render().find('input').attr('id')).toBe('test-id-2');
+  it('renders label based on label prop', () => {
+    const { container: withLabel, getByText } = render(<Checkbox label="test-label" />);
+    const { container: withoutLabel } = render(<Checkbox />);
+    expect(withLabel.querySelectorAll('label')).toHaveLength(1);
+    expect(withoutLabel.querySelectorAll('label')).toHaveLength(0);
+    expect(getByText('test-label')).toBeInTheDocument();
   });
 
   it('sets disabled attribute on input based on disabled prop', () => {
-    const disabled = shallow(<Checkbox disabled />);
-    const notDisabled = shallow(<Checkbox />);
-
-    expect(disabled.render().find('input').attr('disabled')).toBe('disabled');
-    expect(notDisabled.render().find('input').attr('disabled')).toBe(undefined);
+    render(<Checkbox disabled />);
+    expect(screen.getByRole('checkbox')).toBeDisabled();
   });
 
-  it('spreads wrapperProps to root element', () => {
-    const element = shallow(
-      <Checkbox
-        wrapperProps={{
-          'aria-label': 'test-aria-label',
-          style: { border: '1px solid red', lineHeight: '1px' },
-        }}
-      />
-    ).render();
-
-    expect(element.attr('aria-label')).toBe('test-aria-label');
-    expect(element.css('border')).toBe('1px solid red');
-    expect(element.css('line-height')).toBe('1px');
+  it('correctly handles uncontrolled state', () => {
+    render(<Checkbox />);
+    expect(screen.getByRole('checkbox')).not.toBeChecked();
+    userEvent.click(screen.getByRole('checkbox'));
+    expect(screen.getByRole('checkbox')).toBeChecked();
   });
 
-  it('sets checked state based on checked prop', () => {
-    const checked = shallow(<Checkbox checked onChange={() => {}} />);
-    const notChecked = shallow(<Checkbox checked={false} onChange={() => {}} />);
-
-    expect(checked.render().find('input').attr('checked')).toBe('checked');
-    expect(notChecked.render().find('input').attr('checked')).toBe(undefined);
+  it('correctly handles controlled state', () => {
+    const spy = jest.fn();
+    render(<Checkbox checked={false} onChange={spy} />);
+    expect(screen.getByRole('checkbox')).not.toBeChecked();
+    userEvent.click(screen.getByRole('checkbox'));
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(screen.getByRole('checkbox')).not.toBeChecked();
   });
 
   it('sets checked state based on indeterminate prop', () => {
-    const element = shallow(<Checkbox indeterminate checked={false} onChange={() => {}} />);
-    expect(element.render().find('input').attr('checked')).toBe('checked');
-  });
-
-  it('spreads ...others to input element', () => {
-    const element = shallow(<Checkbox aria-checked width="30px" />);
-
-    expect(element.render().find('input').attr('aria-checked')).toBe('true');
-    expect(element.render().find('input').attr('width')).toBe('30px');
-  });
-
-  it('has correct displayName', () => {
-    expect(Checkbox.displayName).toEqual('@mantine/core/Checkbox');
+    render(<Checkbox indeterminate checked={false} />);
+    expect(screen.getByRole('checkbox')).toBeChecked();
   });
 });
