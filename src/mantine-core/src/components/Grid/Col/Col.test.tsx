f32c769cf64017d7e7b81ef75fcc29b78c932b59
Vitaly Rtishchev <rtivital@gmail.com>
2021-12-28 10:48:04 +0300

    [@mantine/core] Grid: Migrate tests to react testing library

--- src/mantine-core/src/components/Grid/Col/Col.test.tsx
+++ src/mantine-core/src/components/Grid/Col/Col.test.tsx
@@ -1,15 +1,7 @@
-import React from 'react';
-import {
-  itRendersChildren,
-  itSupportsClassName,
-  itSupportsStyle,
-  itSupportsMargins,
-  itSupportsSx,
-} from '@mantine/tests';
-import { shallow } from 'enzyme';
-import { Col, isValidSpan } from './Col';
+import { itRendersChildren, itSupportsSystemProps } from '@mantine/tests';
+import { Col, ColProps, isValidSpan } from './Col';
 
-const defaultProps = {
+const defaultProps: ColProps = {
   span: 1,
   columns: 12,
   offset: 0,
@@ -19,10 +11,7 @@ const defaultProps = {
 
 describe('@mantine/core/Col', () => {
   itRendersChildren(Col, defaultProps);
-  itSupportsClassName(Col, defaultProps);
-  itSupportsStyle(Col, defaultProps);
-  itSupportsMargins(Col, defaultProps);
-  itSupportsSx(Col, defaultProps);
+  itSupportsSystemProps({ component: Col, props: defaultProps, displayName: '@mantine/core/Col' });
 
   it('correctly detects invalid spans', () => {
     expect(isValidSpan(2)).toBe(true);
@@ -30,23 +19,5 @@ describe('@mantine/core/Col', () => {
     expect(isValidSpan(-1.2)).toBe(false);
     expect(isValidSpan(-4)).toBe(false);
     expect(isValidSpan(0)).toBe(false);
-  });
-
-  it('does not render column if span prop set incorrectly', () => {
-    const float = shallow(<Col {...defaultProps} span={1.2} />);
-    const zero = shallow(<Col {...defaultProps} span={0} />);
-    const negative = shallow(<Col {...defaultProps} span={-4} />);
-    const overflow = shallow(<Col {...defaultProps} span={500} />);
-    const regular = shallow(<Col {...defaultProps} span={2} />);
-
-    expect(regular.dive().type()).toBe('div');
-    expect(negative.type()).toBe(null);
-    expect(zero.type()).toBe(null);
-    expect(float.type()).toBe(null);
-    expect(overflow.type()).toBe(null);
-  });
-
-  it('has correct displayName', () => {
-    expect(Col.displayName).toEqual('@mantine/core/Col');
   });
 });
