7c143b9acc79b53fa68029b293bd7bbed78ba8e5
Jerebtw <jeremialp41@gmail.com>
2021-12-28 17:45:14 +0100

    [@mantine/core] TransferList: Fix demo

--- src/mantine-core/src/components/TransferList/demos/group.tsx
+++ src/mantine-core/src/components/TransferList/demos/group.tsx
@@ -2,7 +2,39 @@ import React, { useState } from 'react';
 import { TransferList, TransferListData } from '..';
 
 const code = `
-<TransferList />
+import { useState } from 'react';
+import { TransferList, TransferListData } from '@mantine/core';
+
+const initialValues: TransferListData = [
+  [
+    { value: 'react', label: 'React', group: 'Frameworks' },
+    { value: 'ng', label: 'Angular', group: 'Frameworks' },
+    { value: 'next', label: 'Next.js', group: 'Frameworks' },
+    { value: 'jq', label: 'jQuery', group: 'Frameworks' },
+    { value: 'sv', label: 'Svelte', group: 'Libraries' },
+    { value: 'dj', label: 'Django', group: 'Libraries' },
+    { value: 'fl', label: 'Flask', group: 'Libraries' },
+  ],
+  [
+    { value: 'blitz', label: 'Blitz.js', group: 'Frameworks' },
+    { value: 'gatsby', label: 'Gatsby.js', group: 'Frameworks' },
+    { value: 'vue', label: 'Vue', group: 'Frameworks' },
+    { value: 'rw', label: 'Redwood', group: 'Libraries' },
+    { value: 'np', label: 'NumPy', group: 'Libraries' },
+  ],
+];
+
+function Demo() {
+  const [data, setData] = useState<TransferListData>(initialValues);
+  return (
+    <TransferList
+      value={data}
+      onChange={setData}
+      searchPlaceholder="Search..."
+      nothingFound="Nothing here"
+    />
+  );
+}
 `;
 
 const mockdata: TransferListData = [
@@ -27,7 +59,14 @@ const mockdata: TransferListData = [
 function Demo() {
   const [data, setData] = useState<TransferListData>(mockdata);
 
-  return <TransferList value={data} onChange={setData} />;
+  return (
+    <TransferList
+      value={data}
+      onChange={setData}
+      searchPlaceholder="Search..."
+      nothingFound="Nothing here"
+    />
+  );
 }
 
 export const group: MantineDemo = {
