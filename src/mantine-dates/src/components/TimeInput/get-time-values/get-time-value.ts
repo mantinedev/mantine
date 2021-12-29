014b57ccfd993ba65c371d3e9765fc282be6ba6f
Jerebtw <jeremialp41@gmail.com>
2021-12-29 15:57:11 +0100

    [@mantine/dates] TimeInput: Remove useless if

--- src/mantine-dates/src/components/TimeInput/get-time-values/get-time-value.ts
+++ src/mantine-dates/src/components/TimeInput/get-time-values/get-time-value.ts
@@ -1,14 +1,6 @@
 import { padTime } from '../pad-time/pad-time';
 
 export function getTimeValues(value: Date) {
-  if (typeof value === undefined) {
-    return {
-      hours: '00',
-      minutes: '00',
-      seconds: '00',
-    };
-  }
-
   if (!(value instanceof Date)) {
     return { hours: '', minutes: '', seconds: '' };
   }
