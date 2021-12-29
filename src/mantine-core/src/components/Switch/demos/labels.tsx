4e7c1a885e0d8ae3fe26fea5a2f67f341f21a961
Ashwin Shenoy <ashwinshenoy05@gmail.com>
2021-12-29 11:41:16 +0530

    [@mantine/core] Switch: Remove unused `code`.

--- src/mantine-core/src/components/Switch/demos/labels.tsx
+++ src/mantine-core/src/components/Switch/demos/labels.tsx
@@ -1,15 +1,6 @@
 import React from 'react';
 import { Switch } from '../Switch';
 
-const code = `
-  <Switch
-    size="lg"
-    color="green"
-    onLabel="On"
-    offLabel="Off"
-  />
-`;
-
 function Label() {
   return (
     <>
