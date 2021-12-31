d688de4276d6b7b3245fcf7840b74b2e1751aa7c
achmurali <achmurali@github.com>
2021-12-31 20:11:21 +0530

    init use-element-size hook

--- src/mantine-hooks/src/use-element-size/use-element-size.ts
+++ src/mantine-hooks/src/use-element-size/use-element-size.ts
@@ -1,5 +1,7 @@
-//import { useRef } from 'react';
+import { useResizeObserver } from '../use-resize-observer/use-resize-observer';
 
 export function useElementSize<T extends HTMLElement = any>() {
-  //const ref = useRef<T>(null);
+  const [ref, { width, height }] = useResizeObserver<T>();
+
+  return { ref, width, height } as const;
 }
