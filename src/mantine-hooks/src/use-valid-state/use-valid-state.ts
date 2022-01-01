a03ad6420aca6c95f2b21a6b0baefa7b7fc0eaba
Jerebtw <jeremialp41@gmail.com>
2022-01-01 17:49:56 +0100

    [@mantine/core] use-valid-state: Rename `rule` to `validation`

--- src/mantine-hooks/src/use-valid-state/use-valid-state.ts
+++ src/mantine-hooks/src/use-valid-state/use-valid-state.ts
@@ -1,14 +1,14 @@
 import { useState } from 'react';
 
 export function useValidState<T>(
-  rule: (value: T) => boolean,
+  validation: (value: T) => boolean,
   initialValue?: T
 ): readonly [validValue: T, setValue: (value: T) => void, value: T] {
   const [lastValue, setLastValue] = useState<T>(initialValue);
   const [lastValid, setLastValid] = useState<T>(initialValue);
 
   const setValue = (value: T) => {
-    if (rule(value)) {
+    if (validation(value)) {
       setLastValid(value);
     }
     setLastValue(value);
