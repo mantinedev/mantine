cb83feb032884914faf01fd7f813b96bc5bc073a
Jacob Johansson <hello@jacobjohansson.com>
2022-01-01 19:33:32 +0100

    Fixed useHash not getting initial value
    The hashchange event is not firing on initial load, adding window.location.hash directly to the useState would solve this.
    Tried it locally and these changes make it work.
    
    Let me know if something is wrong.

--- src/mantine-hooks/src/use-hash/use-hash.ts
+++ src/mantine-hooks/src/use-hash/use-hash.ts
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useWindowEvent } from '../use-window-event/use-window-event';
 
 export function useHash(): readonly [string | null, (hash: string) => void] {
-  const [hash, setHashValue] = useState<string>(null);
+  const [hash, setHashValue] = useState<string>(window.location.hash);
 
   const setHash = (value: string) => {
     window.location.hash = value;
