fcd6409b044a6cb96fc1227e5ee92e88e6631262
Vitaly Rtishchev <rtivital@gmail.com>
2022-01-02 18:39:21 +0300

    [@mantine/core] Paper: Migrate tests to react testing library

--- src/mantine-core/src/components/Paper/Paper.test.tsx
+++ src/mantine-core/src/components/Paper/Paper.test.tsx
@@ -1,24 +1,17 @@
-import {
-  itRendersChildren,
-  itSupportsClassName,
-  itSupportsStyle,
-  itSupportsRef,
-  itSupportsOthers,
-  itSupportsMargins,
-  itIsPolymorphic,
-} from '@mantine/tests';
-import { Paper } from './Paper';
+import { itRendersChildren, itIsPolymorphic, itSupportsSystemProps } from '@mantine/tests';
+import { Paper, PaperProps } from './Paper';
 
-const defaultProps = {};
+const defaultProps: PaperProps<'div'> = {};
 
 describe('@mantine/core/Paper', () => {
   itRendersChildren(Paper, defaultProps);
-  itSupportsClassName(Paper, defaultProps);
-  itSupportsStyle(Paper, defaultProps);
-  itSupportsRef(Paper, defaultProps, HTMLDivElement);
-  itSupportsOthers(Paper, defaultProps);
-  itSupportsMargins(Paper, defaultProps);
   itIsPolymorphic(Paper, defaultProps);
+  itSupportsSystemProps({
+    component: Paper,
+    props: defaultProps,
+    displayName: '@mantine/core/Paper',
+    refType: HTMLDivElement,
+  });
 
   it('has correct displayName', () => {
     expect(Paper.displayName).toEqual('@mantine/core/Paper');
