9fc928437bfc2fb0b8c3141512275e4287e0004b
Jacob Johansson <hello@jacobjohansson.com>
2022-01-02 13:32:44 +0100

    Fixed SSR check

--- src/mantine-hooks/src/use-hash/use-hash.ts
+++ src/mantine-hooks/src/use-hash/use-hash.ts
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import { useWindowEvent } from '../use-window-event/use-window-event';
 
+const browser = typeof window !== 'undefined';
+
 export function useHash(): readonly [string | null, (hash: string) => void] {
-  const [hash, setHashValue] = useState<string>(window.location.hash);
+  const [hash, setHashValue] = useState<string>(browser ? window.location.hash : "");
 
   const setHash = (value: string) => {
     window.location.hash = value;
