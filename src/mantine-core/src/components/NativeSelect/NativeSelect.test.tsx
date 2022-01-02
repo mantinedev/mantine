1189a995872a00b73c9f7ac750ecf15d61508420
Vitaly Rtishchev <rtivital@gmail.com>
2022-01-02 15:11:42 +0300

    [@mantine/core] NativeSelect: Migrate tests to react testing library

--- src/mantine-core/src/components/NativeSelect/NativeSelect.test.tsx
+++ src/mantine-core/src/components/NativeSelect/NativeSelect.test.tsx
@@ -1,77 +1,39 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import { render } from '@testing-library/react';
 import {
   checkAccessibility,
-  itSupportsClassName,
-  itSupportsRef,
-  itSupportsStyle,
-  itSupportsMargins,
-  itSupportsSx,
+  itSupportsSystemProps,
+  itSupportsInputProps,
+  itSupportsFocusEvents,
 } from '@mantine/tests';
-import { InputWrapper } from '../InputWrapper/InputWrapper';
-import { NativeSelect } from './NativeSelect';
+import { NativeSelect, NativeSelectProps } from './NativeSelect';
 
-const TEST_DATA = [
-  { label: 'test-data-1', value: 'test-data-1' },
-  { label: 'test-data-2', value: 'test-data-2' },
-  { label: 'test-data-3', value: 'test-data-3' },
-];
-
-const defaultProps = {
-  data: TEST_DATA,
+const defaultProps: NativeSelectProps = {
+  data: ['test-item-1', 'test-item-2', 'test-item-3'],
 };
 
 describe('@mantine/core/NativeSelect', () => {
+  itSupportsInputProps(NativeSelect, defaultProps, 'NativeSelect');
+  itSupportsFocusEvents(NativeSelect, defaultProps, 'select');
+  itSupportsSystemProps({
+    component: NativeSelect,
+    props: defaultProps,
+    displayName: '@mantine/core/NativeSelect',
+    excludeOthers: true,
+    refType: HTMLSelectElement,
+  });
+
   checkAccessibility([
-    render(<NativeSelect data={TEST_DATA} label="test-label" />),
-    render(<NativeSelect data={TEST_DATA} aria-label="test-label" />),
+    render(<NativeSelect {...defaultProps} label="test-label" />),
+    render(<NativeSelect {...defaultProps} aria-label="test-label" />),
   ]);
 
-  itSupportsClassName(NativeSelect, defaultProps);
-  itSupportsStyle(NativeSelect, defaultProps);
-  itSupportsSx(NativeSelect, defaultProps);
-  itSupportsMargins(NativeSelect, defaultProps);
-  itSupportsRef(NativeSelect, defaultProps, HTMLSelectElement);
-
-  it('passes required and id props to select element', () => {
-    const element = shallow(<NativeSelect data={TEST_DATA} required id="test-id" />);
-
-    expect(element.render().find('select').attr('id')).toBe('test-id');
-    expect(element.render().find('select').attr('aria-required')).toBe('true');
-  });
-
-  it('passes required, id, label, error and description props to InputWrapper component', () => {
-    const element = shallow(
-      <NativeSelect
-        data={TEST_DATA}
-        id="test-id"
-        required
-        label="test-label"
-        error="test-error"
-        description="test-description"
-      />
-    );
-
-    expect(element.find(InputWrapper).prop('id')).toBe('test-id');
-    expect(element.find(InputWrapper).prop('required')).toBe(true);
-    expect(element.find(InputWrapper).prop('label')).toBe('test-label');
-    expect(element.find(InputWrapper).prop('error')).toBe('test-error');
-    expect(element.find(InputWrapper).prop('description')).toBe('test-description');
-  });
-
-  it('passes wrapperProps to InputWrapper', () => {
-    const element = shallow(
-      <NativeSelect data={TEST_DATA} wrapperProps={{ 'data-label': 'test' }} />
-    );
-    expect(element.render().attr('data-label')).toBe('test');
-  });
-
   it('renders correct amount of options', () => {
-    const withoutPlaceholder = shallow(<NativeSelect data={TEST_DATA} />);
-    const withPlaceholder = shallow(<NativeSelect data={TEST_DATA} placeholder="placeholder" />);
-
-    expect(withoutPlaceholder.render().find('option')).toHaveLength(TEST_DATA.length);
-    expect(withPlaceholder.render().find('option')).toHaveLength(TEST_DATA.length + 1);
+    const { container: withoutPlaceholder } = render(<NativeSelect {...defaultProps} />);
+    const { container: withPlaceholder } = render(
+      <NativeSelect {...defaultProps} placeholder="$" />
+    );
+    expect(withoutPlaceholder.querySelectorAll('option')).toHaveLength(defaultProps.data.length);
+    expect(withPlaceholder.querySelectorAll('option')).toHaveLength(defaultProps.data.length + 1);
   });
 });
