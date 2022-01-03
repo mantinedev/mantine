193a9510fa9b12ca54812a37f0f580cdcc4724fe
Vitaly Rtishchev <rtivital@gmail.com>
2022-01-03 17:13:08 +0300

    [@mantine/core] Title: Migrate tests to react testing library

--- src/mantine-core/src/components/Title/Title.test.tsx
+++ src/mantine-core/src/components/Title/Title.test.tsx
@@ -1,35 +1,14 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import {
-  itRendersChildren,
-  itSupportsOthers,
-  itSupportsStyle,
-  itSupportsClassName,
-  itSupportsMargins,
-  itSupportsRef,
-  itSupportsSx,
-} from '@mantine/tests';
-import { Title } from './Title';
+import { itRendersChildren, itSupportsSystemProps } from '@mantine/tests';
+import { Title, TitleProps } from './Title';
 
-const defaultProps = {};
+const defaultProps: TitleProps = {};
 
 describe('@mantine/core/Title', () => {
   itRendersChildren(Title, defaultProps);
-  itSupportsOthers(Title, defaultProps);
-  itSupportsStyle(Title, defaultProps);
-  itSupportsSx(Title, defaultProps);
-  itSupportsMargins(Title, defaultProps);
-  itSupportsClassName(Title, defaultProps);
-  itSupportsRef(Title, defaultProps, HTMLHeadingElement);
-
-  it('has correct displayName', () => {
-    expect(Title.displayName).toEqual('@mantine/core/Title');
-  });
-
-  it('uses order prop to set tag name', () => {
-    ([1, 2, 3, 4, 5, 6] as const).forEach((order) => {
-      const element = shallow(<Title order={order} />);
-      expect(element.dive().type()).toBe(`h${order}`);
-    });
+  itSupportsSystemProps({
+    component: Title,
+    props: defaultProps,
+    displayName: '@mantine/core/Title',
+    refType: HTMLHeadingElement,
   });
 });
