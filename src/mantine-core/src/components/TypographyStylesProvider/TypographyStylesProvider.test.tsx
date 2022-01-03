73d3dfb2a7a143c9b0652e21b6dd49b9c5287566
Vitaly Rtishchev <rtivital@gmail.com>
2022-01-03 17:20:02 +0300

    [@mantine/core] TypographyStylesProviderProps: Migrate tests to react testing library

--- src/mantine-core/src/components/TypographyStylesProvider/TypographyStylesProvider.test.tsx
+++ src/mantine-core/src/components/TypographyStylesProvider/TypographyStylesProvider.test.tsx
@@ -1,26 +1,19 @@
+import { itRendersChildren, itSupportsSystemProps } from '@mantine/tests';
 import {
-  itSupportsClassName,
-  itSupportsStyle,
-  itSupportsOthers,
-  itRendersChildren,
-  itSupportsMargins,
-  itSupportsRef,
-  itSupportsSx,
-} from '@mantine/tests';
-import { TypographyStylesProvider } from './TypographyStylesProvider';
+  TypographyStylesProvider,
+  TypographyStylesProviderProps,
+} from './TypographyStylesProvider';
 
-const defaultProps = {};
+const defaultProps: TypographyStylesProviderProps = {
+  children: 'test-children',
+};
 
 describe('@mantine/core/TypographyStylesProvider', () => {
-  itSupportsClassName(TypographyStylesProvider, defaultProps);
-  itSupportsStyle(TypographyStylesProvider, defaultProps);
-  itSupportsOthers(TypographyStylesProvider, defaultProps);
   itRendersChildren(TypographyStylesProvider, defaultProps);
-  itSupportsMargins(TypographyStylesProvider, defaultProps);
-  itSupportsSx(TypographyStylesProvider, defaultProps);
-  itSupportsRef(TypographyStylesProvider, defaultProps, HTMLDivElement);
-
-  it('has correct displayName', () => {
-    expect(TypographyStylesProvider.displayName).toEqual('@mantine/core/TypographyStylesProvider');
+  itSupportsSystemProps({
+    component: TypographyStylesProvider,
+    props: defaultProps,
+    displayName: '@mantine/core/TypographyStylesProvider',
+    refType: HTMLDivElement,
   });
 });
