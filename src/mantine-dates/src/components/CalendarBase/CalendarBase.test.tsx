a445029b9a0ee2e8dd9fbfe147f4d6f65657ddf1
Vitaly Rtishchev <rtivital@gmail.com>
2022-01-06 20:48:17 +0300

    [@mantine/dates] CalendarBase: Migrate tests to react testing library

--- src/mantine-dates/src/components/CalendarBase/CalendarBase.test.tsx
+++ src/mantine-dates/src/components/CalendarBase/CalendarBase.test.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { itSupportsSystemProps, checkAccessibility } from '@mantine/tests';
-import { YearPicker } from './YearPicker/YearPicker';
-import { MonthPicker } from './MonthPicker/MonthPicker';
-import { MonthsList } from './MonthsList/MonthsList';
-import { CalendarHeader } from './CalendarHeader/CalendarHeader';
 import { CalendarBase, CalendarBaseProps } from './CalendarBase';
-
-const HEADER_CONTROL_SELECTOR = '.mantine-CalendarBase-calendarHeaderControl';
 
 const labelsProps = {
   nextMonthLabel: 'next month',
@@ -35,101 +30,31 @@ describe('@mantine/core/CalendarBase', () => {
   ]);
 
   it('renders correct views based on initialLevel', () => {
-    const dateLevel = shallow(<CalendarBase initialLevel="date" />);
-    const monthLevel = shallow(<CalendarBase initialLevel="month" />);
-    const yearLevel = shallow(<CalendarBase initialLevel="year" />);
+    const { container: dateLevel } = render(<CalendarBase initialLevel="date" />);
+    const { container: monthLevel } = render(<CalendarBase initialLevel="month" />);
+    const { container: yearLevel } = render(<CalendarBase initialLevel="year" />);
 
-    expect(dateLevel.find(MonthsList)).toHaveLength(1);
-    expect(dateLevel.find(MonthPicker)).toHaveLength(0);
-    expect(dateLevel.find(YearPicker)).toHaveLength(0);
+    expect(dateLevel.querySelectorAll('.mantine-CalendarBase-month')).toHaveLength(1);
+    expect(dateLevel.querySelectorAll('.mantine-CalendarBase-monthPicker')).toHaveLength(0);
+    expect(dateLevel.querySelectorAll('.mantine-CalendarBase-yearPicker')).toHaveLength(0);
 
-    expect(monthLevel.find(MonthsList)).toHaveLength(0);
-    expect(monthLevel.find(MonthPicker)).toHaveLength(1);
-    expect(monthLevel.find(YearPicker)).toHaveLength(0);
+    expect(monthLevel.querySelectorAll('.mantine-CalendarBase-month')).toHaveLength(0);
+    expect(monthLevel.querySelectorAll('.mantine-CalendarBase-monthPicker')).toHaveLength(1);
+    expect(monthLevel.querySelectorAll('.mantine-CalendarBase-yearPicker')).toHaveLength(0);
 
-    expect(yearLevel.find(MonthsList)).toHaveLength(0);
-    expect(yearLevel.find(MonthPicker)).toHaveLength(0);
-    expect(yearLevel.find(YearPicker)).toHaveLength(1);
-  });
-
-  it('passes __staticSelector, styles and classNames to MonthPicker, MonthsList and YearPicker components', () => {
-    const props = {
-      __staticSelector: 'Test',
-      styles: { calendarHeader: { background: 'red' } },
-      classNames: { calendarHeaderControl: 'test-class' },
-    };
-    const dateLevel = shallow(<CalendarBase initialLevel="date" {...props} />);
-    const monthLevel = shallow(<CalendarBase initialLevel="month" {...props} />);
-    const yearLevel = shallow(<CalendarBase initialLevel="year" {...props} />);
-
-    const monthsListProps = dateLevel.find(MonthsList).props();
-    const monthPickerProps = monthLevel.find(MonthPicker).props();
-    const yearPickerProps = yearLevel.find(YearPicker).props();
-
-    expect(monthsListProps.__staticSelector).toBe('Test');
-    expect(monthsListProps.styles).toEqual({ calendarHeader: { background: 'red' } });
-    expect(monthsListProps.classNames).toEqual({ calendarHeaderControl: 'test-class' });
-
-    expect(monthPickerProps.__staticSelector).toBe('Test');
-    expect(monthPickerProps.styles).toEqual({ calendarHeader: { background: 'red' } });
-    expect(monthPickerProps.classNames).toEqual({ calendarHeaderControl: 'test-class' });
-
-    expect(yearPickerProps.__staticSelector).toBe('Test');
-    expect(yearPickerProps.styles).toEqual({ calendarHeader: { background: 'red' } });
-    expect(yearPickerProps.classNames).toEqual({ calendarHeaderControl: 'test-class' });
-  });
-
-  it('passes all props to MonthsList component', () => {
-    const props: CalendarBaseProps = {
-      amountOfMonths: 2,
-      locale: 'ru',
-      minDate: new Date(),
-      maxDate: new Date(),
-      allowLevelChange: false,
-      size: 'md' as const,
-      dayClassName: () => '',
-      dayStyle: () => ({}),
-      disableOutsideEvents: true,
-      excludeDate: () => true,
-      hideWeekdays: true,
-      fullWidth: true,
-      preventFocus: true,
-      firstDayOfWeek: 'sunday',
-      value: new Date(),
-      range: [new Date(), new Date()],
-      onChange: () => {},
-      labelFormat: 'YYYY MMMM',
-      onDayMouseEnter: () => {},
-      hideOutsideDates: true,
-    };
-
-    const element = shallow(<CalendarBase {...props} />);
-    const monthsListProps = element.find(MonthsList).props();
-    Object.keys(props).forEach((prop) => {
-      expect(monthsListProps[prop]).toEqual(props[prop]);
-    });
+    expect(yearLevel.querySelectorAll('.mantine-CalendarBase-month')).toHaveLength(0);
+    expect(yearLevel.querySelectorAll('.mantine-CalendarBase-monthPicker')).toHaveLength(0);
+    expect(yearLevel.querySelectorAll('.mantine-CalendarBase-yearPicker')).toHaveLength(1);
   });
 
   it('calls onMonthChange when month changes', () => {
     const spy = jest.fn();
-    const element = shallow(<CalendarBase onMonthChange={spy} month={new Date(2021, 11, 1)} />);
-    element
-      .find(MonthsList)
-      .dive()
-      .find(CalendarHeader)
-      .dive()
-      .find(HEADER_CONTROL_SELECTOR)
-      .at(0)
-      .simulate('click');
+    const { container } = render(
+      <CalendarBase onMonthChange={spy} month={new Date(2021, 11, 1)} />
+    );
+    userEvent.click(container.querySelectorAll('.mantine-CalendarBase-calendarHeaderControl')[0]);
     expect(spy).toHaveBeenLastCalledWith(new Date(2021, 10, 1));
-    element
-      .find(MonthsList)
-      .dive()
-      .find(CalendarHeader)
-      .dive()
-      .find(HEADER_CONTROL_SELECTOR)
-      .at(1)
-      .simulate('click');
+    userEvent.click(container.querySelectorAll('.mantine-CalendarBase-calendarHeaderControl')[1]);
     expect(spy).toHaveBeenLastCalledWith(new Date(2022, 0, 1));
   });
 });
