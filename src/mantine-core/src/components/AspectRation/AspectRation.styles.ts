9381d3587be59ad7800256d577ac33b7bd52cb70
achmurali <achmurali@github.com>
2022-01-09 22:15:25 +0530

    Fix linting

--- src/mantine-core/src/components/AspectRation/AspectRation.styles.ts
+++ src/mantine-core/src/components/AspectRation/AspectRation.styles.ts
@@ -13,42 +13,40 @@ const sizes = {
   xl: 1920,
 };
 
-export default createStyles((theme, { ratio, maxWidth }: AspectRationStyles) => {
-  return {
-    root: {
-      position: 'relative',
-      maxWidth: theme.fn.size({ size: maxWidth, sizes }),
+export default createStyles((theme, { ratio, maxWidth }: AspectRationStyles) => ({
+  root: {
+    position: 'relative',
+    maxWidth: theme.fn.size({ size: maxWidth, sizes }),
 
-      '&::before': {
-        content: '""',
-        height: 0,
-        display: 'block',
-        paddingBottom: `${(1 / ratio) * 100}%`,
-      },
-
-      '&::after': {
-        content: '""',
-        display: 'table',
-        clear: 'both',
-      },
-
-      '& > *:not(style)': {
-        overflow: 'hidden',
-        position: 'absolute',
-        top: '0',
-        right: '0',
-        bottom: '0',
-        left: '0',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        height: '100%',
-      },
-
-      '& > img, & > video': {
-        objectFit: 'cover',
-      },
+    '&::before': {
+      content: '""',
+      height: 0,
+      display: 'block',
+      paddingBottom: `${(1 / ratio) * 100}%`,
     },
-  };
-});
+
+    '&::after': {
+      content: '""',
+      display: 'table',
+      clear: 'both',
+    },
+
+    '& > *:not(style)': {
+      overflow: 'hidden',
+      position: 'absolute',
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0,
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '100%',
+      height: '100%',
+    },
+
+    '& > img, & > video': {
+      objectFit: 'cover',
+    },
+  },
+}));
