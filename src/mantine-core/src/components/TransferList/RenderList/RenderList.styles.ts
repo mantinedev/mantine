1ca864b8441c90adbc9109cf9907cb0f5cee5bbf
Jerebtw <jeremialp41@gmail.com>
2022-01-09 05:20:12 +0100

    [@mantine/core] TransferList: Fix incorrect radius

--- src/mantine-core/src/components/TransferList/RenderList/RenderList.styles.ts
+++ src/mantine-core/src/components/TransferList/RenderList/RenderList.styles.ts
@@ -61,8 +61,8 @@ export default createStyles((theme, { reversed, native }: RenderListStyles) => (
     borderTopWidth: 0,
     borderRightWidth: 0,
     borderLeftWidth: 0,
-    borderTopLeftRadius: reversed ? 0 : theme.radius.sm,
-    borderTopRightRadius: reversed ? theme.radius.sm : 0,
+    borderTopLeftRadius: reversed ? 0 : theme.radius.sm - 1,
+    borderTopRightRadius: reversed ? theme.radius.sm - 1 : 0,
     display: 'block',
     borderBottomColor: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[4],
   },
@@ -71,9 +71,12 @@ export default createStyles((theme, { reversed, native }: RenderListStyles) => (
     borderTop: 0,
     borderRightWidth: reversed ? undefined : 0,
     borderLeftWidth: reversed ? 0 : undefined,
-    borderTopLeftRadius: reversed ? theme.radius.sm : 0,
-    borderTopRightRadius: reversed ? 0 : theme.radius.sm,
     borderColor: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[4],
+
+    '&:last-of-type': {
+      borderTopLeftRadius: reversed ? theme.radius.sm - 1 : 0,
+      borderTopRightRadius: reversed ? 0 : theme.radius.sm - 1,
+    },
 
     '&:disabled': {
       backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : 'transparent',
