e3ec3595a267f58f701f59fcdd969242fdac7191
Vitaly Rtishchev <rtivital@gmail.com>
2022-01-10 13:07:26 +0300

    [@mantine/core] Button: Add subtle variant support

--- src/mantine-core/src/components/Button/demos/variants.tsx
+++ src/mantine-core/src/components/Button/demos/variants.tsx
@@ -6,8 +6,9 @@ function Demo() {
   return (
     <>
       <Group position="center">
-        <Button>Filled variant</Button>
+        <Button variant="subtle">Subtle variant</Button>
         <Button variant="light">Light variant</Button>
+        <Button>Filled variant</Button>
         <Button variant="outline">Outline variant</Button>
         <Button variant="default">Default variant</Button>
       </Group>
