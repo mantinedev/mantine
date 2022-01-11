ef85bc7dcbdd8164b5d2c357d870177a12a0553c
Vitaly Rtishchev <rtivital@gmail.com>
2022-01-11 11:57:52 +0300

    [core] Setup new rtl toggle in storybook

--- configuration/storybook/preview.tsx
+++ configuration/storybook/preview.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDarkMode } from 'storybook-dark-mode';
-import { MantineProvider, ColorSchemeProvider } from '@mantine/core';
+import { MantineProvider, ColorSchemeProvider, Affix, ActionIcon } from '@mantine/core';
+import { useHotkeys } from '@mantine/hooks';
 import rtlPlugin from 'stylis-plugin-rtl';
 
 export const parameters = { layout: 'fullscreen' };
 
 function ThemeWrapper(props: any) {
+  const [rtl, setRtl] = useState(false);
+  const toggleRtl = () => setRtl((r) => !r);
+  useHotkeys([['mod + L', toggleRtl]]);
+
   return (
     <ColorSchemeProvider colorScheme="light" toggleColorScheme={() => {}}>
       <MantineProvider
@@ -14,14 +19,29 @@ function ThemeWrapper(props: any) {
           headings: { fontFamily: 'Greycliff CF, sans-serif' },
         }}
         emotionOptions={
-          process.env.RTL
-            ? { key: 'mantine-rtl', stylisPlugins: [rtlPlugin as any] }
-            : { key: 'mantine' }
+          rtl ? { key: 'mantine-rtl', stylisPlugins: [rtlPlugin as any] } : { key: 'mantine' }
         }
         withGlobalStyles
         withNormalizeCSS
       >
-        <div dir={process.env.RTL ? 'rtl' : 'ltr'}>{props.children}</div>
+        <Affix position={{ right: rtl ? 'unset' : 0, left: rtl ? 0 : 'unset', top: 0 }}>
+          <ActionIcon
+            onClick={toggleRtl}
+            variant="default"
+            style={{
+              borderTop: 0,
+              borderRight: 0,
+              borderBottomLeftRadius: 4,
+              width: 60,
+              fontWeight: 700,
+            }}
+            radius={0}
+            size={30}
+          >
+            {rtl ? 'RTL' : 'LTR'}
+          </ActionIcon>
+        </Affix>
+        <div dir={rtl ? 'rtl' : 'ltr'}>{props.children}</div>
       </MantineProvider>
     </ColorSchemeProvider>
   );
