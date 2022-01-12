38b5cb4b1acc115202129a257fadcfceb0dcba7c
Vitaly Rtishchev <rtivital@gmail.com>
2022-01-12 17:33:55 +0300

    [@mantine/core] Breadcrumbs: Fix demos to work with rtl

--- src/mantine-core/src/components/Breadcrumbs/demos/usage.tsx
+++ src/mantine-core/src/components/Breadcrumbs/demos/usage.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
+import { useMantineTheme } from '@mantine/styles';
 import { Breadcrumbs } from '../Breadcrumbs';
 import { Anchor } from '../../Anchor/Anchor';
 
 const code = `
-import React from 'react';
 import { Breadcrumbs, Text } from '@mantine/core';
 
-function Demo() {
-  const items = [
-    { title: 'Mantine', href: 'https://mantine.dev' },
-    { title: 'Mantine hooks', href: 'https://mantine.dev/hooks/getting-started' },
-    { title: 'use-id', href: 'https://mantine.dev/hooks/use-id' },
-  ].map((item, index) => (
-    <Anchor href={item.href} key={index}>
-      {item.title}
-    </Anchor>
-  ));
+const items = [
+  { title: 'Mantine', href: 'https://mantine.dev' },
+  { title: 'Mantine hooks', href: 'https://mantine.dev/hooks/getting-started' },
+  { title: 'use-id', href: 'https://mantine.dev/hooks/use-id' },
+].map((item, index) => (
+  <Anchor href={item.href} key={index}>
+    {item.title}
+  </Anchor>
+));
 
+function Demo() {
   return (
     <>
       <Breadcrumbs>{items}</Breadcrumbs>
@@ -26,21 +26,23 @@ function Demo() {
 }
 `;
 
+const items = [
+  { title: 'Mantine', href: 'https://mantine.dev' },
+  { title: 'Mantine hooks', href: 'https://mantine.dev/hooks/getting-started' },
+  { title: 'use-id', href: 'https://mantine.dev/hooks/use-id' },
+].map((item, index) => (
+  <Anchor href={item.href} key={index}>
+    {item.title}
+  </Anchor>
+));
+
 function Demo() {
-  const items = [
-    { title: 'Mantine', href: 'https://mantine.dev' },
-    { title: 'Mantine hooks', href: 'https://mantine.dev/hooks/getting-started' },
-    { title: 'use-id', href: 'https://mantine.dev/hooks/use-id' },
-  ].map((item, index) => (
-    <Anchor href={item.href} key={index}>
-      {item.title}
-    </Anchor>
-  ));
+  const theme = useMantineTheme();
 
   return (
     <>
       <Breadcrumbs>{items}</Breadcrumbs>
-      <Breadcrumbs separator="→" style={{ marginTop: 10 }}>
+      <Breadcrumbs separator={theme.dir === 'ltr' ? '→' : '←'} style={{ marginTop: 10 }}>
         {items}
       </Breadcrumbs>
     </>
