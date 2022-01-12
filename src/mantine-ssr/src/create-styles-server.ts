94fa79c6211bb56b2d4513c45c876f8b1ff07618
Vitaly Rtishchev <rtivital@gmail.com>
2022-01-12 11:43:33 +0300

    [@mantine/ssr] Add better setup for create-styles-server emotion options

--- src/mantine-ssr/src/create-styles-server.ts
+++ src/mantine-ssr/src/create-styles-server.ts
@@ -1,7 +1,7 @@
-import { EmotionCache } from '@emotion/cache';
+import { Options } from '@emotion/cache';
 import createEmotionServer from '@emotion/server/create-instance';
 import { getCache } from '@mantine/styles';
 
-export function createStylesServer(cache?: EmotionCache) {
-  return createEmotionServer(cache || getCache());
+export function createStylesServer(options?: Options) {
+  return createEmotionServer(getCache(options));
 }
