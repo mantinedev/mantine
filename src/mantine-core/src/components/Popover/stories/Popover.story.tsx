138c3185dc7a7b64a6e893d9a046c8ef0528a43a
Vitaly Rtishchev <rtivital@gmail.com>
2022-01-17 11:14:17 +0300

    [@mantine/core] Popover: Fix click ouside events not working in Modal and Drawer

--- src/mantine-core/src/components/Popover/stories/Popover.story.tsx
+++ src/mantine-core/src/components/Popover/stories/Popover.story.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
+import { Modal } from '../../Modal';
+import { Button } from '../../Button';
 import { TextInput } from '../../TextInput/TextInput';
 import { Wrapper } from './_wrapper';
 
-storiesOf('@mantine/core/Popover/stories', module).add('Focus behavior', () => (
-  <div style={{ padding: 100 }}>
-    <Wrapper />
-    <TextInput placeholder="Focus me when popover is opened" />
-  </div>
-));
+function WithinModal() {
+  const [opened, setOpened] = useState(false);
+  return (
+    <>
+      <Button onClick={() => setOpened(true)}>Open modal</Button>
+      <Modal opened={opened} onClose={() => setOpened(false)}>
+        <Wrapper />
+      </Modal>
+    </>
+  );
+}
+
+storiesOf('@mantine/core/Popover/stories', module)
+  .add('Focus behavior', () => (
+    <div style={{ padding: 100 }}>
+      <Wrapper />
+      <TextInput placeholder="Focus me when popover is opened" />
+    </div>
+  ))
+  .add('Within modal', () => <WithinModal />);
