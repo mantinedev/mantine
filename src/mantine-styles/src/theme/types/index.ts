3741cdbfd176f27731ca9dfac310c9294d7fba9e
dbrands <david.brands@me.com>
2022-01-21 16:46:04 +0100

    [@mantine/styles] Export MantineThemeOther from package

--- src/mantine-styles/src/theme/types/index.ts
+++ src/mantine-styles/src/theme/types/index.ts
@@ -5,7 +5,12 @@ export type { MantineGradient } from './MantineGradient';
 export type { MantineMargins, MantineMargin } from './MantineMargins';
 export type { MantineShadow } from './MantineShadow';
 export type { MantineNumberSize, MantineSize, MantineSizes } from './MantineSize';
-export type { MantineTheme, MantineThemeOverride, MantineThemeBase } from './MantineTheme';
+export type {
+  MantineTheme,
+  MantineThemeOverride,
+  MantineThemeBase,
+  MantineThemeOther,
+} from './MantineTheme';
 export type { PolymorphicRef, PolymorphicComponentProps } from './Polymorphic';
 export type { ForwardRefWithStaticComponents } from './ForwardRefWithStaticComponents';
 export type { Tuple } from './Tuple';
