27262a5ee5af1199e71b088b7690864e7c0d6868
Vitaly Rtishchev <rtivital@gmail.com>
2022-01-22 18:19:51 +0300

    [gallery] ArticleCardVertical: Init component

--- gallery/gallery/index.ts
+++ gallery/gallery/index.ts
@@ -10,6 +10,7 @@ export { ImageActionBanner } from './ImageActionBanner/ImageActionBanner';
 // Cards
 export { ArticleCard } from './ArticleCard/ArticleCard';
 export { ArticleCardFooter } from './ArticleCardFooter/ArticleCardFooter';
+export { ArticleCardVertical } from './ArticleCardVertical/ArticleCardVertical';
 export { BadgeCard } from './BadgeCard/BadgeCard';
 export { FeaturesCard } from './FeaturesCard/FeaturesCard';
 export { ImageCard } from './ImageCard/ImageCard';
