b8301069ff095d294becfcbadc676993f8b8db46
Vitaly Rtishchev <rtivital@gmail.com>
2022-01-30 18:43:34 +0300

    [@mantine/demos] Migrate Avatar demos

---
+++ src/mantine-demos/src/demos/Avatar/index.ts
@@ -0,0 +1,6 @@
+export { usage } from './usage';
+export { placeholders } from './placeholders';
+export { configurator } from './configurator';
+export { link } from './link';
+export { avatarsGroup } from './avatarsGroup';
+export { total } from './total';
--- src/mantine-demos/src/index.ts
+++ src/mantine-demos/src/index.ts
@@ -5,3 +5,4 @@ export * as AlertDemos from './demos/Alert';
 export * as AnchorDemos from './demos/Anchor';
 export * as AppShellDemos from './demos/AppShell';
 export * as AutocompleteDemos from './demos/Autocomplete';
+export * as AvatarDemos from './demos/Avatar';
