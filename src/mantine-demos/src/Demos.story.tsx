fac1e90ae798bd555d91640b76073192d7161fa0
Vitaly Rtishchev <rtivital@gmail.com>
2022-01-30 18:36:55 +0300

    [@mantine/demos] Setup storybook

---
+++ src/mantine-demos/src/Demos.story.tsx
@@ -0,0 +1,10 @@
+import { storiesOf } from '@storybook/react';
+import { attachDemos } from '@mantine/ds/src';
+import * as demos from './index';
+
+attachDemos(storiesOf('@mantine/core/Accordion/demos', module), demos.AccordionDemos);
+attachDemos(storiesOf('@mantine/core/ActionIcon/demos', module), demos.ActionIconDemos);
+attachDemos(storiesOf('@mantine/core/Affix/demos', module), demos.AffixDemos);
+attachDemos(storiesOf('@mantine/core/Alert/demos', module), demos.AlertDemos);
+attachDemos(storiesOf('@mantine/core/Anchor/demos', module), demos.AnchorDemos);
+attachDemos(storiesOf('@mantine/core/AppShell/demos', module), demos.AppShellDemos);
