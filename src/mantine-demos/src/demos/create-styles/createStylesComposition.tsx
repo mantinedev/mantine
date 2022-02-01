d7e0f63288ea77dc85ee1f303e95b05d230265c9
Vitaly Rtishchev <rtivital@gmail.com>
2022-02-01 19:40:14 +0300

    [@mantine/demos] Update create styles demos to use new references

--- src/mantine-demos/src/demos/create-styles/createStylesComposition.tsx
+++ src/mantine-demos/src/demos/create-styles/createStylesComposition.tsx
@@ -2,37 +2,32 @@ import React from 'react';
 import { createStyles } from '@mantine/core';
 
 const code = `
-const useStyles = createStyles((theme, _params, getRef) => {
-  // create reference for future use
-  const button = getRef('button');
+const useStyles = createStyles((theme, _params, getRef) => ({
+  button: {
+    // assign reference to selector
+    ref: getRef('button'),
 
-  return {
-    button: {
-      // assign reference to selector
-      ref: button,
+    // and add any other properties
+    backgroundColor: theme.colors.blue[6],
+    color: theme.white,
+    padding: \`\${theme.spacing.sm}px \${theme.spacing.lg}px\`,
+    borderRadius: theme.radius.md,
+    cursor: 'pointer',
+    border: 0,
+  },
 
-      // and add any other properties
-      backgroundColor: theme.colors.blue[6],
-      color: theme.white,
-      padding: \`\${theme.spacing.sm}px \${theme.spacing.lg}px\`,
-      borderRadius: theme.radius.md,
-      cursor: 'pointer',
-      border: 0,
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[1],
+    padding: theme.spacing.xl,
+
+    // reference button with nested selector
+    [\`&:hover .\${getRef('button')}\`]: {
+      backgroundColor: theme.colors.violet[6],
     },
-
-    container: {
-      display: 'flex',
-      justifyContent: 'center',
-      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[1],
-      padding: theme.spacing.xl,
-
-      // reference button with nested selector
-      [\`&:hover .\${button}\`]: {
-        backgroundColor: theme.colors.violet[6],
-      },
-    },
-  };
-});
+  },
+}));
 
 function Demo() {
   const { classes } = useStyles();
@@ -46,37 +41,32 @@ function Demo() {
 }
 `;
 
-const useStyles = createStyles((theme, _params, getRef) => {
-  // create reference for future use
-  const button = getRef('button');
+const useStyles = createStyles((theme, _params, getRef) => ({
+  button: {
+    // assign reference to selector
+    ref: getRef('button'),
 
-  return {
-    button: {
-      // assign reference to selector
-      ref: button,
+    // and add any other properties
+    backgroundColor: theme.colors.blue[6],
+    color: theme.white,
+    padding: `${theme.spacing.sm}px ${theme.spacing.lg}px`,
+    borderRadius: theme.radius.md,
+    cursor: 'pointer',
+    border: 0,
+  },
 
-      // and add any other properties
-      backgroundColor: theme.colors.blue[6],
-      color: theme.white,
-      padding: `${theme.spacing.sm}px ${theme.spacing.lg}px`,
-      borderRadius: theme.radius.md,
-      cursor: 'pointer',
-      border: 0,
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[1],
+    padding: theme.spacing.xl,
+
+    // reference button with nested selector
+    [`&:hover .${getRef('button')}`]: {
+      backgroundColor: theme.colors.violet[6],
     },
-
-    container: {
-      display: 'flex',
-      justifyContent: 'center',
-      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[1],
-      padding: theme.spacing.xl,
-
-      // reference button with nested selector
-      [`&:hover .${button}`]: {
-        backgroundColor: theme.colors.violet[6],
-      },
-    },
-  };
-});
+  },
+}));
 
 function Demo() {
   const { classes } = useStyles();
