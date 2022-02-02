63431ebc8ec646a030a718d598ac8da0d19ae227
Shaade <s.shaade77@gmail.com>
2022-02-02 14:50:55 +0100

    [@mantine/rte] Fix incorrect white space handling in Firefox (#795)

--- src/mantine-rte/src/components/RichTextEditor/RichTextEditor.styles.ts
+++ src/mantine-rte/src/components/RichTextEditor/RichTextEditor.styles.ts
@@ -39,6 +39,7 @@ export default createStyles(
       },
 
       '& .ql-editor': {
+        'white-space': 'pre-wrap',
         outline: 'none',
         padding: `${theme.spacing.sm}px ${theme.spacing.md}px`,
       },
