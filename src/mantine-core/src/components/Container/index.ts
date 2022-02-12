e510c1d65e16ee23ca64d04a00253f3515ee448f
Vitaly Rtishchev <rtivital@gmail.com>
2022-02-12 15:25:33 +0300

    [@mantine/core] Container: Remove sizes export

--- src/mantine-core/src/components/Container/index.ts
+++ src/mantine-core/src/components/Container/index.ts
@@ -1,4 +1,3 @@
-export { sizes as CONTAINER_SIZES } from './Container.styles';
 export { Container } from './Container';
 export type { ContainerProps } from './Container';
 export type { ContainerStylesParams } from './Container.styles';
