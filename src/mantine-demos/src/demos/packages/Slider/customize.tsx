8fb6bb8a5c553e58407ca0e6354692f9ddf473b6
Vitaly Rtishchev <rtivital@gmail.com>
2022-02-13 20:22:41 +0300

    [docs] Cleanup learn the basics code examples

--- src/mantine-demos/src/demos/packages/Slider/customize.tsx
+++ src/mantine-demos/src/demos/packages/Slider/customize.tsx
@@ -2,36 +2,42 @@ import React from 'react';
 import { Slider } from '@mantine/core';
 
 const code = `
-<Slider
-  defaultValue={40}
-  marks={marks}
-  labelTransition="fade"
-  size={2}
-  styles={(theme) =>({
-    track: {
-      backgroundColor:
-        theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.blue[1],
-    },
-    mark: {
-      width: 6,
-      height: 6,
-      borderRadius: 6,
-      transform: 'translateX(-3px) translateY(-2px)',
-      borderColor: theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.blue[1],
-    },
-    markFilled: {
-      borderColor: theme.colors.blue[6],
-    },
-    markLabel: { fontSize: theme.fontSizes.xs, marginBottom: 5, marginTop: 0 },
-    thumb: {
-      height: 16,
-      width: 16,
-      backgroundColor: theme.white,
-      borderWidth: 1,
-      boxShadow: theme.shadows.sm,
-    },
-  })}
-/>
+import { Slider } from '@mantine/core';
+
+function Demo() {
+  return (
+    <Slider
+      defaultValue={40}
+      marks={marks}
+      labelTransition="fade"
+      size={2}
+      styles={(theme) =>({
+        track: {
+          backgroundColor:
+            theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.blue[1],
+        },
+        mark: {
+          width: 6,
+          height: 6,
+          borderRadius: 6,
+          transform: 'translateX(-3px) translateY(-2px)',
+          borderColor: theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.blue[1],
+        },
+        markFilled: {
+          borderColor: theme.colors.blue[6],
+        },
+        markLabel: { fontSize: theme.fontSizes.xs, marginBottom: 5, marginTop: 0 },
+        thumb: {
+          height: 16,
+          width: 16,
+          backgroundColor: theme.white,
+          borderWidth: 1,
+          boxShadow: theme.shadows.sm,
+        },
+      })}
+    />
+  );
+}
 `;
 
 const marks = [
