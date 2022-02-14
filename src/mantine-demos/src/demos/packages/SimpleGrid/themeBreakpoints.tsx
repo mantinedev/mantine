c5a940ab85c60c5677bc009f3e613267b3323f8e
Vitaly Rtishchev <rtivital@gmail.com>
2022-02-14 13:38:07 +0300

    [@mantine/demos] Cleanup SimpleGrid demos code

--- src/mantine-demos/src/demos/packages/SimpleGrid/themeBreakpoints.tsx
+++ src/mantine-demos/src/demos/packages/SimpleGrid/themeBreakpoints.tsx
@@ -3,21 +3,27 @@ import { SimpleGrid } from '@mantine/core';
 import { defaultItems } from './_Item';
 
 const code = `
-<SimpleGrid
-  cols={4}
-  spacing="lg"
-  breakpoints={[
-    { maxWidth: 'md', cols: 3, spacing: 'md' },
-    { maxWidth: 'sm', cols: 2, spacing: 'sm' },
-    { maxWidth: 'xs', cols: 1, spacing: 'sm' },
-  ]}
->
-  <div>1</div>
-  <div>2</div>
-  <div>3</div>
-  <div>4</div>
-  <div>5</div>
-</SimpleGrid>
+import { SimpleGrid } from '@mantine/core';
+
+function Demo() {
+  return (
+    <SimpleGrid
+      cols={4}
+      spacing="lg"
+      breakpoints={[
+        { maxWidth: 'md', cols: 3, spacing: 'md' },
+        { maxWidth: 'sm', cols: 2, spacing: 'sm' },
+        { maxWidth: 'xs', cols: 1, spacing: 'sm' },
+      ]}
+    >
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+      <div>5</div>
+    </SimpleGrid>
+  )
+}
 `;
 
 function Demo() {
