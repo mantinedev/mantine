a7fe3c7ccd80656f9b93a50569054fc334b985d1
Vitaly Rtishchev <rtivital@gmail.com>
2022-02-15 20:35:02 +0300

    [@mantine/demos] Cleanup Highlight component demos code

--- src/mantine-demos/src/demos/packages/Highlight/styles.tsx
+++ src/mantine-demos/src/demos/packages/Highlight/styles.tsx
@@ -2,18 +2,24 @@ import React from 'react';
 import { Highlight } from '@mantine/core';
 
 const code = `
-<Highlight
-  align="center"
-  highlight={['highlighted', 'default']}
-  highlightStyles={(theme) => ({
-    backgroundImage: theme.fn.linearGradient(45, theme.colors.cyan[5], theme.colors.indigo[5]),
-    fontWeight: 700,
-    WebkitBackgroundClip: 'text',
-    WebkitTextFillColor: 'transparent',
-  })}
->
-  You can change styles of highlighted part if you do not like default styles
-</Highlight>
+import { Highlight } from '@mantine/core';
+
+function Demo() {
+  return (
+    <Highlight
+      align="center"
+      highlight={['highlighted', 'default']}
+      highlightStyles={(theme) => ({
+        backgroundImage: theme.fn.linearGradient(45, theme.colors.cyan[5], theme.colors.indigo[5]),
+        fontWeight: 700,
+        WebkitBackgroundClip: 'text',
+        WebkitTextFillColor: 'transparent',
+      })}
+    >
+      You can change styles of highlighted part if you do not like default styles
+    </Highlight>
+  );
+}
 `;
 
 function Demo() {
