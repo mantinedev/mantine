66b3e66fe89de9edc069427083a1b5807688cbe5
Vitaly Rtishchev <rtivital@gmail.com>
2022-02-15 21:06:31 +0300

    [@mantine/demos] Cleanup RingProgress demos code

--- src/mantine-demos/src/demos/packages/RingProgress/label.tsx
+++ src/mantine-demos/src/demos/packages/RingProgress/label.tsx
@@ -1,27 +1,36 @@
 import React from 'react';
-import { CheckIcon } from '@modulz/radix-icons';
+import { Check } from 'tabler-icons-react';
 import { ThemeIcon, RingProgress, Group, Text, Center } from '@mantine/core';
 
 const code = `
-<RingProgress
-  sections={[{ value: 40, color: 'blue' }]}
-  label={
-    <Text color="blue" weight={700} align="center" size="xl">
-      40%
-    </Text>
-  }
-/>
+import { ThemeIcon, RingProgress, Text, Center } from '@mantine/core';
+import { Check } from 'tabler-icons-react';
 
-<RingProgress
-  sections={[{ value: 100, color: 'teal' }]}
-  label={
-    <Center>
-      <ThemeIcon color="teal" variant="light" radius="xl size="xl">
-        <CheckIcon style={{ height: 22, width: 22 }} />
-      </ThemeIcon>
-    </Center>
-  }
-/>
+function Demo() {
+  return (
+    <>
+      <RingProgress
+        sections={[{ value: 40, color: 'blue' }]}
+        label={
+          <Text color="blue" weight={700} align="center" size="xl">
+            40%
+          </Text>
+        }
+      />
+
+      <RingProgress
+        sections={[{ value: 100, color: 'teal' }]}
+        label={
+          <Center>
+            <ThemeIcon color="teal" variant="light" radius="xl" size="xl">
+              <Check size={22} />
+            </ThemeIcon>
+          </Center>
+        }
+      />
+    </>
+  );
+}
 `;
 
 function Demo() {
@@ -41,7 +50,7 @@ function Demo() {
         label={
           <Center>
             <ThemeIcon color="teal" variant="light" radius="xl" size="xl">
-              <CheckIcon style={{ height: 22, width: 22 }} />
+              <Check size={22} />
             </ThemeIcon>
           </Center>
         }
