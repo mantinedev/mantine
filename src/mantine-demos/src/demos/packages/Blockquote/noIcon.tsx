a608b31f7cb37032ed7e051970087f6ee36bcfc8
Vitaly Rtishchev <rtivital@gmail.com>
2022-02-15 20:28:56 +0300

    [@mantine/demos] Cleanup Blockquote demos code

--- src/mantine-demos/src/demos/packages/Blockquote/noIcon.tsx
+++ src/mantine-demos/src/demos/packages/Blockquote/noIcon.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 import { Blockquote } from '@mantine/core';
 
 const code = `
-<Blockquote cite="– Your poor i3 from 2012 struggling its best" icon={null}>
-  webpack built 40a9bf3d1a0a0914ed84 in 28259ms
-</Blockquote>
+import { Blockquote } from '@mantine/core';
+
+function Demo() {
+  return (
+    <Blockquote cite="– Your poor i3 from 2012 struggling its best" icon={null}>
+      webpack built 40a9bf3d1a0a0914ed84 in 28259ms
+    </Blockquote>
+  )
+}
 `;
 
 function Demo() {
