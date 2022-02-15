696c348d70cba9598552264a84e7205d0bee475b
Vitaly Rtishchev <rtivital@gmail.com>
2022-02-15 13:57:02 +0300

    [@mantine/demos] Cleanup Pagination demos code

--- src/mantine-demos/src/demos/packages/Pagination/boundaries.tsx
+++ src/mantine-demos/src/demos/packages/Pagination/boundaries.tsx
@@ -2,19 +2,42 @@ import React from 'react';
 import { Text, Pagination } from '@mantine/core';
 
 const code = `
-  <Pagination total={20} boundaries={3} />
+import { Text, Pagination } from '@mantine/core';
+
+function Demo() {
+  return (
+    <>
+      <Text mb={10}>1 boundary (default)</Text>
+      <Pagination total={20} boundaries={1} initialPage={10} />
+
+      <Text mt={30} mb={10} style={{ marginTop: 30, marginBottom: 10 }}>
+        2 boundaries
+      </Text>
+      <Pagination total={20} boundaries={2} initialPage={10} />
+
+      <Text mt={30} mb={10}>
+        3 boundaries
+      </Text>
+      <Pagination total={20} boundaries={3} initialPage={10} />
+    </>
+  );
+}
 `;
 
 function Demo() {
   return (
     <>
-      <Text style={{ marginBottom: 10 }}>1 boundary (default)</Text>
+      <Text mb={10}>1 boundary (default)</Text>
       <Pagination total={20} boundaries={1} initialPage={10} />
 
-      <Text style={{ marginTop: 30, marginBottom: 10 }}>2 boundaries</Text>
+      <Text mt={30} mb={10} style={{ marginTop: 30, marginBottom: 10 }}>
+        2 boundaries
+      </Text>
       <Pagination total={20} boundaries={2} initialPage={10} />
 
-      <Text style={{ marginTop: 30, marginBottom: 10 }}>3 boundaries</Text>
+      <Text mt={30} mb={10}>
+        3 boundaries
+      </Text>
       <Pagination total={20} boundaries={3} initialPage={10} />
     </>
   );
