3dbc16b863604fc79e1488e25449ab4a157e81e9
Vitaly Rtishchev <rtivital@gmail.com>
2022-02-15 16:56:19 +0300

    [@mantine/demos] Remove useless sizes and radius ThemeIcon demos

--- src/mantine-demos/src/demos/packages/ThemeIcon/index.ts
+++ src/mantine-demos/src/demos/packages/ThemeIcon/index.ts
@@ -1,5 +1,3 @@
 export { configurator } from './configurator';
 export { colors } from './colors';
-export { sizes } from './sizes';
-export { radius } from './radius';
 export { gradient } from './gradient';
