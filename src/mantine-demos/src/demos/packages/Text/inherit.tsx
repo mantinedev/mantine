9dadce694b991d5a931c3191a376ad29fd36c710
Vitaly Rtishchev <rtivital@gmail.com>
2022-02-15 20:50:22 +0300

    [@mantine/demos] Cleanup Text component demos code

--- src/mantine-demos/src/demos/packages/Text/inherit.tsx
+++ src/mantine-demos/src/demos/packages/Text/inherit.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { Text, Title } from '@mantine/core';
 
 const code = `
-<Title order={3}>Highlight <Text color="blue" inherit component="span">something</Text>in title</Title>
+import { Text, Title } from '@mantine/core';
+
+function Demo() {
+  return <Title order={3}>Highlight <Text color="blue" inherit component="span">something</Text>in title</Title>;
+}
 `;
 
 function Demo() {
