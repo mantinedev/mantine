83b4762bc7de7efcc954cf7c19e89f5c07cbbbad
Vitaly Rtishchev <rtivital@gmail.com>
2022-02-15 17:15:07 +0300

    [@mantine/demos] Remove useless Drawer demos

--- src/mantine-demos/src/demos/packages/Drawer/index.ts
+++ src/mantine-demos/src/demos/packages/Drawer/index.ts
@@ -1,6 +1,4 @@
 export { usage } from './usage';
 export { positions } from './positions';
 export { sizes } from './sizes';
-export { behavior } from './behavior';
-export { configurator } from './configurator';
 export { transitions } from './transitions';
