63a30ed1e4b1f947c029f1ecbea2cfeac100df41
Vitaly Rtishchev <rtivital@gmail.com>
2022-02-16 18:11:32 +0300

    [@mantine/demos] Cleanup RangeCalendar component demos code

--- src/mantine-demos/src/demos/packages/RangeCalendar/usage.tsx
+++ src/mantine-demos/src/demos/packages/RangeCalendar/usage.tsx
@@ -7,13 +7,20 @@ import { useState } from 'react';
 import { RangeCalendar } from '@mantine/dates';
 
 function Demo() {
-  const [value, setValue] = useState<[Date, Date]>([new Date(2021, 11, 1), new Date(2021, 11, 5)]);
+  const [value, setValue] = useState<[Date | null, Date | null]>([
+    new Date(2021, 11, 1),
+    new Date(2021, 11, 5),
+  ]);
+
   return <RangeCalendar value={value} onChange={setValue} />;
 }
 `;
 
 function Demo() {
-  const [value, setValue] = useState<[Date, Date]>([new Date(2021, 11, 1), new Date(2021, 11, 5)]);
+  const [value, setValue] = useState<[Date | null, Date | null]>([
+    new Date(2021, 11, 1),
+    new Date(2021, 11, 5),
+  ]);
   return (
     <Group position="center">
       <RangeCalendar value={value} onChange={setValue} />
