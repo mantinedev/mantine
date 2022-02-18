461dc0bd15688cfaacfd38874bf78389007a9899
Vitaly Rtishchev <rtivital@gmail.com>
2022-02-18 16:09:41 +0300

    [docs] Add rtl toggle logic

--- docs/src/components/Layout/Layout.tsx
+++ docs/src/components/Layout/Layout.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MantineProvider, ColorSchemeProvider, ColorScheme, Global } from '@mantine/core';
 import { useHotkeys, useLocalStorageValue } from '@mantine/hooks';
+import rtlPlugin from 'stylis-plugin-rtl';
 import { LayoutInner, LayoutProps } from './LayoutInner';
 import '../../fonts/GreycliffCF/styles.css';
 
 const THEME_KEY = 'mantine-color-scheme';
 
 export default function Layout({ children, location }: LayoutProps) {
+  const [dir, setDir] = useState<'ltr' | 'rtl'>('ltr');
   const [colorScheme, setColorScheme] = useLocalStorageValue<ColorScheme>({
     key: THEME_KEY,
     defaultValue: 'light',
@@ -15,7 +17,12 @@ export default function Layout({ children, location }: LayoutProps) {
   const toggleColorScheme = (value?: ColorScheme) =>
     setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
 
-  useHotkeys([['mod+J', () => toggleColorScheme()]]);
+  const toggleDir = () => setDir((current) => (current === 'rtl' ? 'ltr' : 'rtl'));
+
+  useHotkeys([
+    ['mod+J', () => toggleColorScheme()],
+    ['mod+L', () => toggleDir()],
+  ]);
 
   return (
     <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
@@ -23,9 +30,13 @@ export default function Layout({ children, location }: LayoutProps) {
         withGlobalStyles
         withNormalizeCSS
         theme={{
+          dir,
           colorScheme,
           headings: { fontFamily: 'Greycliff CF, sans serif' },
         }}
+        emotionOptions={
+          dir === 'rtl' ? { key: 'mantine-rtl', stylisPlugins: [rtlPlugin] } : { key: 'mantine' }
+        }
       >
         <Global
           styles={(theme) => ({
@@ -35,7 +46,9 @@ export default function Layout({ children, location }: LayoutProps) {
             },
           })}
         />
-        <LayoutInner location={location}>{children}</LayoutInner>
+        <div dir={dir}>
+          <LayoutInner location={location}>{children}</LayoutInner>
+        </div>
       </MantineProvider>
     </ColorSchemeProvider>
   );
