8263f2dc48c309115be6f85806b05ab9440f7a2b
Vitaly Rtishchev <rtivital@gmail.com>
2022-02-20 20:02:34 +0300

    [@mantine/core] Box: Add padding props extraction logic

--- src/mantine-core/src/components/Box/use-sx/get-margins/get-system-styles.ts
+++ src/mantine-core/src/components/Box/use-sx/get-margins/get-system-styles.ts
@@ -15,11 +15,11 @@ const SYSTEM_PROPS = {
 
 const NEGATIVE_VALUES = ['-xs', '-sm', '-md', '-lg', '-xl'];
 
-function isValidMargin(margin: any) {
+function isValidSizeValue(margin: any) {
   return typeof margin === 'string' || typeof margin === 'number';
 }
 
-function getMarginValue(margin: any, theme: MantineTheme) {
+function getSizeValue(margin: any, theme: MantineTheme) {
   if (NEGATIVE_VALUES.includes(margin)) {
     return theme.fn.size({ size: margin.replace('-', ''), sizes: theme.spacing }) * -1;
   }
@@ -27,25 +27,37 @@ function getMarginValue(margin: any, theme: MantineTheme) {
   return theme.fn.size({ size: margin, sizes: theme.spacing });
 }
 
-export function getSystemStyles(margins: MantineStyleSystemProps, theme: MantineTheme) {
+export function getSystemStyles(systemStyles: MantineStyleSystemProps, theme: MantineTheme) {
   const styles: CSSObject = {};
 
-  if (isValidMargin(margins.my)) {
-    const margin = getMarginValue(margins.my, theme);
-    styles.marginTop = margin;
-    styles.marginBottom = margin;
+  if (isValidSizeValue(systemStyles.py)) {
+    const value = getSizeValue(systemStyles.py, theme);
+    styles.paddingTop = value;
+    styles.paddingBottom = value;
   }
 
-  if (isValidMargin(margins.mx)) {
-    const margin = getMarginValue(margins.mx, theme);
-    styles.marginLeft = margin;
-    styles.marginRight = margin;
+  if (isValidSizeValue(systemStyles.px)) {
+    const value = getSizeValue(systemStyles.px, theme);
+    styles.paddingLeft = value;
+    styles.paddingRight = value;
   }
 
-  Object.keys(SYSTEM_PROPS).forEach((margin) => {
-    if (isValidMargin(margins[margin])) {
-      styles[SYSTEM_PROPS[margin]] = theme.fn.size({
-        size: getMarginValue(margins[margin], theme),
+  if (isValidSizeValue(systemStyles.my)) {
+    const value = getSizeValue(systemStyles.my, theme);
+    styles.marginTop = value;
+    styles.marginBottom = value;
+  }
+
+  if (isValidSizeValue(systemStyles.mx)) {
+    const value = getSizeValue(systemStyles.mx, theme);
+    styles.marginLeft = value;
+    styles.marginRight = value;
+  }
+
+  Object.keys(SYSTEM_PROPS).forEach((property) => {
+    if (isValidSizeValue(systemStyles[property])) {
+      styles[SYSTEM_PROPS[property]] = theme.fn.size({
+        size: getSizeValue(systemStyles[property], theme),
         sizes: theme.spacing,
       });
     }
