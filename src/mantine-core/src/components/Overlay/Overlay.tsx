f2ee8a73ac561734da16d3d9d99d0fef57b5a6dd
wes337 <wesmoses@gmail.com>
2022-02-21 19:34:07 +0200

    [@mantine/core] Overlay: add blur prop

--- src/mantine-core/src/components/Overlay/Overlay.tsx
+++ src/mantine-core/src/components/Overlay/Overlay.tsx
@@ -16,6 +16,9 @@ interface _OverlayProps extends DefaultProps {
   /** Overlay background-color */
   color?: React.CSSProperties['backgroundColor'];
 
+  /** Overlay background blur in px */
+  blur?: MantineNumberSize;
+
   /** Use gradient instead of background-color */
   gradient?: string;
 
@@ -39,35 +42,58 @@ const defaultProps: Partial<OverlayProps<any>> = {
   color: '#fff',
   zIndex: getDefaultZIndex('modal'),
   radius: 0,
+  blur: 0,
 };
 
 export const Overlay: OverlayComponent = forwardRef(
   <C extends React.ElementType = 'div'>(props: OverlayProps<C>, ref: PolymorphicRef<C>) => {
-    const { opacity, color, gradient, zIndex, component, radius, sx, ...others } =
+    const { opacity, blur, color, gradient, zIndex, component, radius, sx, ...others } =
       useMantineDefaultProps('Overlay', defaultProps, props);
     const background = gradient ? { backgroundImage: gradient } : { backgroundColor: color };
 
-    return (
+    const baseStyles = {
+      position: 'absolute' as React.CSSProperties['position'],
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      zIndex,
+    };
+
+    const OverlayBox = (
       <Box<any>
         component={component || 'div'}
         ref={ref}
         sx={[
           (theme) => ({
             ...background,
+            ...baseStyles,
             opacity,
-            position: 'absolute',
-            top: 0,
-            bottom: 0,
-            left: 0,
-            right: 0,
             borderRadius: theme.fn.size({ size: radius, sizes: theme.radius }),
-            zIndex,
           }),
           sx,
         ]}
         {...others}
       />
     );
+
+    if (blur) {
+      return (
+        <Box
+          sx={[
+            (theme) => ({
+              ...baseStyles,
+              backdropFilter: `blur(${theme.fn.size({ size: blur, sizes: theme.blur })}px)`,
+            }),
+            sx,
+          ]}
+        >
+          {OverlayBox}
+        </Box>
+      );
+    }
+
+    return OverlayBox;
   }
 );
 
