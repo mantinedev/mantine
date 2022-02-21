f87826c77d3d39ea7802c54641c461f79dbe5ae8
wes337 <wesmoses@gmail.com>
2022-02-21 19:41:20 +0200

    [@mantine-styles] Add blur to default theme

--- src/mantine-styles/src/theme/default-theme.ts
+++ src/mantine-styles/src/theme/default-theme.ts
@@ -47,6 +47,14 @@ export const _DEFAULT_THEME: MantineThemeBase = {
     xl: 32,
   },
 
+  blur: {
+    xs: 2,
+    sm: 4,
+    md: 8,
+    lg: 16,
+    xl: 32,
+  },
+
   spacing: {
     xs: 10,
     sm: 12,
