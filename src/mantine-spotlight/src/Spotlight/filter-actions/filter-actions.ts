9dfcc395ee3d8e493c592143808d3167a5780e7f
Vitaly Rtishchev <rtivital@gmail.com>
2022-02-21 13:19:07 +0300

    [@mantine/spotlight] Setup actions filtering logic

---
+++ src/mantine-spotlight/src/Spotlight/filter-actions/filter-actions.ts
@@ -0,0 +1,27 @@
+import type { SpotlightAction } from '../../types';
+
+function getKeywords(keywords: string | string[]) {
+  if (Array.isArray(keywords)) {
+    return keywords
+      .map((keyword) => keyword.trim())
+      .join(',')
+      .toLowerCase()
+      .trim();
+  }
+
+  if (typeof keywords === 'string') {
+    return keywords.toLowerCase().trim();
+  }
+
+  return '';
+}
+
+export function filterActions(_query: string, actions: SpotlightAction[]) {
+  const query = _query.trim().toLowerCase();
+  return actions.filter(
+    (action) =>
+      action.title.toLowerCase().includes(query) ||
+      action.description?.toLowerCase().includes(query) ||
+      getKeywords(action.keywords).includes(query)
+  );
+}
