c84ffb039b065e82a4590559e0ee6636d5ecce54
Vitaly Rtishchev <rtivital@gmail.com>
2022-02-23 10:08:28 +0300

    [@mantine/spotlight] Add filter demo

---
+++ src/mantine-demos/src/demos/spotlight/filter.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Search } from 'tabler-icons-react';
+import { Wrapper } from './_wrapper';
+
+const code = `
+import { SpotlightProvider } from '@mantine/spotlight';
+
+function Demo() {
+  return (
+    <SpotlightProvider
+      shortcut="mod + shift + 2"
+      transitionDuration={0}
+      nothingFoundMessage="Nothing found..."
+      filter={(query, actions) =>
+        actions.filter((action) => action.title.toLowerCase().includes(query.toLowerCase()))
+      }
+      {...otherProps}
+    >
+      <YourApp />
+    </SpotlightProvider>
+  );
+}
+`;
+
+function Demo() {
+  return (
+    <Wrapper
+      searchIcon={<Search size={18} />}
+      transitionDuration={0}
+      searchPlaceholder="Search..."
+      shortcut="mod + alt + V"
+      nothingFoundMessage="Nothing found..."
+      filter={(query, actions) =>
+        actions.filter((action) => action.title.toLowerCase().includes(query.toLowerCase()))
+      }
+      buttonLabel="Open spotlight without transition"
+    />
+  );
+}
+
+export const filter: MantineDemo = {
+  type: 'demo',
+  component: Demo,
+  code,
+};
--- src/mantine-demos/src/demos/spotlight/index.ts
+++ src/mantine-demos/src/demos/spotlight/index.ts
@@ -7,3 +7,4 @@ export { register } from './register';
 export { groups } from './groups';
 export { actionComponent } from './actionComponent';
 export { actionsWrapperComponent } from './actionsWrapperComponent';
+export { filter } from './filter';
