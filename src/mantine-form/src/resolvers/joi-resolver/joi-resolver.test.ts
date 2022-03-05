9ea8a6e3a70b1e6a8e2bac58cf4283309423b6e6
Vitaly Rtishchev <rtivital@gmail.com>
2022-03-05 12:22:21 +0300

    [@mantine/form] Remove duplicated resolvers tests

--- src/mantine-form/src/resolvers/joi-resolver/joi-resolver.test.ts
+++ src/mantine-form/src/resolvers/joi-resolver/joi-resolver.test.ts
@@ -1,64 +1,20 @@
 import Joi from 'joi';
 import { joiResolver } from './joi-resolver';
-import { formList } from '../../index';
+import { testResolver, RESOLVER_ERROR_MESSAGES } from '../test-resolver';
 
-const regularSchema = Joi.object({
-  email: Joi.string().email().message('email-error'),
-  age: Joi.number().min(10).message('age-error'),
-});
-
-const listSchema = Joi.object({
-  other: Joi.string().min(2).message('other-short'),
-  fruits: Joi.array().items(
-    Joi.object({
-      fruit: Joi.string().min(2).message('short-fruit'),
-      inStock: Joi.number().min(0).message('negative-stock'),
-    })
-  ),
-});
-
-const validateRegular = joiResolver(regularSchema);
-const validateList = joiResolver(listSchema);
-
-describe('@mantine/form joi-resolver', () => {
-  it('validates regular fields', () => {
-    expect(validateRegular({ email: 'invalid', age: 5 })).toStrictEqual({
-      email: 'email-error',
-      age: 'age-error',
-    });
-  });
-
-  it('returns empty object if all fields are valid', () => {
-    expect(validateRegular({ email: 'email@domain.com', age: 10 })).toStrictEqual({});
-  });
-
-  it('validates lists', () => {
-    expect(
-      validateList({
-        other: '1',
-        fruits: formList([
-          { fruit: '2', inStock: 10 },
-          { fruit: 'orange', inStock: 10 },
-          { fruit: 'banana', inStock: -1 },
-        ]),
+describe('@mantine/form/use-form joi resolver', () => {
+  testResolver(
+    joiResolver(
+      Joi.object({
+        name: Joi.string().min(2).message(RESOLVER_ERROR_MESSAGES.name),
+        age: Joi.number().min(10).message(RESOLVER_ERROR_MESSAGES.age),
+        fruits: Joi.array().items(
+          Joi.object({
+            name: Joi.string().min(2).message(RESOLVER_ERROR_MESSAGES.fruitName),
+            stock: Joi.number().min(0).message(RESOLVER_ERROR_MESSAGES.fruitStock),
+          })
+        ),
       })
-    ).toStrictEqual({
-      other: 'other-short',
-      'fruits.0.fruit': 'short-fruit',
-      'fruits.2.inStock': 'negative-stock',
-    });
-  });
-
-  it('returns empty object when all list items are valid', () => {
-    expect(
-      validateList({
-        other: 'other',
-        fruits: formList([
-          { fruit: 'apple', inStock: 10 },
-          { fruit: 'orange', inStock: 10 },
-          { fruit: 'banana', inStock: 10 },
-        ]),
-      })
-    ).toStrictEqual({});
-  });
+    )
+  );
 });
--- src/mantine-form/src/resolvers/zod-resolver/zod-resolver.test.ts
+++ src/mantine-form/src/resolvers/zod-resolver/zod-resolver.test.ts
@@ -1,64 +1,20 @@
 import { z } from 'zod';
 import { zodResolver } from './zod-resolver';
-import { formList } from '../../index';
+import { testResolver, RESOLVER_ERROR_MESSAGES } from '../test-resolver';
 
-const regularSchema = z.object({
-  email: z.string().email({ message: 'email-error' }),
-  age: z.number().min(10, { message: 'age-error' }),
-});
-
-const listSchema = z.object({
-  other: z.string().min(1, { message: 'other-short' }),
-  fruits: z.array(
-    z.object({
-      fruit: z.string().min(1, { message: 'short-fruit' }),
-      inStock: z.number().min(0, { message: 'negative-stock' }),
-    })
-  ),
-});
-
-const validateRegular = zodResolver(regularSchema);
-const validateList = zodResolver(listSchema);
-
-describe('@mantine/form zod-resolver', () => {
-  it('validates regular fields', () => {
-    expect(validateRegular({ email: 'invalid', age: 5 })).toStrictEqual({
-      email: 'email-error',
-      age: 'age-error',
-    });
-  });
-
-  it('returns empty object if all fields are valid', () => {
-    expect(validateRegular({ email: 'email@domain.com', age: 10 })).toStrictEqual({});
-  });
-
-  it('validates lists', () => {
-    expect(
-      validateList({
-        other: '',
-        fruits: formList([
-          { fruit: '', inStock: 10 },
-          { fruit: 'orange', inStock: 10 },
-          { fruit: 'banana', inStock: -1 },
-        ]),
+describe('@mantine/form zod resolver', () => {
+  testResolver(
+    zodResolver(
+      z.object({
+        name: z.string().min(2, { message: RESOLVER_ERROR_MESSAGES.name }),
+        age: z.number().min(10, { message: RESOLVER_ERROR_MESSAGES.age }),
+        fruits: z.array(
+          z.object({
+            name: z.string().min(2, { message: RESOLVER_ERROR_MESSAGES.fruitName }),
+            stock: z.number().min(0, { message: RESOLVER_ERROR_MESSAGES.fruitStock }),
+          })
+        ),
       })
-    ).toStrictEqual({
-      other: 'other-short',
-      'fruits.0.fruit': 'short-fruit',
-      'fruits.2.inStock': 'negative-stock',
-    });
-  });
-
-  it('returns empty object when all list items are valid', () => {
-    expect(
-      validateList({
-        other: 'other',
-        fruits: formList([
-          { fruit: 'apple', inStock: 10 },
-          { fruit: 'orange', inStock: 10 },
-          { fruit: 'banana', inStock: 10 },
-        ]),
-      })
-    ).toStrictEqual({});
-  });
+    )
+  );
 });
