ea2b4a747507eeb0b46d5108917f743b2117588a
Vitaly Rtishchev <rtivital@gmail.com>
2022-03-05 14:46:40 +0300

    [@mantine/form] Init usage demo

--- src/mantine-demos/src/Demos.story.tsx
+++ src/mantine-demos/src/Demos.story.tsx
@@ -101,6 +101,7 @@ attachDemos(storiesOf('@mantine/rte/demos', module), demos.RichTextEditorDemos);
 attachDemos(storiesOf('@mantine/styles/demos', module), demos.CreateStylesDemos);
 attachDemos(storiesOf('@mantine/theme/demos', module), demos.ThemeDemos);
 attachDemos(storiesOf('@mantine/spotlight/demos', module), demos.SpotlightDemos);
+attachDemos(storiesOf('@mantine/form/demos', module), demos.FormDemos);
 
 // @mantine/labs
 attachDemos(storiesOf('@mantine/labs/PinInput/demos', module), demos.PinInputDemos);
---
+++ src/mantine-demos/src/demos/form/usage.tsx
@@ -0,0 +1,87 @@
+/* eslint-disable no-console */
+import React from 'react';
+import { TextInput, Checkbox, Button, Group, Box } from '@mantine/core';
+import { useForm } from '@mantine/form';
+
+const code = `
+import { TextInput, Checkbox, Button, Group, Box } from '@mantine/core';
+import { useForm } from '@mantine/form';
+
+function Demo() {
+  const form = useForm({
+    initialValues: {
+      email: '',
+      termsOfService: false,
+    },
+
+    validate: {
+      email: (value) => (/^\\S+@\\S+$/.test(value) ? null : true),
+    },
+  });
+
+  return (
+    <Box sx={{ maxWidth: 300 }} mx="auto">
+      <form onSubmit={form.onSubmit((values) => console.log(values))}>
+        <TextInput
+          required
+          label="Email"
+          placeholder="your@email.com"
+          {...form.getInputProps('email')}
+        />
+
+        <Checkbox
+          mt="md"
+          label="I agree to sell my privacy"
+          {...form.getInputProps('termsOfService', { type: 'checkbox' })}
+        />
+
+        <Group position="right" mt="md">
+          <Button type="submit">Submit</Button>
+        </Group>
+      </form>
+    </Box>
+  );
+}
+`;
+
+function Demo() {
+  const form = useForm({
+    initialValues: {
+      email: '',
+      termsOfService: false,
+    },
+
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : true),
+    },
+  });
+
+  return (
+    <Box sx={{ maxWidth: 300 }} mx="auto">
+      <form onSubmit={form.onSubmit((values) => console.log(values))}>
+        <TextInput
+          required
+          label="Email"
+          placeholder="your@email.com"
+          {...form.getInputProps('email')}
+        />
+
+        <Checkbox
+          mt="md"
+          label="I agree to sell my privacy"
+          {...form.getInputProps('termsOfService', { type: 'checkbox' })}
+        />
+
+        <Group position="right" mt="md">
+          <Button type="submit">Submit</Button>
+        </Group>
+      </form>
+    </Box>
+  );
+}
+
+export const usage: MantineDemo = {
+  type: 'demo',
+  component: Demo,
+  code,
+};
