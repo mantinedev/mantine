d2524dda4854e7fceb0c2641522bc38336c55cfb
Vitaly Rtishchev <rtivital@gmail.com>
2022-03-07 22:06:13 +0300

    [@mantine/form] Add joi and yup demos

--- src/mantine-demos/src/demos/form/index.ts
+++ src/mantine-demos/src/demos/form/index.ts
@@ -4,3 +4,5 @@ export { dnd } from './dnd';
 export { zod } from './zod';
 export { password } from './password';
 export { validateFunction } from './validateFunction';
+export { joi } from './joi';
+export { yup } from './yup';
