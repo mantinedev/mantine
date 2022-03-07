df4951761cce8bf68f32fe66eb4b86e932ff57fc
Vitaly Rtishchev <rtivital@gmail.com>
2022-03-07 18:26:18 +0300

    [@mantine/form] Add validation demos

--- src/mantine-demos/src/demos/form/index.ts
+++ src/mantine-demos/src/demos/form/index.ts
@@ -2,3 +2,5 @@ export { usage } from './usage';
 export { lists } from './lists';
 export { dnd } from './dnd';
 export { zod } from './zod';
+export { password } from './password';
+export { validateFunction } from './validateFunction';
