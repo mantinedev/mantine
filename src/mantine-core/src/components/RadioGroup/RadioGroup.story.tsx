6ee11ed00b0fe6ac1a6c38cc18c06612fbdfe6b6
wes337 <wesmoses@gmail.com>
2022-03-10 08:21:57 +0200

    [@mantine/core] RadioGroup: add story for multiline radio labels

--- src/mantine-core/src/components/RadioGroup/RadioGroup.story.tsx
+++ src/mantine-core/src/components/RadioGroup/RadioGroup.story.tsx
@@ -29,6 +29,43 @@ function Controlled() {
   );
 }
 
+function MultiLine() {
+  const [value, setValue] = useState(null);
+  return (
+    <RadioGroup value={value} onChange={setValue}>
+      <Radio value="1">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Tincidunt dui ut ornare lectus. Hac habitasse platea dictumst
+        vestibulum rhoncus est pellentesque. Condimentum mattis pellentesque id nibh. Parturient
+        montes nascetur ridiculus mus mauris vitae ultricies leo. Enim neque volutpat ac tincidunt
+        vitae semper. Sollicitudin tempor id eu nisl. Tempus quam pellentesque nec nam aliquam sem
+        et tortor consequat. Diam sollicitudin tempor id eu. Vitae semper quis lectus nulla at. Sit
+        amet nulla facilisi morbi tempus iaculis urna id. Imperdiet sed euismod nisi porta lorem
+        mollis aliquam ut.
+      </Radio>
+      <Radio value="2">
+        Orci porta non pulvinar neque laoreet suspendisse interdum consectetur libero. Quam
+        vulputate dignissim suspendisse in est ante in nibh mauris. Vulputate sapien nec sagittis
+        aliquam malesuada bibendum arcu vitae. Varius quam quisque id diam vel quam elementum.
+        Ultricies leo integer malesuada nunc vel risus commodo viverra. Arcu odio ut sem nulla
+        pharetra diam sit. Eu volutpat odio facilisis mauris sit amet massa vitae tortor. Fermentum
+        iaculis eu non diam phasellus vestibulum lorem sed risus. Pulvinar elementum integer enim
+        neque. Platea dictumst quisque sagittis purus sit amet volutpat consequat mauris.
+      </Radio>
+      <Radio value="3">
+        Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin. Tellus mauris a diam
+        maecenas sed enim. Pharetra et ultrices neque ornare aenean. Cras pulvinar mattis nunc sed
+        blandit libero volutpat sed cras. Felis eget nunc lobortis mattis aliquam. Ultricies integer
+        quis auctor elit sed vulputate. Fusce ut placerat orci nulla pellentesque dignissim enim sit
+        amet. Aliquet risus feugiat in ante metus dictum at tempor commodo. Tortor pretium viverra
+        suspendisse potenti nullam ac. Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus
+        at.
+      </Radio>
+      <Radio value="4">Imperdiet sed euismod nisi porta</Radio>
+    </RadioGroup>
+  );
+}
+
 storiesOf('@mantine/core/RadioGroup/stories', module)
   .add('Too many items in horizontal', () => (
     <RadioGroup
@@ -44,4 +81,5 @@ storiesOf('@mantine/core/RadioGroup/stories', module)
     </RadioGroup>
   ))
   .add('Sizes', () => <>{sizes}</>)
-  .add('Controlled', () => <Controlled />);
+  .add('Controlled', () => <Controlled />)
+  .add('Multiple lines', () => <MultiLine />);
