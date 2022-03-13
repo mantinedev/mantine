a758d88f83037af30a45a36682d195b3b62bfc5b
Vitaly Rtishchev <rtivital@gmail.com>
2022-03-13 14:38:02 +0300

    [@mantine/hooks] use-local-storage: Rename hook files and docs

--- src/mantine-hooks/src/use-local-storage/use-local-storage.story.tsx
+++ src/mantine-hooks/src/use-local-storage/use-local-storage.story.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useLocalStorage } from './use-local-storage';
 
 export default {
-  title: '@mantine/hooks/use-local-storage-value',
+  title: '@mantine/hooks/use-local-storage',
 };
 
 export function Usage() {
@@ -14,6 +14,22 @@ export function Usage() {
   return (
     <div style={{ padding: 20 }}>
       <input value={value} onChange={(e) => setValue(e.target.value)} />
+    </div>
+  );
+}
+
+export function SerializeJson() {
+  const [value, setValue] = useLocalStorage<{ mantine: string }>({
+    key: '@mantine/localStorage/val',
+    defaultValue: { mantine: 'is awesome' },
+  });
+
+  return (
+    <div style={{ padding: 20 }}>
+      <input
+        value={value.mantine}
+        onChange={(event) => setValue({ mantine: event.target.value })}
+      />
     </div>
   );
 }
