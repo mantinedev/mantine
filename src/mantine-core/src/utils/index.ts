b3f16de5d23dedfd06157fe81578038a6cda6cfb
Vitaly Rtishchev <rtivital@gmail.com>
2022-03-18 20:15:22 +0300

    [@mantine/core] Init create use context util

--- src/mantine-core/src/utils/index.ts
+++ src/mantine-core/src/utils/index.ts
@@ -3,3 +3,4 @@ export { findChildByType } from './find-child-by-type/find-child-by-type';
 export { filterFalsyChildren } from './filter-falsy-children/filter-falsy-children';
 export { groupOptions, getGroupedOptions } from './group-options/group-options';
 export { getContextItemIndex } from './get-context-item-index/get-context-item-index';
+export { createUseContext } from './create-use-context/create-use-context';
