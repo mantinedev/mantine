856f5e6da03ec2b3aebd1b49eb2b2b7277619930
jimmyruann <jimmyruan@hotmail.com>
2022-03-18 11:04:36 +1100

    [@mantine/hooks] use-set-state: add reusable return type

--- src/mantine-hooks/src/use-set-state/use-set-state.ts
+++ src/mantine-hooks/src/use-set-state/use-set-state.ts
@@ -1,11 +1,16 @@
 import { useState } from 'react';
 
-export function useSetState<T extends Record<string, any>>(initialState: T) {
+export type UseSetState<T> = readonly [
+  T,
+  (statePartial: Partial<T> | ((currentState: T) => Partial<T>)) => void
+];
+
+export function useSetState<T extends Record<string, any>>(initialState: T): UseSetState<T> {
   const [state, _setState] = useState(initialState);
   const setState = (statePartial: Partial<T> | ((currentState: T) => Partial<T>)) =>
     _setState((current) => ({
       ...current,
       ...(typeof statePartial === 'function' ? statePartial(current) : statePartial),
     }));
-  return [state, setState] as const;
+  return [state, setState];
 }
