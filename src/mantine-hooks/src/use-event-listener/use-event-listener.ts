dd0095f7c01ace0d655ea7d678ec6174d63d1854
jimmyruann <12026187+jimmyruann@users.noreply.github.com>
2022-03-19 22:43:58 +1100

    [@mantine/hooks] use-event-listener: remove inline type definition

--- src/mantine-hooks/src/use-event-listener/use-event-listener.ts
+++ src/mantine-hooks/src/use-event-listener/use-event-listener.ts
@@ -1,10 +1,12 @@
-import { useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
+
+export type UseEventListener<T> = React.MutableRefObject<T>;
 
 export function useEventListener<K extends keyof HTMLElementEventMap, T extends HTMLElement = any>(
   type: K,
   listener: (this: HTMLDivElement, ev: HTMLElementEventMap[K]) => any,
   options?: boolean | AddEventListenerOptions
-) {
+): UseEventListener<T> {
   const ref = useRef<T>();
 
   useEffect(() => {
