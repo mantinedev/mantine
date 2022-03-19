8b7e4811a0fe50cade0f78e09e85d4aeae9a8a6c
Vitaly Rtishchev <rtivital@gmail.com>
2022-03-19 11:17:50 +0300

    [@mantine/tests] Add itSupportsInputWrapperProps shared test

--- src/mantine-tests/src/is-supports-input-props.tsx
+++ src/mantine-tests/src/is-supports-input-props.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
-import { itSupportsWrapperProps } from './it-supports-wrapper-props';
 import { itConnectsLabelAndInput } from './it-connects-label-and-input';
 import { itSupportsInputIcon } from './it-supports-input-icon';
 import { itSupportsInputRightSection } from './it-supports-input-right-section';
+import { itSupportsInputWrapperProps } from './it-supports-input-wrapper-props';
 import { renderWithAct } from './render-with-act';
 
 export function itSupportsInputProps<P>(
@@ -11,18 +10,10 @@ export function itSupportsInputProps<P>(
   requiredProps: P,
   name: string
 ) {
-  itSupportsWrapperProps(Component, requiredProps);
+  itSupportsInputWrapperProps(Component, requiredProps, name);
   itConnectsLabelAndInput(Component, requiredProps);
   itSupportsInputIcon(Component, requiredProps);
   itSupportsInputRightSection(Component, requiredProps);
-
-  it('handles required attribute correctly', async () => {
-    const { container } = await renderWithAct(
-      <Component {...requiredProps} required id="secret-test-id" label="Test label" />
-    );
-    expect(container.querySelector(`.mantine-${name}-required`)).toBeInTheDocument();
-    expect(container.querySelector('#secret-test-id')).toHaveAttribute('required');
-  });
 
   it('handles error and invalid state', async () => {
     const { container: invalid } = await renderWithAct(
@@ -39,54 +30,10 @@ export function itSupportsInputProps<P>(
     expect(withError.querySelector(`.mantine-${name}-error`).textContent).toBe('Test error');
   });
 
-  it('renders input description', async () => {
-    const { getByText } = await renderWithAct(
-      <Component {...requiredProps} description="Test description" />
-    );
-    expect(getByText('Test description')).toBeInTheDocument();
-  });
-
   it('sets border-radius on input', async () => {
     const { container } = await renderWithAct(
       <Component {...requiredProps} radius={43} id="secret-test-id" />
     );
     expect(container.querySelector('#secret-test-id')).toHaveStyle({ borderRadius: '43px' });
-  });
-
-  it('supports labelProps', async () => {
-    await renderWithAct(
-      <Component {...requiredProps} label="Test label" labelProps={{ 'data-test': 'test-data' }} />
-    );
-
-    const label = screen.getByText('Test label');
-    expect(label).toHaveAttribute('data-test', 'test-data');
-  });
-
-  it('supports errorProps', async () => {
-    await renderWithAct(
-      <Component
-        {...requiredProps}
-        error="Test error"
-        errorProps={{ id: 'test-error', 'data-test': 'test-data' }}
-      />
-    );
-
-    const error = screen.getByText('Test error');
-    expect(error).toHaveAttribute('id', 'test-error');
-    expect(error).toHaveAttribute('data-test', 'test-data');
-  });
-
-  it('supports descriptionProps', async () => {
-    await renderWithAct(
-      <Component
-        {...requiredProps}
-        description="Test description"
-        descriptionProps={{ id: 'test-description', 'data-test': 'test-data' }}
-      />
-    );
-
-    const description = screen.getByText('Test description');
-    expect(description).toHaveAttribute('id', 'test-description');
-    expect(description).toHaveAttribute('data-test', 'test-data');
   });
 }
