03ac3289a8144f72b87fb8afcea888e97ede3636
Vitaly Rtishchev <rtivital@gmail.com>
2022-04-01 16:08:53 +0300

    [@mantine/core] Button: Fix incorrect UnstyledButton type

--- src/mantine-core/src/components/Button/UnstyledButton/UnstyledButton.tsx
+++ src/mantine-core/src/components/Button/UnstyledButton/UnstyledButton.tsx
@@ -15,13 +15,13 @@ type UnstyledButtonComponent = (<C = 'button'>(
 
 export const UnstyledButton: UnstyledButtonComponent = forwardRef(
   <C extends React.ElementType = 'button'>(
-    { className, component, ...others }: UnstyledButtonProps<C>,
+    { className, component = 'button', ...others }: UnstyledButtonProps<C>,
     ref: PolymorphicRef<C>
   ) => {
     const { classes, cx } = useStyles(null, { name: 'UnstyledButton' });
     return (
       <Box<any>
-        component={component || 'button'}
+        component={component}
         ref={ref}
         className={cx(classes.root, className)}
         type={component === 'button' ? 'button' : undefined}
