d41e1de4494ca14861c0ec95728ea5b36f492137
Vitaly Rtishchev <rtivital@gmail.com>
2022-04-02 13:04:31 +0300

    [@mantine/notifications] Fix get-auto-close function tests

--- src/mantine-notifications/src/NotificationContainer/get-auto-close/get-auto-close.test.ts
+++ src/mantine-notifications/src/NotificationContainer/get-auto-close/get-auto-close.test.ts
@@ -2,13 +2,13 @@ import getAutoClose from './get-auto-close';
 
 describe('@mantine/notifications/get-auto-close', () => {
   it('prioritizes notification autoClose prop', () => {
-    expect(getAutoClose(false, { id: 'test', message: 'test', autoClose: 10 })).toBe(10);
-    expect(getAutoClose(400, { id: 'test', message: 'test', autoClose: 10 })).toBe(10);
-    expect(getAutoClose(400, { id: 'test', message: 'test', autoClose: false })).toBe(false);
+    expect(getAutoClose(false, 10)).toBe(10);
+    expect(getAutoClose(400, 10)).toBe(10);
+    expect(getAutoClose(400, false)).toBe(false);
   });
 
   it('returns provider value if notification does not have autoClose prop', () => {
-    expect(getAutoClose(false, { id: 'test', message: 'test' })).toBe(false);
-    expect(getAutoClose(400, { id: 'test', message: 'test' })).toBe(400);
+    expect(getAutoClose(false, undefined)).toBe(false);
+    expect(getAutoClose(400, undefined)).toBe(400);
   });
 });
