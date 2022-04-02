bc8e7f4e8d997d696e61f1925c4b089387c60193
Trevor Carlston <trevor.carlston@gmail.com>
2022-04-02 04:14:28 -0600

    [@mantine/dates] Calendar: Added render day and weekday labels (#1130)
    * Support to render calendar events and set format of weekday headers
    
    * Added unit tests to verify renderEvent and weekdayLabels
    
    * Add comments and update call to renderDay
    
    * Add renderDay to DatePicker and DateRangePicker
    
    Co-authored-by: Trevor Carlston <trevor.carlston@viacustomers.com>

--- src/mantine-dates/src/utils/get-weekdays-names/get-weekdays-names.ts
+++ src/mantine-dates/src/utils/get-weekdays-names/get-weekdays-names.ts
@@ -2,12 +2,16 @@ import dayjs from 'dayjs';
 import { FirstDayOfWeek } from '../../types';
 import { getStartOfWeek } from '../get-start-of-week/get-start-of-week';
 
-export function getWeekdaysNames(locale: string, firstDayOfWeek: FirstDayOfWeek = 'monday') {
+export function getWeekdaysNames(
+  locale: string,
+  firstDayOfWeek: FirstDayOfWeek = 'monday',
+  format: string = 'dd'
+) {
   const names: string[] = [];
   const date = getStartOfWeek(new Date(), firstDayOfWeek);
 
   for (let i = 0; i < 7; i += 1) {
-    names.push(dayjs(date).locale(locale).format('dd'));
+    names.push(dayjs(date).locale(locale).format(format));
     date.setDate(date.getDate() + 1);
   }
 
