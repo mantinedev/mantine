8e1f2caf39d12a7f1e0bcb1151851ad62e1722bd
Vitaly Rtishchev <rtivital@gmail.com>
2022-04-08 17:40:38 +0300

    [docs] Add unstyled and disabled Tabs docs

--- src/mantine-demos/src/demos/core/Tabs/index.ts
+++ src/mantine-demos/src/demos/core/Tabs/index.ts
@@ -4,3 +4,5 @@ export { configurator } from './Tabs.demo.configurator';
 export { positionConfigurator } from './Tabs.demo.positionConfigurator';
 export { stylesApi } from './Tabs.demo.stylesApi';
 export { pull } from './Tabs.demo.pull';
+export { disabled } from './Tabs.demo.disabled';
+export { unstyled } from './Tabs.demo.unstyled';
