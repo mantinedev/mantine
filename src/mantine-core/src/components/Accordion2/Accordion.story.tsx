79581dcd6255e82ec7a883d7f239ac234ddf0174
Vitaly Rtishchev <rtivital@gmail.com>
2022-04-10 17:44:07 +0300

    [@mantine/core] Accordion: Init root component and story

---
+++ src/mantine-core/src/components/Accordion2/Accordion.story.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Accordion } from './Accordion';
+
+export default { title: 'Accordion2' };
+
+export const Usage = () => <Accordion>Accordion</Accordion>;
--- src/mantine-core/src/components/Accordion2/Accordion.tsx
+++ src/mantine-core/src/components/Accordion2/Accordion.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import { DefaultProps } from '@mantine/styles';
-import { AccordionValue } from './Accordion.types';
+import { Box } from '../Box';
+import { AccordionProviderProps, AccordionProvider } from './AccordionProvider';
 
 export interface AccordionProps<Multiple extends boolean = false>
-  extends DefaultProps,
-    Omit<React.ComponentPropsWithoutRef<'div'>, 'onChange'> {
-  value?: AccordionValue<Multiple>;
-  defaultValue?: AccordionValue<Multiple>;
-  onChange?(value: AccordionValue<Multiple>): void;
+  extends AccordionProviderProps<Multiple>,
+    DefaultProps,
+    Omit<React.ComponentPropsWithoutRef<'div'>, keyof AccordionProviderProps<Multiple>> {}
+
+export function Accordion<Multiple extends boolean = false>({
+  id,
+  loop,
+  children,
+  multiple,
+  value,
+  defaultValue,
+  onChange,
+  ...others
+}: AccordionProps<Multiple>) {
+  return (
+    <AccordionProvider
+      id={id}
+      loop={loop}
+      multiple={multiple}
+      value={value}
+      defaultValue={defaultValue}
+      onChange={onChange}
+    >
+      <Box {...others}>{children}</Box>
+    </AccordionProvider>
+  );
 }
