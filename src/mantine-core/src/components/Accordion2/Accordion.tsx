f014f4a48dddef457d2b8ba3e8edd6d62fa86c1a
Vitaly Rtishchev <rtivital@gmail.com>
2022-04-12 10:21:39 +0300

    [@mantine/core] Accordion: Add defualt props support

--- src/mantine-core/src/components/Accordion2/Accordion.tsx
+++ src/mantine-core/src/components/Accordion2/Accordion.tsx
@@ -1,31 +1,49 @@
 import React from 'react';
-import { DefaultProps } from '@mantine/styles';
+import { DefaultProps, useMantineDefaultProps } from '@mantine/styles';
 import { Box } from '../Box';
 import { AccordionProviderProps, AccordionProvider } from './AccordionProvider';
 import { AccordionItem } from './AccordionItem/AccordionItem';
+import { ChevronIcon } from './ChevronIcon';
 
 export interface AccordionProps<Multiple extends boolean = false>
   extends AccordionProviderProps<Multiple>,
     DefaultProps,
     Omit<React.ComponentPropsWithoutRef<'div'>, keyof AccordionProviderProps<Multiple>> {}
 
-export function Accordion<Multiple extends boolean = false>({
-  id,
-  loop,
-  children,
-  multiple,
-  value,
-  defaultValue,
-  onChange,
-  transitionDuration,
-  disableIconRotation,
-  iconPosition,
-  offsetIcon,
-  iconSize,
-  order,
-  icon,
-  ...others
-}: AccordionProps<Multiple>) {
+const defaultProps: Partial<AccordionProps> = {
+  multiple: false,
+  disableIconRotation: false,
+  transitionDuration: 200,
+  iconPosition: 'left',
+  offsetIcon: true,
+  iconSize: 24,
+  order: 3,
+  icon: <ChevronIcon />,
+};
+
+export function Accordion<Multiple extends boolean = false>(props: AccordionProps<Multiple>) {
+  const {
+    id,
+    loop,
+    children,
+    multiple,
+    value,
+    defaultValue,
+    onChange,
+    transitionDuration,
+    disableIconRotation,
+    iconPosition,
+    offsetIcon,
+    iconSize,
+    order,
+    icon,
+    ...others
+  } = useMantineDefaultProps<AccordionProps<Multiple>>(
+    'Accordion',
+    defaultProps as AccordionProps<Multiple>,
+    props
+  );
+
   return (
     <AccordionProvider
       id={id}
