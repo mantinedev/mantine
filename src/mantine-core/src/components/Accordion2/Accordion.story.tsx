e74076a6a323bfd82719dbd3d75027e7ec9f3af6
Vitaly Rtishchev <rtivital@gmail.com>
2022-04-12 10:31:56 +0300

    [@mantine/core] Accordion: Add controlled stories

--- src/mantine-core/src/components/Accordion2/Accordion.story.tsx
+++ src/mantine-core/src/components/Accordion2/Accordion.story.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Accordion } from './Accordion';
 
 export default { title: 'Accordion2' };
 
-export const Usage = () => (
-  <Accordion defaultValue="flex" sx={{ maxWidth: 400 }} mx="auto">
+const _items = (
+  <>
     <Accordion.Item label="Customization" value="customize">
       Colors, fonts, shadows and many other parts are customizable to fit your design needs
     </Accordion.Item>
@@ -17,5 +17,35 @@ export const Usage = () => (
     <Accordion.Item label="No annoying focus ring" value="focus">
       With new :focus-visible pseudo-class focus ring appears only when user navigates with keyboard
     </Accordion.Item>
+  </>
+);
+
+export const Usage = () => (
+  <Accordion defaultValue="flex" sx={{ maxWidth: 400 }} mx="auto">
+    {_items}
   </Accordion>
 );
+
+export const Multiple = () => (
+  <Accordion multiple defaultValue={['flex']} sx={{ maxWidth: 400 }} mx="auto">
+    {_items}
+  </Accordion>
+);
+
+export const ControlledSingle = () => {
+  const [value, setValue] = useState<string | null>(null);
+  return (
+    <Accordion value={value} onChange={setValue} sx={{ maxWidth: 400 }} mx="auto">
+      {_items}
+    </Accordion>
+  );
+};
+
+export const ControlledMultiple = () => {
+  const [value, setValue] = useState<string[]>([]);
+  return (
+    <Accordion multiple value={value} onChange={setValue} sx={{ maxWidth: 400 }} mx="auto">
+      {_items}
+    </Accordion>
+  );
+};
