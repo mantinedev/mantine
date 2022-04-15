b5235dc533b8febda4428cfb4c783de657731c68
Vitaly Rtishchev <rtivital@gmail.com>
2022-04-15 17:17:57 +0300

    [@mantine/core] Accordion: Update styles api descriptions

--- src/mantine-core/src/components/Accordion/index.ts
+++ src/mantine-core/src/components/Accordion/index.ts
@@ -1,3 +1,6 @@
 export { Accordion } from './Accordion';
 
-export type { AccordionProps } from './Accordion';
+export type { AccordionProps, AccordionStylesNames } from './Accordion';
+export type { AccordionControlProps } from './AccordionControl/AccordionControl';
+export type { AccordionItemProps } from './AccordionItem/AccordionItem';
+export type { AccordionPanelProps } from './AccordionPanel/AccordionPanel';
