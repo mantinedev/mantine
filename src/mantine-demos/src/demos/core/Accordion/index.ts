06550699419897c86a2753e3c078df71fa2ab03b
Vitaly Rtishchev <rtivital@gmail.com>
2022-04-16 13:38:54 +0300

    [docs] Add unstyled Accordion demo

--- src/mantine-demos/src/demos/core/Accordion/index.ts
+++ src/mantine-demos/src/demos/core/Accordion/index.ts
@@ -4,3 +4,4 @@ export { chevron } from './Accordion.demo.chevron';
 export { icons } from './Accordion.demo.icons';
 export { configurator } from './Accordion.demo.configurator';
 export { disableTransitions, customTransitions } from './Accordion.demo.transitions';
+export { unstyled } from './Accordion.demo.unstyled';
