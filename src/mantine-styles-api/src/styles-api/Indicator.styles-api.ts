922daca27691346519dbb5c15cb770ed3f711ae1
Vitaly Rtishchev <rtivital@gmail.com>
2022-04-17 18:19:53 +0300

    [@mantine/core] Indicator: Add styles api descriptions

---
+++ src/mantine-styles-api/src/styles-api/Indicator.styles-api.ts
@@ -0,0 +1,6 @@
+import type { IndicatorStylesNames } from '@mantine/core';
+
+export const Indicator: Record<IndicatorStylesNames, string> = {
+  root: 'Root element',
+  indicator: 'Indicator badge',
+};
