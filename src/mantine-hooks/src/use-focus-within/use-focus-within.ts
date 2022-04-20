c61c1df6f3a1804a23410c8e3eee994d040f110f
Vitaly Rtishchev <rtivital@gmail.com>
2022-04-20 20:30:14 +0300

    [@mantine/hooks] use-focus-within: Add tests

--- src/mantine-hooks/src/use-focus-within/use-focus-within.ts
+++ src/mantine-hooks/src/use-focus-within/use-focus-within.ts
@@ -1,8 +1,16 @@
 import { useRef, useState, useEffect } from 'react';
 
-interface UseFocusWithinOptions {
+export interface UseFocusWithinOptions {
   onFocus?(event: FocusEvent): void;
   onBlur?(event: FocusEvent): void;
+}
+
+function containsRelatedTarget(event: FocusEvent) {
+  if (event.currentTarget instanceof HTMLElement && event.relatedTarget instanceof HTMLElement) {
+    return event.currentTarget.contains(event.relatedTarget);
+  }
+
+  return false;
 }
 
 export function useFocusWithin<T extends HTMLElement = any>({
@@ -10,18 +18,27 @@ export function useFocusWithin<T extends HTMLElement = any>({
   onFocus,
 }: UseFocusWithinOptions = {}): { ref: React.MutableRefObject<T>; focused: boolean } {
   const ref = useRef<T>();
-  const [focused, setFocused] = useState(false);
+  const [focused, _setFocused] = useState(false);
+  const focusedRef = useRef(false);
+  const setFocused = (value: boolean) => {
+    _setFocused(value);
+    focusedRef.current = value;
+  };
 
   useEffect(() => {
     if (ref.current) {
       ref.current.addEventListener('focusin', (event) => {
-        setFocused(true);
-        onFocus?.(event);
+        if (!focusedRef.current) {
+          setFocused(true);
+          onFocus?.(event);
+        }
       });
 
       ref.current.addEventListener('focusout', (event) => {
-        setFocused(false);
-        onBlur?.(event);
+        if (focusedRef.current && !containsRelatedTarget(event)) {
+          setFocused(false);
+          onBlur?.(event);
+        }
       });
     }
   }, []);
