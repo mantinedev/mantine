03f9119f7bc4eb1b2a3c0e7551a65702d96682ae
Vitaly Rtishchev <rtivital@gmail.com>
2022-04-28 17:14:31 +0300

    [@mantine/core] FocusTrap: Init demos

--- src/mantine-demos/src/index.ts
+++ src/mantine-demos/src/index.ts
@@ -41,6 +41,7 @@ export * as ContainerDemos from './demos/core/Container';
 export * as DialogDemos from './demos/core/Dialog';
 export * as DividerDemos from './demos/core/Divider';
 export * as DrawerDemos from './demos/core/Drawer';
+export * as FocusTrapDemos from './demos/core/FocusTrap';
 export * as GridDemos from './demos/core/Grid';
 export * as GroupDemos from './demos/core/Group';
 export * as HighlightDemos from './demos/core/Highlight';
