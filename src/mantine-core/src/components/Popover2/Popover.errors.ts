96e54ae664c2ba5956e02626616b4d86f54416a7
Vitaly Rtishchev <rtivital@gmail.com>
2022-04-29 11:11:00 +0300

    [@mantine/core] Popover: Add basic popover target logic

--- src/mantine-core/src/components/Popover2/Popover.errors.ts
+++ src/mantine-core/src/components/Popover2/Popover.errors.ts
@@ -5,4 +5,10 @@ export const POPOVER_ERRORS = {
     message: 'Popover component was not found in the tree',
     code: 'popover-context',
   }),
+
+  children: createError({
+    code: 'popover-children',
+    message:
+      'Popover.Target component children should be an element or a component that accepts ref, fragments, strings, numbers and other primitive values are not supported',
+  }),
 } as const;
--- src/mantine-core/src/components/Popover2/PopoverTarget/PopoverTarget.tsx
+++ src/mantine-core/src/components/Popover2/PopoverTarget/PopoverTarget.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { cloneElement } from 'react';
+import { useMergedRef } from '@mantine/hooks';
+import { isElement } from '@mantine/utils';
+import { usePopoverContext } from '../Popover.context';
+import { POPOVER_ERRORS } from '../Popover.errors';
 
-export function PopoverTarget() {
-  return <div>Target</div>;
+interface PopoverTargetProps {
+  /** Target element */
+  children: React.ReactNode;
+
+  /** Key of the prop that should be used to get element ref */
+  refProp?: string;
+}
+
+export function PopoverTarget({ children, refProp = 'ref' }: PopoverTargetProps) {
+  if (!isElement(children)) {
+    throw new Error(POPOVER_ERRORS.children);
+  }
+
+  const ctx = usePopoverContext();
+  const target = children as React.ReactElement;
+  const targetRef = useMergedRef(ctx.reference, (target as any).ref);
+
+  return cloneElement(target, { [refProp]: targetRef });
 }
 
 PopoverTarget.displayName = '@mantine/core/PopoverTarget';
