27f660432dd638bdabc1e4afc782a40e410e1169
Vitaly Rtishchev <rtivital@gmail.com>
2022-05-01 20:02:01 +0300

    [@mantine/core] Popover: Add exports

---
+++ src/mantine-core/src/components/Popover2/index.ts
@@ -0,0 +1,6 @@
+export { Popover } from './Popover';
+
+export type { PopoverProps } from './Popover';
+export type { PopoverTargetProps } from './PopoverTarget/PopoverTarget';
+export type { PopoverDropdownProps } from './PopoverDropdown/PopoverDropdown';
+export type { PopoverStylesParams, PopoverStylesNames } from './Popover.types';
