b13b636c317a20f625cabe0ae4d7dde7c2420495
Vitaly Rtishchev <rtivital@gmail.com>
2022-05-02 19:20:46 +0300

    [@mantine/core] HoverCard: Add mouse events to dropdown

--- src/mantine-core/src/components/HoverCard/HoverCardDropdown/HoverCardDropdown.tsx
+++ src/mantine-core/src/components/HoverCard/HoverCardDropdown/HoverCardDropdown.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
-import { Popover } from '../../Popover';
+import { Popover, PopoverDropdownProps } from '../../Popover';
+import { useHoverCardContext } from '../HoverCard.context';
 
-export interface HoverCardDropdownProps {
+export interface HoverCardDropdownProps extends PopoverDropdownProps {
   /** Dropdown content */
   children?: React.ReactNode;
 }
 
-export function HoverCardDropdown({ children }: HoverCardDropdownProps) {
-  return <Popover.Dropdown>{children}</Popover.Dropdown>;
+export function HoverCardDropdown({
+  children,
+  onMouseEnter,
+  onMouseLeave,
+  ...others
+}: HoverCardDropdownProps) {
+  const ctx = useHoverCardContext();
+
+  const handleMouseEnter = (event: React.MouseEvent<any>) => {
+    onMouseEnter?.(event);
+    ctx.openDropdown();
+  };
+
+  const handleMouseLeave = (event: React.MouseEvent<any>) => {
+    onMouseLeave?.(event);
+    ctx.closeDropdown();
+  };
+
+  return (
+    <Popover.Dropdown onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} {...others}>
+      {children}
+    </Popover.Dropdown>
+  );
 }
 
 HoverCardDropdown.displayName = '@mantine/core/HoverCardDropdown';
