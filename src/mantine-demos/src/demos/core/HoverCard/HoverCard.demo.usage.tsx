956b24da2990ecd7954f5e5613387ef49ac8a0b3
Vitaly Rtishchev <rtivital@gmail.com>
2022-05-03 11:50:24 +0300

    [@mantine/core] HoverCard: Add all demos

--- src/mantine-demos/src/demos/core/HoverCard/HoverCard.demo.usage.tsx
+++ src/mantine-demos/src/demos/core/HoverCard/HoverCard.demo.usage.tsx
@@ -1,39 +1,44 @@
 import React from 'react';
-import { HoverCard, Button, Text } from '@mantine/core';
+import { HoverCard, Button, Text, Group } from '@mantine/core';
 
 const code = `
-import { HoverCard, Button, Text } from '@mantine/core';
+import { HoverCard, Button, Text, Group } from '@mantine/core';
 
 function Demo() {
   return (
-    <HoverCard width={280} shadow="md">
-      <HoverCard.Target>
-        <Button>Hover to reveal the card</Button>
-      </HoverCard.Target>
-      <HoverCard.Dropdown>
-        <Text size="sm">
-          Hover card is revealed when user hovers over target element, it will be hidden once mouse
-          is not over both target and dropdown elements
-        </Text>
-      </HoverCard.Dropdown>
-    </HoverCard>
+    <Group position="center">
+      <HoverCard width={280} shadow="md">
+        <HoverCard.Target>
+          <Button>Hover to reveal the card</Button>
+        </HoverCard.Target>
+        <HoverCard.Dropdown>
+          <Text size="sm">
+            Hover card is revealed when user hovers over target element, it will be hidden once
+            mouse is not over both target and dropdown elements
+          </Text>
+        </HoverCard.Dropdown>
+      </HoverCard>
+    </Group>
   );
 }
+
 `;
 
 function Demo() {
   return (
-    <HoverCard width={280} shadow="md">
-      <HoverCard.Target>
-        <Button>Hover to reveal the card</Button>
-      </HoverCard.Target>
-      <HoverCard.Dropdown>
-        <Text size="sm">
-          Hover card is revealed when user hovers over target element, it will be hidden once mouse
-          is not over both target and dropdown elements
-        </Text>
-      </HoverCard.Dropdown>
-    </HoverCard>
+    <Group position="center">
+      <HoverCard width={280} shadow="md">
+        <HoverCard.Target>
+          <Button>Hover to reveal the card</Button>
+        </HoverCard.Target>
+        <HoverCard.Dropdown>
+          <Text size="sm">
+            Hover card is revealed when user hovers over target element, it will be hidden once
+            mouse is not over both target and dropdown elements
+          </Text>
+        </HoverCard.Dropdown>
+      </HoverCard>
+    </Group>
   );
 }
 
