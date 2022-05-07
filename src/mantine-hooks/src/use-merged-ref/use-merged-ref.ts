b18c9480565eaf0dd092735b0faa41b735897ec1
izhulien <zhulienivanov@yahoo.com>
2022-05-07 16:27:40 +0300

    [@mantine/hooks] use-merged-ref: Refactor to reuse merging function.

--- src/mantine-hooks/src/use-merged-ref/use-merged-ref.ts
+++ src/mantine-hooks/src/use-merged-ref/use-merged-ref.ts
@@ -3,14 +3,12 @@ import { assignRef } from '../utils';
 
 type Ref<T> = React.Dispatch<React.SetStateAction<T>> | React.ForwardedRef<T>;
 
-export function useMergedRef<T = any>(...refs: Ref<T>[]) {
-  return useCallback((node: T | null) => {
-    refs.forEach((ref) => assignRef(ref, node));
-  }, refs);
-}
-
 export function mergeRefs<T = any>(...refs: Ref<T>[]) {
   return (node: T | null) => {
     refs.forEach((ref) => assignRef(ref, node));
   };
+}
+
+export function useMergedRef<T = any>(...refs: Ref<T>[]) {
+  return useCallback(mergeRefs(...refs), refs);
 }
