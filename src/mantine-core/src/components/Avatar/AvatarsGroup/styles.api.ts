877918f7f2ef49bc00f07f2287eaa96c109139dd
Vitaly Rtishchev <rtivital@gmail.com>
2022-05-13 18:38:57 +0300

    [@mantine/core] Avatar: Remove old AvatarsGroup

--- src/mantine-core/src/components/Avatar/AvatarsGroup/styles.api.ts
+++
@@ -1,7 +0,0 @@
-import { AvatarsGroupStylesNames } from './AvatarsGroup';
-
-export const AvatarsGroup: Record<AvatarsGroupStylesNames, string> = {
-  root: 'Root element',
-  child: 'Child Avatar component',
-  truncated: 'Truncated avatars count label',
-};
