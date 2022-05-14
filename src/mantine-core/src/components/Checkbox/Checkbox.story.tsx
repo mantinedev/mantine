2199efd3633f372fdb3dd21a604d19dbb0d9b63a
Vitaly Rtishchev <rtivital@gmail.com>
2022-05-14 16:15:30 +0300

    [@mantine/core] Checkbox: Cleanup props descriptions and stories

--- src/mantine-core/src/components/Checkbox/Checkbox.story.tsx
+++ src/mantine-core/src/components/Checkbox/Checkbox.story.tsx
@@ -1,70 +1,13 @@
-import React, { useState } from 'react';
-import { storiesOf } from '@storybook/react';
-import { MANTINE_COLORS, MantineProvider } from '@mantine/styles';
-import { Checkbox, CheckboxProps } from './Checkbox';
-import { CheckboxGroup } from './CheckboxGroup/CheckboxGroup';
+import React from 'react';
+import { Checkbox } from './Checkbox';
 
-function Controlled(props: Partial<CheckboxProps>) {
-  const [value, onChange] = useState(true);
+export default { title: 'Checkbox' };
+
+export function Usage() {
   return (
-    <Checkbox
-      checked={value}
-      onChange={(event) => onChange(event.currentTarget.checked)}
-      {...props}
-    />
+    <div style={{ padding: 40 }}>
+      <Checkbox label="Default" defaultChecked />
+      <Checkbox label="Indeterminate" indeterminate mt="xl" />
+    </div>
   );
 }
-
-function CheckboxGroupControlled() {
-  const [value, setValue] = useState(['react']);
-
-  return (
-    <CheckboxGroup
-      label="Checkboxes group"
-      description="Pick libraries that you like"
-      value={value}
-      onChange={setValue}
-    >
-      <Checkbox value="react" label="React" />
-      <Checkbox value="ng" label="Angular" />
-      <Checkbox value="sv" label="Svelte" />
-    </CheckboxGroup>
-  );
-}
-
-const getThemes = (props?: Partial<CheckboxProps>) =>
-  MANTINE_COLORS.map((color) => (
-    <Checkbox key={color} color={color} {...props} label={color} mt={15} />
-  ));
-
-storiesOf('Checkbox', module)
-  .add('Colors', () => <div style={{ padding: 15 }}>{getThemes({ checked: true })}</div>)
-  .add('Controlled', () => <Controlled label="Controlled" style={{ padding: 15 }} />)
-  .add('CheckboxGroup: uncontrolled', () => (
-    <div style={{ padding: 40 }}>
-      <CheckboxGroup
-        label="Checkboxes group"
-        description="Pick libraries that you like"
-        defaultValue={['react']}
-      >
-        <Checkbox value="react" label="React" />
-        <Checkbox value="ng" label="Angular" />
-        <Checkbox value="sv" label="Svelte" />
-      </CheckboxGroup>
-    </div>
-  ))
-  .add('CheckboxGroup: controlled', () => (
-    <div style={{ padding: 40 }}>
-      <CheckboxGroupControlled />
-    </div>
-  ))
-  .add('Default props on MantineProvider', () => (
-    <MantineProvider defaultProps={{ Checkbox: { size: 'xl', color: 'red' } }}>
-      <Controlled label="Controlled" style={{ padding: 15 }} />
-    </MantineProvider>
-  ))
-  .add('Default radius on MantineProvider', () => (
-    <MantineProvider theme={{ defaultRadius: 0 }}>
-      <Controlled label="Controlled" style={{ padding: 15 }} />
-    </MantineProvider>
-  ));
--- src/mantine-core/src/components/Checkbox/Checkbox.tsx
+++ src/mantine-core/src/components/Checkbox/Checkbox.tsx
@@ -8,7 +8,7 @@ import {
   MantineNumberSize,
   useMantineDefaultProps,
 } from '@mantine/styles';
-import { useUuid } from '@mantine/hooks';
+import { useId } from '@mantine/utils';
 import { Box } from '../Box';
 import { CheckboxIcon } from './CheckboxIcon';
 import useStyles from './Checkbox.styles';
@@ -18,10 +18,10 @@ export type CheckboxStylesNames = Selectors<typeof useStyles>;
 export interface CheckboxProps
   extends DefaultProps<CheckboxStylesNames>,
     Omit<React.ComponentPropsWithoutRef<'input'>, 'type' | 'size'> {
-  /** Checkbox checked and indeterminate state color from theme, defaults to theme.primaryColor */
+  /** Key of theme.colors */
   color?: MantineColor;
 
-  /** Radius from theme.radius, or number to set border-radius in px */
+  /** Key of theme.radius or number to set border-radius in px */
   radius?: MantineNumberSize;
 
   /** Predefined label font-size and checkbox width and height in px */
@@ -34,29 +34,25 @@ export interface CheckboxProps
   indeterminate?: boolean;
 
   /** Props spread to wrapper element */
-  wrapperProps?: { [key: string]: any };
+  wrapperProps?: Record<string, any>;
 
-  /** Id is used to bind input and label, if not passed unique id will be generated for each input */
+  /** id to connect label with input */
   id?: string;
 
-  /** Check/uncheck transition duration, set to 0 to disable all transitions */
+  /** Transition duration in ms */
   transitionDuration?: number;
 
-  /** Replace default icon */
+  /** Icon rendered when checkbox has checked or indeterminate state */
   icon?: React.FC<{ indeterminate: boolean; className: string }>;
-
-  /** Static selector base */
-  __staticSelector?: string;
 }
 
 const defaultProps: Partial<CheckboxProps> = {
   size: 'sm',
-  __staticSelector: 'Checkbox',
   transitionDuration: 100,
   icon: CheckboxIcon,
 };
 
-export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>((props: CheckboxProps, ref) => {
+export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>((props, ref) => {
   const {
     className,
     style,
@@ -74,15 +70,14 @@ export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>((props: Chec
     styles,
     transitionDuration,
     icon: Icon,
-    __staticSelector,
     ...others
   } = useMantineDefaultProps('Checkbox', defaultProps, props);
 
-  const uuid = useUuid(id);
+  const uuid = useId(id);
   const { systemStyles, rest } = extractSystemStyles(others);
   const { classes, cx } = useStyles(
     { size, radius, color, transitionDuration },
-    { classNames, styles, name: __staticSelector }
+    { classNames, styles, name: 'Checkbox' }
   );
 
   return (
