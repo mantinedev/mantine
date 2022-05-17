e9cbae5eeba881c4caa5ea0f2af34c4e70788b0a
Vitaly Rtishchev <rtivital@gmail.com>
2022-05-17 17:38:40 +0300

    [@mantine/core] List: Remove ListItem export

--- src/mantine-core/src/components/List/index.ts
+++ src/mantine-core/src/components/List/index.ts
@@ -1,5 +1,4 @@
 export { List } from './List';
-export { ListItem } from './ListItem/ListItem';
 
 export type { ListProps, ListStylesNames } from './List';
 export type { ListItemProps, ListItemStylesNames } from './ListItem/ListItem';
