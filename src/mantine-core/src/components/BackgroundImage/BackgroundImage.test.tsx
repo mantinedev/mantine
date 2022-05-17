50557334a031fb46957bb62428a1f19ebc71eeb0
Vitaly Rtishchev <rtivital@gmail.com>
2022-05-17 16:40:19 +0300

    [@mantine/core] BackgroundImage: Add more tests

--- src/mantine-core/src/components/BackgroundImage/BackgroundImage.test.tsx
+++ src/mantine-core/src/components/BackgroundImage/BackgroundImage.test.tsx
@@ -1,4 +1,6 @@
-import { itSupportsSystemProps, itIsPolymorphic } from '@mantine/tests';
+import React from 'react';
+import { render } from '@testing-library/react';
+import { itSupportsSystemProps, itIsPolymorphic, itRendersChildren } from '@mantine/tests';
 import { BackgroundImage, BackgroundImageProps } from './BackgroundImage';
 
 const defaultProps: BackgroundImageProps = {
@@ -7,11 +9,17 @@ const defaultProps: BackgroundImageProps = {
 
 describe('@mantine/core/BackgroundImage', () => {
   itIsPolymorphic(BackgroundImage, defaultProps);
+  itRendersChildren(BackgroundImage, defaultProps);
   itSupportsSystemProps({
     component: BackgroundImage,
     props: defaultProps,
     displayName: '@mantine/core/BackgroundImage',
     refType: HTMLDivElement,
     providerName: 'BackgroundImage',
+  });
+
+  it('sets correct image as background', () => {
+    const { container } = render(<BackgroundImage src="./image" />);
+    expect(container.firstChild).toHaveStyle({ backgroundImage: "url('./image')" });
   });
 });
