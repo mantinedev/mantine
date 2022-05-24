23894a723a43dad60dd9c0fe272693362e82485d
Vitaly Rtishchev <rtivital@gmail.com>
2022-05-24 16:21:07 +0300

    [docs] Add default props documentation

--- src/mantine-demos/src/demos/theme/index.ts
+++ src/mantine-demos/src/demos/theme/index.ts
@@ -1,7 +1,8 @@
-export * from './Theme.demo.dark-theme-toggle';
-export * from './Theme.demo.nested-providers';
-export * from './Theme.demo.primary-color';
-export * from './Theme.demo.theme-colors-extend';
-export * from './Theme.demo.theme-fonts-extend';
-export * from './Theme.demo.provider-styles';
-export * from './Theme.demo.primary-shade';
+export { darkThemeToggle } from './Theme.demo.dark-theme-toggle';
+export { nestedProviders } from './Theme.demo.nested-providers';
+export { primaryColor } from './Theme.demo.primary-color';
+export { themeColorsExtend } from './Theme.demo.theme-colors-extend';
+export { themeFontsExtend } from './Theme.demo.theme-fonts-extend';
+export { providerStyles } from './Theme.demo.provider-styles';
+export { primaryShade } from './Theme.demo.primary-shade';
+export { defaultProps } from './Theme.demo.default-props';
