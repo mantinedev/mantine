21943995891b448063f732fd4500c8de237faa80
Vitaly Rtishchev <rtivital@gmail.com>
2022-05-26 18:31:45 +0300

    [@mantine/demos] Update Input demo to use new tooltip api

--- src/mantine-demos/src/demos/core/Input/Input.demo.tooltip.tsx
+++ src/mantine-demos/src/demos/core/Input/Input.demo.tooltip.tsx
@@ -7,24 +7,34 @@ import { Input, Tooltip } from '@mantine/core';
 import { IconAlertCircle } from '@tabler/icons';
 
 function Demo() {
-  const rightSection = (
-    <Tooltip label="We do not send spam" position="top-end">
-      <IconAlertCircle size={16} style={{ display: 'block', opacity: 0.5 }} />
-    </Tooltip>
+  return (
+    <Input
+      placeholder="Your email"
+      rightSection={
+        <Tooltip label="We do not send spam" position="top-end" withinPortal withArrow>
+          <div>
+            <IconAlertCircle size={18} style={{ display: 'block', opacity: 0.5 }} />
+          </div>
+        </Tooltip>
+      }
+    />
   );
-
-  return <Input placeholder="Your email" rightSection={rightSection} />;
 }
 `;
 
 function Demo() {
-  const rightSection = (
-    <Tooltip label="We do not send spam" position="top-end">
-      <IconAlertCircle size={16} style={{ display: 'block', opacity: 0.5 }} />
-    </Tooltip>
+  return (
+    <Input
+      placeholder="Your email"
+      rightSection={
+        <Tooltip label="We do not send spam" position="top-end" withinPortal withArrow>
+          <div>
+            <IconAlertCircle size={18} style={{ display: 'block', opacity: 0.5 }} />
+          </div>
+        </Tooltip>
+      }
+    />
   );
-
-  return <Input placeholder="Your email" rightSection={rightSection} />;
 }
 
 export const tooltip: MantineDemo = {
