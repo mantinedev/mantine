3f3bf6617543f56a3369867718fbefc398a24c02
Frederic Barthelemy <git@fbartho.com>
2022-05-27 15:59:15 -0700

    [@mantine/prism] rename Demo themeOverride to colorSchemeOverride

--- src/mantine-demos/src/demos/prism/index.ts
+++ src/mantine-demos/src/demos/prism/index.ts
@@ -1,6 +1,6 @@
 export { languages } from './Prism.demo.languages';
 export { copyLabel } from './Prism.demo.copyLabel';
-export { themeOverride } from './Prism.demo.themeOverride';
+export { colorSchemeOverride } from './Prism.demo.colorSchemeOverride';
 export { linesHighlight } from './Prism.demo.linesHighlight';
 export { lineNumbers } from './Prism.demo.lineNumbers';
 export { usage } from './Prism.demo.usage';
