0c804f574846e98e6df037a5006adc6322886336
Vitaly Rtishchev <rtivital@gmail.com>
2022-05-29 12:57:14 +0300

    [@mantine/form] Fix incorrect FormList type export

--- src/mantine-form/src/index.ts
+++ src/mantine-form/src/index.ts
@@ -1,7 +1,8 @@
-export { formList, isFormList, FormList } from './form-list/form-list';
+export { formList, isFormList } from './form-list/form-list';
 export { useForm } from './use-form';
 export { zodResolver } from './resolvers/zod-resolver/zod-resolver';
 export { yupResolver } from './resolvers/yup-resolver/yup-resolver';
 export { joiResolver } from './resolvers/joi-resolver/joi-resolver';
 
 export type { FormErrors, FormRules } from './types';
+export type { FormList } from './form-list/form-list';
