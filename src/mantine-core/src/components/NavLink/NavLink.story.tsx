cefe28add52e49c3ed48f8bf191ec085479969f0
Vitaly Rtishchev <rtivital@gmail.com>
2022-06-03 13:37:30 +0300

    [@mantine/core] NavLink: Init component

---
+++ src/mantine-core/src/components/NavLink/NavLink.story.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { IconHome2, IconChevronRight } from '@tabler/icons';
+import { NavLink } from './NavLink';
+
+export default { title: 'NavLink' };
+
+export function Usage() {
+  return (
+    <div style={{ padding: 40, maxWidth: 320 }}>
+      <NavLink
+        label="Navigation link"
+        description="Some description here"
+        icon={<IconHome2 size={16} stroke={1.5} />}
+        rightSection={<IconChevronRight size={16} stroke={1.5} />}
+      />
+    </div>
+  );
+}
---
+++ src/mantine-core/src/components/NavLink/NavLink.tsx
@@ -0,0 +1,49 @@
+/* eslint-disable react/no-unused-prop-types */
+import React, { forwardRef } from 'react';
+import { DefaultProps, Selectors, useComponentDefaultProps } from '@mantine/styles';
+import { createPolymorphicComponent } from '@mantine/utils';
+import { UnstyledButton } from '../UnstyledButton';
+import useStyles from './NavLink.styles';
+
+export type NavLinkStylesNames = Selectors<typeof useStyles>;
+
+export interface NavLinkProps extends DefaultProps<NavLinkStylesNames> {
+  /** Main link content */
+  label?: React.ReactNode;
+
+  /** Secondary link description */
+  description?: React.ReactNode;
+
+  /** Icon displayed on the left side of the label */
+  icon?: React.ReactNode;
+
+  /** Section displayed on the right side of the label */
+  rightSection?: React.ReactNode;
+}
+
+export const _NavLink = forwardRef<HTMLButtonElement, NavLinkProps>((props, ref) => {
+  const {
+    label,
+    description,
+    icon,
+    rightSection,
+    className,
+    classNames,
+    styles,
+    unstyled,
+    ...others
+  } = useComponentDefaultProps('NavLink', {}, props);
+
+  const { classes, cx } = useStyles(null, { name: 'NavLink', classNames, styles, unstyled });
+
+  return (
+    <UnstyledButton ref={ref} className={cx(classes.root, className)} {...others}>
+      {icon}
+      {label}
+      {description}
+      {rightSection}
+    </UnstyledButton>
+  );
+});
+
+export const NavLink = createPolymorphicComponent<'button', NavLinkProps>(_NavLink);
