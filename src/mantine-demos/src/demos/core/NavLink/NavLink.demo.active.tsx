fdd6ead673eb5b8e36b567c614a0adaf700dd33f
Vitaly Rtishchev <rtivital@gmail.com>
2022-06-03 21:01:43 +0300

    [@mantine/demos] NavLink: Update active demo to use configurator

--- src/mantine-demos/src/demos/core/NavLink/NavLink.demo.active.tsx
+++ src/mantine-demos/src/demos/core/NavLink/NavLink.demo.active.tsx
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
-import { IconGauge, IconFingerprint, IconActivity } from '@tabler/icons';
-import { Box, NavLink } from '@mantine/core';
+import { IconGauge, IconFingerprint, IconActivity, IconChevronRight } from '@tabler/icons';
+import { Box, NavLink, Group } from '@mantine/core';
 
-const code = `
+const codeTemplate = (props: string) => `
 import { useState } from 'react';
-import { IconGauge, IconFingerprint, IconActivity } from '@tabler/icons';
+import { IconGauge, IconFingerprint, IconActivity, IconChevronRight } from '@tabler/icons';
 import { Box, NavLink } from '@mantine/core';
 
 const data = [
-  { icon: IconGauge, label: 'Dashboard' },
-  { icon: IconFingerprint, label: 'Security' },
+  { icon: IconGauge, label: 'Dashboard', description: 'Item with description' },
+  {
+    icon: IconFingerprint,
+    label: 'Security',
+    rightSection: <IconChevronRight size={14} stroke={1.5} />,
+  },
   { icon: IconActivity, label: 'Activity' },
 ];
 
@@ -21,8 +25,11 @@ function Demo() {
       key={item.label}
       active={index === active}
       label={item.label}
+      description={item.description}
+      rightSection={item.rightSection}
       icon={<item.icon size={16} stroke={1.5} />}
       onClick={() => setActive(index)}
+     ${props}
     />
   ));
 
@@ -31,12 +38,16 @@ function Demo() {
 `;
 
 const data = [
-  { icon: IconGauge, label: 'Dashboard' },
-  { icon: IconFingerprint, label: 'Security' },
+  { icon: IconGauge, label: 'Dashboard', description: 'Item with description' },
+  {
+    icon: IconFingerprint,
+    label: 'Security',
+    rightSection: <IconChevronRight size={14} stroke={1.5} />,
+  },
   { icon: IconActivity, label: 'Activity' },
 ];
 
-function Demo() {
+function Demo(props: any) {
   const [active, setActive] = useState(0);
 
   const items = data.map((item, index) => (
@@ -44,16 +55,39 @@ function Demo() {
       key={item.label}
       active={index === active}
       label={item.label}
+      description={item.description}
+      rightSection={item.rightSection}
       icon={<item.icon size={16} stroke={1.5} />}
       onClick={() => setActive(index)}
+      {...props}
     />
   ));
 
-  return <Box sx={{ width: 220 }}>{items}</Box>;
+  return (
+    <Group position="center">
+      <Box sx={{ width: 220 }}>{items}</Box>
+    </Group>
+  );
 }
 
 export const active: MantineDemo = {
-  type: 'demo',
+  type: 'configurator',
   component: Demo,
-  code,
+  codeTemplate,
+  configuratorProps: { multiline: 3 },
+  configurator: [
+    { name: 'color', type: 'color', initialValue: 'blue', defaultValue: 'blue' },
+    {
+      name: 'variant',
+      type: 'segmented',
+      data: [
+        { value: 'subtle', label: 'Subtle' },
+        { value: 'light', label: 'Light' },
+        { value: 'filled', label: 'Filled' },
+      ],
+
+      defaultValue: 'light',
+      initialValue: 'light',
+    },
+  ],
 };
