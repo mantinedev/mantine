9e9431e753c03d94d8207420f2b746ee130dbb35
Vitaly Rtishchev <rtivital@gmail.com>
2022-06-07 12:26:23 +0300

    [docs] Add carousel documentation

--- src/mantine-demos/src/demos/carousel/Carousel.demo.dragFree.tsx
+++ src/mantine-demos/src/demos/carousel/Carousel.demo.dragFree.tsx
@@ -7,7 +7,15 @@ import { Carousel } from '@mantine/carousel';
 
 function Demo() {
   return (
-    <Carousel sx={{ maxWidth: 320 }} mx="auto" withIndicators height={200}>
+    <Carousel
+      sx={{ maxWidth: 320 }}
+      mx="auto"
+      withIndicators
+      height={200}
+      dragFree
+      slideGap="md"
+      align="start"
+    >
       <Carousel.Slide>1</Carousel.Slide>
       <Carousel.Slide>2</Carousel.Slide>
       <Carousel.Slide>3</Carousel.Slide>
