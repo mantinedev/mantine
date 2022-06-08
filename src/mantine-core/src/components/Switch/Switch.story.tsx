340adf8af50a5d37ee5d12cc0e8c2d2bf88a5ee0
Zach Smith <36892129+SmitharyZach@users.noreply.github.com>
2022-06-08 03:20:53 -0400

    [@mantine/core] Switch: fix on / off label positions (#1588)
    * [@mantine/core] Switch: fix on off label
    
    * [@mantine/core] Switch: add stories
    
    * [@mantine/core] Switch: run prettier
    
    Co-authored-by: Zach Smith <zachsmith@Zachs-MacBook-Air.local>

--- src/mantine-core/src/components/Switch/Switch.story.tsx
+++ src/mantine-core/src/components/Switch/Switch.story.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
-import { DEFAULT_THEME, MANTINE_SIZES } from '@mantine/styles';
+import { DEFAULT_THEME, MantineSize, MANTINE_SIZES } from '@mantine/styles';
 import { Switch, SwitchProps } from './Switch';
 
 function SwitchWrapper(props: Omit<SwitchProps, 'value' | 'onChange'>) {
@@ -23,7 +23,24 @@ const getThemes = (props?: Partial<SwitchProps>) =>
     <Switch key={color} color={color} {...props} label={color} style={{ marginTop: 15 }} />
   ));
 
+const switchWithLabels = [
+  { on: 'D', off: 'L', size: 'md' },
+  { on: 'On', off: 'Off', size: 'lg' },
+  { on: 'Dark', off: 'Light', size: 'xl' },
+].map((label) => (
+  <Switch
+    color="blue"
+    label={`On: ${label.on} Off: ${label.off}`}
+    key={label.on}
+    size={label.size as MantineSize}
+    onLabel={label.on}
+    offLabel={label.off}
+    style={{ marginTop: 15 }}
+  />
+));
+
 storiesOf('Switch', module)
   .add('Themes', () => <div style={{ padding: 15 }}>{getThemes({ checked: true })}</div>)
   .add('Sizes', () => <div style={{ padding: 15 }}>{sizes}</div>)
+  .add('With on off labels', () => <div style={{ padding: 15 }}>{switchWithLabels}</div>)
   .add('Controlled', () => <SwitchWrapper label="Controlled" style={{ padding: 15 }} />);
