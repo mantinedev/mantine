43b4ef8f4455569f76b15aabf2bbe8c5e93d9edc
Vitaly Rtishchev <rtivital@gmail.com>
2022-06-15 17:40:50 +0300

    [@mantine/demos] Update floating tooltip demo

--- src/mantine-demos/src/demos/core/Tooltip/Tooltip.demo.floating.tsx
+++ src/mantine-demos/src/demos/core/Tooltip/Tooltip.demo.floating.tsx
@@ -1,27 +1,41 @@
 import React from 'react';
-import { TooltipFloatingProps, Group, Button, Tooltip } from '@mantine/core';
+import { TooltipFloatingProps, Group, Tooltip, Box } from '@mantine/core';
 
 const Wrapper = (props: TooltipFloatingProps) => (
   <div style={{ padding: 30 }}>
     <Group position="center">
       <Tooltip.Floating label="Tooltip" {...props}>
-        <Button variant="outline" size="xl">
-          With floating tooltip
-        </Button>
+        <Box
+          sx={(theme) => ({
+            padding: theme.spacing.xl * 1.5,
+            cursor: 'default',
+            backgroundColor:
+              theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+          })}
+        >
+          Hover over the box to see tooltip
+        </Box>
       </Tooltip.Floating>
     </Group>
   </div>
 );
 
 const codeTemplate = (props: string) => `
-import { Tooltip, Button } from '@mantine/core';
+import { Tooltip, Box } from '@mantine/core';
 
 function Demo() {
   return (
     <Tooltip.Floating label="Tooltip"${props}>
-      <Button variant="outline" size="xl">
-        With floating tooltip
-      </Button>
+      <Box
+        sx={(theme) => ({
+          padding: theme.spacing.xl * 1.5,
+          cursor: 'default',
+          backgroundColor:
+            theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+        })}
+      >
+        Hover over the box to see tooltip
+      </Box>
     </Tooltip.Floating>
   );
 }
