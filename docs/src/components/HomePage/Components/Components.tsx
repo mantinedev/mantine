3a10e7c651d835d0f48fc67a09f92f9ea7046930
Vitaly Rtishchev <rtivital@gmail.com>
2022-06-24 22:38:26 +0300

    [docs] Extract DemoTabs component

--- docs/src/components/HomePage/Components/Components.tsx
+++ docs/src/components/HomePage/Components/Components.tsx
@@ -1,5 +1,4 @@
-import React, { useState, useRef } from 'react';
-import { Container, Grid, UnstyledButton, Text } from '@mantine/core';
+import React from 'react';
 import {
   IconForms,
   IconCalendar,
@@ -8,15 +7,13 @@ import {
   IconNotebook,
   IconSlideshow,
 } from '@tabler/icons';
-import { useMediaQuery } from '@mantine/hooks';
-import { SectionTitle } from '../SectionTitle/SectionTitle';
 import { Inputs } from './demos/Inputs';
 import { Dates } from './demos/Dates';
 import { Overlays } from './demos/Overlays';
 import { Content } from './demos/Content';
 import { RichText } from './demos/RichText';
 import { Carousel } from './demos/Carousel';
-import useStyles from './Components.styles';
+import { DemoTabs } from '../DemoTabs/DemoTabs';
 
 const data = [
   { demo: Inputs, icon: IconForms, name: 'Inputs', description: '20+ input components' },
@@ -43,63 +40,5 @@ const data = [
 ];
 
 export function Components() {
-  const [shouldAnimate, setShouldAnimate] = useState(false);
-  const { classes, theme } = useStyles({ shouldAnimate });
-  const animationTimeout = useRef<number>();
-  const [active, setActive] = useState(0);
-  const controlSize = useMediaQuery(`(max-width: ${theme.breakpoints.sm}px)`) ? 60 : 80;
-
-  const handleActiveChange = (index: number) => {
-    setActive(index);
-    if (index !== active) {
-      setShouldAnimate(true);
-      window.clearTimeout(animationTimeout.current);
-      animationTimeout.current = window.setTimeout(() => setShouldAnimate(false), 500);
-    }
-  };
-
-  const controls = data.map((item, index) => (
-    <UnstyledButton<'button'>
-      key={item.name}
-      onClick={() => handleActiveChange(index)}
-      data-active={index === active || undefined}
-      className={classes.control}
-    >
-      <div className={classes.controlInner}>
-        <item.icon size={28} stroke={1.5} className={classes.controlIcon} />
-        <div>
-          <Text className={classes.controlTitle}>{item.name}</Text>
-          <Text color="dimmed" size="sm" className={classes.controlDescription}>
-            {item.description}
-          </Text>
-        </div>
-      </div>
-    </UnstyledButton>
-  ));
-
-  const ActiveDemo = data[active].demo;
-
-  return (
-    <div className={classes.root}>
-      <Container size={1100}>
-        <SectionTitle>100+ components</SectionTitle>
-        <Grid gutter={0} mt="md">
-          <Grid.Col md={4}>
-            <div className={classes.controls}>
-              <div
-                className={classes.controlsIndicator}
-                style={{ height: controlSize, transform: `translateY(${controlSize * active}px)` }}
-              />
-              {controls}
-            </div>
-          </Grid.Col>
-          <Grid.Col md={8}>
-            <div className={classes.demo}>
-              <ActiveDemo />
-            </div>
-          </Grid.Col>
-        </Grid>
-      </Container>
-    </div>
-  );
+  return <DemoTabs data={data} />;
 }
