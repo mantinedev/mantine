eda6e7d0243d02c87dccac6914817d0f4265e793
Vitaly Rtishchev <rtivital@gmail.com>
2022-06-25 21:55:03 +0300

    [docs] Add context styles demo to the home page

--- docs/src/components/HomePage/Styles/Styles.tsx
+++ docs/src/components/HomePage/Styles/Styles.tsx
@@ -5,6 +5,7 @@ import { SxDemo } from './demos/SxDemo';
 import { CreateStylesDemo } from './demos/CreateStylesDemo';
 import { ThemeDemo } from './demos/ThemeDemo';
 import { FunctionsDemo } from './demos/FunctionsDemo';
+import { ContextStylesDemo } from './demos/ContextStylesDemo';
 
 export function Styles() {
   return (
@@ -41,6 +42,7 @@ export function Styles() {
           <Tabs.Tab value="createStyles">createStyles</Tabs.Tab>
           <Tabs.Tab value="theme">Theme subscription</Tabs.Tab>
           <Tabs.Tab value="functions">Theme functions</Tabs.Tab>
+          <Tabs.Tab value="contextStyles">Context styles</Tabs.Tab>
         </Tabs.List>
 
         <Tabs.Panel value="sx">
@@ -54,6 +56,9 @@ export function Styles() {
         </Tabs.Panel>
         <Tabs.Panel value="functions">
           <FunctionsDemo />
+        </Tabs.Panel>
+        <Tabs.Panel value="contextStyles">
+          <ContextStylesDemo />
         </Tabs.Panel>
       </Tabs>
     </PageSection>
