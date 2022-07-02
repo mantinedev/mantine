d503ab4ae8f33fa8f6c1ea7f27aebe7956bf8e3f
Vitaly Rtishchev <rtivital@gmail.com>
2022-07-02 20:06:58 +0300

    [@mantine/core] Stack: Add missing props descriptions

--- src/mantine-core/src/components/Stack/Stack.tsx
+++ src/mantine-core/src/components/Stack/Stack.tsx
@@ -7,8 +7,13 @@ import useStyles, { StackStylesParams } from './Stack.styles';
 export interface StackProps
   extends DefaultProps<never, StackStylesParams>,
     React.ComponentPropsWithoutRef<'div'> {
+  /** Key of theme.spacing or number to set gap in px */
   spacing?: MantineNumberSize;
+
+  /** align-items CSS property */
   align?: React.CSSProperties['alignItems'];
+
+  /** justify-content CSS property */
   justify?: React.CSSProperties['justifyContent'];
 }
 
