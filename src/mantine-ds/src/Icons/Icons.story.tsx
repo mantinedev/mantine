175762f0851d614b838b92dae6cfaf7882678c21
Vitaly Rtishchev <rtivital@gmail.com>
2022-07-03 22:25:03 +0300

    [@mantine/ds] Add YarnIcon

--- src/mantine-ds/src/Icons/Icons.story.tsx
+++ src/mantine-ds/src/Icons/Icons.story.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { DiscordIcon } from './DiscordIcon';
 import { GithubIcon } from './GithubIcon';
 import { TwitterIcon } from './TwitterIcon';
+import { NpmIcon } from './NpmIcon';
+import { YarnIcon } from './YarnIcon';
 
 export default { title: 'DS/Icons' };
 
@@ -11,6 +13,8 @@ export function Usage() {
       <DiscordIcon size={40} />
       <GithubIcon size={40} />
       <TwitterIcon size={40} />
+      <NpmIcon size={40} />
+      <YarnIcon size={40} />
     </div>
   );
 }
--- src/mantine-ds/src/Icons/index.ts
+++ src/mantine-ds/src/Icons/index.ts
@@ -2,3 +2,4 @@ export { DiscordIcon } from './DiscordIcon';
 export { TwitterIcon } from './TwitterIcon';
 export { GithubIcon } from './GithubIcon';
 export { NpmIcon } from './NpmIcon';
+export { YarnIcon } from './YarnIcon';
