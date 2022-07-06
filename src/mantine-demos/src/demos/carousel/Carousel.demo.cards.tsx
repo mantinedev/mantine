7ee8ea5f23db200f33a7da95108deaf58736550c
D3SOX <nico@d3sox.me>
2022-07-06 09:48:56 +0200

    [@mantine/carousel] Fix incorrect fontFamily style in cards demo

--- src/mantine-demos/src/demos/carousel/Carousel.demo.cards.tsx
+++ src/mantine-demos/src/demos/carousel/Carousel.demo.cards.tsx
@@ -21,7 +21,7 @@ const useStyles = createStyles((theme) => ({
   },
 
   title: {
-    fontFamily: \`Greycliff CF \${theme.fontFamily}\`,
+    fontFamily: \`Greycliff CF, \${theme.fontFamily}\`,
     fontWeight: 900,
     color: theme.white,
     lineHeight: 1.2,
