2bfb6bf19c559409cf1551111da4d13f4edd176e
Vitaly Rtishchev <rtivital@gmail.com>
2022-07-19 09:38:15 +0300

    [@mantine/demos] Fix outdated Menu demo

--- src/mantine-demos/src/demos/core/Menu/Menu.demo.customControl.tsx
+++ src/mantine-demos/src/demos/core/Menu/Menu.demo.customControl.tsx
@@ -54,18 +54,15 @@ const UserButton = forwardRef<HTMLButtonElement, UserButtonProps>(
 function Demo() {
   return (
     <Group position="center">
-      <Menu
-        withArrow
-        placement="center"
-        control={
+      <Menu withArrow>
+        <Menu.Target>
           <UserButton
             image="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=255&q=80"
             name="Harriette Spoonlicker"
             email="hspoonlicker@outlook.com"
           />
-        }
-      >
-        {/* ...menu items */}
+        </Menu.Target>
+        <DemoMenuItems withTarget={false} />
       </Menu>
     </Group>
   );
