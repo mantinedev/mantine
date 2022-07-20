61ac36acfe7d00197bfd389a7260957aba401b60
Vitaly Rtishchev <rtivital@gmail.com>
2022-07-20 16:26:08 +0300

    [@mantine/ssr] Remove old inject-styles-into-static-markup.ts

--- src/mantine-ssr/src/inject-styles-into-static-markup.ts
+++
@@ -1,8 +0,0 @@
-import type { EmotionCache } from '@mantine/styles';
-import { createStylesServer } from './create-styles-server';
-import { getSSRStyles } from './get-ssr-styles';
-
-export function injectStylesIntoStaticMarkup(markup: string, cache?: EmotionCache) {
-  const server = createStylesServer(cache);
-  return markup.replace('<head>', `<head>${getSSRStyles(markup, server)}`);
-}
