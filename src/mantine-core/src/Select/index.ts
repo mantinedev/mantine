57b1a7dafc9ade32dce40bba006578e2d65da92f
Vitaly Rtishchev <rtivital@gmail.com>
2022-07-21 18:10:26 +0300

    [@mantine/core] Select: Add missing BaseSelectProps type export

--- src/mantine-core/src/Select/index.ts
+++ src/mantine-core/src/Select/index.ts
@@ -2,4 +2,8 @@ export { Select } from './Select';
 export { ChevronIcon as SelectChevronIcon } from './SelectRightSection/ChevronIcon';
 export type { SelectProps } from './Select';
 export type { SelectItemProps } from './DefaultItem/DefaultItem';
-export type { BaseSelectStylesNames as SelectStylesNames, SelectItem } from './types';
+export type {
+  BaseSelectStylesNames as SelectStylesNames,
+  SelectItem,
+  BaseSelectProps,
+} from './types';
