4c06473760d84bd3cd2c0d7968bec26c9b6c2497
qwq <qwq@qwq.cat>
2022-07-26 18:27:21 +0800

    [@mantine/hooks] use-did-update: Optimize fast refresh

--- src/mantine-hooks/src/use-did-update/use-did-update.ts
+++ src/mantine-hooks/src/use-did-update/use-did-update.ts
@@ -3,9 +3,16 @@ import { useEffect, useRef } from 'react';
 export function useDidUpdate(fn: () => void, dependencies?: any[]) {
   const mounted = useRef(false);
 
+  // for react-refresh
+  useEffect(() => {
+    return () => {
+      mounted.current = false;
+    };
+  }, []);
+
   useEffect(() => {
     if (mounted.current) {
-      fn();
+      return fn();
     } else {
       mounted.current = true;
     }
