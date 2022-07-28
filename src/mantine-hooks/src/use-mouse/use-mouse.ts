fe4cae7d11ded3a702ec071161310b3f530ac21a
Siris <40269790+Siris01@users.noreply.github.com>
2022-07-28 18:00:46 +0530

    [@mantine/hooks] use-mouse: Add `resetOnExit` option (#1893)
    * [@mantine/hooks] use-mouse: Add resetOnExit option
    
    * [@mantine/hooks] use-mouse: Use object for options
    
    * [docs] use-mouse: Add documentation for resetOnExit option
    
    * [docs] use-mouse: Add example

--- src/mantine-hooks/src/use-mouse/use-mouse.ts
+++ src/mantine-hooks/src/use-mouse/use-mouse.ts
@@ -1,7 +1,9 @@
 import { useEffect, useRef, useState } from 'react';
 import type { MouseEvent } from 'react';
 
-export function useMouse<T extends HTMLElement = any>() {
+export function useMouse<T extends HTMLElement = any>(
+  options: { resetOnExit?: boolean } = { resetOnExit: false }
+) {
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
   const ref = useRef<T>();
@@ -26,11 +28,17 @@ export function useMouse<T extends HTMLElement = any>() {
     }
   };
 
+  const resetMousePosition = () => setPosition({ x: 0, y: 0 });
+
   useEffect(() => {
     const element = ref?.current ? ref.current : document;
     element.addEventListener('mousemove', setMousePosition as any);
+    if (options.resetOnExit) element.addEventListener('mouseleave', resetMousePosition as any);
 
-    return () => element.removeEventListener('mousemove', setMousePosition as any);
+    return () => {
+      element.removeEventListener('mousemove', setMousePosition as any);
+      if (options.resetOnExit) element.removeEventListener('mouseleave', resetMousePosition as any);
+    };
   }, [ref.current]);
 
   return { ref, ...position };
