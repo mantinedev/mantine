4aec0d24e2714da2957de3334ffc7165c44b4da5
Vitaly Rtishchev <rtivital@gmail.com>
2022-07-28 15:08:32 +0300

    [@mantine/core] Tooltip: Forward className to target element to allow usage with Group and MediaQuery components

--- src/mantine-styles-api/src/styles-api/Tooltip.styles-api.ts
+++ src/mantine-styles-api/src/styles-api/Tooltip.styles-api.ts
@@ -1,6 +1,6 @@
 import type { TooltipStylesNames } from '@mantine/core';
 
 export const Tooltip: Record<TooltipStylesNames, string> = {
-  root: 'Tooltip body',
+  tooltip: 'Tooltip body',
   arrow: 'Tooltip arrow',
 };
