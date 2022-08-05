c9b5e7e7d0f82a0df2daaac1af9cfbf290729721
Vitaly Rtishchev <rtivital@gmail.com>
2022-08-05 14:03:26 +0300

    [@mantine/core] Migrate components to theme.fn.variant from theme.fn.themeColor

--- src/mantine-core/src/Text/Text.story.tsx
+++ src/mantine-core/src/Text/Text.story.tsx
@@ -6,3 +6,7 @@ export default { title: 'Text' };
 export function WithNumberAsSize() {
   return <Text size={112}>112px text</Text>;
 }
+
+export function ColorIndex() {
+  return <Text color="red.1">Red 1 text</Text>;
+}
