b12d1c5940cf832f594fd9508a3e9cbfc1d0223b
Vitaly Rtishchev <rtivital@gmail.com>
2022-08-11 18:33:32 +0300

    [@mantine/core] Indicator: Fix incorrect color applied if color prop is not set

--- src/mantine-core/src/Indicator/Indicator.styles.ts
+++ src/mantine-core/src/Indicator/Indicator.styles.ts
@@ -87,8 +87,11 @@ export default createStyles(
       paddingLeft: withLabel ? theme.spacing.xs / 2 : 0,
       paddingRight: withLabel ? theme.spacing.xs / 2 : 0,
       borderRadius: theme.fn.size({ size: radius, sizes: theme.radius }),
-      backgroundColor: theme.fn.variant({ variant: 'filled', primaryFallback: false, color })
-        .background,
+      backgroundColor: theme.fn.variant({
+        variant: 'filled',
+        primaryFallback: false,
+        color: color || theme.primaryColor,
+      }).background,
       border: withBorder
         ? `2px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white}`
         : undefined,
