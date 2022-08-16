c87e8571f2878924fd6bf3db2bed9192ca8af90d
mdodell <dodellmitchell@gmail.com>
2022-08-16 17:37:27 -0400

    [@mantine/core] Grid: Added responsive order props to demo

--- src/mantine-demos/src/demos/core/Grid/Grid.demo.order.tsx
+++ src/mantine-demos/src/demos/core/Grid/Grid.demo.order.tsx
@@ -8,9 +8,9 @@ import { Grid } from '@mantine/core';
 function Demo() {
   return (
     <Grid>
-      <Col span={3} order={2}>2</Col>
-      <Col span={3} order={3}>3</Col>
-      <Col span={3}>1</Col>
+      <Col span={3} order={2} orderSm={1} orderLg={3}>2</Col>
+      <Col span={3} order={3} orderSm={1} orderLg={2}>3</Col>
+      <Col span={3} order={1} orderSm={3} orderLg={1}>1</Col>
     </Grid>
   );
 }
@@ -19,13 +19,15 @@ function Demo() {
 function Demo() {
   return (
     <Grid>
-      <Col span={3} order={2}>
+      <Col span={3} order={2} orderSm={1} orderLg={3}>
         2
       </Col>
-      <Col span={3} order={3}>
+      <Col span={3} order={3} orderSm={1} orderLg={2}>
         3
       </Col>
-      <Col span={3}>1</Col>
+      <Col span={3} order={1} orderSm={3} orderLg={1}>
+        1
+      </Col>
     </Grid>
   );
 }
