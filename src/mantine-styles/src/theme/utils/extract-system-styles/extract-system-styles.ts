be4933138a3b7e70e2d5a7ce7113901c0aecd70b
ryuhangyeong <hangyeong.ryu@gmail.com>
2022-08-18 20:19:52 +0900

    [@mantine/styles] Improved extractSystemStyles function

--- src/mantine-styles/src/theme/utils/extract-system-styles/extract-system-styles.ts
+++ src/mantine-styles/src/theme/utils/extract-system-styles/extract-system-styles.ts
@@ -1,16 +1,11 @@
 import type { MantineStyleSystemProps } from '../../types';
+import { filterProps } from '../filter-props/filter-props';
 
 export function extractSystemStyles<T extends Record<string, any>>(
   others: MantineStyleSystemProps & T
 ) {
   const { m, mx, my, mt, mb, ml, mr, p, px, py, pt, pb, pl, pr, ...rest } = others;
-  const systemStyles = { m, mx, my, mt, mb, ml, mr, p, px, py, pt, pb, pl, pr };
-
-  Object.keys(systemStyles).forEach((key) => {
-    if (systemStyles[key] === undefined) {
-      delete systemStyles[key];
-    }
-  });
+  const systemStyles = filterProps({ m, mx, my, mt, mb, ml, mr, p, px, py, pt, pb, pl, pr });
 
   return { systemStyles, rest };
 }
