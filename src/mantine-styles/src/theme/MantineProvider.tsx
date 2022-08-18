336840de3a00d1029a27e854bd28d57775c4f66f
Vitaly Rtishchev <rtivital@gmail.com>
2022-08-18 20:39:30 +0300

    [@mantine/styles] Remove emotion cache provider from MantineProvider for now

--- src/mantine-styles/src/theme/MantineProvider.tsx
+++ src/mantine-styles/src/theme/MantineProvider.tsx
@@ -1,13 +1,12 @@
 import React, { createContext, useContext } from 'react';
 import type { EmotionCache } from '@emotion/cache';
-import { ThemeProvider, CacheProvider } from '@emotion/react';
+import { ThemeProvider } from '@emotion/react';
 import { DEFAULT_THEME } from './default-theme';
 import { GlobalStyles } from './GlobalStyles';
 import { MantineCssVariables } from './MantineCssVariables';
 import type { MantineThemeOverride, MantineTheme } from './types';
 import { mergeThemeWithFunctions } from './utils/merge-theme/merge-theme';
 import { filterProps } from './utils/filter-props/filter-props';
-import { defaultMantineEmotionCache } from '../tss/default-emotion-cache';
 import { NormalizeCSS } from './NormalizeCSS';
 
 interface MantineProviderContextType {
@@ -79,14 +78,12 @@ export function MantineProvider({
 
   return (
     <ThemeProvider theme={mergedTheme}>
-      <CacheProvider value={emotionCache || defaultMantineEmotionCache}>
-        <MantineProviderContext.Provider value={{ theme: mergedTheme, emotionCache }}>
-          {withNormalizeCSS && <NormalizeCSS />}
-          {withGlobalStyles && <GlobalStyles theme={mergedTheme} />}
-          {withCSSVariables && <MantineCssVariables theme={mergedTheme} />}
-          {children}
-        </MantineProviderContext.Provider>
-      </CacheProvider>
+      <MantineProviderContext.Provider value={{ theme: mergedTheme, emotionCache }}>
+        {withNormalizeCSS && <NormalizeCSS />}
+        {withGlobalStyles && <GlobalStyles theme={mergedTheme} />}
+        {withCSSVariables && <MantineCssVariables theme={mergedTheme} />}
+        {children}
+      </MantineProviderContext.Provider>
     </ThemeProvider>
   );
 }
