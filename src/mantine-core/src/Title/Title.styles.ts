c92ee9370a4e4c89c9034ff7745a99c95d7ba6c2
Narcha <42248344+Narcha@users.noreply.github.com>
2022-08-20 22:02:25 +0200

    [@mantine/core] Title: Fix `inline` prop being ignored (#2182)
    * [@mantine/core] Title: Fixed `inline` prop
    
    * [@mantine/core] Title: Fixed small error

--- src/mantine-core/src/Title/Title.styles.ts
+++ src/mantine-core/src/Title/Title.styles.ts
@@ -7,6 +7,7 @@ export interface TitleStylesParams {
   element: HeadingElement;
   size: TitleSize;
   weight: React.CSSProperties['fontWeight'];
+  inline: boolean;
 }
 
 function getFontSize(size: TitleSize, element: HeadingElement, theme: MantineTheme) {
@@ -19,19 +20,19 @@ function getFontSize(size: TitleSize, element: HeadingElement, theme: MantineThe
 
 function getLineHeight(size: TitleSize, element: HeadingElement, theme: MantineTheme) {
   if (typeof size !== 'undefined' && size in theme.headings.sizes) {
-    return theme.headings.sizes[element].lineHeight;
+    return theme.headings.sizes[size].lineHeight;
   }
 
   return theme.headings.sizes[element].lineHeight;
 }
 
-export default createStyles((theme, { element, weight, size }: TitleStylesParams) => ({
+export default createStyles((theme, { element, weight, size, inline }: TitleStylesParams) => ({
   root: {
     ...theme.fn.fontStyles(),
     fontFamily: theme.headings.fontFamily,
     fontWeight: weight || theme.headings.sizes[element].fontWeight || theme.headings.fontWeight,
     fontSize: getFontSize(size, element, theme),
-    lineHeight: getLineHeight(size, element, theme),
+    lineHeight: inline ? 1 : getLineHeight(size, element, theme),
     margin: 0,
   },
 }));
