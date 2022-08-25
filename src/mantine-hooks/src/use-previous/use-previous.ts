21feb6d19aa8b9ba21771de35daf0b7267563d43
kabeer05 <kabeerarora13@gmail.com>
2022-08-26 00:57:34 +0530

    [@mantine/hooks] use-previous: Get previous value of a state

---
+++ src/mantine-hooks/src/use-previous/use-previous.ts
@@ -0,0 +1,11 @@
+import { useEffect, useRef } from 'react';
+
+export function usePrevious<T>(value: T): T | undefined {
+  const ref = useRef<T>();
+
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+
+  return ref.current;
+}
