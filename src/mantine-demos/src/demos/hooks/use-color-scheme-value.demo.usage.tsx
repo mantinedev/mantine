7f89bad9b8a83c685c2eb6663c9f4580bc2a3cd8
Aster <x.b.he@accenture.com>
2022-08-25 13:23:25 +0800

    [@mantine/demos] use-color-scheme-value.demo.usage

--- src/mantine-demos/src/demos/hooks/use-color-scheme-value.demo.usage.tsx
+++ src/mantine-demos/src/demos/hooks/use-color-scheme-value.demo.usage.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import { Group, Badge } from '@mantine/core';
-import { useColorSchemeValue, useColorScheme } from '@mantine/hooks';
+import { useColorSchemeValue } from '@mantine/hooks';
 
 const code = `
 import { Badge } from '@mantine/core';
-import { useColorSchemeValue, useColorScheme } from '@mantine/hooks';
+import { Group, Badge } from '@mantine/core';
 
 function Demo() {
-
-  const colorScheme = useColorScheme();
-
   return (
-    Your System  color scheme is {colorScheme}
+    <p>If System color scheme is Light it will be teal</p>
+    <p>If System color scheme is Light it will be blu</p>
     <Badge color={useColorSchemeValue('teal', 'blue')} variant="filled">
       Your Badge color scheme is {useColorSchemeValue('teal', 'blue')}
     </Badge>
@@ -20,14 +18,18 @@ function Demo() {
 `;
 
 function Demo() {
-  const colorScheme = useColorScheme();
   return (
-    <Group position="center">
-      Your System color scheme is {colorScheme}
-      <Badge color={useColorSchemeValue('teal', 'blue')} variant="filled">
-        Your Badge color scheme is{useColorSchemeValue('teal', 'blue')}
-      </Badge>
-    </Group>
+    <>
+      <div style={{ textAlign: 'center' }}>
+        <p>If System color scheme is Light it color will be teal</p>
+        <p>If System color scheme is Dark it color will be blue</p>
+      </div>
+      <Group position="center">
+        <Badge color={useColorSchemeValue('teal', 'blue')} variant="filled">
+          Your Badge color scheme is {useColorSchemeValue('teal', 'blue')}
+        </Badge>
+      </Group>
+    </>
   );
 }
 
