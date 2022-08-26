5cc7ace7c177b89056a5ccc2b6ccf00ae62f4783
kabeer05 <kabeerarora13@gmail.com>
2022-08-26 15:21:28 +0530

    [@mantine/hooks] use-previous: Implement hook 2

--- src/mantine-hooks/src/use-previous/use-previous.test.ts
+++ src/mantine-hooks/src/use-previous/use-previous.test.ts
@@ -1,4 +1,4 @@
-import { renderHook, act } from '@testing-library/react';
+import { renderHook } from '@testing-library/react';
 import { usePrevious } from './use-previous';
 
 describe('@mantine/hooks/use-previous', () => {
