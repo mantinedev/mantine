c66cb1ec675643aa88bc82ef5d2dbffebdc5f143
Meier Lukas <meierschlumpf@gmail.com>
2022-08-27 17:10:32 +0200

    [@mantine/dropzone] Change onDrop type from File to FileWithPath #2250 (#2251)

--- src/mantine-dropzone/src/index.ts
+++ src/mantine-dropzone/src/index.ts
@@ -9,3 +9,4 @@ export type { DropzoneFullScreenProps, DropzoneFullScreenStylesName } from './Dr
 export type { DropzoneStylesParams } from './Dropzone.styles';
 export type { DropzoneAcceptProps, DropzoneRejectProps, DropzoneIdleProps } from './DropzoneStatus';
 export * from './mime-types';
+export { FileWithPath } from 'react-dropzone';
