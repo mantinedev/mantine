d66b20621eea520dac053b4bdb8ca07ed1324d03
Vitaly Rtishchev <rtivital@gmail.com>
2022-08-28 11:58:32 +0300

    [@mantine/form] Add createFormProvider

---
+++ src/mantine-form/src/FormProvider/FormProvider.tsx
@@ -0,0 +1,28 @@
+import React, { createContext, useContext } from 'react';
+import { UseFormReturnType } from '../types';
+
+export interface FormProviderProps<Form> {
+  form: Form;
+  children: React.ReactNode;
+}
+
+export function createFormProvider<Values>() {
+  type Form = UseFormReturnType<Values>;
+
+  const FormContext = createContext<Form>(null);
+
+  function FormProvider({ form, children }: FormProviderProps<Form>) {
+    return <FormContext.Provider value={form}>{children}</FormContext.Provider>;
+  }
+
+  function useFormContext() {
+    const ctx = useContext(FormContext);
+    if (!ctx) {
+      throw new Error('useFormContext was called outside of FormProvider context');
+    }
+
+    return ctx;
+  }
+
+  return [FormProvider, useFormContext] as const;
+}
