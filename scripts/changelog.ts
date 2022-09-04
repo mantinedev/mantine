8f6c0cef1c67972d616c19ae67872c131cec4b9f
Vitaly Rtishchev <rtivital@gmail.com>
2022-09-04 12:35:00 +0400

    [core] Add changelog script

--- scripts/changelog.ts
+++ scripts/changelog.ts
@@ -2,4 +2,17 @@ import simpleGit from 'simple-git';
 
 const git = simpleGit();
 
-console.log(git.status());
+async function getChangelog() {
+  const logs = await git.log({ maxCount: 100 });
+  const messages = logs.all.map((commit) => commit.message);
+  const lastRelease = messages.findIndex((message) => message.includes('release'));
+  const notes = messages
+    .slice(0, lastRelease)
+    .filter((message) => /\[@mantine/.test(message))
+    .map((message) => message.replace('[', '- `[').replace(']', ']`'))
+    .join('\n');
+
+  process.stdout.write(`${notes || 'No significant changes yet'}\n\n`);
+}
+
+getChangelog();
