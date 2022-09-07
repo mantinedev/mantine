e9d201713ef47b5bde9e4d37eb0b43aa95643127
Dhiraj Arun <dhrjarun@gmail.com>
2022-09-07 14:27:39 +0530

    [@matine/styles-api] update Switch style api

--- src/mantine-styles-api/src/styles-api/Switch.styles-api.ts
+++ src/mantine-styles-api/src/styles-api/Switch.styles-api.ts
@@ -4,4 +4,9 @@ export const Switch: Record<SwitchStylesNames, string> = {
   root: 'Root element',
   input: 'Checkbox input',
   label: 'Label element',
+  body: 'Container Of Switch',
+  track: 'Track',
+  thumb: 'Thumb of Switch',
+  onLabel: 'On Label Element',
+  offLabel: 'Off Label Element',
 };
