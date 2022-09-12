98bc4af68d91dfa95b5b1ef4dbc5dc505bbad3d0
Dhiraj Arun <dhrjarun@gmail.com>
2022-09-12 18:15:41 +0530

    [core, demos, docs] Switch.Group: add demo and docs for Switch.Group

--- src/mantine-demos/src/demos/core/Switch/index.ts
+++ src/mantine-demos/src/demos/core/Switch/index.ts
@@ -2,3 +2,4 @@ export { configurator } from './Switch.demo.configurator';
 export { labels } from './Switch.demo.labels';
 export { thumbIcon } from './Switch.demo.thumbIcon';
 export { iconLabels } from './Switch.demo.iconLabels';
+export { groupConfigurator } from './Switch.demo.groupConfigurator';
