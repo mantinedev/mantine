e7b9d1b8a12860a5bc8481bf61fdf34b9f8ea797
Vitaly Rtishchev <rtivital@gmail.com>
2022-09-24 11:52:22 +0400

    [core] Reindex incorrectly named files

--- src/mantine-demos/src/demos/core/Collapse/index.ts
+++ src/mantine-demos/src/demos/core/Collapse/index.ts
@@ -1,4 +1,4 @@
 export { usage } from './Collapse.demo.usage';
 export { transition } from './Collapse.demo.transition';
 export { nested } from './Collapse.demo.nested';
-export { horizontal } from './collapse.demo.horizontal';
+export { horizontal } from './Collapse.demo.horizontal';
