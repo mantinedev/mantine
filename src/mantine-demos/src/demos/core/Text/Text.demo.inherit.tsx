538ffc5094f3f3e082b8842af2f1b495ebacd5e2
G3N <fengyuan.yap@gmail.com>
2022-09-26 22:11:36 +0900

    [docs] Text - update inherit demo to have consistency between example and code example

--- src/mantine-demos/src/demos/core/Text/Text.demo.inherit.tsx
+++ src/mantine-demos/src/demos/core/Text/Text.demo.inherit.tsx
@@ -5,7 +5,13 @@ const code = `
 import { Text, Title } from '@mantine/core';
 
 function Demo() {
-  return <Title order={3}>Highlight <Text color="blue" inherit component="span">something</Text>in title</Title>;
+  return <Title order={3}>
+      Title in which you want to
+      <Text color="blue" inherit component="span">
+        highlight
+      </Text>
+      something
+    </Title>;
 }
 `;
 
