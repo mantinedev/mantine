527e53a41e51cb91c582fa461c2666666afc30a6
Alf <62615304+Alf-Melmac@users.noreply.github.com>
2022-09-29 23:23:08 +0200

    [@mantine/hooks] Fix typo in use-previous test (#2584)

--- src/mantine-hooks/src/use-previous/use-previous.test.ts
+++ src/mantine-hooks/src/use-previous/use-previous.test.ts
@@ -2,7 +2,7 @@ import { renderHook } from '@testing-library/react';
 import { usePrevious } from './use-previous';
 
 describe('@mantine/hooks/use-previous', () => {
-  it('returns undefined on intial render', () => {
+  it('returns undefined on initial render', () => {
     const hook = renderHook(() => usePrevious(1));
     expect(hook.result.current).toBeUndefined();
   });
