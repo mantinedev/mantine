24af2e6bb1cc67a683c4c2d88c254991d846e62e
Vitaly Rtishchev <rtivital@gmail.com>
2022-10-01 13:05:10 +0400

    [@mantine/core] Radio: Cleanup error and description props

--- src/mantine-demos/src/demos/core/Radio/Radio.demo.configurator.tsx
+++ src/mantine-demos/src/demos/core/Radio/Radio.demo.configurator.tsx
@@ -5,14 +5,18 @@ const codeTemplate = (props: string) => `
 import { Radio } from '@mantine/core';
 
 function Demo() {
-  return <Radio value="react" label="Radio button" defaultChecked${props} />
+  return (
+    <Radio
+     ${props}
+    />
+  );
 }
 `;
 
 function Wrapper(props: RadioProps) {
   return (
     <Group position="center">
-      <Radio value="react" label="Radio button" defaultChecked {...props} />
+      <Radio value="react" label="Radio button" {...props} />
     </Group>
   );
 }
@@ -21,8 +25,25 @@ export const configurator: MantineDemo = {
   type: 'configurator',
   component: Wrapper,
   codeTemplate,
+  configuratorProps: {
+    multiline: 3,
+  },
   configurator: [
+    {
+      name: 'labelPosition',
+      type: 'segmented',
+      data: [
+        { value: 'right', label: 'Right' },
+        { value: 'left', label: 'Left' },
+      ],
+      initialValue: 'right',
+      defaultValue: 'right',
+    },
+    { name: 'label', type: 'string', initialValue: 'I agree to sell my privacy' },
+    { name: 'description', type: 'string', initialValue: '' },
+    { name: 'error', type: 'string', initialValue: '' },
     { name: 'size', type: 'size', initialValue: 'sm', defaultValue: 'sm' },
     { name: 'color', type: 'color', initialValue: 'blue', defaultValue: 'blue' },
+    { name: 'checked', type: 'boolean', initialValue: false, defaultValue: false },
   ],
 };
