6c8a8b1403e63140dc41751140c5b79532f23de2
7iomka <7iomka@gmail.com>
2022-10-03 18:24:46 +0300

    [@mantine/core] List: Fix incorrect indentation for nested list items (#2606)
    * [@mantine/core] Fix indentation on List (listStylePosition: 'inside')
    
    * [@mantine/core] List - fix nested items

--- src/mantine-core/src/List/ListItem/ListItem.styles.ts
+++ src/mantine-core/src/List/ListItem/ListItem.styles.ts
@@ -8,10 +8,13 @@ interface ListItemStyles {
 export default createStyles((theme, { spacing, center }: ListItemStyles, getRef) => ({
   itemWrapper: {
     ref: getRef('itemWrapper'),
-    display: 'inline',
+    display: 'inline-flex',
+    flexDirection: 'column',
+    whiteSpace: 'normal',
   },
 
   item: {
+    whiteSpace: 'nowrap',
     lineHeight: center ? 1 : theme.lineHeight,
 
     '&:not(:first-of-type)': {
