81deb37a66a9081fcf67adb72a0a7f3d8c5db7de
Kirill Artihovich <new.shtirlic@mail.ru>
2022-10-03 03:06:50 +0300

    Fix mouse click for closing modal
    Modal should not close on right mouse click, on 4/5 buttons and so on

--- src/mantine-core/src/Modal/Modal.tsx
+++ src/mantine-core/src/Modal/Modal.tsx
@@ -263,7 +263,7 @@ export function Modal(props: ModalProps) {
                   className={classes.overlay}
                   sx={{ position: 'fixed' }}
                   zIndex={0}
-                  onMouseDown={() => closeOnClickOutside && onClose()}
+                  onClick={() => closeOnClickOutside && onClose()}
                   blur={overlayBlur}
                   color={
                     overlayColor ||
