949917982f55ab6a13e6ac0a4d85c78d5efcaa67
wes337 <wesmoses@gmail.com>
2022-10-07 11:53:29 +0300

    [@mantine/core] Slider: fix numeric max and negative numbers

--- src/mantine-core/src/Slider/utils/get-change-value/get-change-value.ts
+++ src/mantine-core/src/Slider/utils/get-change-value/get-change-value.ts
@@ -19,11 +19,16 @@ export function getChangeValue({
     ? value
     : Math.min(Math.max(value, 0), containerWidth) / containerWidth;
   const dx = left * (max - min);
-  const nextValue = (dx !== 0 ? Math.round(dx / step) * step : 0) + min;
 
-  if (precision !== undefined) {
-    return Number(nextValue.toFixed(precision));
+  const valueIsMax = dx + min >= max;
+  if (valueIsMax) {
+    return max;
   }
 
-  return nextValue;
+  const minIsNegative = min <= 0;
+  const nextValue =
+    dx !== 0 ? Math.round(dx / step) * step + (minIsNegative ? min : 0) : Math.min(min, 0);
+  const nextValueWithPrecision = precision ? Number(nextValue.toFixed(precision)) : nextValue;
+
+  return Math.min(nextValueWithPrecision, max);
 }
