9b5cf049946eee16d9724fb171cdbf038f23f0e7
Vitaly Rtishchev <rtivital@gmail.com>
2022-10-12 19:31:49 +0400

    [@mantine/hooks] use-favicon: Remove all favicons before inserting a new one

--- src/mantine-hooks/src/use-favicon/use-favicon.ts
+++ src/mantine-hooks/src/use-favicon/use-favicon.ts
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import { useIsomorphicEffect } from '../use-isomorphic-effect/use-isomorphic-effect';
 
 const MIME_TYPES = {
@@ -8,20 +9,28 @@ const MIME_TYPES = {
 };
 
 export function useFavicon(url: string) {
+  const link = useRef<HTMLLinkElement>();
+
   useIsomorphicEffect(() => {
     if (!url) {
       return;
     }
 
-    const element =
-      document.querySelector<HTMLLinkElement>('link[rel*="icon"]') ||
-      document.createElement('link');
+    if (!link.current) {
+      const existingElements = document.querySelectorAll<HTMLLinkElement>('link[rel*="icon"]');
+      existingElements.forEach((element) => document.head.removeChild(element));
+
+      const element = document.createElement('link');
+      element.rel = 'shortcut icon';
+      link.current = element;
+      document.querySelector('head').appendChild(element);
+    }
 
     const splittedUrl = url.split('.');
-    element.type = MIME_TYPES[splittedUrl[splittedUrl.length - 1].toLowerCase()];
-    element.href = url;
-    element.rel = 'shortcut icon';
-
-    document.querySelector('head').appendChild(element);
+    link.current.setAttribute(
+      'type',
+      MIME_TYPES[splittedUrl[splittedUrl.length - 1].toLowerCase()]
+    );
+    link.current.setAttribute('href', url);
   }, [url]);
 }
