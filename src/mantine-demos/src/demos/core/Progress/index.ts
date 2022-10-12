8c0c0d3f7bc9aaed89a5daff8cb1b796ed64d5be
Vitaly Rtishchev <rtivital@gmail.com>
2022-10-12 11:26:01 +0400

    [docs] Add Progress sections props documentation

--- src/mantine-demos/src/demos/core/Progress/index.ts
+++ src/mantine-demos/src/demos/core/Progress/index.ts
@@ -4,3 +4,4 @@ export { label } from './Progress.demo.label';
 export { animations } from './Progress.demo.animations';
 export { colors } from './Progress.demo.colors';
 export { tooltip } from './Progress.demo.tooltip';
+export { sectionsProps } from './Progress.demo.sectionsProps';
