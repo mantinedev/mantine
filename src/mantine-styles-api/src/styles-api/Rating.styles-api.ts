6103f9a37f61374ad1256a3472437133a6b2a6ee
Dhiraj Arun <dhrjarun@gmail.com>
2022-10-13 11:03:29 +0530

    [mantine/cord] Rating: add Rating style api

---
+++ src/mantine-styles-api/src/styles-api/Rating.styles-api.ts
@@ -0,0 +1,10 @@
+import { RatingStylesNames } from '@mantine/core/src/Rating';
+
+export const Rating: Record<RatingStylesNames, string> = {
+  root: 'Root element',
+  symbolGroup: 'container for fraction symbols',
+  input: 'input element',
+  label: 'label element',
+  symbolLabel: 'A div element inside label for the label text',
+  symbolBody: 'A div element inside label, which hold symbol icon',
+};
