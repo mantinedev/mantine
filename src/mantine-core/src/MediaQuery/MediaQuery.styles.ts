aa72d3ad9432ec8bc1b3dca3bcde279ddb54ee0e
Vitaly Rtishchev <rtivital@gmail.com>
2022-10-14 14:23:27 +0400

    [@mantine/core] MediaQuery: Add option to subscribe to theme

--- src/mantine-core/src/MediaQuery/MediaQuery.styles.ts
+++ src/mantine-core/src/MediaQuery/MediaQuery.styles.ts
@@ -1,9 +1,9 @@
-import { createStyles, MantineNumberSize, CSSObject } from '@mantine/styles';
+import { createStyles, MantineNumberSize, CSSObject, MantineTheme } from '@mantine/styles';
 
 export interface MediaQueryStylesParams {
   smallerThan: MantineNumberSize;
   largerThan: MantineNumberSize;
-  styles: CSSObject;
+  styles: CSSObject | ((theme: MantineTheme) => CSSObject);
   query: string;
 }
 
@@ -12,14 +12,15 @@ export default createStyles(
     const media: CSSObject = {};
     const minWidth = theme.fn.size({ size: largerThan, sizes: theme.breakpoints });
     const maxWidth = theme.fn.size({ size: smallerThan, sizes: theme.breakpoints });
+    const _styles = typeof styles === 'function' ? styles(theme) : styles;
 
     if (largerThan !== undefined && smallerThan !== undefined) {
-      media[`@media (min-width: ${minWidth}px) and (max-width: ${maxWidth - 1}px)`] = styles;
+      media[`@media (min-width: ${minWidth}px) and (max-width: ${maxWidth - 1}px)`] = _styles;
     } else {
       if (largerThan !== undefined) {
         media[
           `@media (min-width: ${theme.fn.size({ size: largerThan, sizes: theme.breakpoints })}px)`
-        ] = styles;
+        ] = _styles;
       }
 
       if (smallerThan !== undefined) {
@@ -27,12 +28,12 @@ export default createStyles(
           `@media (max-width: ${
             theme.fn.size({ size: smallerThan, sizes: theme.breakpoints }) - 1
           }px)`
-        ] = styles;
+        ] = _styles;
       }
     }
 
     if (query) {
-      media[`@media ${query}`] = styles;
+      media[`@media ${query}`] = _styles;
     }
 
     return { media };
