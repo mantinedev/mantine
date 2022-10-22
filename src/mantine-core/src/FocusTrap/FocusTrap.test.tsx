2599b47ed23457018a7f8523e3cd7cbf505398be
Keilo75 <82605039+Keilo75@users.noreply.github.com>
2022-10-22 11:20:58 +0200

    [@mantine/hooks] use-focus-trap: Fix incorrect aria-hidden handling (#2735)
    * [@mantine/hooks] use-focus-trap: Fix aria-hidden="true" not being removed
    
    * [@mantine/hooks] use-focus-trap: Fix aria-hidden not being applied multiple times
    
    * [@mantine/core] FocusTrap: Add test for aria-hidden

--- src/mantine-core/src/FocusTrap/FocusTrap.test.tsx
+++ src/mantine-core/src/FocusTrap/FocusTrap.test.tsx
@@ -51,6 +51,29 @@ describe('@mantine/core/FocusTrap', () => {
     expect(document.body).toHaveFocus();
   });
 
+  it('manages aria-hidden attributes', () => {
+    const adjacentDiv = document.createElement('div');
+    adjacentDiv.setAttribute('data-testid', 'adjacent');
+    document.body.appendChild(adjacentDiv);
+
+    const { rerender } = render(
+      <FocusTrap active>
+        <div />
+      </FocusTrap>
+    );
+
+    const adjacent = screen.getByTestId('adjacent');
+    expect(adjacent).toHaveAttribute('aria-hidden', 'true');
+
+    rerender(
+      <FocusTrap active={false}>
+        <div />
+      </FocusTrap>
+    );
+
+    expect(adjacent).not.toHaveAttribute('aria-hidden');
+  });
+
   it('has correct displayName', () => {
     expect(FocusTrap.displayName).toEqual('@mantine/core/FocusTrap');
   });
