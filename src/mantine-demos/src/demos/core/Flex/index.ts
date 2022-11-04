d6f2977739b3759bd88aee1b0171993903101aa8
Vitaly Rtishchev <rtivital@gmail.com>
2022-11-04 19:55:40 +0400

    [@mantine/demos] Add Flex component demos

---
+++ src/mantine-demos/src/demos/core/Flex/index.ts
@@ -0,0 +1,2 @@
+export { configurator } from './Flex.demo.configurator';
+export { responsive } from './Flex.demo.responsive';
--- src/mantine-demos/src/index.ts
+++ src/mantine-demos/src/index.ts
@@ -46,6 +46,7 @@ export * as DividerDemos from './demos/core/Divider';
 export * as DrawerDemos from './demos/core/Drawer';
 export * as FileButtonDemos from './demos/core/FileButton';
 export * as FileInputDemos from './demos/core/FileInput';
+export * as FlexDemos from './demos/core/Flex';
 export * as FocusTrapDemos from './demos/core/FocusTrap';
 export * as GridDemos from './demos/core/Grid';
 export * as GroupDemos from './demos/core/Group';
