d432077bf590982499dbf6cff681c8cac5eb88aa
Vitaly Rtishchev <rtivital@gmail.com>
2022-11-04 15:57:56 +0400

    [@mantine/demos] Update Text demos to include only style props

--- src/mantine-demos/src/demos/core/Text/index.ts
+++ src/mantine-demos/src/demos/core/Text/index.ts
@@ -1,5 +1,4 @@
 export { usage } from './Text.demo.usage';
-export { dimmed } from './Text.demo.dimmed';
 export { linesConfigurator } from './Text.demo.linesConfigurator';
 export { lineClamp } from './Text.demo.lineClamp';
 export { gradient } from './Text.demo.gradient';
