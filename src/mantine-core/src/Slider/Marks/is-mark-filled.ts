cc111e229de7e99482c449c5de166de652d0c60a
Bob Wade <bobbywadebrown@gmail.com>
2022-11-06 10:11:26 +0100

    [@mantine/core] Slider: fix issue #2877 mark fill inverted slider

--- src/mantine-core/src/Slider/Marks/is-mark-filled.ts
+++ src/mantine-core/src/Slider/Marks/is-mark-filled.ts
@@ -2,10 +2,15 @@ interface IsMarkFilled {
   mark: { value: number; label?: any };
   offset?: number;
   value: number;
+  inverted?: boolean;
 }
 
-export function isMarkFilled({ mark, offset, value }: IsMarkFilled) {
-  return typeof offset === 'number'
+export function isMarkFilled({ mark, offset, value, inverted = false }: IsMarkFilled) {
+  return inverted
+    ? typeof offset === 'number'
+      ? mark.value <= offset || mark.value >= value
+      : mark.value >= value
+    : typeof offset === 'number'
     ? mark.value >= offset && mark.value <= value
     : mark.value <= value;
 }
