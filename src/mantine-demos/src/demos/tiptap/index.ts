4bc21ae8699d00d398f97d3f8fa9257e2b7713d6
Vitaly Rtishchev <rtivital@gmail.com>
2022-11-16 16:38:55 +0400

    [docs] Add floating and bubble menus documentation to tiptap

--- src/mantine-demos/src/demos/tiptap/index.ts
+++ src/mantine-demos/src/demos/tiptap/index.ts
@@ -2,3 +2,5 @@ export { usage } from './TipTap.demo.usage';
 export { colors } from './TipTap.demo.colors';
 export { codeHighlight } from './TipTap.demo.codeHighlight';
 export { codeHighlightStyles } from './TipTap.demo.codeHighlightStyles';
+export { bubbleMenu } from './TipTap.demo.bubbleMenu';
+export { floatingMenu } from './TipTap.demo.floatingMenu';
