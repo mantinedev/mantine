ff9ac7d4ce502b4c94c92132b761870fa7d801ed
Kinbaum <mkinbaum@gmail.com>
2022-11-17 08:45:22 -0500

    fix: running prettier on changed files

--- src/mantine-hooks/src/use-focus-trap/create-aria-hider.ts
+++ src/mantine-hooks/src/use-focus-trap/create-aria-hider.ts
@@ -7,8 +7,8 @@ export function createAriaHider(
   containerNode: HTMLElement,
   selector: string = 'body > :not(script)'
 ) {
-  const rootNodes: Value[] = Array.from<HTMLElement>(document.querySelectorAll(selector))
-    .map((node) => {
+  const rootNodes: Value[] = Array.from<HTMLElement>(document.querySelectorAll(selector)).map(
+    (node) => {
       if (node?.shadowRoot?.contains(containerNode) || node.contains(containerNode)) {
         return undefined;
       }
@@ -20,7 +20,8 @@ export function createAriaHider(
       }
 
       return { node, ariaHidden };
-    });
+    }
+  );
 
   return () => {
     rootNodes.forEach((item) => {
