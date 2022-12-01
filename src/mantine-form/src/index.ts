68472d0f9da9df10e484bab7edcc3bb4f4ec8824
Vitaly Rtishchev <rtivital@gmail.com>
2022-12-01 23:23:46 +0400

    [@mantine/form] Init validators

--- src/mantine-form/src/index.ts
+++ src/mantine-form/src/index.ts
@@ -1,6 +1,7 @@
 export { useForm } from './use-form';
 export { createFormContext } from './FormProvider/FormProvider';
 export { FORM_INDEX } from './form-index';
+export * from './validators';
 
 export { zodResolver } from './resolvers/zod-resolver/zod-resolver';
 export { yupResolver } from './resolvers/yup-resolver/yup-resolver';
