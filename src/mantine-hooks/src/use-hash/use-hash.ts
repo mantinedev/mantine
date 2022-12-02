466a9bc46c5f41bbb4fbafe4ec4c9ff3a25b775c
hunterwilhelm <zicy@zicy.net>
2022-12-02 11:17:06 -0700

    fix formatting

--- src/mantine-hooks/src/use-hash/use-hash.ts
+++ src/mantine-hooks/src/use-hash/use-hash.ts
@@ -1,19 +1,18 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 import { useWindowEvent } from '../use-window-event/use-window-event';
 
-
 export function useHash() {
-  const [hash, setHashValue] = useState<string>("");
+  const [hash, setHashValue] = useState<string>('');
 
   const setHash = (value: string) => {
-    if (!value.startsWith("#")) {
-      value = "#" + value;
+    if (!value.startsWith('#')) {
+      value = '#' + value;
     }
     window.location.hash = value;
     setHashValue(value);
   };
 
-  useWindowEvent("hashchange", () => {
+  useWindowEvent('hashchange', () => {
     const newHash = window.location.hash;
     if (hash !== newHash) {
       setHashValue(hash);
