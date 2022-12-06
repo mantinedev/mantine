02eeb1bd20b5aaee103a3402ec64249b4b71c6f7
Hichem Fantar <hichemfantar2049@gmail.com>
2022-12-06 02:48:59 +0100

    Put back onMouseDown for accessibility but keep onClick event

--- src/mantine-core/src/Select/SelectItems/SelectItems.tsx
+++ src/mantine-core/src/Select/SelectItems/SelectItems.tsx
@@ -76,6 +76,14 @@ export function SelectItems({
               }
             : null
         }
+        onMouseDown={
+          !item.disabled
+            ? (event: React.MouseEvent<HTMLDivElement>) => {
+                event.preventDefault();
+                onItemSelect(item);
+              }
+            : null
+        }
         disabled={item.disabled}
         {...item}
       />
@@ -111,6 +119,10 @@ export function SelectItems({
         data-hovered={hovered === creatableDataIndex || undefined}
         onMouseEnter={() => onItemHover(creatableDataIndex)}
         onClick={(event: React.MouseEvent<HTMLDivElement>) => {
+          event.preventDefault();
+          onItemSelect(creatableDataItem);
+        }}
+        onMouseDown={(event: React.MouseEvent<HTMLDivElement>) => {
           event.preventDefault();
           onItemSelect(creatableDataItem);
         }}
