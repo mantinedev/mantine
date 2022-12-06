3537369218131acec0637d8f692e83389d53b9bd
Vitaly Rtishchev <rtivital@gmail.com>
2022-12-06 14:05:53 +0400

    [@mantine/form] Add hasLength validator

--- src/mantine-form/src/validators/index.ts
+++ src/mantine-form/src/validators/index.ts
@@ -1,2 +1,4 @@
 export { notEmpty } from './not-empty/not-empty';
 export { matches } from './matches/matches';
+export { isEmail } from './is-email/is-email';
+export { hasLength } from './has-length/has-length';
