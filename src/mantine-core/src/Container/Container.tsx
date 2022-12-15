fb36adbf3bd10f1b4986e9713abaca61ca4870dc
Vitaly Rtishchev <rtivital@gmail.com>
2022-12-15 22:23:07 +0400

    [@mantine/core] Container: Add variants and sizes api support

--- src/mantine-core/src/Container/Container.tsx
+++ src/mantine-core/src/Container/Container.tsx
@@ -9,6 +9,8 @@ import { Box } from '../Box';
 import useStyles from './Container.styles';
 
 export interface ContainerProps extends DefaultProps, React.ComponentPropsWithoutRef<'div'> {
+  variant?: string;
+
   /** Predefined container max-width or number for max-width in px */
   size?: MantineNumberSize;
 
@@ -31,13 +33,14 @@ const defaultProps: Partial<ContainerProps> = {
 
 export const Container = forwardRef<HTMLDivElement, ContainerProps>(
   (props: ContainerProps, ref) => {
-    const { className, fluid, size, unstyled, sizes, ...others } = useComponentDefaultProps(
-      'Container',
-      defaultProps,
-      props
+    const { className, fluid, size, unstyled, sizes, variant, ...others } =
+      useComponentDefaultProps('Container', defaultProps, props);
+
+    const { classes, cx } = useStyles(
+      { fluid, size, sizes },
+      { unstyled, name: 'Container', variant, size }
     );
 
-    const { classes, cx } = useStyles({ fluid, size, sizes }, { unstyled, name: 'Container' });
     return <Box className={cx(classes.root, className)} ref={ref} {...others} />;
   }
 );
