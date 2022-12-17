41fed0cf4e2e3488ee7ce9d17242c33176899213
Dmitry Shvetsov <shvetsovdm@gmail.com>
2022-12-17 19:02:59 +0800

    [@mantine/form] Add superstruct support (#2957)
    * [@mantine/form] Add superstruct support
    
    * fix code style issues
    
    * [@mantine/form] update README
    
    add superstruct to the list of supported validation libraries
    
    * [@mantine/demos] add superstructResolver demo
    
    Co-authored-by: Dmitry Shvetsov <d24v@Dmitrys-MacBook-Pro.local>

--- src/mantine-form/src/index.ts
+++ src/mantine-form/src/index.ts
@@ -4,6 +4,7 @@ export { FORM_INDEX } from './form-index';
 export * from './validators';
 
 export { zodResolver } from './resolvers/zod-resolver/zod-resolver';
+export { superstructResolver } from './resolvers/superstruct-resolver/superstruct-resolver';
 export { yupResolver } from './resolvers/yup-resolver/yup-resolver';
 export { joiResolver } from './resolvers/joi-resolver/joi-resolver';
 
