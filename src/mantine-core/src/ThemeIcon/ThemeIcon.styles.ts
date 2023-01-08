1ad1559bf04fd0c8e86e4b525710d94bc7c5b993
Vitaly Rtishchev <rtivital@gmail.com>
2023-01-08 18:32:31 +0400

    [@mantine/core] ThemeIcon: Migrate to rem

--- src/mantine-core/src/ThemeIcon/ThemeIcon.styles.ts
+++ src/mantine-core/src/ThemeIcon/ThemeIcon.styles.ts
@@ -4,6 +4,7 @@ import {
   MantineColor,
   MantineGradient,
   MantineTheme,
+  rem,
 } from '@mantine/styles';
 
 export interface ThemeIconStylesParams {
@@ -14,11 +15,11 @@ export interface ThemeIconStylesParams {
 }
 
 const sizes = {
-  xs: 16,
-  sm: 20,
-  md: 26,
-  lg: 32,
-  xl: 40,
+  xs: rem(16),
+  sm: rem(20),
+  md: rem(26),
+  lg: rem(32),
+  xl: rem(40),
 };
 
 const THEME_ICON_VARIANTS = ['filled', 'light', 'gradient', 'outline', 'default'];
@@ -46,13 +47,13 @@ function getVariantStyles({ theme, variant, color, gradient }: GetVariantStylesI
     backgroundColor: colors.background,
     color: colors.color,
     backgroundImage: variant === 'gradient' ? colors.background : undefined,
-    border: `${variant === 'gradient' ? 0 : 1}px solid ${colors.border}`,
+    border: `${rem(variant === 'gradient' ? 0 : 1)} solid ${colors.border}`,
   };
 }
 
 export default createStyles(
   (theme, { color, radius, gradient }: ThemeIconStylesParams, { variant, size }) => {
-    const iconSize = theme.fn.size({ size, sizes });
+    const iconSize = rem(theme.fn.size({ size, sizes }));
 
     return {
       root: {
