1ca61555334cedb3238d1a96e8a6e351df65c188
Vitaly Rtishchev <rtivital@gmail.com>
2023-01-08 12:31:10 +0400

    [@mantine/core] MediaQuery: Migrate to rem

--- src/mantine-core/src/MediaQuery/MediaQuery.styles.ts
+++ src/mantine-core/src/MediaQuery/MediaQuery.styles.ts
@@ -1,4 +1,11 @@
-import { createStyles, MantineNumberSize, CSSObject, MantineTheme } from '@mantine/styles';
+import {
+  createStyles,
+  MantineNumberSize,
+  CSSObject,
+  MantineTheme,
+  rem,
+  getBreakpointValue,
+} from '@mantine/styles';
 
 export interface MediaQueryStylesParams {
   smallerThan: MantineNumberSize;
@@ -15,19 +22,25 @@ export default createStyles(
     const _styles = typeof styles === 'function' ? styles(theme) : styles;
 
     if (largerThan !== undefined && smallerThan !== undefined) {
-      media[`@media (min-width: ${minWidth}px) and (max-width: ${maxWidth - 1}px)`] = _styles;
+      media[
+        `@media (min-width: ${rem(minWidth)}) and (max-width: ${rem(
+          getBreakpointValue(maxWidth) - 1
+        )})`
+      ] = _styles;
     } else {
       if (largerThan !== undefined) {
         media[
-          `@media (min-width: ${theme.fn.size({ size: largerThan, sizes: theme.breakpoints })}px)`
+          `@media (min-width: ${rem(
+            theme.fn.size({ size: largerThan, sizes: theme.breakpoints })
+          )})`
         ] = _styles;
       }
 
       if (smallerThan !== undefined) {
         media[
-          `@media (max-width: ${
-            theme.fn.size({ size: smallerThan, sizes: theme.breakpoints }) - 1
-          }px)`
+          `@media (max-width: ${rem(
+            getBreakpointValue(theme.fn.size({ size: smallerThan, sizes: theme.breakpoints })) - 1
+          )})`
         ] = _styles;
       }
     }
