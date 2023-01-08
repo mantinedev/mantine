0663aa5ffe017791fb9f01c28cbc2565109137fc
Vitaly Rtishchev <rtivital@gmail.com>
2023-01-08 13:17:37 +0400

    [@mantine/core] Menu: Migrate to rem

--- src/mantine-core/src/Menu/MenuDivider/MenuDivider.styles.ts
+++ src/mantine-core/src/Menu/MenuDivider/MenuDivider.styles.ts
@@ -1,9 +1,9 @@
-import { createStyles } from '@mantine/styles';
+import { createStyles, rem } from '@mantine/styles';
 
 export default createStyles((theme) => ({
   divider: {
-    margin: `calc(${theme.spacing.xs}px / 2) -5px`,
-    borderTop: `1px solid ${
+    margin: `calc(${theme.spacing.xs} / 2) - ${rem(5)}`,
+    borderTop: `${rem(1)} solid ${
       theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]
     }`,
   },
