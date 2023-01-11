f17d70fb918c31550d6c04a88d2845dd2bda2276
Vitaly Rtishchev <rtivital@gmail.com>
2023-01-11 21:30:02 +0400

    [@mantine/demos] Migrate hooks demos to rem

--- src/mantine-demos/src/demos/hooks/use-scroll-into-view.demo.parent.tsx
+++ src/mantine-demos/src/demos/hooks/use-scroll-into-view.demo.parent.tsx
@@ -1,30 +1,13 @@
 import React from 'react';
 import { useScrollIntoView } from '@mantine/hooks';
 import { MantineDemo } from '@mantine/ds';
-import { Button, Text, Group, useMantineTheme, Paper } from '@mantine/core';
+import { Button, Text, Group, Paper, Box } from '@mantine/core';
 
 const code = `
 import { useScrollIntoView } from '@mantine/hooks';
-import { Button, Paper } from '@mantine/core';
+import { Button, Text, Group, Paper, Box } from '@mantine/core';
 
 function Demo() {
-  const { scrollIntoView, targetRef, scrollableRef } = useScrollIntoView();
-
-  return (
-    <>
-      <Paper ref={scrollableRef} style={{ overflowY: 'scroll', height: 300, flex: 1 }}>
-        <Paper ref={targetRef}>Scroll me into view</Paper>
-      </Paper>
-
-      <Button onClick={() => scrollIntoView()}>Scroll to target</Button>
-    </>
-  );
-}
-`;
-
-function Demo() {
-  const theme = useMantineTheme();
-
   const { scrollIntoView, targetRef, scrollableRef } = useScrollIntoView<
     HTMLDivElement,
     HTMLDivElement
@@ -32,20 +15,49 @@ function Demo() {
 
   return (
     <Group position="center">
-      <Paper ref={scrollableRef} style={{ overflowY: 'scroll', height: 300, flex: 1 }}>
-        <div style={{ paddingTop: 260, paddingBottom: 450 }}>
+      <Paper ref={scrollableRef} h={300} sx={{ overflowY: 'scroll', flex: 1 }}>
+        <Box pt={260} pb={450}>
           <Paper
             ref={targetRef}
             p="xl"
-            style={{
+            sx={(theme) => ({
               backgroundColor:
                 theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2],
               width: '100%',
-            }}
+            })}
           >
             <Text>Scroll me into view</Text>
           </Paper>
-        </div>
+        </Box>
+      </Paper>
+      <Button onClick={() => scrollIntoView()}>Scroll to target</Button>
+    </Group>
+  );
+}
+`;
+
+function Demo() {
+  const { scrollIntoView, targetRef, scrollableRef } = useScrollIntoView<
+    HTMLDivElement,
+    HTMLDivElement
+  >();
+
+  return (
+    <Group position="center">
+      <Paper ref={scrollableRef} h={300} sx={{ overflowY: 'scroll', flex: 1 }}>
+        <Box pt={260} pb={450}>
+          <Paper
+            ref={targetRef}
+            p="xl"
+            sx={(theme) => ({
+              backgroundColor:
+                theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2],
+              width: '100%',
+            })}
+          >
+            <Text>Scroll me into view</Text>
+          </Paper>
+        </Box>
       </Paper>
       <Button onClick={() => scrollIntoView()}>Scroll to target</Button>
     </Group>
