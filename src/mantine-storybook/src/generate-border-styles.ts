21de83d1a7210cac3a086c8440d195741d583345
Vitaly Rtishchev <rtivital@gmail.com>
2023-01-17 18:08:22 +0400

    [@mantine/storybook] Remove unused components and functions

--- src/mantine-storybook/src/generate-border-styles.ts
+++
@@ -1,8 +0,0 @@
-import { rem } from '@mantine/core';
-
-export function generateBorderStyles(stylesApi: Record<string, any>) {
-  return Object.keys(stylesApi).reduce((acc, key) => {
-    acc[key] = { border: `${rem(1)} solid #${Math.floor(Math.random() * 16777215).toString(16)}` };
-    return acc;
-  }, {});
-}
--- src/mantine-storybook/src/index.ts
+++ src/mantine-storybook/src/index.ts
@@ -1,4 +1,1 @@
-export { generateBorderStyles } from './generate-border-styles';
-export { InputStylesApiWrapper } from './InputStylesApiWrapper/InputStylesApiWrapper';
 export { WithinOverlays } from './WithinOverlays/WithinOverlays';
-export { SubmitForm } from './SubmitForm/SubmitForm';
