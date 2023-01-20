51c7409eadb5d651c2cd979b3d5eace2d7184a82
Vitaly Rtishchev <rtivital@gmail.com>
2023-01-20 11:32:58 +0400

    [core] Remove storiesOf from all packages

--- src/mantine-core/src/ColorSwatch/ColorSwatch.story.tsx
+++ src/mantine-core/src/ColorSwatch/ColorSwatch.story.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
 import { DEFAULT_THEME } from '@mantine/styles';
 import { Group } from '../Group/Group';
 import { ColorSwatch } from './ColorSwatch';
+
+export default { title: 'ColorSwatch' };
 
 const swatches = Object.keys(DEFAULT_THEME.colors).map((theme) => (
   <ColorSwatch color={DEFAULT_THEME.colors[theme][5]} key={theme} size={20} />
@@ -16,12 +17,19 @@ const transparent = Object.keys(DEFAULT_THEME.colors).map((theme) => (
   />
 ));
 
-storiesOf('ColorSwatch', module)
-  .add('Colors', () => <Group style={{ padding: 15 }}>{swatches}</Group>)
-  .add('Opaque colors', () => <Group style={{ padding: 15 }}>{transparent}</Group>)
-  .add('Custom components', () => (
-    <Group style={{ padding: 15 }}>
+export function Colors() {
+  return <Group p={40}>{swatches}</Group>;
+}
+
+export function Opacity() {
+  return <Group p={40}>{transparent}</Group>;
+}
+
+export function CustomComponent() {
+  return (
+    <Group p={40}>
       <ColorSwatch color="#f300f3" component="button" />
       <ColorSwatch color="#000" component="a" href="https://mantine.dev" />
     </Group>
-  ));
+  );
+}
