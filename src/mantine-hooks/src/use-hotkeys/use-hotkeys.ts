31270f2a183a52fd484df1a56f786c0cb4be8729
colinricardo <colin@colin.fun>
2023-01-23 17:26:36 +0000

    feat: allow shortcuts on contenteditable

--- src/mantine-hooks/src/use-hotkeys/use-hotkeys.ts
+++ src/mantine-hooks/src/use-hotkeys/use-hotkeys.ts
@@ -1,25 +1,38 @@
 import { useEffect } from 'react';
-import { getHotkeyMatcher, getHotkeyHandler, HotkeyItemOptions } from './parse-hotkey';
+import { getHotkeyHandler, getHotkeyMatcher, HotkeyItemOptions } from './parse-hotkey';
 
 export { getHotkeyHandler, HotkeyItemOptions };
 
 export type HotkeyItem = [string, (event: KeyboardEvent) => void, HotkeyItemOptions?];
 
-function shouldFireEvent(event: KeyboardEvent, tagsToIgnore: string[]) {
+function shouldFireEvent(
+  event: KeyboardEvent,
+  tagsToIgnore: string[],
+  triggerOnContentEditable = false
+) {
   if (event.target instanceof HTMLElement) {
+    if (triggerOnContentEditable) {
+      return !tagsToIgnore.includes(event.target.tagName);
+    }
+
     return !event.target.isContentEditable && !tagsToIgnore.includes(event.target.tagName);
   }
+
   return true;
 }
 
 export function useHotkeys(
   hotkeys: HotkeyItem[],
-  tagsToIgnore: string[] = ['INPUT', 'TEXTAREA', 'SELECT']
+  tagsToIgnore: string[] = ['INPUT', 'TEXTAREA', 'SELECT'],
+  triggerOnContentEditable = false
 ) {
   useEffect(() => {
     const keydownListener = (event: KeyboardEvent) => {
       hotkeys.forEach(([hotkey, handler, options = { preventDefault: true }]) => {
-        if (getHotkeyMatcher(hotkey)(event) && shouldFireEvent(event, tagsToIgnore)) {
+        if (
+          getHotkeyMatcher(hotkey)(event) &&
+          shouldFireEvent(event, tagsToIgnore, triggerOnContentEditable)
+        ) {
           if (options.preventDefault) {
             event.preventDefault();
           }
