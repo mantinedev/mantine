d536a2a3c10c6224cf901edd3065f265c2c90b38
Vitaly Rtishchev <rtivital@gmail.com>
2023-02-02 12:44:14 +0400

    [@mantine/core] JsonInput: Add serialize/deserialize functions support

--- src/mantine-core/src/JsonInput/validate-json/validate-json.ts
+++ src/mantine-core/src/JsonInput/validate-json/validate-json.ts
@@ -1,10 +1,10 @@
-export function validateJson(value: string) {
+export function validateJson(value: string, deserialize: typeof JSON.parse) {
   if (typeof value === 'string' && value.trim().length === 0) {
     return true;
   }
 
   try {
-    JSON.parse(value);
+    deserialize(value);
     return true;
   } catch (e) {
     return false;
