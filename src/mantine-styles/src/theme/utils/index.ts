7f1785b63996e79381f7f9f42b7c43637490b911
Vitaly Rtishchev <rtivital@gmail.com>
2023-02-02 12:16:59 +0400

    [@mantine/styles] Add px function

--- src/mantine-styles/src/theme/utils/index.ts
+++ src/mantine-styles/src/theme/utils/index.ts
@@ -1,4 +1,5 @@
 export { getDefaultZIndex } from './get-default-z-index/get-default-z-index';
 export { filterProps } from './filter-props/filter-props';
 export { rem } from './rem/rem';
+export { px } from './px/px';
 export { getSize } from './get-size/get-size';
