820c2790c408f992d78273423139fabd92d2ab93
Jason McGhee <mcghee.j@gmail.com>
2023-02-03 11:25:55 -0800

    lint

--- src/mantine-core/src/MultiSelect/MultiSelect.tsx
+++ src/mantine-core/src/MultiSelect/MultiSelect.tsx
@@ -147,7 +147,7 @@ const defaultProps: Partial<MultiSelectProps> = {
   selectOnBlur: false,
   clearButtonTabIndex: 0,
   positionDependencies: [],
-  dropdownPosition: "flip"
+  dropdownPosition: 'flip',
 };
 
 export const MultiSelect = forwardRef<HTMLInputElement, MultiSelectProps>((props, ref) => {
