cbabea2c805a431f47e9384b6b16838a83588e63
Vitaly Rtishchev <rtivital@gmail.com>
2023-02-05 17:19:48 +0400

    [@mantine/styles] Add em converter function

--- src/mantine-styles/src/theme/utils/rem/rem.test.ts
+++ src/mantine-styles/src/theme/utils/rem/rem.test.ts
@@ -1,4 +1,4 @@
-import { rem } from './rem';
+import { rem, em } from './rem';
 
 describe('@mantine/styles/rem', () => {
   it('converts numbers to rem', () => {
@@ -15,5 +15,23 @@ describe('@mantine/styles/rem', () => {
     expect(rem('2rem')).toBe('2rem');
     expect(rem('10%')).toBe('10%');
     expect(rem('5vh')).toBe('5vh');
+  });
+});
+
+describe('@mantine/styles/em', () => {
+  it('converts numbers to em', () => {
+    expect(em(0)).toBe('0em');
+    expect(em(16)).toBe('1em');
+    expect(em(-32)).toBe('-2em');
+  });
+
+  it('converts string px to ems', () => {
+    expect(em('32px')).toBe('2em');
+  });
+
+  it('does not modify other values', () => {
+    expect(em('2em')).toBe('2em');
+    expect(em('10%')).toBe('10%');
+    expect(em('5vh')).toBe('5vh');
   });
 });
--- src/mantine-styles/src/theme/utils/rem/rem.ts
+++ src/mantine-styles/src/theme/utils/rem/rem.ts
@@ -1,14 +1,19 @@
-export function rem(px: unknown) {
-  if (typeof px === 'number') {
-    return `${px / 16}rem`;
-  }
-
-  if (typeof px === 'string') {
-    const replaced = px.replace('px', '');
-    if (!Number.isNaN(Number(replaced))) {
-      return `${Number(replaced) / 16}rem`;
+function createConverter(units: string) {
+  return (px: unknown) => {
+    if (typeof px === 'number') {
+      return `${px / 16}${units}`;
     }
-  }
 
-  return px as string;
+    if (typeof px === 'string') {
+      const replaced = px.replace('px', '');
+      if (!Number.isNaN(Number(replaced))) {
+        return `${Number(replaced) / 16}${units}`;
+      }
+    }
+
+    return px as string;
+  };
 }
+
+export const rem = createConverter('rem');
+export const em = createConverter('em');
