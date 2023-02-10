38bd6df39e6ec77ed4a21cf3fe0cd037ae34e005
Vitaly Rtishchev <rtivital@gmail.com>
2023-02-10 12:06:40 +0400

    [@mantine/core] Pagination: Add context types

--- src/mantine-core/src/Pagination/Pagination.context.ts
+++ src/mantine-core/src/Pagination/Pagination.context.ts
@@ -1,6 +1,15 @@
 import { createSafeContext } from '@mantine/utils';
 
-interface PaginationContext {}
+interface PaginationContext {
+  range: (number | 'dots')[];
+  active: number;
+  disabled: boolean;
+  onChange(page: number): void;
+  onNext(): void;
+  onPrevious(): void;
+  onFirst(): void;
+  onLast(): void;
+}
 
 export const [PaginationProvider, usePaginationContext] = createSafeContext<PaginationContext>(
   'Pagination.Root component was not found in tree'
