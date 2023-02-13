258c7bdb68bad2005c259bf0d7e08510e6029011
Vitaly Rtishchev <rtivital@gmail.com>
2023-02-13 14:16:10 +0400

    [@mantine/core] Pagination: Update demos

--- src/mantine-demos/src/demos/core/Pagination/Pagination.demo.boundaries.tsx
+++ src/mantine-demos/src/demos/core/Pagination/Pagination.demo.boundaries.tsx
@@ -9,13 +9,13 @@ function Demo() {
   return (
     <>
       <Text mb="xs">1 boundary (default)</Text>
-      <Pagination total={20} boundaries={1} initialPage={10} />
+      <Pagination total={20} boundaries={1} defaultValue={10} />
 
       <Text mt="xl" mb="xs">2 boundaries</Text>
-      <Pagination total={20} boundaries={2} initialPage={10} />
+      <Pagination total={20} boundaries={2} defaultValue={10} />
 
       <Text mt="xl" mb="xs">3 boundaries</Text>
-      <Pagination total={20} boundaries={3} initialPage={10} />
+      <Pagination total={20} boundaries={3} defaultValue={10} />
     </>
   );
 }
@@ -25,17 +25,17 @@ function Demo() {
   return (
     <>
       <Text mb="xs">1 boundary (default)</Text>
-      <Pagination total={20} boundaries={1} initialPage={10} />
+      <Pagination total={20} boundaries={1} defaultValue={10} />
 
       <Text mt="xl" mb="xs">
         2 boundaries
       </Text>
-      <Pagination total={20} boundaries={2} initialPage={10} />
+      <Pagination total={20} boundaries={2} defaultValue={10} />
 
       <Text mt="xl" mb="xs">
         3 boundaries
       </Text>
-      <Pagination total={20} boundaries={3} initialPage={10} />
+      <Pagination total={20} boundaries={3} defaultValue={10} />
     </>
   );
 }
