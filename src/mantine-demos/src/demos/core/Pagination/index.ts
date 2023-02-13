172fe56084bdb670d2499b31aa90294ce1946ab2
Vitaly Rtishchev <rtivital@gmail.com>
2023-02-13 14:48:55 +0400

    [@mantine/demos] Add Pagination icons demos

--- src/mantine-demos/src/demos/core/Pagination/index.ts
+++ src/mantine-demos/src/demos/core/Pagination/index.ts
@@ -4,3 +4,4 @@ export { boundaries } from './Pagination.demo.boundaries';
 export { usage } from './Pagination.demo.usage';
 export { stylesApi } from './Pagination.demo.stylesApi';
 export { composition } from './Pagination.demo.composition';
+export { icons } from './Pagination.demo.icons';
