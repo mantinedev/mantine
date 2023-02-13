2d5997c7e82fab4ed53e5cf117c315950728bb87
Vitaly Rtishchev <rtivital@gmail.com>
2023-02-13 14:23:33 +0400

    [@mantine/demos] Add Pagination composition demo

--- src/mantine-demos/src/demos/core/Pagination/index.ts
+++ src/mantine-demos/src/demos/core/Pagination/index.ts
@@ -3,3 +3,4 @@ export { siblings } from './Pagination.demo.siblings';
 export { boundaries } from './Pagination.demo.boundaries';
 export { usage } from './Pagination.demo.usage';
 export { stylesApi } from './Pagination.demo.stylesApi';
+export { composition } from './Pagination.demo.composition';
