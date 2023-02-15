ec0fe414f03510730bd46397411911487268f098
Vitaly Rtishchev <rtivital@gmail.com>
2023-02-15 10:25:02 +0400

    [@mantine/core] PasswordInput: Hide native browser password reveal button in Edge

--- src/mantine-core/src/PasswordInput/PasswordInput.styles.ts
+++ src/mantine-core/src/PasswordInput/PasswordInput.styles.ts
@@ -34,6 +34,10 @@ export default createStyles((theme, { size, rightSectionWidth }: PasswordInputSt
     left: 0,
     right: 0,
 
+    '&::-ms-revel, &::-ms-clear': {
+      display: 'none',
+    },
+
     '&:focus': {
       outline: 0,
     },
