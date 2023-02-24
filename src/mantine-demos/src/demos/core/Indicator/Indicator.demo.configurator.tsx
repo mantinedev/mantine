232f5637460a41c6cc62ec7206a949f5c51d0839
cyantree - Christoph Schreiber <mail@cyantree.com>
2023-02-24 21:19:02 +0100

    [@mantine/core] Indicator: Remove obsolete space in configurator

--- src/mantine-demos/src/demos/core/Indicator/Indicator.demo.configurator.tsx
+++ src/mantine-demos/src/demos/core/Indicator/Indicator.demo.configurator.tsx
@@ -21,7 +21,7 @@ import { Indicator, Avatar, Group } from '@mantine/core';
 function Demo() {
   return (
     <Group position="center">
-      <Indicator ${props}>
+      <Indicator${props}>
         <Avatar
           size="lg"
           src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=250&q=80"
