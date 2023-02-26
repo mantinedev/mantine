a37d6b590165a681b7c15d4cf639627acc2cfc31
Vitaly Rtishchev <rtivital@gmail.com>
2023-02-26 10:52:27 +0400

    [docs] Update 5.x docs versions

--- docs/src/components/Layout/Header/HeaderDesktop.tsx
+++ docs/src/components/Layout/Header/HeaderDesktop.tsx
@@ -10,7 +10,7 @@ import { useDirectionContext } from '../DirectionContext';
 import useStyles from './HeaderDesktop.styles';
 
 const versions = [
-  { v: 'v5', name: '5.10.4', link: 'https://v5.mantine.dev/' },
+  { v: 'v5', name: '5.10.5', link: 'https://v5.mantine.dev/' },
   { v: 'v4', name: '4.2.12', link: 'https://v4.mantine.dev/' },
   { v: 'v3', name: '3.6.14', link: 'https://v3.mantine.dev/' },
   { v: 'v2', name: '2.5.1', link: 'https://v2.mantine.dev/' },
