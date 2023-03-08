1cdb69098dbe71e512bcf70a470456a464749abe
ignisiitech <ignisii@outlook.com>
2023-03-08 12:24:38 +0000

    [@mantine/core] PinInput: onComplete fires correclty - issue 3714

--- src/mantine-core/src/PinInput/PinInput.test.tsx
+++ src/mantine-core/src/PinInput/PinInput.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { checkAccessibility, itSupportsSystemProps } from '@mantine/tests';
 import { PinInput, PinInputProps } from './PinInput';
 
@@ -18,5 +18,23 @@ describe('@mantine/core/PinInput', () => {
   it('renders correct amount of inputs based on length prop', () => {
     const { container } = render(<PinInput {...defaultProps} length={5} />);
     expect(container.querySelectorAll('.mantine-PinInput-input')).toHaveLength(5);
+  });
+
+  it('onComplete is called on last input', () => {
+    const log = jest.spyOn(global.console, 'log');
+    const { container } = render(
+      <PinInput
+        {...defaultProps}
+        onComplete={(value) => console.log(value)}
+      />
+    );
+
+    expect(container.querySelectorAll('.mantine-PinInput-input')).toHaveLength(4);
+
+    container.querySelectorAll('.mantine-PinInput-input').forEach(element => {
+      fireEvent.change(element, { target: { value: '1' }});
+    });
+
+    expect(log).toHaveBeenCalledWith('1111');
   });
 });
