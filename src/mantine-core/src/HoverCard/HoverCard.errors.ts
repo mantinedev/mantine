edcc3da41794af342e0cbe597df2b9d7b979f2f8
Vitaly Rtishchev <rtivital@gmail.com>
2023-03-08 11:37:55 +0400

    [@mantine/core] Fix incorrect error messages in Popover, HoverCard and Menu components (#3638)

--- src/mantine-core/src/HoverCard/HoverCard.errors.ts
+++ src/mantine-core/src/HoverCard/HoverCard.errors.ts
@@ -1,5 +1,5 @@
 export const HOVER_CARD_ERRORS = {
   context: 'HoverCard component was not found in the tree',
   children:
-    'HoverCard.Target component children should be an element or a component that accepts ref, fragments, strings, numbers and other primitive values are not supported',
+    'HoverCard.Target component children should be an element or a component that accepts ref. Fragments, strings, numbers and other primitive values are not supported',
 };
