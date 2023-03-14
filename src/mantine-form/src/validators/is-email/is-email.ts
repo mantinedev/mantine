4dfd9b442018b02465465d176bf68771b8e57011
Vitaly Rtishchev <rtivital@gmail.com>
2023-03-14 20:23:58 +0400

    [@mantine/form] Make isEmail validation simpler to allow usage of dots and plus signs

--- src/mantine-form/src/validators/is-email/is-email.ts
+++ src/mantine-form/src/validators/is-email/is-email.ts
@@ -2,5 +2,5 @@ import React from 'react';
 import { matches } from '../matches/matches';
 
 export function isEmail(error?: React.ReactNode) {
-  return matches(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,63})+$/, error);
+  return matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, error);
 }
