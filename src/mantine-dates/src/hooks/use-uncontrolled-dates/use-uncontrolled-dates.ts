928c1269c87b25a91f9ff588eca600559f70bb8a
cyantree - Christoph Schreiber <mail@cyantree.com>
2023-04-03 19:15:02 +0200

    [@mantine/dates] useControlledDates: Fix and improve type changing #3919

--- src/mantine-dates/src/hooks/use-uncontrolled-dates/use-uncontrolled-dates.ts
+++ src/mantine-dates/src/hooks/use-uncontrolled-dates/use-uncontrolled-dates.ts
@@ -1,4 +1,5 @@
 import { useUncontrolled } from '@mantine/hooks';
+import { useRef } from 'react';
 import { DatePickerType, DatePickerValue } from '../../types';
 
 interface UseUncontrolledDates<Type extends DatePickerType = 'default'> {
@@ -8,16 +9,63 @@ interface UseUncontrolledDates<Type extends DatePickerType = 'default'> {
   onChange(value: DatePickerValue<Type>): void;
 }
 
+const getEmptyValue = <Type extends DatePickerType = 'default'>(type: Type) =>
+  type === 'range' ? [null, null] : type === 'multiple' ? [] : null;
+
 export function useUncontrolledDates<Type extends DatePickerType = 'default'>({
   type,
   value,
   defaultValue,
   onChange,
 }: UseUncontrolledDates<Type>) {
-  return useUncontrolled<any>({
+  const storedType = useRef<Type>(type);
+  const [_value, _setValue] = useUncontrolled<any>({
     value,
     defaultValue,
     onChange,
-    finalValue: type === 'range' ? [null, null] : type === 'multiple' ? [] : null,
+    finalValue: getEmptyValue(type),
   });
+
+  let _finalValue = _value;
+
+  if (storedType.current !== type) {
+    // Type has changed. Do some checks or resets
+
+    storedType.current = type;
+    if (value === undefined) {
+      // Reset uncontrolled value as types aren't compatible
+      _finalValue = getEmptyValue(type);
+      _setValue(_finalValue);
+    } else if (process.env.NODE_ENV === 'development') {
+      // Throw errors in dev mode in case type of value isn't correct
+      switch (type) {
+        case 'default':
+          if (value !== null && typeof value !== 'string') {
+            // eslint-disable-next-line no-console
+            console.error(
+              '[@mantine/dates/use-uncontrolled-dates] Value must be type of `null` or `string`'
+            );
+          }
+          break;
+        case 'multiple':
+          if (!(value instanceof Array)) {
+            // eslint-disable-next-line no-console
+            console.error(
+              '[@mantine/dates/use-uncontrolled-dates] Value must be type of `string[]`'
+            );
+          }
+          break;
+        case 'range':
+          if (!(value instanceof Array) || value.length !== 2) {
+            // eslint-disable-next-line no-console
+            console.error(
+              '[@mantine/dates/use-uncontrolled-dates] Value must be type of `[string, string]`'
+            );
+          }
+          break;
+      }
+    }
+  }
+
+  return [_finalValue, _setValue];
 }
