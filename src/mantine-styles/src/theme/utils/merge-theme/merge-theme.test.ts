2d40b6f786ef03693792b3921f5261ed4f371c07
7iomka <7iomka@gmail.com>
2023-04-13 03:11:29 +0300

    [@mantine/core] Sort merged breakpoints (test attempt)

--- src/mantine-styles/src/theme/utils/merge-theme/merge-theme.test.ts
+++ src/mantine-styles/src/theme/utils/merge-theme/merge-theme.test.ts
@@ -34,6 +34,15 @@ describe('@mantine/styles/merge-theme', () => {
     });
   });
 
+  it('merges breakpoints with valid order', () => {
+    const themeBase = getThemeBase();
+
+    expect(mergeTheme(themeBase, { breakpoints: { min: '1em', xxl: '999em' } })).toStrictEqual({
+      ...themeBase,
+      breakpoints: { min: '1em', ...themeBase.breakpoints, xxl: '999em' },
+    });
+  });
+
   it('merges headings correctly', () => {
     const themeBase = getThemeBase();
     expect(
