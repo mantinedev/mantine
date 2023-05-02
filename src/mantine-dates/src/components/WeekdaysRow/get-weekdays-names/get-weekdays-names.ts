460cd4dffea5a99a3125fe2203f6278d869f01d7
Ivan Kalinin <12299212+IvanKalinin@users.noreply.github.com>
2023-05-01 23:16:45 -0700

    [@mantine/dates] Add callback function support to `weekdayFormat` prop (#4156)

--- src/mantine-dates/src/components/WeekdaysRow/get-weekdays-names/get-weekdays-names.ts
+++ src/mantine-dates/src/components/WeekdaysRow/get-weekdays-names/get-weekdays-names.ts
@@ -1,9 +1,10 @@
+import React from 'react';
 import dayjs from 'dayjs';
 import type { DayOfWeek } from '../../../types';
 
 interface GetWeekdaysNamesInput {
   locale: string;
-  format?: string;
+  format?: string | ((date: Date) => React.ReactNode);
   firstDayOfWeek?: DayOfWeek;
 }
 
@@ -13,10 +14,14 @@ export function getWeekdayNames({
   firstDayOfWeek = 1,
 }: GetWeekdaysNamesInput) {
   const baseDate = dayjs().day(firstDayOfWeek);
-  const labels: string[] = [];
+  const labels: Array<string | React.ReactNode> = [];
 
   for (let i = 0; i < 7; i += 1) {
-    labels.push(dayjs(baseDate).add(i, 'days').locale(locale).format(format));
+    if (typeof format === 'string') {
+      labels.push(dayjs(baseDate).add(i, 'days').locale(locale).format(format));
+    } else {
+      labels.push(format(dayjs(baseDate).add(i, 'days').toDate()));
+    }
   }
 
   return labels;
