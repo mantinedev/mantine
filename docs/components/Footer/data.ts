d7cbd13de308ceb91b10c98a85ef89214641a52f
Chris Bendel <chrissbendel@gmail.com>
2023-09-18 11:55:24 -0400

    Update changelog link
    I don't know if there is an internal link to @latest anywhere, perhaps could use that instead of `previous-versions`. Feels wrong to hardcode `/changelog/7-0-0` somewhere

--- docs/components/Footer/data.ts
+++ docs/components/Footer/data.ts
@@ -7,7 +7,7 @@ export const FOOTER_LINKS_DATA: LinksGroupProps[] = [
     data: [
       { type: 'next', label: 'Contribute', link: '/pages/contributing/' },
       { type: 'next', label: 'About Mantine', link: '/pages/about/' },
-      { type: 'next', label: 'Changelog', link: '/pages/changelog/' },
+      { type: 'next', label: 'Changelog', link: '/changelog/previous-versions/' },
       { type: 'link', label: 'Releases', link: meta.gitHubLinks.releases },
     ],
   },
