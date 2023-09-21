ae89589939a799ca153c1aa57add1c877ef96c8f
Vitaly Rtishchev <rtivital@gmail.com>
2023-09-21 13:36:22 +0400

    [@mantine/core] PasswordInput: Fix input field not taking entire width of the field in Firefox

--- src/mantine-core/src/components/PasswordInput/PasswordInput.story.tsx
+++ src/mantine-core/src/components/PasswordInput/PasswordInput.story.tsx
@@ -4,11 +4,7 @@ import { PasswordInput } from './PasswordInput';
 export default { title: 'PasswordInput' };
 
 export function Usage() {
-  return (
-    <div style={{ padding: 40, maxWidth: 340 }}>
-      <PasswordInput placeholder="Your password" label="Your password" />
-    </div>
-  );
+  return <PasswordInput placeholder="Your password" label="Your password" />;
 }
 
 export function Unstyled() {
