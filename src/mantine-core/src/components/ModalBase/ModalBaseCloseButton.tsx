a6fce5d5938c11a42598a3de729d494d1aeddfce
Vitaly Rtishchev <rtivital@gmail.com>
2023-09-21 14:42:34 +0400

    [@mantine/core] Modal: Fix `closeButtonProps` not including `CloseButtonProps` (#4793)

--- src/mantine-core/src/components/ModalBase/ModalBaseCloseButton.tsx
+++ src/mantine-core/src/components/ModalBase/ModalBaseCloseButton.tsx
@@ -1,20 +1,26 @@
 import React, { forwardRef } from 'react';
 import cx from 'clsx';
 import { BoxProps, ElementProps } from '../../core';
-import { CloseButton } from '../CloseButton';
+import { CloseButton, CloseButtonProps } from '../CloseButton';
 import { useModalBaseContext } from './ModalBase.context';
 import classes from './ModalBase.module.css';
 
-export interface ModalBaseCloseButtonProps extends BoxProps, ElementProps<'button'> {}
+export interface ModalBaseCloseButtonProps
+  extends CloseButtonProps,
+    BoxProps,
+    ElementProps<'button'> {}
 
 export const ModalBaseCloseButton = forwardRef<HTMLButtonElement, ModalBaseCloseButtonProps>(
-  ({ className, ...others }, ref) => {
+  ({ className, onClick, ...others }, ref) => {
     const ctx = useModalBaseContext();
     return (
       <CloseButton
         ref={ref}
         {...others}
-        onClick={ctx.onClose}
+        onClick={(event) => {
+          ctx.onClose();
+          onClick?.(event);
+        }}
         className={cx({ [classes.close]: !ctx.unstyled }, className)}
         unstyled={ctx.unstyled}
       />
