476135b93d448e85e72827a41b946e220e7c6812
Vitaly Rtishchev <rtivital@gmail.com>
2023-09-25 16:48:44 +0400

    [core] Update changelog script to ignore alpha and beta versions

--- scripts/changelog.ts
+++ scripts/changelog.ts
@@ -1,14 +1,29 @@
 import simpleGit from 'simple-git';
+import chalk from 'chalk';
 import packageJson from '../package.json';
 
 const git = simpleGit();
+
+function getPreviousRelease(message: string) {
+  const splitted = message.split(' ');
+  return splitted[splitted.length - 1];
+}
 
 async function getChangelog() {
   const logs = await git.log({ maxCount: 100 });
   const messages = logs.all.map((commit) => commit.message);
   const lastRelease = messages.findIndex(
-    (message) => message.includes('release') && !message.includes(packageJson.version)
+    (message) =>
+      message.includes('[release]') &&
+      !message.includes(packageJson.version) &&
+      !message.includes('beta') &&
+      !message.includes('alpha')
   );
+
+  process.stdout.write(
+    chalk.cyan(`Previous release: ${getPreviousRelease(messages[lastRelease])}\n\n`)
+  );
+
   const notes = messages
     .slice(0, lastRelease)
     .filter((message) => /\[@mantine/.test(message) && !message.includes('[@mantine/demos]'))
