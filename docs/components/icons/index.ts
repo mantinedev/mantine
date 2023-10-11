329e25ea89f9fccb30d374b6130dc71eb48e24e2
WillKirkmanM <98240335+WillKirkmanM@users.noreply.github.com>
2023-10-11 11:16:17 +0000

    Feat(index.ts): Added T3 Icon

--- docs/components/icons/index.ts
+++ docs/components/icons/index.ts
@@ -1,4 +1,5 @@
 import { NextIcon } from './NextIcon';
+import { T3Icon } from './T3Icon';
 import { ViteIcon } from './ViteIcon';
 import { GatsbyIcon } from './GatsbyIcon';
 import { RemixIcon } from './RemixIcon';
@@ -7,6 +8,7 @@ import type { Template } from '../MdxProvider/MdxTemplatesList/data';
 
 export const frameworkIcons: Record<Template['type'], typeof NextIcon> = {
   next: NextIcon,
+  t3: T3Icon,
   vite: ViteIcon,
   gatsby: GatsbyIcon,
   redwood: RedwoodIcon,
