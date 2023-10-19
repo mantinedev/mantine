b77aeb5a409d8137aacb29beab3e5258acd76d13
Vitaly Rtishchev <rtivital@gmail.com>
2023-10-19 18:04:04 +0400

    [docs] Update Switch documentation

--- src/mantine-demos/src/demos/core/Switch/index.ts
+++ src/mantine-demos/src/demos/core/Switch/index.ts
@@ -4,3 +4,4 @@ export { thumbIcon } from './Switch.demo.thumbIcon';
 export { iconLabels } from './Switch.demo.iconLabels';
 export { groupConfigurator } from './Switch.demo.groupConfigurator';
 export { stylesApi } from './Switch.demo.stylesApi';
+export { tooltip } from './Switch.demo.tooltip';
