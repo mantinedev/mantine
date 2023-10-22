aab71ff99111b48146b0af7a3ce786af30f59db8
Vitaly Rtishchev <rtivital@gmail.com>
2023-10-22 13:30:23 +0400

    [@mantine/carousel] Fix `slideSize` prop not handling number values (#5050)

--- src/mantine-carousel/src/Carousel.story.tsx
+++ src/mantine-carousel/src/Carousel.story.tsx
@@ -9,17 +9,17 @@ export default { title: 'Carousel' };
 
 const slides = (
   <>
-    <Carousel.Slide>Slide 1</Carousel.Slide>
-    <Carousel.Slide>Slide 2</Carousel.Slide>
-    <Carousel.Slide>Slide 3</Carousel.Slide>
-    <Carousel.Slide>Slide 4</Carousel.Slide>
+    <Carousel.Slide bg="red">Slide 1</Carousel.Slide>
+    <Carousel.Slide bg="red">Slide 2</Carousel.Slide>
+    <Carousel.Slide bg="red">Slide 3</Carousel.Slide>
+    <Carousel.Slide bg="red">Slide 4</Carousel.Slide>
   </>
 );
 
 export function Usage() {
   return (
     <div style={{ padding: 40, maxWidth: 500 }}>
-      <Carousel slideSize="70%" slideGap="md" height={200}>
+      <Carousel slideSize={70} slideGap="md" height={200}>
         {slides}
       </Carousel>
     </div>
