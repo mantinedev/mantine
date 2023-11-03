1457217b61e26a2636515d9936563856b1516b38
Vitaly Rtishchev <rtivital@gmail.com>
2023-11-03 14:40:41 +0400

    [@mantine/ds] Add option to customize header controls though props

--- src/mantine-ds/src/HeaderControl/DiscordControl.tsx
+++ src/mantine-ds/src/HeaderControl/DiscordControl.tsx
@@ -4,14 +4,13 @@ import { DiscordIcon } from '../Icons';
 import { meta } from '../meta';
 import classes from './DiscordControl.module.css';
 
-export function DiscordControl() {
+interface DiscordControlProps {
+  link?: string;
+}
+
+export function DiscordControl({ link = meta.discordLink }: DiscordControlProps) {
   return (
-    <HeaderControl
-      tooltip="Discord"
-      component="a"
-      href={meta.discordLink}
-      className={classes.discord}
-    >
+    <HeaderControl tooltip="Discord" component="a" href={link} className={classes.discord}>
       <DiscordIcon size={20} />
     </HeaderControl>
   );
