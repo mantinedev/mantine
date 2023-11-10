efc238b9e1bc00b65b5dd8b231bef6525401b1dc
Vitaly Rtishchev <rtivital@gmail.com>
2023-11-10 15:19:04 +0400

    [docs] Add more MultiSelect demos

---
+++ src/mantine-demos/src/demos/core/MultiSelect/MultiSelect.demo.dropdownOpened.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { MantineDemo } from '@mantine/ds';
+import { useDisclosure } from '@mantine/hooks';
+import { MultiSelect, Button } from '@mantine/core';
+
+const code = `
+import { MultiSelect, Button } from '@mantine/core';
+
+function Demo() {
+  const [dropdownOpened, { toggle }] = useDisclosure();
+  return (
+    <>
+      <Button onClick={toggle} mb="md">
+        Toggle dropdown
+      </Button>
+
+      <MultiSelect
+        label="Your favorite library"
+        placeholder="Pick values"
+        data={['React', 'Angular', 'Vue', 'Svelte']}
+        dropdownOpened={dropdownOpened}
+      />
+    </>
+  );
+}
+`;
+
+function Demo() {
+  const [dropdownOpened, { toggle }] = useDisclosure();
+  return (
+    <>
+      <Button onClick={toggle} mb="md">
+        Toggle dropdown
+      </Button>
+
+      <MultiSelect
+        label="Your favorite library"
+        placeholder="Pick values"
+        data={['React', 'Angular', 'Vue', 'Svelte']}
+        dropdownOpened={dropdownOpened}
+      />
+    </>
+  );
+}
+
+export const dropdownOpened: MantineDemo = {
+  type: 'code',
+  component: Demo,
+  code,
+  maxWidth: 340,
+  centered: true,
+};
