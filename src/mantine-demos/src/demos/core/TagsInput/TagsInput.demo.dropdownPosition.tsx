dbcdb44298f7c315cf0ef31aa480d3ed2b16a398
Vitaly Rtishchev <rtivital@gmail.com>
2023-11-10 15:32:48 +0400

    [docs] Improve TagsInput documentation

---
+++ src/mantine-demos/src/demos/core/TagsInput/TagsInput.demo.dropdownPosition.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { MantineDemo } from '@mantine/ds';
+import { TagsInput } from '@mantine/core';
+
+const code = `
+import { TagsInput } from '@mantine/core';
+
+function Demo() {
+  return (
+    <TagsInput
+      label="Your favorite library"
+      placeholder="Pick value or enter anything"
+      data={['React', 'Angular', 'Vue', 'Svelte']}
+      comboboxProps={{ position: 'top', middlewares: { flip: false, shift: false } }}
+    />
+  );
+}
+`;
+
+function Demo() {
+  return (
+    <TagsInput
+      label="Your favorite library"
+      placeholder="Pick value or enter anything"
+      data={['React', 'Angular', 'Vue', 'Svelte']}
+      comboboxProps={{ position: 'top', middlewares: { flip: false, shift: false } }}
+    />
+  );
+}
+
+export const dropdownPosition: MantineDemo = {
+  type: 'code',
+  component: Demo,
+  code,
+  maxWidth: 340,
+  centered: true,
+};
---
+++ src/mantine-demos/src/demos/core/TagsInput/TagsInput.demo.dropdownShadow.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { MantineDemo } from '@mantine/ds';
+import { TagsInput } from '@mantine/core';
+
+const code = `
+import { TagsInput } from '@mantine/core';
+
+function Demo() {
+  return (
+    <TagsInput
+      label="Your favorite library"
+      placeholder="Pick value or enter anything"
+      data={['React', 'Angular', 'Vue', 'Svelte']}
+      comboboxProps={{ shadow: 'md' }}
+    />
+  );
+}
+`;
+
+function Demo() {
+  return (
+    <TagsInput
+      label="Your favorite library"
+      placeholder="Pick value or enter anything"
+      data={['React', 'Angular', 'Vue', 'Svelte']}
+      comboboxProps={{ shadow: 'md' }}
+    />
+  );
+}
+
+export const dropdownShadow: MantineDemo = {
+  type: 'code',
+  component: Demo,
+  code,
+  maxWidth: 340,
+  centered: true,
+};
