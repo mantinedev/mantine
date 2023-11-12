1d3580f4cee0117385a7745715ef021ffe9ef95e
I.B. Dwi <hi@ibedwi.dev>
2023-11-12 11:48:12 +0700

    chore: clean up comments

--- src/mantine-code-highlight-lite/src/use-highlight.ts
+++ src/mantine-code-highlight-lite/src/use-highlight.ts
@@ -1,10 +1,5 @@
 import { useState, useEffect, useContext } from 'react';
-// import hljs from 'highlight.js/lib/core';
-// import hljs from 'highlight.js/lib/core';
-// import plaintext from 'highlight.js/lib/languages/plaintext';
 import { HighlightContext } from './HighlightProvider';
-// import hljs from 'highlight.js';
-// hljs.registerLanguage('plaintext', plaintext);
 
 interface UseHighlightInput {
   code: string;
@@ -12,25 +7,11 @@ interface UseHighlightInput {
   highlightOnClient: boolean | undefined;
 }
 
-// async function registerLanguage(language: string) {
-//   await import(`/node_modules/highlight.js/lib/languages/${language}.js`).then((module) => {
-//     hljs.registerLanguage(language, module.default);
-//   });
-// }
-
 export function useHighlight({ code, language, highlightOnClient }: UseHighlightInput) {
   const highlight = useContext(HighlightContext);
   const { highlight: hljs } = highlight;
 
-  console.log('with core');
-  console.log('hljs.listLanguage', hljs!.listLanguages());
-  // if (!hljs.getLanguage(language)) {
-  //   console.warn('registering language', language);
-  //   registerLanguage(language);
-  // }
-
   const lang = hljs?.getLanguage(language) ? language : 'plaintext';
-  console.log('lang', lang);
 
   const getHighlightedCode = () => hljs!.highlight(code.trim(), { language: lang }).value;
   const [highlighted, setHighlighted] = useState(!highlightOnClient);
