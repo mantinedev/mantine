9deb4e172c56eb1b7027d7cae11485588d8c93fd
Vitaly Rtishchev <rtivital@gmail.com>
2023-11-17 20:46:46 +0400

    [core] Move @tests to packages folder

--- jest.config.cjs
+++ jest.config.cjs
@@ -8,7 +8,7 @@ module.exports = {
   globalSetup: './jest.setup.cjs',
   moduleNameMapper: {
     '@mantine/(.*)': '<rootDir>/src/mantine-$1/src',
-    '@tests/(.*)': '<rootDir>/@tests/$1/src',
+    '@tests/(.*)': '<rootDir>/packages/@tests/$1/src',
     '\\.(css)$': 'identity-obj-proxy',
   },
 };
