c4e26f0a78cffd1e95b1e50f44c148f7b83b1cfe
Vitaly Rtishchev <rtivital@gmail.com>
2023-11-19 14:58:36 +0400

    [core] Update tsconfig files of all packages

--- scripts/build/generate-dts.ts
+++ scripts/build/generate-dts.ts
@@ -5,13 +5,10 @@ import fs from 'fs-extra';
 
 export async function generateDts(packagePath: string) {
   await execa('yarn', ['tsc', '--project', path.join(packagePath, 'tsconfig.build.json')]);
-  const files = await fg(['lib/**/*.js'], { cwd: packagePath });
 
   // Duplicate the type definitions for ESM
   await fs.copy(
     path.join(packagePath, 'lib/index.d.ts'),
     path.join(packagePath, 'lib/index.d.mts')
   );
-
-  return Promise.all(files.map((file) => fs.remove(path.join(packagePath, file))));
 }
