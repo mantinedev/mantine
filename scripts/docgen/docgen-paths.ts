3b21287d0daae2b1c842f0fdffe98c7f96dd8c14
Vitaly Rtishchev <rtivital@gmail.com>
2023-11-19 10:42:19 +0400

    [core] Update docgen script with new paths

--- scripts/docgen/docgen-paths.ts
+++ scripts/docgen/docgen-paths.ts
@@ -3,128 +3,128 @@ import { getPath, getPaths } from '../utils/get-path';
 
 const FILES_PATHS = getPaths([
   // Input
-  'src/mantine-core/src/components/Input/InputLabel/InputLabel.tsx',
-  'src/mantine-core/src/components/Input/InputWrapper/InputWrapper.tsx',
-  'src/mantine-core/src/components/Input/InputDescription/InputDescription.tsx',
-  'src/mantine-core/src/components/Input/InputError/InputError.tsx',
+  'packages/@mantine/core/src/components/Input/InputLabel/InputLabel.tsx',
+  'packages/@mantine/core/src/components/Input/InputWrapper/InputWrapper.tsx',
+  'packages/@mantine/core/src/components/Input/InputDescription/InputDescription.tsx',
+  'packages/@mantine/core/src/components/Input/InputError/InputError.tsx',
 
   // Button
-  'src/mantine-core/src/components/Button/ButtonGroup/ButtonGroup.tsx',
+  'packages/@mantine/core/src/components/Button/ButtonGroup/ButtonGroup.tsx',
 
   // ActionIcon
-  'src/mantine-core/src/components/ActionIcon/ActionIconGroup/ActionIconGroup.tsx',
+  'packages/@mantine/core/src/components/ActionIcon/ActionIconGroup/ActionIconGroup.tsx',
 
   // Popover
-  'src/mantine-core/src/components/Popover/PopoverTarget/PopoverTarget.tsx',
-  'src/mantine-core/src/components/Popover/PopoverDropdown/PopoverDropdown.tsx',
+  'packages/@mantine/core/src/components/Popover/PopoverTarget/PopoverTarget.tsx',
+  'packages/@mantine/core/src/components/Popover/PopoverDropdown/PopoverDropdown.tsx',
 
   // Slider
-  'src/mantine-core/src/components/Slider/Slider/Slider.tsx',
-  'src/mantine-core/src/components/Slider/RangeSlider/RangeSlider.tsx',
+  'packages/@mantine/core/src/components/Slider/Slider/Slider.tsx',
+  'packages/@mantine/core/src/components/Slider/RangeSlider/RangeSlider.tsx',
 
   // Switch
-  'src/mantine-core/src/components/Switch/SwitchGroup/SwitchGroup.tsx',
+  'packages/@mantine/core/src/components/Switch/SwitchGroup/SwitchGroup.tsx',
 
   // Checkbox
-  'src/mantine-core/src/components/Checkbox/CheckboxGroup/CheckboxGroup.tsx',
+  'packages/@mantine/core/src/components/Checkbox/CheckboxGroup/CheckboxGroup.tsx',
 
   // Radio
-  'src/mantine-core/src/components/Radio/RadioGroup/RadioGroup.tsx',
+  'packages/@mantine/core/src/components/Radio/RadioGroup/RadioGroup.tsx',
 
   // Tabs
-  'src/mantine-core/src/components/Tabs/TabsTab/TabsTab.tsx',
-  'src/mantine-core/src/components/Tabs/TabsList/TabsList.tsx',
-  'src/mantine-core/src/components/Tabs/TabsPanel/TabsPanel.tsx',
+  'packages/@mantine/core/src/components/Tabs/TabsTab/TabsTab.tsx',
+  'packages/@mantine/core/src/components/Tabs/TabsList/TabsList.tsx',
+  'packages/@mantine/core/src/components/Tabs/TabsPanel/TabsPanel.tsx',
 
   // Accordion
-  'src/mantine-core/src/components/Accordion/AccordionItem/AccordionItem.tsx',
-  'src/mantine-core/src/components/Accordion/AccordionControl/AccordionControl.tsx',
+  'packages/@mantine/core/src/components/Accordion/AccordionItem/AccordionItem.tsx',
+  'packages/@mantine/core/src/components/Accordion/AccordionControl/AccordionControl.tsx',
 
   // Pill
-  'src/mantine-core/src/components/Pill/PillGroup/PillGroup.tsx',
+  'packages/@mantine/core/src/components/Pill/PillGroup/PillGroup.tsx',
 
   // PillsInput
-  'src/mantine-core/src/components/PillsInput/PillsInputField/PillsInputField.tsx',
+  'packages/@mantine/core/src/components/PillsInput/PillsInputField/PillsInputField.tsx',
 
   // Combobox
-  'src/mantine-core/src/components/Combobox/ComboboxOption/ComboboxOption.tsx',
-  'src/mantine-core/src/components/Combobox/ComboboxDropdown/ComboboxDropdown.tsx',
-  'src/mantine-core/src/components/Combobox/ComboboxTarget/ComboboxTarget.tsx',
-  'src/mantine-core/src/components/Combobox/ComboboxDropdownTarget/ComboboxDropdownTarget.tsx',
-  'src/mantine-core/src/components/Combobox/ComboboxEventsTarget/ComboboxEventsTarget.tsx',
-  'src/mantine-core/src/components/Combobox/ComboboxDropdown/ComboboxDropdown.tsx',
-  'src/mantine-core/src/components/Combobox/ComboboxGroup/ComboboxGroup.tsx',
+  'packages/@mantine/core/src/components/Combobox/ComboboxOption/ComboboxOption.tsx',
+  'packages/@mantine/core/src/components/Combobox/ComboboxDropdown/ComboboxDropdown.tsx',
+  'packages/@mantine/core/src/components/Combobox/ComboboxTarget/ComboboxTarget.tsx',
+  'packages/@mantine/core/src/components/Combobox/ComboboxDropdownTarget/ComboboxDropdownTarget.tsx',
+  'packages/@mantine/core/src/components/Combobox/ComboboxEventsTarget/ComboboxEventsTarget.tsx',
+  'packages/@mantine/core/src/components/Combobox/ComboboxDropdown/ComboboxDropdown.tsx',
+  'packages/@mantine/core/src/components/Combobox/ComboboxGroup/ComboboxGroup.tsx',
 
   // AppShell
-  'src/mantine-core/src/components/AppShell/AppShellAside/AppShellAside.tsx',
-  'src/mantine-core/src/components/AppShell/AppShellNavbar/AppShellNavbar.tsx',
-  'src/mantine-core/src/components/AppShell/AppShellHeader/AppShellHeader.tsx',
-  'src/mantine-core/src/components/AppShell/AppShellFooter/AppShellFooter.tsx',
-  'src/mantine-core/src/components/AppShell/AppShellSection/AppShellSection.tsx',
+  'packages/@mantine/core/src/components/AppShell/AppShellAside/AppShellAside.tsx',
+  'packages/@mantine/core/src/components/AppShell/AppShellNavbar/AppShellNavbar.tsx',
+  'packages/@mantine/core/src/components/AppShell/AppShellHeader/AppShellHeader.tsx',
+  'packages/@mantine/core/src/components/AppShell/AppShellFooter/AppShellFooter.tsx',
+  'packages/@mantine/core/src/components/AppShell/AppShellSection/AppShellSection.tsx',
 
   // Grid
-  'src/mantine-core/src/components/Grid/GridCol/GridCol.tsx',
+  'packages/@mantine/core/src/components/Grid/GridCol/GridCol.tsx',
 
   // HoverCard
-  'src/mantine-core/src/components/HoverCard/HoverCardTarget/HoverCardTarget.tsx',
+  'packages/@mantine/core/src/components/HoverCard/HoverCardTarget/HoverCardTarget.tsx',
 
   // Menu
-  'src/mantine-core/src/components/Menu/MenuItem/MenuItem.tsx',
-  'src/mantine-core/src/components/Menu/MenuTarget/MenuTarget.tsx',
+  'packages/@mantine/core/src/components/Menu/MenuItem/MenuItem.tsx',
+  'packages/@mantine/core/src/components/Menu/MenuTarget/MenuTarget.tsx',
 
   // Progress
-  'src/mantine-core/src/components/Progress/ProgressSection/ProgressSection.tsx',
-  'src/mantine-core/src/components/Progress/ProgressRoot/ProgressRoot.tsx',
+  'packages/@mantine/core/src/components/Progress/ProgressSection/ProgressSection.tsx',
+  'packages/@mantine/core/src/components/Progress/ProgressRoot/ProgressRoot.tsx',
 
   // Chip
-  'src/mantine-core/src/components/Chip/ChipGroup/ChipGroup.tsx',
+  'packages/@mantine/core/src/components/Chip/ChipGroup/ChipGroup.tsx',
 
   // Card
-  'src/mantine-core/src/components/Card/CardSection/CardSection.tsx',
+  'packages/@mantine/core/src/components/Card/CardSection/CardSection.tsx',
 
   // Stepper
-  'src/mantine-core/src/components/Stepper/StepperStep/StepperStep.tsx',
+  'packages/@mantine/core/src/components/Stepper/StepperStep/StepperStep.tsx',
 
   // Timeline
-  'src/mantine-core/src/components/Timeline/TimelineItem/TimelineItem.tsx',
+  'packages/@mantine/core/src/components/Timeline/TimelineItem/TimelineItem.tsx',
 
   // List
-  'src/mantine-core/src/components/List/ListItem/ListItem.tsx',
+  'packages/@mantine/core/src/components/List/ListItem/ListItem.tsx',
 
   // Spotlight
-  'src/mantine-spotlight/src/Spotlight.tsx',
-  'src/mantine-spotlight/src/SpotlightAction.tsx',
-  'src/mantine-spotlight/src/SpotlightActionsGroup.tsx',
-  'src/mantine-spotlight/src/SpotlightRoot.tsx',
-  'src/mantine-spotlight/src/SpotlightSearch.tsx',
+  'packages/@mantine/spotlight/src/Spotlight.tsx',
+  'packages/@mantine/spotlight/src/SpotlightAction.tsx',
+  'packages/@mantine/spotlight/src/SpotlightActionsGroup.tsx',
+  'packages/@mantine/spotlight/src/SpotlightRoot.tsx',
+  'packages/@mantine/spotlight/src/SpotlightSearch.tsx',
 
   // Carousel
-  'src/mantine-carousel/src/Carousel.tsx',
+  'packages/@mantine/carousel/src/Carousel.tsx',
 
   // Dropzone
-  'src/mantine-dropzone/src/Dropzone.tsx',
-  'src/mantine-dropzone/src/DropzoneFullScreen.tsx',
+  'packages/@mantine/dropzone/src/Dropzone.tsx',
+  'packages/@mantine/dropzone/src/DropzoneFullScreen.tsx',
 
   // CodeHighlight
-  'src/mantine-code-highlight/src/CodeHighlight.tsx',
-  'src/mantine-code-highlight/src/CodeHighlightTabs.tsx',
-  'src/mantine-code-highlight/src/InlineCodeHighlight.tsx',
+  'packages/@mantine/code-highlight/src/CodeHighlight.tsx',
+  'packages/@mantine/code-highlight/src/CodeHighlightTabs.tsx',
+  'packages/@mantine/code-highlight/src/InlineCodeHighlight.tsx',
 
   // Nprogress
-  'src/mantine-nprogress/src/NavigationProgress.tsx',
+  'packages/@mantine/nprogress/src/NavigationProgress.tsx',
 
   // Modals
-  'src/mantine-modals/src/ModalsProvider.tsx',
+  'packages/@mantine/modals/src/ModalsProvider.tsx',
 
   // Tiptap
-  'src/mantine-tiptap/src/RichTextEditor.tsx',
+  'packages/@mantine/tiptap/src/RichTextEditor.tsx',
 
   // Notifications
-  'src/mantine-notifications/src/Notifications.tsx',
+  'packages/@mantine/notifications/src/Notifications.tsx',
 ]);
 
 export const DOCGEN_PATHS = getDeclarationsPaths([
-  { type: 'package', path: getPath('src/mantine-core/src/components') },
-  { type: 'package', path: getPath('src/mantine-dates/src/components') },
+  { type: 'package', path: getPath('packages/@mantine/core/src/components') },
+  { type: 'package', path: getPath('packages/@mantine/dates/src/components') },
   ...FILES_PATHS.map((filePath) => ({ type: 'file' as const, path: filePath })),
 ]);
