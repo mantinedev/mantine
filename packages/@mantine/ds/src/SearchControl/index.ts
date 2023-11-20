e12e7e901cd177321234a94f2231470972bc6185
Vitaly Rtishchev <rtivital@gmail.com>
2023-11-20 15:00:21 +0400

    [core] Remove header components from ds package

--- packages/@mantine/ds/src/SearchControl/index.ts
+++
@@ -1,1 +0,0 @@
-export { SearchControl } from './SearchControl';
--- packages/@mantine/ds/src/index.ts
+++ packages/@mantine/ds/src/index.ts
@@ -2,8 +2,6 @@ export * from './Icons';
 export { Demo } from './Demo/Demo';
 export { getCodeFileIcon } from './DemoCode';
 export type { MantineDemo } from './Demo/Demo';
-export * from './SearchControl';
-export * from './HeaderControl';
 export { MantineLogo } from './MantineLogo';
 export type { MantineLogoProps } from './MantineLogo';
 export type { ConfiguratorControlOptions } from './ConfiguratorDemo/ConfiguratorDemo';
