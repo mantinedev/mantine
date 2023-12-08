46fcf6f066c12e9d0c9b0c2b8dac876a58821090
Vitaly Rtishchev <rtivital@gmail.com>
2023-12-08 13:20:12 +0400

    [@mantine/core] Menu: Fix incorrect aria roles (#5372)

--- packages/@mantine/core/src/components/Menu/MenuDropdown/MenuDropdown.tsx
+++ packages/@mantine/core/src/components/Menu/MenuDropdown/MenuDropdown.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import React, { useRef } from 'react';
+import { useMergedRef } from '@mantine/hooks';
 import {
   BoxProps,
   CompoundStylesApiProps,
@@ -71,19 +72,14 @@ export const MenuDropdown = factory<MenuDropdownFactory>((props, ref) => {
       onMouseLeave={handleMouseLeave}
       role="menu"
       aria-orientation="vertical"
-      ref={ref}
+      ref={useMergedRef(ref, wrapperRef)}
       {...ctx.getStyles('dropdown', { className, style, styles, classNames })}
+      tabIndex={-1}
+      data-menu-dropdown
+      onKeyDown={handleKeyDown}
     >
-      <div
-        tabIndex={-1}
-        data-menu-dropdown
-        data-autofocus
-        onKeyDown={handleKeyDown}
-        ref={wrapperRef}
-        style={{ outline: 0 }}
-      >
-        {children}
-      </div>
+      <div tabIndex={-1} data-autofocus />
+      {children}
     </Popover.Dropdown>
   );
 });
