a678dbaa287b1480d084b7cf72ed22e4435abe4d
Vitaly Rtishchev <rtivital@gmail.com>
2023-12-08 13:06:33 +0400

    [refactor] Fix linting warning

--- packages/@mantine/core/src/components/Autocomplete/Autocomplete.story.tsx
+++ packages/@mantine/core/src/components/Autocomplete/Autocomplete.story.tsx
@@ -40,6 +40,7 @@ export function WithinForm() {
       <form
         onSubmit={(event) => {
           event.preventDefault();
+          // eslint-disable-next-line no-console
           console.log('submit');
         }}
       >
