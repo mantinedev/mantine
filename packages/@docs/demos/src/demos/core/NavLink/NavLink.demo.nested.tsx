df6c614a88be9b86d0230e9144f1119392d955eb
Vitaly Rtishchev <rtivital@gmail.com>
2023-12-09 18:30:51 +0400

    [docs] Fix NavLink in demos not having focus

--- packages/@docs/demos/src/demos/core/NavLink/NavLink.demo.nested.tsx
+++ packages/@docs/demos/src/demos/core/NavLink/NavLink.demo.nested.tsx
@@ -11,28 +11,30 @@ function Demo() {
   return (
     <>
       <NavLink
+        href="#required-for-focus"
         label="First parent link"
         leftSection={<IconGauge size="1rem" stroke={1.5} />}
         childrenOffset={28}
       >
-        <NavLink label="First child link" />
-        <NavLink label="Second child link" />
-        <NavLink label="Nested parent link" childrenOffset={28}>
-          <NavLink label="First child link" />
-          <NavLink label="Second child link" />
-          <NavLink label="Third child link" />
+        <NavLink href="#required-for-focus" label="First child link" />
+        <NavLink label="Second child link" href="#required-for-focus" />
+        <NavLink label="Nested parent link" childrenOffset={28} href="#required-for-focus">
+          <NavLink label="First child link" href="#required-for-focus" />
+          <NavLink label="Second child link" href="#required-for-focus" />
+          <NavLink label="Third child link" href="#required-for-focus" />
         </NavLink>
       </NavLink>
 
       <NavLink
+        href="#required-for-focus"
         label="Second parent link"
         leftSection={<IconFingerprint size="1rem" stroke={1.5} />}
         childrenOffset={28}
         defaultOpened
       >
-        <NavLink label="First child link" />
-        <NavLink label="Second child link" />
-        <NavLink label="Third child link" />
+        <NavLink label="First child link" href="#required-for-focus" />
+        <NavLink label="Second child link" href="#required-for-focus" />
+        <NavLink label="Third child link" href="#required-for-focus" />
       </NavLink>
     </>
   );
@@ -43,28 +45,30 @@ function Demo() {
   return (
     <>
       <NavLink
+        href="#required-for-focus"
         label="First parent link"
         leftSection={<IconGauge size="1rem" stroke={1.5} />}
         childrenOffset={28}
       >
-        <NavLink label="First child link" />
-        <NavLink label="Second child link" />
-        <NavLink label="Nested parent link" childrenOffset={28}>
-          <NavLink label="First child link" />
-          <NavLink label="Second child link" />
-          <NavLink label="Third child link" />
+        <NavLink href="#required-for-focus" label="First child link" />
+        <NavLink label="Second child link" href="#required-for-focus" />
+        <NavLink label="Nested parent link" childrenOffset={28} href="#required-for-focus">
+          <NavLink label="First child link" href="#required-for-focus" />
+          <NavLink label="Second child link" href="#required-for-focus" />
+          <NavLink label="Third child link" href="#required-for-focus" />
         </NavLink>
       </NavLink>
 
       <NavLink
+        href="#required-for-focus"
         label="Second parent link"
         leftSection={<IconFingerprint size="1rem" stroke={1.5} />}
         childrenOffset={28}
         defaultOpened
       >
-        <NavLink label="First child link" />
-        <NavLink label="Second child link" />
-        <NavLink label="Third child link" />
+        <NavLink label="First child link" href="#required-for-focus" />
+        <NavLink label="Second child link" href="#required-for-focus" />
+        <NavLink label="Third child link" href="#required-for-focus" />
       </NavLink>
     </>
   );
