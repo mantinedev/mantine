43a41be05a5fa1340fd0aaf915f3f86daa23c6c9
Mark Gibbons <markgibbons@aceik.com.au>
2023-12-12 09:05:03 +1100

    Menu demo update

--- packages/@docs/demos/src/demos/core/Menu/Menu.demo.navigation.tsx
+++ packages/@docs/demos/src/demos/core/Menu/Menu.demo.navigation.tsx
@@ -7,46 +7,40 @@ const code = `
 import { Group, Menu } from '@mantine/core';
 
 function Demo() {
-  return (
-    <Group>
-      {Array(4)
-        .fill(0)
-        .map((e, i) => (
-          <Menu
-            key={i}
-            trigger="click-hover"
-            loop={false}
-            withinPortal={false}
-            trapFocus={false}
-            menuItemTabIndex={0}
-          >
-            <DemoMenuItems />
-          </Menu>
-        ))}
-    </Group>
-  );
+  const menus = Array(4)
+    .fill(0)
+    .map((e, i) => (
+      <Menu
+        key={i}
+        trigger="click-hover"
+        loop={false}
+        withinPortal={false}
+        trapFocus={false}
+        menuItemTabIndex={0}
+      >
+        {/* ... menu items */}
+      </Menu>
+    ));
+  return <Group>{menus}</Group>;
 }
 `;
 
 function Demo() {
-  return (
-    <Group>
-      {Array(4)
-        .fill(0)
-        .map((e, i) => (
-          <Menu
-            key={i}
-            trigger="click-hover"
-            loop={false}
-            withinPortal={false}
-            trapFocus={false}
-            menuItemTabIndex={0}
-          >
-            <DemoMenuItems />
-          </Menu>
-        ))}
-    </Group>
-  );
+  const menus = Array(4)
+    .fill(0)
+    .map((e, i) => (
+      <Menu
+        key={i}
+        trigger="click-hover"
+        loop={false}
+        withinPortal={false}
+        trapFocus={false}
+        menuItemTabIndex={0}
+      >
+        <DemoMenuItems />
+      </Menu>
+    ));
+  return <Group>{menus}</Group>;
 }
 
 export const navigation: MantineDemo = {
