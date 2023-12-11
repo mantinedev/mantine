826e038b00168589edde5832ecbb369106fb330c
Mark Gibbons <markgibbons@aceik.com.au>
2023-12-11 17:16:33 +1100

    Fix prettier

--- packages/@docs/demos/src/demos/core/Menu/Menu.demo.navigation.tsx
+++ packages/@docs/demos/src/demos/core/Menu/Menu.demo.navigation.tsx
@@ -9,18 +9,20 @@ import { Group, Menu } from '@mantine/core';
 function Demo() {
   return (
     <Group>
-      {Array(4).fill(0).map((e, i) => (
-        <Menu
-          key={i}
-          trigger="click-hover"
-          loop={false}
-          withinPortal={false}
-          trapFocus={false}
-          menuItemTabIndex={0}
-        >
-          <DemoMenuItems />
-        </Menu>
-      ))}
+      {Array(4)
+        .fill(0)
+        .map((e, i) => (
+          <Menu
+            key={i}
+            trigger="click-hover"
+            loop={false}
+            withinPortal={false}
+            trapFocus={false}
+            menuItemTabIndex={0}
+          >
+            <DemoMenuItems />
+          </Menu>
+        ))}
     </Group>
   );
 }
@@ -29,18 +31,20 @@ function Demo() {
 function Demo() {
   return (
     <Group>
-      {Array(4).fill(0).map((e, i) => (
-        <Menu
-          key={i}
-          trigger="click-hover"
-          loop={false}
-          withinPortal={false}
-          trapFocus={false}
-          menuItemTabIndex={0}
-        >
-          <DemoMenuItems />
-        </Menu>
-      ))}
+      {Array(4)
+        .fill(0)
+        .map((e, i) => (
+          <Menu
+            key={i}
+            trigger="click-hover"
+            loop={false}
+            withinPortal={false}
+            trapFocus={false}
+            menuItemTabIndex={0}
+          >
+            <DemoMenuItems />
+          </Menu>
+        ))}
     </Group>
   );
 }
