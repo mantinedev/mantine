274c7af8a8a9a99a6a27d76592ef466b190371ad
Vitaly Rtishchev <rtivital@gmail.com>
2023-12-19 10:47:12 +0400

    [docs] Move exports reference to docs older

---
+++ docs/src/exports-reference/index.ts
@@ -0,0 +1,4 @@
+import { HOOKS_EXPORTS_REFERENCE } from './hooks-exports-reference';
+import type { ExportReferenceGroup } from './types';
+
+export const EXPORTS_REFERENCE: ExportReferenceGroup[] = [HOOKS_EXPORTS_REFERENCE];
--- packages/@mantinex/mantine-meta/src/index.ts
+++ packages/@mantinex/mantine-meta/src/index.ts
@@ -1,7 +1,5 @@
 export { meta } from './meta';
 export { majorVersions } from './major-versions';
 export { allVersions } from './versions';
-export { EXPORTS_REFERENCE } from './exports-reference';
 
 export type { Version, PatchVersion } from './versions';
-export type { ExportReference, ExportReferenceGroup } from './exports-reference';
