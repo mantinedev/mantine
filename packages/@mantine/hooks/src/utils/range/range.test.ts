8deac1a3f0f0bd42e7c23d17a9e3808abded7e28
Vitaly Rtishchev <rtivital@gmail.com>
2023-12-20 18:15:25 +0400

    [@mantine/hooks] Add reverse order support to range function

--- packages/@mantine/hooks/src/utils/range/range.test.ts
+++ packages/@mantine/hooks/src/utils/range/range.test.ts
@@ -4,4 +4,8 @@ describe('@mantine/hooks/range', () => {
   it('returns range between given numbers', () => {
     expect(range(1, 5)).toStrictEqual([1, 2, 3, 4, 5]);
   });
+
+  it('allows generating reversed range', () => {
+    expect(range(5, 1)).toStrictEqual([5, 4, 3, 2, 1]);
+  });
 });
--- packages/@mantine/hooks/src/utils/range/range.ts
+++ packages/@mantine/hooks/src/utils/range/range.ts
@@ -1,4 +1,10 @@
 export function range(start: number, end: number) {
-  const length = end - start + 1;
-  return Array.from({ length }, (_, index) => index + start);
+  const length = Math.abs(end - start) + 1;
+  const reversed = start > end;
+
+  if (!reversed) {
+    return Array.from({ length }, (_, index) => index + start);
+  }
+
+  return Array.from({ length }, (_, index) => start - index);
 }
