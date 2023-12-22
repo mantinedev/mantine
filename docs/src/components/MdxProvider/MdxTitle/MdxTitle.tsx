06a16d8420f58fc644e857bcd72dc38108e252b1
Vitaly Rtishchev <rtivital@gmail.com>
2023-12-22 10:47:26 +0400

    [docs] Add copy link to section button

--- docs/src/components/MdxProvider/MdxTitle/MdxTitle.tsx
+++ docs/src/components/MdxProvider/MdxTitle/MdxTitle.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { IconCheck } from '@tabler/icons-react';
 import cx from 'clsx';
-import { FOCUS_CLASS_NAMES, Title } from '@mantine/core';
+import { FOCUS_CLASS_NAMES, Title, UnstyledButton } from '@mantine/core';
+import { useClipboard } from '@mantine/hooks';
+import { LinkIcon } from './LinkIcon';
 import classes from './MdxTitle.module.css';
 
 export function MdxTitle({
@@ -9,6 +12,8 @@ export function MdxTitle({
   order = 2,
   ...others
 }: React.ComponentPropsWithoutRef<typeof Title>) {
+  const { copied, copy, reset } = useClipboard();
+
   if (order === 1) {
     return (
       <Title className={classes.title} data-h1>
@@ -24,6 +29,20 @@ export function MdxTitle({
         <a className={cx(classes.link, FOCUS_CLASS_NAMES.auto)} href={`#${id}`}>
           {children}
         </a>
+        <UnstyledButton
+          className={classes.copyButton}
+          tabIndex={-1}
+          aria-label="Copy link to the section"
+          onClick={() => copy(`${window.location.origin}${window.location.pathname}#${id}`)}
+          onMouseLeave={reset}
+          title={copied ? '✓ Copied to clipboard' : 'Copy link'}
+        >
+          {copied ? (
+            <IconCheck className={classes.copyIcon} />
+          ) : (
+            <LinkIcon className={classes.copyIcon} />
+          )}
+        </UnstyledButton>
       </Title>
     </>
   );
