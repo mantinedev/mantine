3e4a294128f86a33aea17f2ca263f2e3b734ae8f
Vitaly Rtishchev <rtivital@gmail.com>
2023-12-23 11:54:37 +0400

    [docs] Cleanup remaining buttons docs

--- docs/src/components/MdxProvider/MdxSharedContent/MdxServerComponentsIncompatible.tsx
+++ docs/src/components/MdxProvider/MdxSharedContent/MdxServerComponentsIncompatible.tsx
@@ -14,8 +14,8 @@ export function MdxServerComponentsIncompatible({
       <MdxTitle id="server-components-incompatible">Incompatible with server components</MdxTitle>
       <MdxParagraph>
         <MdxCode>{component}</MdxCode> is not compatible with React server components as it requires
-        function as children. To use <MdxCode>{component}</MdxCode> set{' '}
-        <MdxCode>&quot;use client;&quot;</MdxCode> at the top of your file.
+        function as children. To use <MdxCode>{component}</MdxCode> add{' '}
+        <MdxCode>&quot;use client;&quot;</MdxCode> at the top of the file.
       </MdxParagraph>
     </>
   );
