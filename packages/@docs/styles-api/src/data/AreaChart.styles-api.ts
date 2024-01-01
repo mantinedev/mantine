25d5290c9e5dd7ac2cee9bffe3083a7b5bb240cd
Vitaly Rtishchev <rtivital@gmail.com>
2024-01-01 15:44:32 +0400

    [docs] Add full AreaChart styles api data

--- packages/@docs/styles-api/src/data/AreaChart.styles-api.ts
+++ packages/@docs/styles-api/src/data/AreaChart.styles-api.ts
@@ -8,9 +8,26 @@ export const AreaChartStylesApi: StylesApiData<AreaChartFactory> = {
     axis: 'X and Y axis of the chart',
     container: 'Recharts ResponsiveContainer component',
     grid: 'Recharts CartesianGrid component',
+    legend: 'Legend root element',
+    legendItem: 'Legend item representing data series',
+    legendItemColor: 'Legend item color',
+    legendItemName: 'Legend item name',
+    tooltip: 'Tooltip root element',
+    tooltipBody: 'Tooltip wrapper around all items',
+    tooltipItem: 'Tooltip item representing data series',
+    tooltipItemBody: 'Tooltip item wrapper around item color and name',
+    tooltipItemColor: 'Tooltip item color',
+    tooltipItemName: 'Tooltip item name',
+    tooltipItemData: 'Tooltip item data',
+    tooltipLabel: 'Label of the tooltip',
   },
 
-  vars: {},
+  vars: {
+    root: {
+      '--area-chart-grid-color': 'Controls color of the grid and cursor lines',
+      '--area-chart-text-color': 'Controls color of the axis labels',
+    },
+  },
 
   modifiers: [],
 };
