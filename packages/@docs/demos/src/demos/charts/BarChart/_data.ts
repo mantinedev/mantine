d1d19cd001ae7874845de953f73eecc156a8fca7
Vitaly Rtishchev <rtivital@gmail.com>
2024-01-02 12:54:24 +0400

    [docs] Init BarChart demos

---
+++ packages/@docs/demos/src/demos/charts/BarChart/_data.ts
@@ -0,0 +1,19 @@
+export const data = [
+  { month: 'January', Smartphones: 120, Laptops: 150, Tablets: 180 },
+  { month: 'February', Smartphones: 80, Laptops: 100, Tablets: 120 },
+  { month: 'March', Smartphones: 50, Laptops: 60, Tablets: 75 },
+  { month: 'April', Smartphones: 30, Laptops: 40, Tablets: 50 },
+  { month: 'May', Smartphones: 20, Laptops: 25, Tablets: 30 },
+  { month: 'June', Smartphones: 40, Laptops: 45, Tablets: 50 },
+];
+
+export const dataCode = `
+export const data = [
+  { month: 'January', Smartphones: 120, Laptops: 150, Tablets: 180 },
+  { month: 'February', Smartphones: 80, Laptops: 100, Tablets: 120 },
+  { month: 'March', Smartphones: 50, Laptops: 60, Tablets: 75 },
+  { month: 'April', Smartphones: 30, Laptops: 40, Tablets: 50 },
+  { month: 'May', Smartphones: 20, Laptops: 25, Tablets: 30 },
+  { month: 'June', Smartphones: 40, Laptops: 45, Tablets: 50 },
+];
+`;
---
+++ packages/@docs/demos/src/demos/charts/BarChart/index.ts
@@ -0,0 +1,1 @@
+export { usage } from './BarChart.demo.usage';
