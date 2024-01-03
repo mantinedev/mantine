7639a3131888a0666aca1ad5e582eb089f5d1359
Alex Yovev <ayovev511@gmail.com>
2024-01-03 01:35:00 -0800

    [@mantine/core] ScrollArea: Fix ScrollArea.Autosize working incorrectly with some tables (#5481)

--- packages/@mantine/core/src/components/ScrollArea/ScrollArea.tsx
+++ packages/@mantine/core/src/components/ScrollArea/ScrollArea.tsx
@@ -206,7 +206,7 @@ export const ScrollAreaAutosize = factory<ScrollAreaFactory>((props, ref) => {
   } = useProps('ScrollAreaAutosize', defaultProps, props);
 
   return (
-    <Box {...others} ref={ref} style={[{ display: 'flex' }, style]}>
+    <Box {...others} ref={ref} style={[{ display: 'flex', overflow: 'auto' }, style]}>
       <Box style={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
         <ScrollArea
           classNames={classNames}
