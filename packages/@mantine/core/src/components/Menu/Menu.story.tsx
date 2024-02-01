3e8b0da9ca8d75429a434531fd55110ab5b1759c
Vitaly Rtishchev <rtivital@gmail.com>
2024-02-01 13:09:35 +0400

    [@mantine/core] Menu: Fix unexpected focus trap when keepMounted is false (#4502)

--- packages/@mantine/core/src/components/Menu/Menu.story.tsx
+++ packages/@mantine/core/src/components/Menu/Menu.story.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { IconSearch, IconTable } from '@tabler/icons-react';
 import { useDisclosure } from '@mantine/hooks';
 import { Button } from '../Button';
+import { Input } from '../Input';
 import { Text } from '../Text';
 import { Tooltip } from '../Tooltip';
 import { Menu } from './Menu';
@@ -190,17 +191,22 @@ export function WithUseDisclosure() {
 
 export function WithKeepMounted() {
   return (
-    <div style={{ padding: 40 }}>
-      <Menu id="new-notification-menu" keepMounted position="bottom-end">
+    <div className="App">
+      <Menu keepMounted>
+        {/* Menu without keepMounted will let you 'TAB' through form-fields */}
+        {/* <Menu> */}
         <Menu.Target>
-          <Button>Open Menu</Button>
+          <Button>Hey</Button>
         </Menu.Target>
         <Menu.Dropdown>
-          <Menu.Item>Menu Item 1</Menu.Item>
-          <Menu.Item>Menu Item 2</Menu.Item>
-          <Menu.Item>Menu Item 3</Menu.Item>
+          <Menu.Item>Baby</Menu.Item>
+          <Menu.Item>Girl</Menu.Item>
         </Menu.Dropdown>
       </Menu>
+      <form>
+        <Input />
+        <Input />
+      </form>
     </div>
   );
 }
