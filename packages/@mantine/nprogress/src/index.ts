41ef9a2ac7011b2c1371c1a784559ff24acf84a4
smyja <akposlive59@gmail.com>
2024-02-06 11:09:19 +0100

    fix: added nprogress export

--- packages/@mantine/nprogress/src/index.ts
+++ packages/@mantine/nprogress/src/index.ts
@@ -1,4 +1,4 @@
-export { NavigationProgress } from './NavigationProgress';
+export { NavigationProgress,nprogress } from './NavigationProgress';
 export * from './nprogress.store';
 
 export type { NavigationProgressProps } from './NavigationProgress';
