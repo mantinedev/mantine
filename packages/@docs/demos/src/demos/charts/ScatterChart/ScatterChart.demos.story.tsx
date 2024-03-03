d63614194a55bdd96e1af6ed94a3f57dc48fed7c
Vitaly Rtishchev <rtivital@gmail.com>
2024-03-03 14:52:47 +0400

    [docs] Add basic scatter chart demos

--- packages/@docs/demos/src/demos/charts/ScatterChart/ScatterChart.demos.story.tsx
+++ packages/@docs/demos/src/demos/charts/ScatterChart/ScatterChart.demos.story.tsx
@@ -7,3 +7,48 @@ export const Demo_usage = {
   name: '⭐ Demo: usage',
   render: renderDemo(demos.usage),
 };
+
+export const Demo_multipleSeries = {
+  name: '⭐ Demo: multipleSeries',
+  render: renderDemo(demos.multipleSeries),
+};
+
+export const Demo_axisProps = {
+  name: '⭐ Demo: axisProps',
+  render: renderDemo(demos.axisProps),
+};
+
+export const Demo_gridColor = {
+  name: '⭐ Demo: gridColor',
+  render: renderDemo(demos.gridColor),
+};
+
+export const Demo_legend = {
+  name: '⭐ Demo: legend',
+  render: renderDemo(demos.legend),
+};
+
+export const Demo_legendPosition = {
+  name: '⭐ Demo: legendPosition',
+  render: renderDemo(demos.legendPosition),
+};
+
+export const Demo_noTooltip = {
+  name: '⭐ Demo: noTooltip',
+  render: renderDemo(demos.noTooltip),
+};
+
+export const Demo_referenceLines = {
+  name: '⭐ Demo: referenceLines',
+  render: renderDemo(demos.referenceLines),
+};
+
+export const Demo_strokeDasharray = {
+  name: '⭐ Demo: strokeDasharray',
+  render: renderDemo(demos.strokeDasharray),
+};
+
+export const Demo_tooltipAnimation = {
+  name: '⭐ Demo: tooltipAnimation',
+  render: renderDemo(demos.tooltipAnimation),
+};
--- packages/@docs/demos/src/demos/charts/ScatterChart/index.ts
+++ packages/@docs/demos/src/demos/charts/ScatterChart/index.ts
@@ -1,1 +1,10 @@
 export { usage } from './ScatterChart.demo.usage';
+export { multipleSeries } from './ScatterChart.demo.multipleSeries';
+export { axisProps } from './ScatterChart.demo.axisProps';
+export { gridColor } from './ScatterChart.demo.gridColor';
+export { legend } from './ScatterChart.demo.legend';
+export { legendPosition } from './ScatterChart.demo.legendPosition';
+export { noTooltip } from './ScatterChart.demo.noTooltip';
+export { referenceLines } from './ScatterChart.demo.referenceLines';
+export { strokeDasharray } from './ScatterChart.demo.strokeDasharray';
+export { tooltipAnimation } from './ScatterChart.demo.tooltipAnimation';
