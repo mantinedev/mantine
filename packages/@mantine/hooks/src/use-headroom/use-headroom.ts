6671e46739cc4a71e94032c7a45d2aa3da474031
Mir Ali Mobasheri <Alimobasheri@users.noreply.github.com>
2024-03-10 13:26:36 +0330

    [@mantine/hooks]: fix header not pinned when scrolling up
    added useScrollDirection to handle if user is scrolling up and make sure the scroll is not caused by a resize event like mobile address bar showing

--- packages/@mantine/hooks/src/use-headroom/use-headroom.ts
+++ packages/@mantine/hooks/src/use-headroom/use-headroom.ts
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useIsomorphicEffect } from '../use-isomorphic-effect/use-isomorphic-effect';
 import { useWindowScroll } from '../use-window-scroll/use-window-scroll';
 
@@ -11,6 +11,7 @@ export const isPinnedOrReleased = (
   current: number,
   fixedAt: number,
   isCurrentlyPinnedRef: React.MutableRefObject<boolean>,
+  isScrollingUp: boolean,
   onPin?: () => void,
   onRelease?: () => void
 ) => {
@@ -18,10 +19,50 @@ export const isPinnedOrReleased = (
   if (isInFixedPosition && !isCurrentlyPinnedRef.current) {
     isCurrentlyPinnedRef.current = true;
     onPin?.();
+  } else if (!isInFixedPosition && isScrollingUp && !isCurrentlyPinnedRef.current) {
+    isCurrentlyPinnedRef.current = true;
+    onPin?.();
   } else if (!isInFixedPosition && isCurrentlyPinnedRef.current) {
     isCurrentlyPinnedRef.current = false;
     onRelease?.();
   }
+};
+
+export const useScrollDirection = () => {
+  const [lastScrollTop, setLastScrollTop] = useState(0);
+  const [isScrollingUp, setIsScrollingUp] = useState(false);
+  const [isResizing, setIsResizing] = useState(false);
+
+  useEffect(() => {
+    let resizeTimer: NodeJS.Timeout | undefined;
+
+    const onResize = () => {
+      setIsResizing(true);
+      clearTimeout(resizeTimer);
+      resizeTimer = setTimeout(() => {
+        setIsResizing(false);
+      }, 300); // Reset the resizing flag after a timeout
+    };
+
+    const onScroll = () => {
+      if (isResizing) {
+        return; // Skip scroll events if resizing is in progress
+      }
+      const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      setIsScrollingUp(currentScrollTop < lastScrollTop);
+      setLastScrollTop(currentScrollTop);
+    };
+
+    window.addEventListener('scroll', onScroll);
+    window.addEventListener('resize', onResize);
+
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+      window.removeEventListener('resize', onResize);
+    };
+  }, [lastScrollTop, isResizing]);
+
+  return isScrollingUp;
 };
 
 interface UseHeadroomInput {
@@ -40,10 +81,18 @@ interface UseHeadroomInput {
 
 export function useHeadroom({ fixedAt = 0, onPin, onFix, onRelease }: UseHeadroomInput = {}) {
   const isCurrentlyPinnedRef = useRef(false);
+  const isScrollingUp = useScrollDirection();
   const [{ y: scrollPosition }] = useWindowScroll();
 
   useIsomorphicEffect(() => {
-    isPinnedOrReleased(scrollPosition, fixedAt, isCurrentlyPinnedRef, onPin, onRelease);
+    isPinnedOrReleased(
+      scrollPosition,
+      fixedAt,
+      isCurrentlyPinnedRef,
+      isScrollingUp,
+      onPin,
+      onRelease
+    );
   }, [scrollPosition]);
 
   useIsomorphicEffect(() => {
@@ -52,7 +101,7 @@ export function useHeadroom({ fixedAt = 0, onPin, onFix, onRelease }: UseHeadroo
     }
   }, [scrollPosition, fixedAt, onFix]);
 
-  if (isFixed(scrollPosition, fixedAt)) {
+  if (isFixed(scrollPosition, fixedAt) || isScrollingUp) {
     return true;
   }
 
