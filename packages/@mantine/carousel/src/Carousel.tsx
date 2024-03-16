761fc2deb86fd5ba2cc579fa065bc8ec37484c90
“Kenzo-Wada” <used.acormhib@gmail.com>
2024-03-16 15:58:53 +0900

    [@mantine/carousel] chore: fix displayname `@mantine/core` -> `@mantine/carousel`

--- packages/@mantine/carousel/src/Carousel.tsx
+++ packages/@mantine/carousel/src/Carousel.tsx
@@ -417,5 +417,5 @@ export const Carousel = factory<CarouselFactory>((_props, ref) => {
 });
 
 Carousel.classes = classes;
-Carousel.displayName = '@mantine/core/Carousel';
+Carousel.displayName = '@mantine/carousel/Carousel';
 Carousel.Slide = CarouselSlide;
