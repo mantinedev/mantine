da8ffa7b5cc5f4cce458d173654b390f97940b52
Evgeniy Savitskiy <sei.olympus@gmail.com>
2024-03-23 13:19:53 +0300

    [@mantine/data-table] DataTable: Init component

---
+++ packages/@mantine/data-table/src/DataTable.story.tsx
@@ -0,0 +1,72 @@
+import React, { useMemo } from 'react';
+import { faker } from '@faker-js/faker';
+import { createColumnHelper, getCoreRowModel, useReactTable } from '@tanstack/react-table';
+import { DataTable } from './DataTable';
+
+type Person = {
+  id: string;
+  firstName: string
+  lastName: string
+  age: number
+  visits: number
+  progress: number
+  status: 'relationship' | 'complicated' | 'single'
+  subRows?: Person[]
+};
+
+const makePerson = (): Person => ({
+  id: faker.string.uuid(),
+  firstName: faker.person.firstName(),
+  lastName: faker.person.lastName(),
+  age: faker.number.int(40),
+  visits: faker.number.int(1000),
+  progress: faker.number.int(100),
+  status: faker.helpers.shuffle<Person['status']>([
+    'relationship',
+    'complicated',
+    'single',
+  ])[0],
+});
+
+const makeData = (...counts: number[]) => {
+  const makeDataLevel = (depth = 0): Person[] => {
+    const count = counts[depth];
+    return faker.helpers.multiple(makePerson, {
+      count,
+    }).map((person) => ({
+      ...person,
+      subRows: counts[depth + 1] ? makeDataLevel(depth + 1) : undefined,
+    }));
+  };
+
+  return makeDataLevel();
+};
+
+const columnHelper = createColumnHelper<Person>();
+
+export default { title: 'DataTable' };
+
+export const Basic = () => {
+  const data = useMemo(() => makeData(10), []);
+
+  const columns = useMemo(() => [
+    columnHelper.accessor('firstName', { header: 'First Name' }),
+    columnHelper.accessor('lastName', { header: 'Last Name' }),
+    columnHelper.accessor('age', { header: 'Age' }),
+    columnHelper.accessor('visits', { header: 'Visits' }),
+    columnHelper.accessor('status', { header: 'Status' }),
+    columnHelper.accessor('progress', { header: 'Profile Progress' }),
+  ], []);
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div style={{ padding: 40 }}>
+      <DataTable table={table} withTableBorder withColumnBorders />
+    </div>
+  );
+};
---
+++ packages/@mantine/data-table/src/DataTable.tsx
@@ -0,0 +1,149 @@
+import React from 'react';
+import { flexRender, RowData, Table as TableDefinition } from '@tanstack/react-table';
+import {
+  factory,
+  Factory,
+  MantineComponentStaticProperties,
+  Table,
+  TableCssVariables,
+  TableProps,
+  TableStylesNames,
+  TableTbody,
+  TableTd,
+  TableTfoot,
+  TableTh,
+  TableThead,
+  TableTr,
+  useProps,
+  useStyles,
+} from '@mantine/core';
+import classes from './DataTable.module.css';
+
+export type DataTableStylesNames = TableStylesNames;
+
+export type DataTableCssVariables = TableCssVariables;
+
+export type DataTableProps<TData extends RowData = RowData> = Omit<TableProps, 'data'> & {
+  table: TableDefinition<TData>;
+};
+
+export type DataTableFactory = Factory<{
+  props: DataTableProps;
+  ref: HTMLTableElement;
+  stylesNames: DataTableStylesNames;
+  vars: DataTableCssVariables;
+}>;
+
+const defaultProps: Partial<DataTableProps> = {};
+
+type DataTableComponent = (<TData extends RowData>(
+  props: DataTableProps<TData> & { ref?: React.ForwardedRef<HTMLTableElement> }
+) => React.ReactElement) & {
+  displayName?: string;
+} & MantineComponentStaticProperties<DataTableFactory>;
+
+export const DataTable: DataTableComponent = factory<DataTableFactory>(
+  (_props, ref) => {
+    const props = useProps('DataTable', defaultProps, _props);
+
+    const {
+      className,
+      style,
+      classNames,
+      styles,
+      vars,
+      table,
+      ...others
+    } = props;
+
+    const getStyles = useStyles<DataTableFactory>({
+      name: 'DataTable',
+      props,
+      classes,
+      className,
+      style,
+      classNames,
+      styles,
+      vars,
+    });
+
+    return (
+      <Table
+        ref={ref}
+        {...getStyles('table')}
+        {...others}
+      >
+        <TableThead
+          {...getStyles('thead')}
+        >
+          {table.getHeaderGroups().map((group) => (
+            <TableTr
+              key={group.id}
+              {...getStyles('tr')}
+            >
+              {group.headers.map((header) => (
+                <TableTh
+                  key={header.id}
+                  {...getStyles('th')}
+                >
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+                </TableTh>
+              ))}
+            </TableTr>
+          ))}
+        </TableThead>
+        <TableTbody
+          {...getStyles('tbody')}
+        >
+          {table.getRowModel().rows.map((row) => (
+            <TableTr
+              key={row.id}
+              {...getStyles('tr')}
+            >
+              {row.getVisibleCells().map((cell) => (
+                <TableTd
+                  key={cell.id}
+                  {...getStyles('td')}
+                >
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </TableTd>
+              ))}
+            </TableTr>
+          ))}
+        </TableTbody>
+        <TableTfoot
+          {...getStyles('tfoot')}
+        >
+          {table.getFooterGroups().map((group) => (
+            <TableTr
+              key={group.id}
+              {...getStyles('tr')}
+            >
+              {group.headers.map((header) => (
+                <TableTh
+                  key={header.id}
+                  {...getStyles('th')}
+                >
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                      header.column.columnDef.footer,
+                      header.getContext()
+                    )}
+                </TableTh>
+              ))}
+            </TableTr>
+          ))}
+        </TableTfoot>
+      </Table>
+    );
+  }
+) as any;
+
+DataTable.displayName = '@mantine/data-table/DataTable';
+DataTable.classes = classes;
