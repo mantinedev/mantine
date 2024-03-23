96841ad35343becdd80896a7d2e0bfbd0be99d43
Vitaly Rtishchev <rtivital@gmail.com>
2024-03-23 15:56:34 +0400

    [@mantine/core] Add headings values support to `fz` prop

--- packages/@mantine/core/src/core/Box/style-props/resolvers/font-size-resolver/font-size-resolver.ts
+++ packages/@mantine/core/src/core/Box/style-props/resolvers/font-size-resolver/font-size-resolver.ts
@@ -1,9 +1,15 @@
 import { MantineTheme } from '../../../../MantineProvider';
 import { rem } from '../../../../utils';
 
+const headings = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+
 export function fontSizeResolver(value: unknown, theme: MantineTheme) {
   if (typeof value === 'string' && value in theme.fontSizes) {
     return `var(--mantine-font-size-${value})`;
+  }
+
+  if (typeof value === 'string' && headings.includes(value)) {
+    return `var(--mantine-${value}-font-size)`;
   }
 
   if (typeof value === 'number') {
