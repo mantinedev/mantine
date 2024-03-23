8f3c99241ecfac9fab30c349f5a96b63b3f1e84a
Vitaly Rtishchev <rtivital@gmail.com>
2024-03-23 13:12:08 +0400

    [docs] Add more detailed documentation for typography CSS variables

---
+++ apps/mantine.dev/src/components/CssVariablesGroup/CssVariablesGroup.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Code, Table, Text } from '@mantine/core';
+
+interface CssVariableData {
+  variable: string;
+  description: React.ReactNode;
+  defaultValue: React.ReactNode;
+}
+
+export interface CssVariablesGroupProps {
+  data: CssVariableData[];
+}
+
+export function CssVariablesGroup({ data }: CssVariablesGroupProps) {
+  const rows = data.map((item) => (
+    <Table.Tr key={item.variable}>
+      <Table.Td>
+        <Code>{item.variable}</Code>
+        <Text fz="xs" mt={3}>
+          {item.description}
+        </Text>
+      </Table.Td>
+      <Table.Td>{item.defaultValue}</Table.Td>
+    </Table.Tr>
+  ));
+
+  return (
+    <Table.ScrollContainer minWidth={500}>
+      <Table layout="fixed">
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th w={400}>Variable</Table.Th>
+            <Table.Th>Default value</Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>{rows}</Table.Tbody>
+      </Table>
+    </Table.ScrollContainer>
+  );
+}
---
+++ apps/mantine.dev/src/components/CssVariablesGroup/index.ts
@@ -0,0 +1,1 @@
+export { CssVariablesGroup } from './CssVariablesGroup';
