c5d55248e8640ea07d4f8d7cd115c0cb01bc4795
Vitaly Rtishchev <rtivital@gmail.com>
2024-03-26 10:10:34 +0400

    [core] Fix script to generate css

--- scripts/build/generate-css.ts
+++ scripts/build/generate-css.ts
@@ -51,7 +51,7 @@ export async function generateCoreCSS() {
   const global = files.find((file) => file.endsWith('global.css'))!;
 
   fs.writeJsonSync(
-    getPath('docs/src/.docgen/css-exports.json'),
+    getPath('apps/mantine.dev/src/.docgen/css-exports.json'),
     { modules: modules.map(transformFileName), global: transformFileName(global) },
     { spaces: 2 }
   );
