fc70f8d3bfe479bdaada667b3d336b40c7c8a4a9
Vitaly Rtishchev <rtivital@gmail.com>
2024-04-12 13:23:31 +0400

    [@mantine/core] Add option to customize middlewares

--- packages/@mantine/core/src/components/Popover/Popover.story.tsx
+++ packages/@mantine/core/src/components/Popover/Popover.story.tsx
@@ -32,18 +32,16 @@ export function Uncontrolled() {
   );
 }
 
-export function withFloatingAutoUpdate() {
+export function AtTheEdge() {
   return (
-    <div style={{ padding: 40, height: 400, overflow: 'scroll' }}>
-      <div style={{ height: 150 }} />
-      <Popover>
+    <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+      <Popover position="bottom-end" middlewares={{ shift: { padding: 20 } }}>
         <Popover.Target>
           <button type="button">Toggle popover</button>
         </Popover.Target>
 
         <Popover.Dropdown>Dropdown</Popover.Dropdown>
       </Popover>
-      <div style={{ height: 300 }} />
     </div>
   );
 }
