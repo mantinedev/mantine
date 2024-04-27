3037956939c979c46b1c3455e982f499f8cf1db5
Vitaly Rtishchev <rtivital@gmail.com>
2024-04-27 09:39:37 +0400

    [core] Fix jest to run with new transform

---
+++ jest.setup.ts
@@ -0,0 +1,3 @@
+export default async () => {
+  process.env.TZ = 'America/New_York'; // -05:00 (STD) or -04:00 (DST)
+};
--- jsdom.mocks.ts
+++ jsdom.mocks.ts
@@ -1,4 +1,4 @@
-require('@testing-library/jest-dom');
+import '@testing-library/jest-dom';
 
 const { getComputedStyle } = window;
 window.getComputedStyle = (elt) => getComputedStyle(elt);
