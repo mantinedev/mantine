1fe86c8eb8c471011996ea0e60a2ec1ad4cfeae1
Ali Ozinan <aozinan@gmail.com>
2024-04-29 09:51:11 +0100

    storybook v8 update

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,4 @@
+import { dirname, join } from "path";
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin').default;
 const path = require('path');
 const fg = require('fast-glob');
@@ -18,10 +19,15 @@ const storiesPath = !argv._[1]
   ? [...getStoryPaths()]
   : [...getStoryPaths(argv._[1]), ...getStoryPaths(`${argv._[1]}.demos`)];
 
-module.exports = {
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
+}
+
+export default {
   stories: storiesPath,
 
   addons: [
+    '@storybook/addon-webpack5-compiler-babel',
     'storybook-dark-mode',
     {
       name: '@storybook/addon-styling-webpack',
@@ -38,11 +44,31 @@ module.exports = {
   ],
 
   framework: {
-    name: '@storybook/react-webpack5',
-    options: {},
+    name: getAbsolutePath("@storybook/nextjs"),
+    options: {}
   },
 
-  webpackFinal: async (config) => {
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env', '@babel/preset-typescript'],
+            plugins: ['@babel/plugin-transform-runtime', 'react-refresh/babel'],
+          },
+        },
+      },
+    ]
+  },
+
+  webpackFinal: async function(config) {
     config.resolve = {
       ...config.resolve,
       // handle modules with .js extensions
@@ -59,9 +85,13 @@ module.exports = {
     };
 
     // Turn off docgen plugin as it breaks bundle with displayName
-    config.plugins.pop();
+    // config.plugins.pop();
 
     return config;
+  },
+
+  core: {
+    disableTelemetry: true,
   },
 
   docs: {
