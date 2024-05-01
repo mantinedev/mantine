8a3fd320852b929be931afd94b668d3f33761bd2
Vitaly Rtishchev <rtivital@gmail.com>
2024-05-01 15:49:10 +0400

    [@mantine/ssr] Add ssr

---
+++ packages/@mantine/emotion/src/index.ts
@@ -0,0 +1,7 @@
+export * from './create-styles';
+export * from './emotion-transform';
+export * from './Global';
+export * from './MantineEmotionProvider';
+export * from './use-css';
+export * from './types';
+export * from './ssr';
---
+++ packages/@mantine/emotion/src/ssr.tsx
@@ -0,0 +1,37 @@
+import htmlReactParser from 'html-react-parser';
+
+// type is not imported to avoid dependency on @emotion/server
+type EmotionServer = any;
+type NextDocumentType = any;
+
+export function getSSRStyles(html: string, server: EmotionServer) {
+  return server.constructStyleTagsFromChunks(server.extractCriticalToChunks(html));
+}
+
+interface ServerStylesProps {
+  html: string;
+  server: EmotionServer;
+}
+
+export function ServerStyles({ html, server }: ServerStylesProps): any {
+  const styles = getSSRStyles(html, server);
+  return <>{htmlReactParser(styles)}</>;
+}
+
+export function createGetInitialProps(
+  NextDocument: NextDocumentType,
+  server: EmotionServer
+): (ctx: any) => any {
+  return async function getInitialProps(ctx: any) {
+    const initialProps = await NextDocument.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          <ServerStyles html={initialProps.html} server={server} />
+        </>
+      ),
+    };
+  };
+}
