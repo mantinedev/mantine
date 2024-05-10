3afad4da0a276228aa30b8373d938c3a260ef689
Vitaly Rtishchev <rtivital@gmail.com>
2024-05-10 13:09:31 +0400

    [@mantine/charts] Fix incorrect tooltip position in LineChart, AreaChart and BarChart with vertical orientation

--- packages/@mantine/charts/src/AreaChart/AreaChart.tsx
+++ packages/@mantine/charts/src/AreaChart/AreaChart.tsx
@@ -409,7 +409,7 @@ export const AreaChart = factory<AreaChartFactory>((_props, ref) => {
             <Tooltip
               animationDuration={tooltipAnimationDuration}
               isAnimationActive={isAnimationActive}
-              position={{ y: 0 }}
+              position={orientation === 'vertical' ? {} : { y: 0 }}
               cursor={{
                 stroke: 'var(--chart-grid-color)',
                 strokeWidth: 1,
