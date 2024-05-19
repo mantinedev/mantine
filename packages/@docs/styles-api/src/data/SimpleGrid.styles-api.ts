5ed6bc01dd8a4275ce62fda3f3a50ced81584536
Vitaly Rtishchev <rtivital@gmail.com>
2024-05-19 10:12:41 +0400

    [mantine.dev] Update SimpleGrid styles api data

--- packages/@docs/styles-api/src/data/SimpleGrid.styles-api.ts
+++ packages/@docs/styles-api/src/data/SimpleGrid.styles-api.ts
@@ -4,6 +4,7 @@ import type { StylesApiData } from '../types';
 export const SimpleGridStylesApi: StylesApiData<SimpleGridFactory> = {
   selectors: {
     root: 'Root element',
+    container: 'Container element, available only when `type="container"` is set',
   },
 
   vars: {},
