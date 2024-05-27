1719382eacdb0fb1c1147734e4d79c5d081f3dc7
Vitaly Rtishchev <rtivital@gmail.com>
2024-05-27 22:29:51 +0400

    [mantine.dev] Add dropdown width documentation to combobox based components

--- packages/@docs/demos/src/demos/core/TagsInput/index.ts
+++ packages/@docs/demos/src/demos/core/TagsInput/index.ts
@@ -23,3 +23,4 @@ export { dropdownPadding } from './TagsInput.demo.dropdownPadding';
 export { dropdownShadow } from './TagsInput.demo.dropdownShadow';
 export { withinPopover } from './TagsInput.demo.withinPopover';
 export { renderOption } from './TagsInput.demo.renderOption';
+export { dropdownWidth } from './TagsInput.demo.dropdownWidth';
