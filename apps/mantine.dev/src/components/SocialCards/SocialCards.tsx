af30c2a1dfdd21c250e864a2d8963ceff450409d
Vitaly Rtishchev <rtivital@gmail.com>
2024-06-24 11:52:26 +0400

    [mantine.dev] Update social cards on home and getting started pages

--- apps/mantine.dev/src/components/SocialCards/SocialCards.tsx
+++ apps/mantine.dev/src/components/SocialCards/SocialCards.tsx
@@ -1,10 +1,13 @@
+import { IconArrowUpRight } from '@tabler/icons-react';
 import cx from 'clsx';
-import { DiscordIcon, GithubIcon, TwitterIcon } from '@mantinex/dev-icons';
+import { SimpleGrid, SimpleGridProps } from '@mantine/core';
+import { DiscordIcon, GithubIcon, XIcon } from '@mantinex/dev-icons';
+import { MantineLogo } from '@mantinex/mantine-logo';
 import { meta } from '@mantinex/mantine-meta';
 import classes from './SocialCards.module.css';
 
 interface CardBaseProps extends React.ComponentPropsWithoutRef<'a'> {
-  icon: 'discord' | 'twitter' | 'github';
+  icon: 'discord' | 'twitter' | 'github' | 'mantine';
   title: string;
   description: string;
   href: string;
@@ -12,14 +15,20 @@ interface CardBaseProps extends React.ComponentPropsWithoutRef<'a'> {
 
 const icons: Record<CardBaseProps['icon'], React.ReactNode> = {
   discord: <DiscordIcon size={30} className={classes.icon} />,
-  twitter: <TwitterIcon size={30} className={classes.icon} />,
+  twitter: <XIcon size={30} className={classes.icon} />,
   github: <GithubIcon size={30} className={classes.icon} />,
+  mantine: (
+    <div className={classes.icon}>
+      <MantineLogo size={50} type="mark" />
+    </div>
+  ),
 };
 
 export function CardBase({ icon, title, description, className, ...others }: CardBaseProps) {
   return (
     <a className={cx(classes.card, className)} target="_blank" rel="noreferrer" {...others}>
       {icons[icon]}
+      <IconArrowUpRight size={20} className={classes.arrow} />
       <div className={classes.body}>
         <div className={classes.title}>{title}</div>
         <div className={classes.description}>{description}</div>
@@ -35,7 +44,7 @@ export function DiscordCard() {
       className={classes.discord}
       href={meta.discordLink}
       title="Join Discord community"
-      description="Ask questions, share feedback, get updates, learn about upcoming features and more"
+      description="Join a community of 10,000+ developers to ask questions, share feedback and explore upcoming features"
     />
   );
 }
@@ -52,13 +61,25 @@ export function GitHubCard() {
   );
 }
 
+export function HelpCenterCard() {
+  return (
+    <CardBase
+      icon="mantine"
+      className={classes.hc}
+      href="https://help.mantine.dev"
+      title="Help center"
+      description="Explore 30+ frequently asked questions"
+    />
+  );
+}
+
 export function TwitterCard() {
   return (
     <CardBase
       icon="twitter"
       className={classes.twitter}
       href={meta.twitterLink}
-      title="Follow on Twitter"
+      title="Follow on X"
       description="Get notified about new minor and major releases"
     />
   );
@@ -68,14 +89,23 @@ interface SocialCardsProps {
   discord?: boolean;
   github?: boolean;
   twitter?: boolean;
+  helpCenter?: boolean;
+  cols?: SimpleGridProps['cols'];
 }
 
-export function SocialCards({ discord = true, github = true, twitter = true }: SocialCardsProps) {
+export function SocialCards({
+  discord = true,
+  github = true,
+  twitter = true,
+  helpCenter = true,
+  cols = { md: 2 },
+}: SocialCardsProps) {
   return (
-    <div className={classes.cards}>
+    <SimpleGrid cols={cols}>
       {discord && <DiscordCard />}
       {github && <GitHubCard />}
       {twitter && <TwitterCard />}
-    </div>
+      {helpCenter && <HelpCenterCard />}
+    </SimpleGrid>
   );
 }
