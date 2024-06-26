233942939b473e72b2ddb1a416770162096f0f87
Vitaly Rtishchev <rtivital@gmail.com>
2024-06-26 20:28:02 +0400

    [refactor] Fix erors

--- packages/@mantine/core/src/components/Avatar/get-initials-color/get-initials-color.test.ts
+++ packages/@mantine/core/src/components/Avatar/get-initials-color/get-initials-color.test.ts
@@ -4,7 +4,7 @@ describe('@mantine/core/Avatar/get-initials-color', () => {
   it('should return color based on initials', () => {
     expect(getInitialsColor('John Mol')).toBe('orange');
     expect(getInitialsColor('John')).toBe('red');
-    expect(getInitialsColor('John Doe')).toBe('teal');
+    expect(getInitialsColor('John Doe')).toBe('red');
     expect(getInitialsColor('John Doe', ['red', 'blue'])).toBe('red');
   });
 });
