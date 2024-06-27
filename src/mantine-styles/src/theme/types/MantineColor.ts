21c6aaf61a8bb9614cabea2845b4f6b925736c9b
ihmpavel <42217494+ihmpavel@users.noreply.github.com>
2024-06-27 14:02:26 +0200

    [@mantine/core] Fix compatibility with TypeScript 5.5 (#6445)

--- src/mantine-styles/src/theme/types/MantineColor.ts
+++ src/mantine-styles/src/theme/types/MantineColor.ts
@@ -17,7 +17,7 @@ export type DefaultMantineColor =
   | 'teal'
   | (string & {});
 
-export type MantineThemeColorsOverride = {};
+export interface MantineThemeColorsOverride {};
 
 export type MantineThemeColors = MantineThemeColorsOverride extends {
   colors: Record<infer CustomColors, Tuple<string, 10>>;
