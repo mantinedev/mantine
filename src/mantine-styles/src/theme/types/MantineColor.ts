c413e354309291a6dd17d6c15c8ba07d4cdc0878
Vitaly Rtishchev <rtivital@gmail.com>
2024-07-13 19:06:24 +0400

    Fix prettier errors

--- src/mantine-styles/src/theme/types/MantineColor.ts
+++ src/mantine-styles/src/theme/types/MantineColor.ts
@@ -17,7 +17,7 @@ export type DefaultMantineColor =
   | 'teal'
   | (string & {});
 
-export interface MantineThemeColorsOverride {};
+export interface MantineThemeColorsOverride {}
 
 export type MantineThemeColors = MantineThemeColorsOverride extends {
   colors: Record<infer CustomColors, Tuple<string, 10>>;
