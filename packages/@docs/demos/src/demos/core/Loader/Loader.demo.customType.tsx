7d68306b29f27a81f3e1b77c5771c588a8650501
Vitaly Rtishchev <rtivital@gmail.com>
2024-07-16 14:51:15 +0400

    [mantine.dev] Update most of theming demos to use createTheme instead of direct theme passing

--- packages/@docs/demos/src/demos/core/Loader/Loader.demo.customType.tsx
+++ packages/@docs/demos/src/demos/core/Loader/Loader.demo.customType.tsx
@@ -1,4 +1,4 @@
-import { Loader, MantineThemeProvider } from '@mantine/core';
+import { createTheme, Loader, MantineThemeProvider } from '@mantine/core';
 import { MantineDemo } from '@mantinex/demo';
 import { RingLoader, ringLoaderCode } from './_RingLoader';
 
@@ -6,40 +6,40 @@ const code = `
 import { MantineProvider, Loader } from '@mantine/core';
 import { RingLoader } from './RingLoader';
 
+const theme = createTheme({
+  components: {
+    Loader: Loader.extend({
+      defaultProps: {
+        loaders: { ...Loader.defaultLoaders, ring: RingLoader },
+        type: 'ring',
+      },
+    }),
+  },
+});
+
 function Demo() {
   return (
-    <MantineProvider
-      theme={{
-        components: {
-          Loader: Loader.extend({
-            defaultProps: {
-              loaders: { ...Loader.defaultLoaders, ring: RingLoader },
-              type: 'ring',
-            },
-          }),
-        },
-      }}
-    >
+    <MantineThemeProvider theme={theme}>
       <Loader />
-    </MantineProvider>
+    </MantineThemeProvider>
   );
 }
 `;
 
+const theme = createTheme({
+  components: {
+    Loader: Loader.extend({
+      defaultProps: {
+        loaders: { ...Loader.defaultLoaders, ring: RingLoader },
+        type: 'ring',
+      },
+    }),
+  },
+});
+
 function Demo() {
   return (
-    <MantineThemeProvider
-      theme={{
-        components: {
-          Loader: Loader.extend({
-            defaultProps: {
-              loaders: { ...Loader.defaultLoaders, ring: RingLoader },
-              type: 'ring',
-            },
-          }),
-        },
-      }}
-    >
+    <MantineThemeProvider theme={theme}>
       <Loader />
     </MantineThemeProvider>
   );
