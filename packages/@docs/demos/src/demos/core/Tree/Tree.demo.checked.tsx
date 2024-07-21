246f043025572830b09faa91eed90e0ccb66704d
Vitaly Rtishchev <rtivital@gmail.com>
2024-07-21 15:15:49 +0400

    [mantine.dev] Add checked Tree demo code

--- packages/@docs/demos/src/demos/core/Tree/Tree.demo.checked.tsx
+++ packages/@docs/demos/src/demos/core/Tree/Tree.demo.checked.tsx
@@ -5,28 +5,43 @@ import { data, dataCode } from './data';
 
 const code = `
 import { IconChevronDown } from '@tabler/icons-react';
-import { Group, Tree } from '@mantine/core';
+import { Checkbox, Group, RenderTreeNodePayload, Tree } from '@mantine/core';
 import { data } from './data';
 
-function Demo() {
-  return (
-    <Tree
-      data={data}
-      levelOffset={23}
-      renderNode={({ node, expanded, hasChildren, elementProps }) => (
-        <Group gap={5} {...elementProps}>
-          {hasChildren && (
-            <IconChevronDown
-              size={18}
-              style={{ transform: expanded ? 'rotate(180deg)' : 'rotate(0deg)' }}
-            />
-          )}
+const renderTreeNode = ({
+  node,
+  expanded,
+  hasChildren,
+  elementProps,
+  tree,
+}: RenderTreeNodePayload) => {
+  const checked = tree.isNodeChecked(node.value);
+  const indeterminate = tree.isNodeIndeterminate(node.value);
 
-          <span>{node.label}</span>
-        </Group>
-      )}
-    />
+  return (
+    <Group gap="xs" {...elementProps}>
+      <Checkbox.Indicator
+        checked={checked}
+        indeterminate={indeterminate}
+        onClick={() => (!checked ? tree.checkNode(node.value) : tree.uncheckNode(node.value))}
+      />
+
+      <Group gap={5} onClick={() => tree.toggleExpanded(node.value)}>
+        <span>{node.label}</span>
+
+        {hasChildren && (
+          <IconChevronDown
+            size={14}
+            style={{ transform: expanded ? 'rotate(180deg)' : 'rotate(0deg)' }}
+          />
+        )}
+      </Group>
+    </Group>
   );
+};
+
+function Demo() {
+  return <Tree data={data} levelOffset={23} expandOnClick={false} renderNode={renderTreeNode} />;
 }
 `;
 
