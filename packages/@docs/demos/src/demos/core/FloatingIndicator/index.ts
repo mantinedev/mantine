dafea9c69a2ac006d36819b6f4b4cad2c3c6f763
Vitaly Rtishchev <rtivital@gmail.com>
2024-07-21 19:13:07 +0400

    [mantine.dev] Add Tabs with FloatingIndicator demo

--- packages/@docs/demos/src/demos/core/FloatingIndicator/index.ts
+++ packages/@docs/demos/src/demos/core/FloatingIndicator/index.ts
@@ -1,2 +1,3 @@
 export { direction } from './FloatingIndicator.demo.direction';
 export { segmented } from './FloatingIndicator.demo.segmented';
+export { tabs } from './FloatingIndicator.demo.tabs';
