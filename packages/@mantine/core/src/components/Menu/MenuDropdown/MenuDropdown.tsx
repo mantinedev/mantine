7bdfe8b3a9690310bdb172c7f4303c420ead0ae1
Shawn O'Connor <shawnoconnor@uchicago.edu>
2024-07-24 13:02:42 -0500

    [@mantine/core] Menu: fix keboard Esc from closing menu and modal when in a modal

--- packages/@mantine/core/src/components/Menu/MenuDropdown/MenuDropdown.tsx
+++ packages/@mantine/core/src/components/Menu/MenuDropdown/MenuDropdown.tsx
@@ -84,7 +84,13 @@ export const MenuDropdown = factory<MenuDropdownFactory>((props, ref) => {
       data-menu-dropdown
       onKeyDown={handleKeyDown}
     >
-      <div tabIndex={-1} data-autofocus aria-hidden style={{ outline: 0 }} />
+      <div
+        tabIndex={-1}
+        data-autofocus
+        data-mantine-stop-propagation
+        aria-hidden
+        style={{ outline: 0 }}
+      />
       {children}
     </Popover.Dropdown>
   );
