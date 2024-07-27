c4b898d39182085307fb972a207a93ba3d84a580
Vitaly Rtishchev <rtivital@gmail.com>
2024-07-27 20:44:30 +0400

    [@mantine/core] Grid: Add container queries support

--- packages/@mantine/core/src/components/Grid/Grid.context.ts
+++ packages/@mantine/core/src/components/Grid/Grid.context.ts
@@ -1,10 +1,15 @@
+import { MantineSize } from '../../../lib';
 import { createSafeContext, GetStylesApi } from '../../core';
 import type { GridFactory } from './Grid';
+
+export type GridBreakpoints = Record<MantineSize, string>;
 
 interface GridContextValue {
   getStyles: GetStylesApi<GridFactory>;
   grow: boolean | undefined;
   columns: number;
+  breakpoints: GridBreakpoints | undefined;
+  type: 'container' | 'media' | undefined;
 }
 
 export const [GridProvider, useGridContext] = createSafeContext<GridContextValue>(
