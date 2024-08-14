4b10bce4a6d4c79a6ca6f1c1254a26779419e225
Li Qiang <Ives7@users.noreply.github.com>
2024-08-14 21:32:42 +0800

    feat: Make SearchControl's text customizable

--- packages/@mantinex/mantine-header/src/SearchControl/SearchControl.tsx
+++ packages/@mantinex/mantine-header/src/SearchControl/SearchControl.tsx
@@ -2,19 +2,23 @@ import { IconSearch } from '@tabler/icons-react';
 import cx from 'clsx';
 import { BoxProps, ElementProps, Group, rem, Text, UnstyledButton } from '@mantine/core';
 import classes from './SearchControl.module.css';
+import type { ReactNode } from 'react'
 
-interface SearchControlProps extends BoxProps, ElementProps<'button'> {}
+interface SearchControlProps extends BoxProps, ElementProps<'button'> {
+  placeholder?: ReactNode
+  shortcuts?: ReactNode
+}
 
-export function SearchControl({ className, ...others }: SearchControlProps) {
+export function SearchControl({ className, placeholder, shortcuts,  ...others }: SearchControlProps) {
   return (
     <UnstyledButton {...others} className={cx(classes.root, className)}>
       <Group gap="xs">
         <IconSearch style={{ width: rem(15), height: rem(15) }} stroke={1.5} />
         <Text fz="sm" c="dimmed" pr={80}>
-          Search
+          { placeholder || "Search" }
         </Text>
         <Text fw={700} className={classes.shortcut}>
-          Ctrl + K
+          { shortcuts || "Ctrl + K" }
         </Text>
       </Group>
     </UnstyledButton>
