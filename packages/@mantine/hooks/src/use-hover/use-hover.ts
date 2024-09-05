7559db35b57a095722ab092b5f74a2a612e86ab4
Max <max@degterev.me>
2024-09-06 00:32:09 +0200

    Update use-hover.ts to react to node changes

--- packages/@mantine/hooks/src/use-hover/use-hover.ts
+++ packages/@mantine/hooks/src/use-hover/use-hover.ts
@@ -1,24 +1,21 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
 
-export function useHover<T extends HTMLElement = HTMLDivElement>() {
+export const useHover = <T extends HTMLElement = HTMLDivElement>() => {
   const [hovered, setHovered] = useState(false);
   const ref = useRef<T>(null);
   const onMouseEnter = useCallback(() => setHovered(true), []);
   const onMouseLeave = useCallback(() => setHovered(false), []);
 
   useEffect(() => {
-    if (ref.current) {
-      ref.current.addEventListener('mouseenter', onMouseEnter);
-      ref.current.addEventListener('mouseleave', onMouseLeave);
+    if (!ref.current) return;
+    ref.current.addEventListener('mouseenter', onMouseEnter);
+    ref.current.addEventListener('mouseleave', onMouseLeave);
 
-      return () => {
-        ref.current?.removeEventListener('mouseenter', onMouseEnter);
-        ref.current?.removeEventListener('mouseleave', onMouseLeave);
-      };
-    }
-
-    return undefined;
-  }, []);
+    return () => {
+      ref.current?.removeEventListener('mouseenter', onMouseEnter);
+      ref.current?.removeEventListener('mouseleave', onMouseLeave);
+    };
+  }, [ref.current]);
 
   return { ref, hovered };
-}
+};
