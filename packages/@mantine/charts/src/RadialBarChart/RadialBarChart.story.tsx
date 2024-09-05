7113c5f89ee92931a72de171e19674ff6873ddc0
Vitaly Rtishchev <rtivital@gmail.com>
2024-09-05 18:46:11 +0400

    [@mantine/charts] RadialBarChart: Add basic features

--- packages/@mantine/charts/src/RadialBarChart/RadialBarChart.story.tsx
+++ packages/@mantine/charts/src/RadialBarChart/RadialBarChart.story.tsx
@@ -6,51 +6,44 @@ const data = [
   {
     name: '18-24',
     uv: 31.47,
-    pv: 2400,
-    fill: '#8884d8',
+    color: 'blue.7',
   },
   {
     name: '25-29',
     uv: 26.69,
-    pv: 4567,
-    fill: '#83a6ed',
+    color: 'orange.6',
   },
   {
     name: '30-34',
     uv: 15.69,
-    pv: 1398,
-    fill: '#8dd1e1',
+    color: 'yellow.7',
   },
   {
     name: '35-39',
     uv: 8.22,
-    pv: 9800,
-    fill: '#82ca9d',
+    color: 'cyan.6',
   },
   {
     name: '40-49',
     uv: 8.63,
-    pv: 3908,
-    fill: '#a4de6c',
+    color: 'green',
   },
   {
     name: '50+',
     uv: 2.63,
-    pv: 4800,
-    fill: '#d0ed57',
+    color: 'pink',
   },
   {
-    name: 'unknow',
+    name: 'unknown',
     uv: 6.67,
-    pv: 4800,
-    fill: '#ffc658',
+    color: 'red',
   },
 ];
 
 export function Usage() {
   return (
-    <div style={{ padding: 40 }}>
-      <RadialBarChart data={data} h={400} />
+    <div style={{}}>
+      <RadialBarChart data={data} h={400} dataKey="uv" />
     </div>
   );
 }
--- packages/@mantine/charts/src/RadialBarChart/RadialBarChart.tsx
+++ packages/@mantine/charts/src/RadialBarChart/RadialBarChart.tsx
@@ -1,4 +1,9 @@
-import { RadialBar, RadialBarChart as ReChartsRadialBarChart, ResponsiveContainer } from 'recharts';
+import {
+  RadialBar,
+  RadialBarProps,
+  RadialBarChart as ReChartsRadialBarChart,
+  ResponsiveContainer,
+} from 'recharts';
 import {
   Box,
   BoxProps,
@@ -6,7 +11,9 @@ import {
   ElementProps,
   factory,
   Factory,
+  getThemeColor,
   StylesApiProps,
+  useMantineTheme,
   useProps,
   useStyles,
 } from '@mantine/core';
@@ -14,14 +21,36 @@ import classes from './RadialBarChart.module.css';
 
 export type RadialBarChartStylesNames = 'root';
 export type RadialBarChartCssVariables = {
-  root: '--test';
+  root: '--chart-empty-background';
 };
 
 export interface RadialBarChartProps
   extends BoxProps,
     StylesApiProps<RadialBarChartFactory>,
     ElementProps<'div'> {
+  /** Chart data */
   data: Record<string, any>[];
+
+  /** Key from data object to use as data key */
+  dataKey: string;
+
+  /** Size of bars in px, `20` by default */
+  barSize?: number;
+
+  /** Determines whether empty bars area should be visible, `true` by default */
+  withBackground?: boolean;
+
+  /** Determines whether labels should be displayed, true` by default */
+  withLabels?: boolean;
+
+  /** Color of the empty background, by default depends on the color scheme */
+  emptyBackgroundColor?: string;
+
+  /** Props passed down to recharts RadialBar component */
+  radialBarProps?: Omit<RadialBarProps, 'ref'>;
+
+  /** Props passed down to recharts RadarChartChart component */
+  radialBarChartProps?: React.ComponentPropsWithoutRef<typeof ReChartsRadialBarChart>;
 }
 
 export type RadialBarChartFactory = Factory<{
@@ -31,17 +60,40 @@ export type RadialBarChartFactory = Factory<{
   vars: RadialBarChartCssVariables;
 }>;
 
-const defaultProps: Partial<RadialBarChartProps> = {};
+const defaultProps: Partial<RadialBarChartProps> = {
+  barSize: 20,
+  withBackground: true,
+  withLabels: true,
+};
 
-const varsResolver = createVarsResolver<RadialBarChartFactory>(() => ({
-  root: {
-    '--test': 'test',
-  },
-}));
+const varsResolver = createVarsResolver<RadialBarChartFactory>(
+  (theme, { emptyBackgroundColor }) => ({
+    root: {
+      '--chart-empty-background': emptyBackgroundColor
+        ? getThemeColor(emptyBackgroundColor, theme)
+        : undefined,
+    },
+  })
+);
 
 export const RadialBarChart = factory<RadialBarChartFactory>((_props, ref) => {
   const props = useProps('RadialBarChart', defaultProps, _props);
-  const { classNames, className, style, styles, unstyled, vars, data, ...others } = props;
+  const {
+    classNames,
+    className,
+    style,
+    styles,
+    unstyled,
+    vars,
+    data,
+    barSize,
+    withBackground,
+    dataKey,
+    radialBarProps,
+    radialBarChartProps,
+    withLabels,
+    ...others
+  } = props;
 
   const getStyles = useStyles<RadialBarChartFactory>({
     name: 'RadialBarChart',
@@ -56,18 +108,33 @@ export const RadialBarChart = factory<RadialBarChartFactory>((_props, ref) => {
     varsResolver,
   });
 
+  const theme = useMantineTheme();
+  const dataWithResolvedColor = data.map(({ color, ...item }) => ({
+    ...item,
+    fill: getThemeColor(color, theme),
+  }));
+
   return (
     <Box ref={ref} {...getStyles('root')} {...others}>
       <ResponsiveContainer>
         <ReChartsRadialBarChart
-          cx="50%"
-          cy="50%"
           innerRadius="10%"
-          outerRadius="80%"
-          barSize={10}
-          data={data}
+          outerRadius="100%"
+          barSize={barSize}
+          data={dataWithResolvedColor}
+          {...radialBarChartProps}
         >
-          <RadialBar label={{ position: 'insideStart', fill: '#fff' }} background dataKey="uv" />
+          <RadialBar
+            label={
+              withLabels
+                ? { position: 'insideStart', fill: 'var(--mantine-color-white)', fontSize: 12 }
+                : undefined
+            }
+            background={withBackground ? { fill: 'var(--chart-empty-background)' } : undefined}
+            dataKey={dataKey}
+            isAnimationActive={false}
+            {...radialBarProps}
+          />
         </ReChartsRadialBarChart>
       </ResponsiveContainer>
     </Box>
