d3d316a859b3e0f84d60527dc3ae95fce92f8177
“Kenzo-Wada” <used.acormhib@gmail.com>
2024-09-18 01:31:07 +0900

    [@mantine/core] chore: add sizes to `Title`

--- packages/@mantine/core/src/components/Title/get-title-size.ts
+++ packages/@mantine/core/src/components/Title/get-title-size.ts
@@ -2,6 +2,7 @@ import { rem } from '../../core';
 import type { TitleOrder, TitleSize } from './Title';
 
 const headings: unknown[] = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+const sizes: unknown[] = ['xs', 'sm', 'md', 'lg', 'xl'];
 
 export interface GetTitleSizeResult {
   fontSize: string;
@@ -9,7 +10,7 @@ export interface GetTitleSizeResult {
   lineHeight: string;
 }
 
-export function getTitleSize(order: TitleOrder, size: TitleSize | undefined): GetTitleSizeResult {
+export function getTitleSize(order: TitleOrder, size?: TitleSize): GetTitleSizeResult {
   const titleSize = size !== undefined ? size : `h${order}`;
 
   if (headings.includes(titleSize)) {
@@ -17,6 +18,12 @@ export function getTitleSize(order: TitleOrder, size: TitleSize | undefined): Ge
       fontSize: `var(--mantine-${titleSize}-font-size)`,
       fontWeight: `var(--mantine-${titleSize}-font-weight)`,
       lineHeight: `var(--mantine-${titleSize}-line-height)`,
+    };
+  } else if (sizes.includes(titleSize)) {
+    return {
+      fontSize: `var(--mantine-font-size-${titleSize})`,
+      fontWeight: `var(--mantine-h${order}-font-weight)`,
+      lineHeight: `var(--mantine-h${order}-line-height)`,
     };
   }
 
