de6b9f3b74be1cb4bc88c2d6d4e79e6cf55f080b
Vitaly Rtishchev <rtivital@gmail.com>
2024-09-29 17:33:59 +0400

    [@mantine/core] AngleSlider: Improve color contrast of the indicator

--- packages/@mantine/core/src/components/AngleSlider/AngleSlider.story.tsx
+++ packages/@mantine/core/src/components/AngleSlider/AngleSlider.story.tsx
@@ -5,6 +5,36 @@ export default { title: 'AngleSlider' };
 export function Usage() {
   return (
     <div style={{ padding: 40 }}>
+      <AngleSlider size={100} formatLabel={(value) => `${value}°`} restrictToMarks />
+    </div>
+  );
+}
+
+export function WithMarks() {
+  return (
+    <div style={{ padding: 40 }}>
+      <AngleSlider
+        size={100}
+        formatLabel={(value) => `${value}°`}
+        marks={[
+          { value: 0 },
+          { value: 45 },
+          { value: 90 },
+          { value: 135 },
+          { value: 180 },
+          { value: 225 },
+          { value: 270 },
+          { value: 315 },
+        ]}
+        restrictToMarks
+      />
+    </div>
+  );
+}
+
+export function RestrictToMarks() {
+  return (
+    <div style={{ padding: 40 }}>
       <AngleSlider
         size={100}
         formatLabel={(value) => `${value}°`}
