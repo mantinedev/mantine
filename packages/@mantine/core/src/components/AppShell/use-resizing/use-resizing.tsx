9e8d47aa1bf9578563a73fcf8fd8f3208bc400a9
Rabee AbuBaker <38038348+RabeeAbuBaker@users.noreply.github.com>
2024-10-02 15:07:11 +0300

    [@mantine/core] AppShell: Fix error when Suspense is rendered inside AppShell (#6927)

--- packages/@mantine/core/src/components/AppShell/use-resizing/use-resizing.tsx
+++ packages/@mantine/core/src/components/AppShell/use-resizing/use-resizing.tsx
@@ -14,18 +14,25 @@ export function useResizing({ transitionDuration, disabled }: UseResizingInput) 
   useWindowEvent('resize', () => {
     setResizing(true);
     clearTimeout(resizingTimeout.current);
-    resizingTimeout.current = window.setTimeout(() => setResizing(false), 200);
+    resizingTimeout.current = window.setTimeout(
+      () =>
+        startTransition(() => {
+          setResizing(false);
+        }),
+      200
+    );
   });
 
   useIsomorphicEffect(() => {
-    startTransition(() => {
-      setResizing(true);
-      clearTimeout(disabledTimeout.current);
-      disabledTimeout.current = window.setTimeout(
-        () => setResizing(false),
-        transitionDuration || 0
-      );
-    });
+    setResizing(true);
+    clearTimeout(disabledTimeout.current);
+    disabledTimeout.current = window.setTimeout(
+      () =>
+        startTransition(() => {
+          setResizing(false);
+        }),
+      transitionDuration || 0
+    );
   }, [disabled, transitionDuration]);
 
   return resizing;
