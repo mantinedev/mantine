9430bca048842b44cd8ff5e7a1766237c2ad29a8
Vitaly Rtishchev <rtivital@gmail.com>
2024-11-15 16:07:34 +0400

    [mantine.dev] Add new component for colors preview

--- apps/mantine.dev/src/components/ThemeColors/ColorsGroup/ColorsGroup.tsx
+++ apps/mantine.dev/src/components/ThemeColors/ColorsGroup/ColorsGroup.tsx
@@ -1,4 +1,6 @@
-import { Box, ColorSwatch, Text, useMantineTheme } from '@mantine/core';
+import { useState } from 'react';
+import { getContrastColor, Text, Tooltip, UnstyledButton, useMantineTheme } from '@mantine/core';
+import { useClipboard } from '@mantine/hooks';
 import classes from './ColorsGroup.module.css';
 
 interface ColorsGroupProps {
@@ -7,24 +9,54 @@ interface ColorsGroupProps {
 
 export function ColorsGroup({ group }: ColorsGroupProps) {
   const theme = useMantineTheme();
+  const colors = theme.colors[group];
+  const [active, setActive] = useState({ color: colors[6], index: 6 });
+  const clipboard = useClipboard({ timeout: 500 });
 
-  const colors = theme.colors[group].map((color, index) => (
-    <div key={color} className={classes.color}>
-      <ColorSwatch color={color} radius="md" className={classes.swatch} />
-      <Box mt={7}>
-        <Text size="xs" className={classes.colorName}>
-          {group} {index}
-        </Text>
-        <Text c="dimmed" size="xs" tt="uppercase" mt={2} fz={10}>
-          {color}
-        </Text>
-      </Box>
-    </div>
+  const swatches = colors.map((color, index) => (
+    <UnstyledButton
+      aria-label="Copy color value"
+      className={classes.secondarySwatch}
+      key={color}
+      onMouseEnter={() => setActive({ color, index })}
+      onClick={() => clipboard.copy(color)}
+      style={{
+        background: color,
+        color: getContrastColor({
+          color,
+          theme: { ...theme, luminanceThreshold: 0.25 },
+          autoContrast: true,
+        }),
+      }}
+    />
   ));
 
   return (
-    <div className={classes.wrapper}>
-      <div className={classes.group}>{colors}</div>
-    </div>
+    <Tooltip
+      color={clipboard.copied ? 'teal.8' : undefined}
+      label={clipboard.copied ? 'Copied!' : 'Hover colors for preview, click to copy HEX value'}
+      multiline
+      maw={190}
+      position="top-start"
+    >
+      <div className={classes.root} onMouseLeave={() => setActive({ color: colors[6], index: 6 })}>
+        <div className={classes.inner}>
+          <UnstyledButton
+            className={classes.primarySwatch}
+            onClick={() => clipboard.copy(active.color)}
+            style={{
+              background: active.color,
+              color: getContrastColor({ color: active.color, theme, autoContrast: true }),
+            }}
+          >
+            <Text className={classes.name}>{group}</Text>
+            <div className={classes.colorIndex}>{active.index}</div>
+            <div className={classes.colorValue}>{active.color}</div>
+          </UnstyledButton>
+
+          <div className={classes.swatches}>{swatches}</div>
+        </div>
+      </div>
+    </Tooltip>
   );
 }
--- apps/mantine.dev/src/components/ThemeColors/ThemeColors.tsx
+++ apps/mantine.dev/src/components/ThemeColors/ThemeColors.tsx
@@ -1,11 +1,14 @@
-import { DEFAULT_THEME } from '@mantine/core';
+import { DEFAULT_THEME, SimpleGrid } from '@mantine/core';
 import { ColorsGroup } from './ColorsGroup/ColorsGroup';
-import classes from './ThemeColors.module.css';
 
 export function ThemeColors() {
   const groups = Object.keys(DEFAULT_THEME.colors).map((group) => (
     <ColorsGroup group={group} key={group} />
   ));
 
-  return <div className={classes.root}>{groups}</div>;
+  return (
+    <SimpleGrid type="container" cols={{ base: 1, '440px': 2, '760px': 3 }}>
+      {groups}
+    </SimpleGrid>
+  );
 }
