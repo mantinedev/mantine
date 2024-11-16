b2364bf49430798f9dc5660584eeecd52b5c8759
Vitaly Rtishchev <rtivital@gmail.com>
2024-11-16 10:40:14 +0400

    [core] Add script to generate default css variables

---
+++ scripts/codegen/generate-default-css-variables.ts
@@ -0,0 +1,13 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { convertCssVariables } from '../../packages/@mantine/core/src/core/MantineProvider/convert-css-variables/convert-css-variables';
+import { DEFAULT_THEME } from '../../packages/@mantine/core/src/core/MantineProvider/default-theme';
+import { defaultCssVariablesResolver } from '../../packages/@mantine/core/src/core/MantineProvider/MantineCssVariables/default-css-variables-resolver';
+
+fs.writeFileSync(
+  path.join(
+    process.cwd(),
+    'packages/@mantine/core/src/core/MantineProvider/default-css-variables.css'
+  ),
+  convertCssVariables(defaultCssVariablesResolver(DEFAULT_THEME), ':root')
+);
