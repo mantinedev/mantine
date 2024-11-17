deabebde9bcef0335b8bef36a65f4caaec6bdb2a
Vitaly Rtishchev <rtivital@gmail.com>
2024-11-17 22:02:03 +0400

    [mantine.dev] Add stats section to the about page

--- scripts/docs/count.ts
+++ scripts/docs/count.ts
@@ -1,8 +1,6 @@
+import fs from 'fs';
 import fg from 'fast-glob';
 import { getPath } from '../utils/get-path';
-import { createLogger } from '../utils/signale';
-
-const logger = createLogger('count');
 
 const components = fg.sync([
   getPath('apps/mantine.dev/src/pages/core/*.mdx'),
@@ -14,7 +12,16 @@ const hooks = fg.sync([getPath('apps/mantine.dev/src/pages/hooks/*.mdx')]);
 const pages = fg.sync([getPath('apps/mantine.dev/src/pages/**/*.mdx')]);
 const demos = fg.sync([getPath('packages/@docs/demos/src/**/*.demo.*.tsx')]);
 
-logger.info('%d components', components.length);
-logger.info('%d  hooks', hooks.length);
-logger.info('%d documentation pages', pages.length);
-logger.info('%d demos', demos.length);
+fs.writeFileSync(
+  getPath('apps/mantine.dev/src/.docgen/count.json'),
+  JSON.stringify(
+    {
+      components: components.length,
+      hooks: hooks.length,
+      pages: pages.length,
+      demos: demos.length,
+    },
+    null,
+    2
+  )
+);
