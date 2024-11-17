3b0436e80316950c26b3433a6ded308aad9ff75b
Vitaly Rtishchev <rtivital@gmail.com>
2024-11-17 07:38:41 +0400

    [mantine.dev] Fix scroll being locked when the mobile sidebar was opened and then resized to desktop

--- apps/mantine.dev/src/components/Shell/DocsNavbar/DocsMobileNavbar.tsx
+++ apps/mantine.dev/src/components/Shell/DocsNavbar/DocsMobileNavbar.tsx
@@ -1,5 +1,6 @@
 import ReactRemoveScroll from 'react-remove-scroll/dist/es5/Combination';
 import { keys, ScrollArea } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 import { MDX_NAV_DATA } from '@/mdx';
 import { CategoriesList } from './DocsNavbar';
 import classes from './DocsNavbar.module.css';
@@ -8,9 +9,10 @@ const groups = keys(MDX_NAV_DATA);
 
 export function DocsMobileNavbar() {
   const categories = groups.map((group) => <CategoriesList categories={MDX_NAV_DATA[group]} />);
+  const isMobile = useMediaQuery('(max-width: 67.5em)');
 
   return (
-    <ReactRemoveScroll>
+    <ReactRemoveScroll enabled={isMobile}>
       <nav className={classes.mobileNavbar}>
         <ScrollArea h="calc(100vh - var(--docs-header-height))" type="never">
           <div className={classes.mobileNavbarInner}>{categories}</div>
