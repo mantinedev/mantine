b45f2c588138cd315d705d83cb1e43892b74c6c3
Vitaly Rtishchev <rtivital@gmail.com>
2024-11-17 17:24:26 +0400

    [mantine.dev] Finish hooks section on the new home page

--- apps/mantine.dev/src/components/HomePage2/HomePageHooks/HomePageHooks.tsx
+++ apps/mantine.dev/src/components/HomePage2/HomePageHooks/HomePageHooks.tsx
@@ -1,4 +1,5 @@
-import { SimpleGrid } from '@mantine/core';
+import Link from 'next/link';
+import { SimpleGrid, Text, Title } from '@mantine/core';
 import { useMediaQuery } from '@mantine/hooks';
 import { Demo } from '@mantinex/demo';
 import { HooksDemos, SliderDemos } from '@docs/demos';
@@ -7,11 +8,30 @@ import { HomePageDescription } from '../shared/HomePageDescription/HomePageDescr
 import { HomePageTitle } from '../shared/HomePageTitle/HomePageTitle';
 import classes from './HomePageHooks.module.css';
 
-// use-resize-observer +
-// use-move +
-// use-hotkeys +
+interface DemoColumnProps {
+  children: React.ReactNode;
+  title: string;
+  description: string;
+  link: string;
+}
 
-// use-eye-dropper
+function DemoColumn({ children, title, description, link }: DemoColumnProps) {
+  return (
+    <section className={classes.column}>
+      <header className={classes.header}>
+        <Title order={3} className={classes.title}>
+          <Link href={link} className={classes.titleLink}>
+            {title}
+          </Link>
+        </Title>
+
+        <Text className={classes.description}>{description}</Text>
+      </header>
+
+      {children}
+    </section>
+  );
+}
 
 export function HomePageHooks() {
   const isMobile = useMediaQuery('(max-width: 62em)');
@@ -20,27 +40,46 @@ export function HomePageHooks() {
     <section className={classes.root}>
       <HomePageContainer>
         <HomePageTitle order={2}>Hooks library</HomePageTitle>
-        <HomePageDescription className={classes.description}>70+ hooks for</HomePageDescription>
+        <HomePageDescription>70+ hooks for</HomePageDescription>
 
-        <SimpleGrid cols={{ md: 2 }}>
-          <div className={classes.column}>
+        <SimpleGrid cols={{ md: 2 }} className={classes.demos} spacing="xl">
+          <DemoColumn
+            title="use-move"
+            description="use-move hook handles move behavior over given element, can be used to build custom sliders"
+            link="/hooks/use-move"
+          >
             <Demo
               data={SliderDemos.customSlider}
               demoProps={{ defaultExpanded: false, maxCollapsedHeight: isMobile ? 150 : 428 }}
             />
-          </div>
-          <div className={classes.column}>
+          </DemoColumn>
+
+          <DemoColumn
+            title="use-resize-observer"
+            description="use-resize-observer hook tracks element size and position changes"
+            link="/hooks/use-resize-observer"
+          >
             <Demo data={HooksDemos.useResizeObserverDemo} demoProps={{ defaultExpanded: false }} />
-          </div>
-          <div className={classes.column}>
+          </DemoColumn>
+
+          <DemoColumn
+            title="use-hotkeys"
+            description="use-hotkeys hook allows binding keyboard shortcuts to actions"
+            link="/hooks/use-hotkeys"
+          >
             <Demo data={HooksDemos.useHotkeysDemoIndex} demoProps={{ defaultExpanded: false }} />
-          </div>
-          <div className={classes.column}>
+          </DemoColumn>
+
+          <DemoColumn
+            title="use-eye-dropper"
+            description="use-eye-dropper hook allows picking colors from anywhere on the screen"
+            link="/hooks/use-eye-dropper"
+          >
             <Demo
               data={HooksDemos.useEyeDropperUsage}
               demoProps={{ defaultExpanded: false, maxCollapsedHeight: isMobile ? 150 : 204 }}
             />
-          </div>
+          </DemoColumn>
         </SimpleGrid>
       </HomePageContainer>
     </section>
