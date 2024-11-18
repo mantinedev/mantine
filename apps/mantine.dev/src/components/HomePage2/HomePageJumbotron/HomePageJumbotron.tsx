829218dce798fff6113ab4f448357eb1f1b21353
Vitaly Rtishchev <rtivital@gmail.com>
2024-11-18 22:05:45 +0400

    [mantine.dev] Add initial jumbotron scrolling image implementation

--- apps/mantine.dev/src/components/HomePage2/HomePageJumbotron/HomePageJumbotron.tsx
+++ apps/mantine.dev/src/components/HomePage2/HomePageJumbotron/HomePageJumbotron.tsx
@@ -6,43 +6,48 @@ import { meta } from '@mantinex/mantine-meta';
 import { HomePageContainer } from '../shared/HomePageContainer/HomePageContainer';
 import { HomePageDescription } from '../shared/HomePageDescription/HomePageDescription';
 import { HomePageTitle } from '../shared/HomePageTitle/HomePageTitle';
+import img from './img.png';
 import classes from './HomePageJumbotron.module.css';
 
 export function HomePageJumbotron() {
   return (
     <section className={classes.root}>
-      <HomePageContainer>
-        <HomePageTitle order={1}>
-          A fully featured React
-          <br /> components library
-        </HomePageTitle>
+      <div className={classes.overlay} />
+      <HomePageContainer className={classes.container}>
+        <div className={classes.image} style={{ backgroundImage: `url(${img.src})` }} />
+        <div className={classes.main}>
+          <HomePageTitle order={1}>
+            A fully featured React
+            <br /> components library
+          </HomePageTitle>
 
-        <HomePageDescription>
-          Build fully functional accessible web applications faster than ever – Mantine includes
-          more than 120 customizable components and 70 hooks to cover you in any situation
-        </HomePageDescription>
+          <HomePageDescription>
+            Build fully functional accessible web applications faster than ever – Mantine includes
+            more than 120 customizable components and 70 hooks to cover you in any situation
+          </HomePageDescription>
 
-        <div className={classes.actions}>
-          <Button
-            component={Link}
-            href="/getting-started"
-            className={classes.action}
-            data-get-started
-            justify="space-between"
-            rightSection={<IconArrowUpRight size={26} stroke={1.5} />}
-          >
-            Get Started
-          </Button>
-          <Button
-            className={classes.action}
-            data-github
-            rightSection={<GithubIcon size={26} />}
-            justify="space-between"
-            component="a"
-            href={meta.gitHubLinks.mantine}
-          >
-            GitHub
-          </Button>
+          <div className={classes.actions}>
+            <Button
+              component={Link}
+              href="/getting-started"
+              className={classes.action}
+              data-get-started
+              justify="space-between"
+              rightSection={<IconArrowUpRight size={26} stroke={1.5} />}
+            >
+              Get Started
+            </Button>
+            <Button
+              className={classes.action}
+              data-github
+              rightSection={<GithubIcon size={26} />}
+              justify="space-between"
+              component="a"
+              href={meta.gitHubLinks.mantine}
+            >
+              GitHub
+            </Button>
+          </div>
         </div>
       </HomePageContainer>
     </section>
