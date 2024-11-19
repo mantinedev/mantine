07fed8cd247855f71c1afad0cb62ef169f24a593
Patrick <pfo@omicsstudio.com>
2024-11-19 14:30:37 +0100

    [@mantine/modals] Add `updateModal` and `updateContextModal` functions (#7104)
    * Update index.ts
    
    * add updateModal and updateContextModal
    
    * update storybook example
    
    * update formatting with prettier
    
    * open modal documentation
    
    * Update events.ts
    
    * Update events.ts
    
    * Update events.ts
    
    * Update Modals.demo.updateModal.tsx
    
    * update modal docs
    
    * wip context modal documentation
    
    * finish context modal documentation
    
    * Update Modals.demo.updateContextModal.tsx
    
    * formatting

---
+++ packages/@docs/demos/src/demos/modals/Modals.demo.updateContextModal.tsx
@@ -0,0 +1,116 @@
+import { Button } from '@mantine/core';
+import { modals } from '@mantine/modals';
+import { MantineDemo } from '@mantinex/demo';
+
+const code = `
+import { Button, Text, Stack, Center, Loader, rem } from '@mantine/core';
+import { modals, ContextModalProps, ModalsProvider } from '@mantine/modals';
+import { IconCheck } from '@tabler/icons-react';
+
+const TestModal = ({
+  context,
+  id,
+  innerProps,
+}: ContextModalProps<{ modalBody: string, loading: boolean }>) => (
+  <>
+    <Stack>
+      <Text size="sm">{innerProps.modalBody}</Text>
+      <Center>
+        {innerProps.loading ? (
+          <Loader size={32}/>
+        ): (
+          <IconCheck style={{ width: rem(32), height: rem(32), color: "green" }} />
+        )}
+      </Center>
+    </Stack>
+    <Button fullWidth mt="md" disabled={innerProps.loading} onClick={() => context.closeModal(id)}>
+      Close modal
+    </Button>
+  </>
+);
+
+function Demo() {
+  return (
+    <ModalsProvider
+      modals={{ demonstration: TestModal /* ...other modals */ }}
+    >
+      <Button
+        onClick={() => {
+          const modalId = modals.openContextModal({
+            modal: 'asyncDemonstration',
+            title: 'Processing...',
+            closeOnEscape: false,
+            closeOnClickOutside: false,
+            closeButtonProps:{ disabled:true },
+            innerProps: {
+              modalBody:
+                'You cannot close this modal until 2 seconds have passed.',
+              loading: true,
+            },
+          });
+
+          setTimeout(() => {
+            modals.updateContextModal({
+              modalId,
+              title: "Processing Complete!",
+              closeOnEscape: true,
+              closeOnClickOutside: true,
+              closeButtonProps:{ disabled: false },
+              innerProps: {
+                modalBody:
+                  'You can now close the modal.',
+                loading: false,
+              },
+            })
+          }, 2000);
+        }}
+      >
+        Open updating context modal
+      </Button>
+    </ModalsProvider>
+  );
+}
+`;
+
+function Demo() {
+  return (
+    <Button
+      onClick={() => {
+        const modalId = modals.openContextModal({
+          modal: 'asyncDemonstration',
+          title: 'Processing...',
+          closeOnEscape: false,
+          closeOnClickOutside: false,
+          closeButtonProps: { disabled: true },
+          innerProps: {
+            modalBody: 'You cannot close this modal until 2 seconds have passed.',
+            loading: true,
+          },
+        });
+
+        setTimeout(() => {
+          modals.updateContextModal({
+            modalId,
+            title: 'Processing Complete!',
+            closeOnEscape: true,
+            closeOnClickOutside: true,
+            closeButtonProps: { disabled: false },
+            innerProps: {
+              modalBody: 'You can now close the modal.',
+              loading: false,
+            },
+          });
+        }, 2000);
+      }}
+    >
+      Open updating context modal
+    </Button>
+  );
+}
+
+export const updateContextModal: MantineDemo = {
+  type: 'code',
+  centered: true,
+  component: Demo,
+  code,
+};
