8b37c8d71d1ace35ff1b62a7121e7c5b33b45250
Vitaly Rtishchev <rtivital@gmail.com>
2024-11-25 17:10:08 +0400

    [help.mantine.dev] Add color scheme flickering question

--- apps/help.mantine.dev/src/mdx.ts
+++ apps/help.mantine.dev/src/mdx.ts
@@ -7,6 +7,7 @@ import { meta as canIUseMantineWithAstro } from './pages/q/can-i-use-mantine-wit
 import { meta as canIUseMantineWithCra } from './pages/q/can-i-use-mantine-with-cra.mdx';
 import { meta as carouselMissingStyles } from './pages/q/carousel-missing-styles.mdx';
 import { meta as colorSchemeColor } from './pages/q/color-scheme-color.mdx';
+import { meta as colorSchemeFlickering } from './pages/q/color-scheme-flickering.mdx';
 import { meta as comboboxTesting } from './pages/q/combobox-testing.mdx';
 import { meta as customInputUseForm } from './pages/q/custom-input-use-form.mdx';
 import { meta as dataGridINeed } from './pages/q/data-grid-i-need.mdx';
@@ -72,6 +73,7 @@ export const MDX_DATA = [
   canIUseMantineWithCra,
   carouselMissingStyles,
   colorSchemeColor,
+  colorSchemeFlickering,
   comboboxTesting,
   customInputUseForm,
   dataGridINeed,
