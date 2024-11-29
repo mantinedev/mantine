675fc8d4b3ee9db5f22755b14ae74d174449d971
Patrick <pfo@omicsstudio.com>
2024-11-29 13:49:51 +0100

    Update ModalsProvider.tsx

--- packages/@mantine/modals/src/ModalsProvider.tsx
+++ packages/@mantine/modals/src/ModalsProvider.tsx
@@ -148,8 +148,8 @@ export function ModalsProvider({ children, modalProps, labels, modals }: ModalsP
   );
 
   const updateContextModal = useCallback(
-    (payload: { modalId: string } & Partial<OpenContextModal<any>>) => {
-      dispatch({ type: 'UPDATE', modalId: payload.modalId, newProps: payload });
+    ({ modalId, ...newProps }: { modalId: string } & Partial<OpenContextModal<any>>) => {
+      dispatch({ type: 'UPDATE', modalId, newProps });
     },
     [dispatch]
   );
