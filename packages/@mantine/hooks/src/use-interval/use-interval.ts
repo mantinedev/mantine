ecfd1da646015e7d369c48eaa30d11a77924e20b
Vitaly Rtishchev <rtivital@gmail.com>
2024-12-08 12:41:18 +0400

    [@mantine/hooks] use-interval: Fix interval not starting correctly in some cases

--- packages/@mantine/hooks/src/use-interval/use-interval.ts
+++ packages/@mantine/hooks/src/use-interval/use-interval.ts
@@ -11,12 +11,12 @@ export function useInterval(
   { autoInvoke = false }: UseIntervalOptions = {}
 ) {
   const [active, setActive] = useState(false);
-  const intervalRef = useRef<number>(-1);
+  const intervalRef = useRef<number | null>(null);
   const fnRef = useRef<() => void>(null);
 
   const start = () => {
     setActive((old) => {
-      if (!old && !intervalRef.current) {
+      if (!old && (!intervalRef.current || intervalRef.current === -1)) {
         intervalRef.current = window.setInterval(fnRef.current!, interval);
       }
       return true;
@@ -25,7 +25,7 @@ export function useInterval(
 
   const stop = () => {
     setActive(false);
-    window.clearInterval(intervalRef.current);
+    window.clearInterval(intervalRef.current || -1);
     intervalRef.current = -1;
   };
 
