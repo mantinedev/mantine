ea73b9fb060c99e8e85bb4ae422d1bead3dc97b7
Vitaly Rtishchev <rtivital@gmail.com>
2024-12-08 14:59:33 +0400

    [mantine.dev] Add `restrictToMarks` RangeSlider documentation

--- packages/@docs/demos/src/demos/core/Slider/Slider.demo.restrictToMarks.tsx
+++ packages/@docs/demos/src/demos/core/Slider/Slider.demo.restrictToMarks.tsx
@@ -1,27 +1,59 @@
-import { Slider } from '@mantine/core';
+import { RangeSlider, Slider, Stack } from '@mantine/core';
 import { MantineDemo } from '@mantinex/demo';
 
 const code = `
-import { Slider } from '@mantine/core';
+import { RangeSlider, Slider, Stack } from '@mantine/core';
 
 function Demo() {
   return (
-    <Slider
-      restrictToMarks
-      defaultValue={25}
-      marks={Array.from({ length: 5 }).map((_, index) => ({ value: index * 25 }))}
-    />
+    <Stack>
+      <Slider
+        restrictToMarks
+        defaultValue={25}
+        marks={Array.from({ length: 5 }).map((_, index) => ({ value: index * 25 }))}
+      />
+
+      <RangeSlider
+        restrictToMarks
+        defaultValue={[5, 15]}
+        marks={[
+          { value: 5 },
+          { value: 15 },
+          { value: 25 },
+          { value: 35 },
+          { value: 70 },
+          { value: 80 },
+          { value: 90 },
+        ]}
+      />
+    </Stack>
   );
 }
 `;
 
 function Demo() {
   return (
-    <Slider
-      restrictToMarks
-      defaultValue={25}
-      marks={Array.from({ length: 5 }).map((_, index) => ({ value: index * 25 }))}
-    />
+    <Stack>
+      <Slider
+        restrictToMarks
+        defaultValue={25}
+        marks={Array.from({ length: 5 }).map((_, index) => ({ value: index * 25 }))}
+      />
+
+      <RangeSlider
+        restrictToMarks
+        defaultValue={[5, 15]}
+        marks={[
+          { value: 5 },
+          { value: 15 },
+          { value: 25 },
+          { value: 35 },
+          { value: 70 },
+          { value: 80 },
+          { value: 90 },
+        ]}
+      />
+    </Stack>
   );
 }
 
