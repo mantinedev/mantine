d225a6417ee4cf43b62a28727fea0e05c9fb1089
Stanislas Lange <git@slange.me>
2024-12-24 15:07:09 +0100

    order

--- packages/@docs/demos/src/demos/hooks/use-favicon.demo.usage.tsx
+++ packages/@docs/demos/src/demos/hooks/use-favicon.demo.usage.tsx
@@ -10,15 +10,15 @@ import { Group, Button } from '@mantine/core';
 
 function Demo() {
   const [favicon, setFavicon] = useState('https://mantine.dev/favicon.svg');
-  const setMantineUIFavicon = () => setFavicon('https://ui.mantine.dev/favicon.svg');
   const setMantineFavicon = () => setFavicon('https://mantine.dev/favicon.svg');
+  const setMantineUIFavicon = () => setFavicon('https://ui.mantine.dev/favicon.svg');
 
   useFavicon(favicon);
 
   return (
     <Group justify="center">
-      <Button onClick={setMantineUIFavicon}>Mantine UI favicon</Button>
       <Button onClick={setMantineFavicon}>Mantine favicon</Button>
+      <Button onClick={setMantineUIFavicon}>Mantine UI favicon</Button>
     </Group>
   );
 }
@@ -26,15 +26,15 @@ function Demo() {
 
 function Demo() {
   const [favicon, setFavicon] = useState('https://mantine.dev/favicon.svg');
-  const setMantineUIFavicon = () => setFavicon('https://ui.mantine.dev/favicon.svg');
   const setMantineFavicon = () => setFavicon('https://mantine.dev/favicon.svg');
+  const setMantineUIFavicon = () => setFavicon('https://ui.mantine.dev/favicon.svg');
 
   useFavicon(favicon);
 
   return (
     <Group justify="center">
-      <Button onClick={setMantineUIFavicon}>Mantine UI favicon</Button>
       <Button onClick={setMantineFavicon}>Mantine favicon</Button>
+      <Button onClick={setMantineUIFavicon}>Mantine UI favicon</Button>
     </Group>
   );
 }
