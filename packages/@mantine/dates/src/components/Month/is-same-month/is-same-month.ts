ce3e3df4bccb9b4ce2d760a788d541572df369da
abouhatef <itb.baravak@gmail.com>
2024-12-24 17:00:44 +0330

    Removed custom  function as the same functionality is already provided by . Refactored relevant parts of the code to use the  implementation instead.

--- packages/@mantine/dates/src/components/Month/is-same-month/is-same-month.ts
+++
@@ -1,9 +0,0 @@
-import dayjs from "dayjs";
-
-export function isSameMonth(date: Date, comparison: Date) {
-  const dayDate = dayjs(date)
-  const dayComparison = dayjs(comparison)
-  return (
-    dayDate.year() === dayComparison.year() && dayDate.month() === dayComparison.month()
-  );
-}
