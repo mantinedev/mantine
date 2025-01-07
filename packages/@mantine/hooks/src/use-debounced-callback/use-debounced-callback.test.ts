d68d40bdc02ece47826543ea3923aa24e89e6478
Sterling Camden <rusty.camden@gmail.com>
2025-01-06 19:21:08 -1000

    [@mantine/hooks] use-debounced-callback: Add `flush` method to returned callback (#7272)

--- packages/@mantine/hooks/src/use-debounced-callback/use-debounced-callback.test.ts
+++ packages/@mantine/hooks/src/use-debounced-callback/use-debounced-callback.test.ts
@@ -26,4 +26,59 @@ describe('@mantine/hooks/use-debounced-callback', () => {
     jest.advanceTimersByTime(100);
     expect(callback).toHaveBeenCalledWith(3);
   });
+
+  it('can be flushed immediately', () => {
+    const callback = jest.fn();
+    const { result } = renderHook(() => useDebouncedCallback(callback, 100));
+    result.current(1);
+    result.current(2);
+    result.current(3);
+    result.current.flush();
+    expect(callback).toHaveBeenCalledWith(3);
+  });
+
+  it('can flush on unmount', () => {
+    const callback = jest.fn();
+    const { result, unmount } = renderHook(() =>
+      useDebouncedCallback(callback, { delay: 100, flushOnUnmount: true })
+    );
+    result.current(1);
+    result.current(2);
+    result.current(3);
+    unmount();
+    expect(callback).toHaveBeenCalledWith(3);
+  });
+
+  it('does not call after unmount if timer lapsed', () => {
+    const callback = jest.fn();
+    const { result, unmount } = renderHook(() =>
+      useDebouncedCallback(callback, { delay: 100, flushOnUnmount: false })
+    );
+    result.current(1);
+    unmount();
+    jest.advanceTimersByTime(100);
+    expect(callback).not.toHaveBeenCalled();
+  });
+
+  it('does not call on unmount if never called', () => {
+    const callback = jest.fn();
+    const { unmount } = renderHook(() =>
+      useDebouncedCallback(callback, { delay: 100, flushOnUnmount: true })
+    );
+    unmount();
+    expect(callback).not.toHaveBeenCalled();
+  });
+
+  it('does not call on unmount if already called and not called since', () => {
+    const callback = jest.fn();
+    const { result, unmount } = renderHook(() =>
+      useDebouncedCallback(callback, { delay: 100, flushOnUnmount: true })
+    );
+    result.current(1);
+    jest.advanceTimersByTime(100);
+    expect(callback).toHaveBeenCalled();
+    callback.mockClear();
+    unmount();
+    expect(callback).not.toHaveBeenCalled();
+  });
 });
