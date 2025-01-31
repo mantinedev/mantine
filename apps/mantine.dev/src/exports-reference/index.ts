0164aef2b17f4a728b19288e5069466326e78e48
Vitaly Rtishchev <rtivital@gmail.com>
2025-01-31 13:46:51 +0400

    [mantine.dev] Remove exports reference files

--- apps/mantine.dev/src/exports-reference/index.ts
+++
@@ -1,4 +0,0 @@
-import { HOOKS_EXPORTS_REFERENCE } from './hooks-exports-reference';
-import type { ExportReferenceGroup } from './types';
-
-export const EXPORTS_REFERENCE: ExportReferenceGroup[] = [HOOKS_EXPORTS_REFERENCE];
