66b8b4ce6e88abebededbefd5f9ab393f6795ef3
Vitaly Rtishchev <rtivital@gmail.com>
2025-01-31 16:04:47 +0400

    [help.mantine.dev] Update styles of code blocks to match new design

--- apps/help.mantine.dev/src/components/MdxElements/MdxNpmScript/MdxNpmScript.tsx
+++ apps/help.mantine.dev/src/components/MdxElements/MdxNpmScript/MdxNpmScript.tsx
@@ -1,4 +1,5 @@
-import { CodeHighlightTabs } from '@mantine/code-highlight';
+import { CodeHighlight } from '@mantine/code-highlight';
+import { Group, Tabs } from '@mantine/core';
 import { useLocalStorage } from '@mantine/hooks';
 import { NpmIcon, YarnIcon } from '@mantinex/dev-icons';
 import classes from './MdxNpmScript.module.css';
@@ -14,29 +15,39 @@ export function MdxNpmScript({ yarnScript, npmScript }: MdxNpmScriptProps) {
   // https://github.com/mantinedev/mantine/pulls?q=is%3Apr+pnpm
 
   const [tab, setTab] = useLocalStorage({
-    key: 'script-tab',
-    defaultValue: 0,
+    key: 'script-tab-value',
+    defaultValue: 'yarn',
   });
 
   return (
-    <CodeHighlightTabs
-      classNames={{ root: classes.root }}
-      activeTab={tab}
-      onTabChange={setTab}
-      code={[
-        {
-          fileName: 'yarn',
-          code: yarnScript,
-          language: 'bash',
-          icon: <YarnIcon className={classes.icon} size={16} />,
-        },
-        {
-          fileName: 'npm',
-          code: npmScript,
-          language: 'bash',
-          icon: <NpmIcon className={classes.icon} size={16} />,
-        },
-      ]}
-    />
+    <Tabs value={tab} onChange={(val) => setTab(val!)} variant="pills" classNames={classes}>
+      <Tabs.List>
+        <Tabs.Tab value="yarn">
+          <Group gap={5}>
+            <YarnIcon className={classes.icon} size={16} />
+            <span>yarn</span>
+          </Group>
+        </Tabs.Tab>
+        <Tabs.Tab value="npm">
+          <Group gap={5}>
+            <NpmIcon className={classes.icon} size={16} />
+            <span>npm</span>
+          </Group>
+        </Tabs.Tab>
+      </Tabs.List>
+
+      <Tabs.Panel value="yarn">
+        <CodeHighlight
+          className={classes.code}
+          code={yarnScript}
+          language="bash"
+          data-without-radius
+        />
+      </Tabs.Panel>
+
+      <Tabs.Panel value="npm">
+        <CodeHighlight className={classes.code} code={npmScript} language="bash" />
+      </Tabs.Panel>
+    </Tabs>
   );
 }
