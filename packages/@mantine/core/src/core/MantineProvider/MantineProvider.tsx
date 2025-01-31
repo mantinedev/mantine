b24b6cb470e3b31704300186f6a07b8969d27182
Vitaly Rtishchev <rtivital@gmail.com>
2025-01-31 11:13:42 +0400

    [@mantine/core] Remove Next.js warning supression

--- packages/@mantine/core/src/core/MantineProvider/MantineProvider.tsx
+++ packages/@mantine/core/src/core/MantineProvider/MantineProvider.tsx
@@ -7,12 +7,9 @@ import { MantineContext, MantineStylesTransform } from './Mantine.context';
 import { MantineClasses } from './MantineClasses';
 import { CSSVariablesResolver, MantineCssVariables } from './MantineCssVariables';
 import { MantineThemeProvider } from './MantineThemeProvider';
-import { suppressNextjsWarning } from './suppress-nextjs-warning';
 import type { MantineColorScheme, MantineThemeOverride } from './theme.types';
 import { useProviderColorScheme } from './use-mantine-color-scheme';
 import { useRespectReduceMotion } from './use-respect-reduce-motion';
-
-suppressNextjsWarning();
 
 export interface MantineProviderProps {
   /** Theme override object */
