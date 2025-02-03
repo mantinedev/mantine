80de9c704a09ad0865670ab23d0a28a9d862f2de
Vitaly Rtishchev <rtivital@gmail.com>
2025-02-03 14:02:47 +0400

    [mantine.dev] Fix incorrect link on the home page

--- apps/mantine.dev/src/components/HomePage/HomePageCommunity/HomePageCommunity.tsx
+++ apps/mantine.dev/src/components/HomePage/HomePageCommunity/HomePageCommunity.tsx
@@ -72,7 +72,9 @@ export function HomePageCommunity() {
                 community-driven project with more than 500 contributors.
               </HomePageDescription>
 
-              <HomePageLearnMore href="/">View all reviews from the community</HomePageLearnMore>
+              <HomePageLearnMore href="https://github.com/orgs/mantinedev/discussions/categories/kind-words">
+                View all reviews from the community
+              </HomePageLearnMore>
 
               <div className={classes.stats}>
                 <Stat value="28,000+" label="Stars on GitHub" link={meta.gitHubLinks.mantine} />
