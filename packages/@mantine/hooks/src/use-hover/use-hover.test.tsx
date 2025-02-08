12d63ac08184d097fe080cd36dedab1bdd4dc204
Daniel Segal <danielsegal909@gmail.com>
2025-02-09 01:48:11 +0200

    added failing test to validate

--- packages/@mantine/hooks/src/use-hover/use-hover.test.tsx
+++ packages/@mantine/hooks/src/use-hover/use-hover.test.tsx
@@ -1,8 +1,11 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import { useHover } from './use-hover';
+import { useState } from 'react';
 
 const Target: React.FunctionComponent<any> = () => {
   const { ref, hovered } = useHover();
+
+  console.log({hovered});
 
   return (
     <div data-testid="target" ref={ref}>
@@ -11,11 +14,35 @@ const Target: React.FunctionComponent<any> = () => {
   );
 };
 
+const AppearingTarget: React.FunctionComponent<any> = ({showTarget}: {showTarget: boolean}) => {
+  const { ref, hovered } = useHover();
+
+  return showTarget && (
+      <div data-testid="target" ref={ref}>
+        {hovered ? 'true' : 'false'}
+      </div>
+    );
+};
+
 describe('@mantine/hooks/use-hover', () => {
   it('changes `hovered` on mouseenter/mouseleave correctly', () => {
     render(<Target />);
     const target = screen.getByTestId('target');
 
+    expect(target).toHaveTextContent('false');
+
+    fireEvent.mouseEnter(target);
+    expect(target).toHaveTextContent('true');
+
+    fireEvent.mouseLeave(target);
+    expect(target).toHaveTextContent('false');
+  });
+
+  it('changes `hovered` when element was unmounted at first', async () => {
+    const {rerender} = render(<AppearingTarget showDiv={false} />);
+    rerender(<AppearingTarget showDiv />);
+
+    const target = screen.getByTestId('target');
     expect(target).toHaveTextContent('false');
 
     fireEvent.mouseEnter(target);
