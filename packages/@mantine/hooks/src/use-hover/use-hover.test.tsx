aa97e72c902ecb5b5a2fca992ecd15253f56762a
Daniel Segal <danielsegal909@gmail.com>
2025-02-15 15:53:20 +0200

    Added a failing test (TDD)

--- packages/@mantine/hooks/src/use-hover/use-hover.test.tsx
+++ packages/@mantine/hooks/src/use-hover/use-hover.test.tsx
@@ -11,11 +11,37 @@ const Target: React.FunctionComponent<any> = () => {
   );
 };
 
+const AppearingTarget: React.FunctionComponent<any> = ({ showTarget }: { showTarget: boolean }) => {
+  const { ref, hovered } = useHover();
+
+  return (
+    showTarget && (
+      <div data-testid="target" ref={ref}>
+        {hovered ? 'true' : 'false'}
+      </div>
+    )
+  );
+};
+
 describe('@mantine/hooks/use-hover', () => {
   it('changes `hovered` on mouseenter/mouseleave correctly', () => {
     render(<Target />);
     const target = screen.getByTestId('target');
 
+    expect(target).toHaveTextContent('false');
+
+    fireEvent.mouseEnter(target);
+    expect(target).toHaveTextContent('true');
+
+    fireEvent.mouseLeave(target);
+    expect(target).toHaveTextContent('false');
+  });
+
+  it('changes `hovered` when element was not mounted at first', async () => {
+    const { rerender } = render(<AppearingTarget showTarget={false} />);
+    rerender(<AppearingTarget showTarget />);
+
+    const target = screen.getByTestId('target');
     expect(target).toHaveTextContent('false');
 
     fireEvent.mouseEnter(target);
