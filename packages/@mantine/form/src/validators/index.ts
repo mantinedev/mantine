176831a661e96774de36aec6d8fd86dc9e42fea2
Yaroslav Salo <yaroslav.salo>
2025-02-28 10:56:54 -0800

    - fix formatting

--- packages/@mantine/form/src/validators/index.ts
+++ packages/@mantine/form/src/validators/index.ts
@@ -5,4 +5,4 @@ export { hasLength } from './has-length/has-length';
 export { isInRange } from './is-in-range/is-in-range';
 export { matchesField } from './matches-field/matches-field';
 export { isNotEmptyHTML } from './is-not-empty-html/is-not-empty-html';
-export { isJSONString} from './is-json-string/is-json-string';
+export { isJSONString } from './is-json-string/is-json-string';
