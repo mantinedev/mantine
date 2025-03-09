6bb6a00e2615817d538ad188feb8d8c436dbdfe1
Vitaly Rtishchev <rtivital@gmail.com>
2025-03-09 14:39:34 +0400

    [@mantine/core] ColorInput: Update the default eye dropper icon

--- packages/@mantine/core/src/components/ColorInput/EyeDropperIcon.tsx
+++ packages/@mantine/core/src/components/ColorInput/EyeDropperIcon.tsx
@@ -16,8 +16,12 @@ export function EyeDropperIcon({ style, ...others }: React.ComponentPropsWithout
       {...others}
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M11 7l6 6" />
-      <path d="M4 16l11.7 -11.7a1 1 0 0 1 1.4 0l2.6 2.6a1 1 0 0 1 0 1.4l-11.7 11.7h-4v-4z" />
+      <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+      <path d="M12 3l0 4" />
+      <path d="M12 21l0 -3" />
+      <path d="M3 12l4 0" />
+      <path d="M21 12l-3 0" />
+      <path d="M12 12l0 .01" />
     </svg>
   );
 }
