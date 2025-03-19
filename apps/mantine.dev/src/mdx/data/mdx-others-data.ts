3d250e3480c7dabe21d80b952b5ca3698c2ccc8a
Vitaly Rtishchev <rtivital@gmail.com>
2025-03-19 16:49:59 +0400

    [mantine.dev] Update code highlight package description

--- apps/mantine.dev/src/mdx/data/mdx-others-data.ts
+++ apps/mantine.dev/src/mdx/data/mdx-others-data.ts
@@ -79,7 +79,7 @@ export const MDX_OTHERS_DATA: Record<string, Frontmatter> = {
     slug: '/x/code-highlight',
     props: ['CodeHighlight', 'CodeHighlightTabs', 'InlineCodeHighlight'],
     styles: ['CodeHighlight', 'CodeHighlightTabs', 'InlineCodeHighlight'],
-    description: 'Highlight code with highlight.js',
+    description: 'Highlight code with shiki or highlight.js',
     source: '@mantine/code-highlight/src',
     license: 'MIT',
     docs: 'x/code-highlight.mdx',
