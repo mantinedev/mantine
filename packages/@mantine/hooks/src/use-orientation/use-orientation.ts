7fc155642ba869ab7d656c41b7f02b6dac1d5938
Vitaly Rtishchev <rtivital@gmail.com>
2025-04-08 17:18:31 +0400

    [@mantine/hooks] use-orientation: Add option to customize initial values (#7657)

--- packages/@mantine/hooks/src/use-orientation/use-orientation.ts
+++ packages/@mantine/hooks/src/use-orientation/use-orientation.ts
@@ -1,8 +1,63 @@
 import { useState } from 'react';
 import { useIsomorphicEffect } from '../use-isomorphic-effect/use-isomorphic-effect';
 
-export function useOrientation() {
-  const [orientation, setOrientation] = useState({ angle: 0, type: 'landscape-primary' });
+interface UseOrientationOptions {
+  /** Default angle value, used until the real can be retrieved
+   * (during server side rendering and before js executes on the page)
+   * If not provided, the default value is `0`
+   * */
+  defaultAngle?: number;
+
+  /** Default angle value, used until the real can be retrieved
+   * (during server side rendering and before js executes on the page)
+   * If not provided, the default value is `'landscape-primary'`
+   * */
+  defaultType?: OrientationType;
+
+  /** If true, the initial value will be resolved in useEffect (ssr safe)
+   *  If false, the initial value will be resolved in useLayoutEffect (ssr unsafe)
+   *  True by default.
+   */
+  getInitialValueInEffect?: boolean;
+}
+
+interface UseOrientationReturnType {
+  angle: number;
+  type: OrientationType;
+}
+
+function getInitialValue(
+  initialValue: UseOrientationReturnType,
+  getInitialValueInEffect: boolean
+): UseOrientationReturnType {
+  if (getInitialValueInEffect) {
+    return initialValue;
+  }
+
+  if (typeof window !== 'undefined' && 'screen' in window) {
+    return {
+      angle: window.screen.orientation?.angle ?? initialValue.angle,
+      type: window.screen.orientation?.type ?? initialValue.type,
+    };
+  }
+
+  return initialValue;
+}
+
+export function useOrientation({
+  defaultAngle = 0,
+  defaultType = 'landscape-primary',
+  getInitialValueInEffect = true,
+}: UseOrientationOptions = {}): UseOrientationReturnType {
+  const [orientation, setOrientation] = useState<UseOrientationReturnType>(
+    getInitialValue(
+      {
+        angle: defaultAngle,
+        type: defaultType,
+      },
+      getInitialValueInEffect
+    )
+  );
 
   const handleOrientationChange = (event: Event) => {
     const target = event.currentTarget as ScreenOrientation;
