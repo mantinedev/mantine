f1a9cf24359f9e997a777e6782ef15d8d260aae4
abouhatef <itb.baravak@gmail.com>
2025-04-19 20:50:48 +0330

    bugfix fully year

--- packages/@mantine/dates/src/components/Month/is-same-month/is-same-month.ts
+++ packages/@mantine/dates/src/components/Month/is-same-month/is-same-month.ts
@@ -1,5 +1,5 @@
 import dayjs from 'dayjs';
 
 export function isSameMonth(date: Date, comparison: Date) {
-  return dayjs(date).format('YYY-MM') === dayjs(comparison).format('YYY-MM');
+  return dayjs(date).format('YYYY-MM') === dayjs(comparison).format('YYYY-MM');
 }
