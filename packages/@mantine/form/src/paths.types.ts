e41bdc3afbce84b1f7748284dda4e2e5a0f651b0
Vitaly Rtishchev <rtivital@gmail.com>
2025-04-24 12:26:58 +0400

    [@mantine/form] Fix incorrect circular types resolving (#7668)

--- packages/@mantine/form/src/paths.types.ts
+++ packages/@mantine/form/src/paths.types.ts
@@ -1,44 +1,51 @@
 // Types from https://github.com/g-makarov/dot-path-value
 
 type Primitive = null | undefined | string | number | boolean | symbol | bigint;
-
 type ArrayKey = number;
 
 type IsTuple<T extends readonly any[]> = number extends T['length'] ? false : true;
-
 type TupleKeys<T extends readonly any[]> = Exclude<keyof T, keyof any[]>;
 
-type PathConcat<TKey extends string | number, TValue> = TValue extends Primitive
+type Prev = [never, 0, 1, 2, 3, 4, 5];
+type Depth = 5;
+
+type PathConcat<TKey extends string | number, TValue, D extends number> = TValue extends Primitive
   ? `${TKey}`
-  : `${TKey}` | `${TKey}.${Path<TValue>}`;
+  : `${TKey}` | `${TKey}.${Path<TValue, Prev[D]>}`;
 
-type Path<T> = T extends readonly (infer V)[]
-  ? IsTuple<T> extends true
-    ? {
-        [K in TupleKeys<T>]-?: PathConcat<K & string, T[K]>;
-      }[TupleKeys<T>]
-    : PathConcat<ArrayKey, V>
-  : {
-      [K in keyof T]-?: PathConcat<K & string, T[K]>;
-    }[keyof T];
-
-type ArrayPathConcat<TKey extends string | number, TValue> = TValue extends Primitive
+export type Path<T, D extends number = Depth> = [D] extends [never]
   ? never
-  : TValue extends readonly (infer U)[]
-    ? U extends Primitive
-      ? never
-      : `${TKey}` | `${TKey}.${ArrayPath<TValue>}`
-    : `${TKey}.${ArrayPath<TValue>}`;
+  : T extends readonly (infer V)[]
+    ? IsTuple<T> extends true
+      ? {
+          [K in TupleKeys<T>]-?: PathConcat<K & string, T[K], D>;
+        }[TupleKeys<T>]
+      : PathConcat<ArrayKey, V, D>
+    : {
+        [K in keyof T]-?: PathConcat<K & string, T[K], D>;
+      }[keyof T];
 
-type ArrayPath<T> = T extends readonly (infer V)[]
-  ? IsTuple<T> extends true
-    ? {
-        [K in TupleKeys<T>]-?: ArrayPathConcat<K & string, T[K]>;
-      }[TupleKeys<T>]
-    : ArrayPathConcat<ArrayKey, V>
-  : {
-      [K in keyof T]-?: ArrayPathConcat<K & string, T[K]>;
-    }[keyof T];
+type ArrayPathConcat<TKey extends string | number, TValue, D extends number> = [D] extends [never]
+  ? never
+  : TValue extends Primitive
+    ? never
+    : TValue extends readonly (infer U)[]
+      ? U extends Primitive
+        ? never
+        : `${TKey}` | `${TKey}.${ArrayPath<TValue, Prev[D]>}`
+      : `${TKey}.${ArrayPath<TValue, Prev[D]>}`;
+
+export type ArrayPath<T, D extends number = Depth> = [D] extends [never]
+  ? never
+  : T extends readonly (infer V)[]
+    ? IsTuple<T> extends true
+      ? {
+          [K in TupleKeys<T>]-?: ArrayPathConcat<K & string, T[K], D>;
+        }[TupleKeys<T>]
+      : ArrayPathConcat<ArrayKey, V, D>
+    : {
+        [K in keyof T]-?: ArrayPathConcat<K & string, T[K], D>;
+      }[keyof T];
 
 export type PathValue<T, TPath extends string> = T extends any
   ? TPath extends `${infer K}.${infer R}`
