dc54c6be3fa0cd850e2da18803d67b0b1d28f607
Vitaly Rtishchev <rtivital@gmail.com>
2025-05-08 15:11:56 +0400

    [@mantine/hooks] use-counter: Add callbacks memoization

--- packages/@mantine/hooks/src/use-counter/use-counter.ts
+++ packages/@mantine/hooks/src/use-counter/use-counter.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { clamp } from '../utils';
 
 const DEFAULT_OPTIONS = {
@@ -10,10 +10,22 @@ export function useCounter(initialValue = 0, options?: Partial<{ min: number; ma
   const { min, max } = { ...DEFAULT_OPTIONS, ...options };
   const [count, setCount] = useState<number>(clamp(initialValue, min, max));
 
-  const increment = () => setCount((current) => clamp(current + 1, min, max));
-  const decrement = () => setCount((current) => clamp(current - 1, min, max));
-  const set = (value: number) => setCount(clamp(value, min, max));
-  const reset = () => setCount(clamp(initialValue, min, max));
+  const increment = useCallback(
+    () => setCount((current) => clamp(current + 1, min, max)),
+    [min, max]
+  );
+
+  const decrement = useCallback(
+    () => setCount((current) => clamp(current - 1, min, max)),
+    [min, max]
+  );
+
+  const set = useCallback((value: number) => setCount(clamp(value, min, max)), [min, max]);
+
+  const reset = useCallback(
+    () => setCount(clamp(initialValue, min, max)),
+    [initialValue, min, max]
+  );
 
   return [count, { increment, decrement, set, reset }] as const;
 }
