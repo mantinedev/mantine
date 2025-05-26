e2b3941fdf3a895b1f121599f3e18e8f898d7c86
ゆき <YukiKoNeko2002@gmail.com>
2025-05-26 22:25:19 +0900

    Update to-date-string.ts

--- packages/@mantine/dates/src/utils/to-date-string/to-date-string.ts
+++ packages/@mantine/dates/src/utils/to-date-string/to-date-string.ts
@@ -1,14 +1,20 @@
 import dayjs, { Dayjs } from 'dayjs';
-import { DateStringValue } from '../../types';
+import { DateStringValue, DateTimeStringValue } from '../../types';
 
+export function toDateString(value: string | number | Date | Dayjs): DateStringValue;
+export function toDateString(value: undefined): undefined;
+export function toDateString(value: null): null;
 export function toDateString(
   value: string | number | Date | Dayjs | undefined | null
 ): DateStringValue | undefined | null {
   return value == null || value === '' ? value : dayjs(value).format('YYYY-MM-DD');
 }
 
+export function toDateTimeString(value: string | number | Date | Dayjs): DateTimeStringValue;
+export function toDateTimeString(value: undefined): undefined;
+export function toDateTimeString(value: null): null;
 export function toDateTimeString(
   value: string | number | Date | Dayjs | undefined | null
-): DateStringValue | undefined | null {
+): DateTimeStringValue | undefined | null {
   return value == null || value === '' ? value : dayjs(value).format('YYYY-MM-DD HH:mm:ss');
 }
