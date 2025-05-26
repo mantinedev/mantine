45684883129644f70e847c8bf49c2f595e90cbe3
ゆき <YukiKoNeko2002@gmail.com>
2025-05-26 23:31:25 +0900

    Update to-date-string.ts

--- packages/@mantine/dates/src/utils/to-date-string/to-date-string.ts
+++ packages/@mantine/dates/src/utils/to-date-string/to-date-string.ts
@@ -1,12 +1,17 @@
 import dayjs, { Dayjs } from 'dayjs';
 import { DateStringValue, DateTimeStringValue } from '../../types';
 
+// Helper type to determine the return type based on T and the target main type
 type ExactOptionalReturn<T, MainType> = T extends undefined ? undefined : T extends null ? null : MainType;
 
-export function toDateString<T extends string | number | Date | Dayjs | undefined | null>(value: T): ExactOptionalReturn<T, DateStringValue> {
+export function toDateString<T extends string | number | Date | Dayjs | undefined | null>(
+  value: T
+): ExactOptionalReturn<T, DateStringValue> {
   return (value == null || value === '' ? value : dayjs(value).format('YYYY-MM-DD')) as ExactOptionalReturn<T, DateStringValue>;
 }
 
-export function toDateTimeString<T extends string | number | Date | Dayjs | undefined | null>(value: T): ExactOptionalReturn<T, DateTimeStringValue> {
+export function toDateTimeString<T extends string | number | Date | Dayjs | undefined | null>(
+  value: T
+): ExactOptionalReturn<T, DateTimeStringValue> {
   return (value == null || value === '' ? value : dayjs(value).format('YYYY-MM-DD HH:mm:ss')) as ExactOptionalReturn<T, DateTimeStringValue>;
 }
