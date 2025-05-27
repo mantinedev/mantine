df2c90a42f4112250b055672519be904308b5285
Vitaly Rtishchev <rtivital@gmail.com>
2025-05-27 09:27:34 +0400

    [@mantine/form] Fix `form.reorderItem` reordering only the first item (#7892)

--- packages/@mantine/form/src/lists/reorder-errors.ts
+++ packages/@mantine/form/src/lists/reorder-errors.ts
@@ -5,25 +5,34 @@ export function reorderErrors<T>(path: unknown, { from, to }: ReorderPayload, er
   const newKeyStart = `${path}.${to}`;
 
   const clone: any = { ...errors };
-  Object.keys(errors as any).every((key) => {
+  const processedKeys = new Set<string>();
+
+  Object.keys(errors as any).forEach((key) => {
+    if (processedKeys.has(key)) {
+      return;
+    }
+
     let oldKey;
     let newKey;
+
     if (key.startsWith(oldKeyStart)) {
       oldKey = key;
       newKey = key.replace(oldKeyStart, newKeyStart);
-    }
-    if (key.startsWith(newKeyStart)) {
+    } else if (key.startsWith(newKeyStart)) {
       oldKey = key.replace(newKeyStart, oldKeyStart);
       newKey = key;
     }
+
     if (oldKey && newKey) {
       const value1 = clone[oldKey];
       const value2 = clone[newKey];
+
       value2 === undefined ? delete clone[oldKey] : (clone[oldKey] = value2);
       value1 === undefined ? delete clone[newKey] : (clone[newKey] = value1);
-      return false;
+
+      processedKeys.add(oldKey);
+      processedKeys.add(newKey);
     }
-    return true;
   });
 
   return clone;
