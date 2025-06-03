05ef1cbccf8a2498eab7166e19cad9cc0116b0c5
Vitaly Rtishchev <rtivital@gmail.com>
2025-06-03 09:31:24 +0400

    [@mantine/core] PasswordInput: Update visibility toggle icon

--- packages/@mantine/core/src/components/PasswordInput/PasswordInput.story.tsx
+++ packages/@mantine/core/src/components/PasswordInput/PasswordInput.story.tsx
@@ -4,21 +4,8 @@ export default { title: 'PasswordInput' };
 
 export function Usage() {
   return (
-    <div data-disabled>
-      <PasswordInput
-        placeholder="Your password"
-        label="Your password"
-        error="test error"
-        withErrorStyles={false}
-      />
-    </div>
-  );
-}
-
-export function Unstyled() {
-  return (
-    <div style={{ padding: 40, maxWidth: 340 }}>
-      <PasswordInput placeholder="Your password" description="Hello" label="There" unstyled />
+    <div style={{ padding: 40, maxWidth: 440 }}>
+      <PasswordInput size="xl" placeholder="Your password" label="Your password" />
     </div>
   );
 }
