338931e6904ce8dee619d69e033bee6d9dfea5c3
Vitaly Rtishchev <rtivital@gmail.com>
2025-06-16 13:57:15 +0400

    [@mantine/dates] DatePicker: Fix incorrect value type for `type="range" (#7937)`

--- packages/@mantine/dates/src/types/DatePickerValue.ts
+++ packages/@mantine/dates/src/types/DatePickerValue.ts
@@ -1,7 +1,7 @@
 import { DateStringValue } from './GeneralTypes';
 
 export type DateValue = DateStringValue | Date | null;
-export type DatesRangeValue<ValueType = DateValue> = [ValueType, ValueType];
+export type DatesRangeValue<ValueType = DateValue> = [ValueType | null, ValueType | null];
 export type DatePickerType = 'default' | 'multiple' | 'range';
 
 export type DatePickerValue<
