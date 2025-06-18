923efd1e9d7097b81e739d061fbcee4fc6532874
Vitaly Rtishchev <rtivital@gmail.com>
2025-06-18 12:53:18 +0400

    [refactor] Fix incorrect attributes errors for visibility toggle icon in PasswordInpu

--- packages/@mantine/core/src/components/PasswordInput/PasswordInput.story.tsx
+++ packages/@mantine/core/src/components/PasswordInput/PasswordInput.story.tsx
@@ -5,7 +5,7 @@ export default { title: 'PasswordInput' };
 export function Usage() {
   return (
     <div style={{ padding: 40, maxWidth: 440 }}>
-      <PasswordInput size="xl" placeholder="Your password" label="Your password" />
+      <PasswordInput size="md" placeholder="Your password" label="Your password" />
     </div>
   );
 }
--- packages/@mantine/core/src/components/PasswordInput/PasswordToggleIcon.tsx
+++ packages/@mantine/core/src/components/PasswordInput/PasswordToggleIcon.tsx
@@ -17,17 +17,17 @@ export function PasswordToggleIcon({ reveal }: PasswordToggleIconProps) {
           <path
             fill="none"
             stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="16"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="16"
             d="M48 40l160 176M154.91 157.6a40 40 0 01-53.82-59.2M135.53 88.71a40 40 0 0132.3 35.53"
           />
           <path
             fill="none"
             stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="16"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="16"
             d="M208.61 169.1C230.41 149.58 240 128 240 128s-32-72-112-72a126 126 0 00-20.68 1.68M74 68.6C33.23 89.24 16 128 16 128s32 72 112 72a118.05 118.05 0 0054-12.6"
           />
         </>
@@ -37,9 +37,9 @@ export function PasswordToggleIcon({ reveal }: PasswordToggleIconProps) {
           <path
             fill="none"
             stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="16"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="16"
             d="M128 56c-80 0-112 72-112 72s32 72 112 72 112-72 112-72-32-72-112-72z"
           />
           <circle
@@ -48,9 +48,9 @@ export function PasswordToggleIcon({ reveal }: PasswordToggleIconProps) {
             r="40"
             fill="none"
             stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="16"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="16"
           />
         </>
       )}
