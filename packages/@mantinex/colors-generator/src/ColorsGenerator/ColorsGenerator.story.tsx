1dd66418844990102b497446cd4c81746dbdb175
Vitaly Rtishchev <rtivital@gmail.com>
2025-07-10 16:48:36 +0400

    [@mantinex/colors-generator] Add some colors

--- packages/@mantinex/colors-generator/src/ColorsGenerator/ColorsGenerator.story.tsx
+++ packages/@mantinex/colors-generator/src/ColorsGenerator/ColorsGenerator.story.tsx
@@ -1,11 +1,107 @@
-import { ColorsGenerator } from './ColorsGenerator';
+import { ColorsList } from './ColorsList/ColorsList';
 
 export default { title: 'ColorsGenerator' };
 
+// - [ ] Blue – `#052F5F`, `255C99`, `05299E`
+// - [ ] Red – `#E71D36`, `#E5231F`, `#BF211E`
+// - [ ] Pink – `#D73767`
+// - [ ] Grape
+// ---
+// - [ ] Violet – `#502274`, `#54457F`, `2A1E5C`
+// - [ ] Indigo – `3626A7`
+// - [ ] Cyan – `#06A77D`
+// - [ ] Teal
+// - [ ] Green – `#00AF54`
+// - [ ] Lime
+// - [ ] Yellow – `#F0C808`
+// - [ ] Orange – `#FF9F1C`
+// - [ ] ---
+// - [ ] Dark – `#2F242C`, `#2A1F2D`
+
+// '#0',
+// '#1',
+// '#2',
+// '#3',
+// '#4',
+// '#5',
+// '#6',
+// '#7',
+// '#8',
+// '#9',
+
+// light color group:
+// dark color group: blue, red, pink, grape
+
+const colors = {
+  blue: [
+    '#EBF3FF',
+    '#E0ECFF',
+    '#BDD5FF',
+    '#94BDFF',
+    '#5294FF',
+    '#1A71FF',
+    '#005CF0',
+    '#0054DB',
+    '#004AC2',
+    '#003FA3',
+  ],
+
+  indigo: [
+    '#F0EDFC',
+    '#DFDDF8',
+    '#CCC8F4',
+    '#BAB3EF',
+    '#A49BE9',
+    '#8A7EE2',
+    '#6455D8',
+    '#5544D5',
+    '#412ECC',
+    '#3728AF',
+  ],
+
+  red: [
+    '#FDF1F3',
+    '#F9DCDE',
+    '#F9C2C9',
+    '#F58F9B',
+    '#EE5869',
+    '#E92F45',
+    '#E71D36',
+    '#D9172D',
+    '#C6152A',
+    '#AF1225',
+  ],
+
+  pink: [
+    '#FCEDF1',
+    '#FADCE5',
+    '#F4C3D2',
+    '#E891AB',
+    '#DE5981',
+    '#DA4471',
+    '#D73767',
+    '#C82858',
+    '#BB2552',
+    '#A62149',
+  ],
+
+  grape: [
+    '#F8EDF8',
+    '#F4DCF4',
+    '#EDC5ED',
+    '#D98FDC',
+    '#C85CCC',
+    '#C149C5',
+    '#BD3DC2',
+    '#AA37AE',
+    '#943097',
+    '#812A84',
+  ],
+};
+
 export function Usage() {
-  return (
-    <div style={{ padding: 40 }}>
-      <ColorsGenerator />
-    </div>
-  );
+  const lists = Object.values(colors).map((colors, index) => (
+    <ColorsList key={index} colors={colors} />
+  ));
+  return <div style={{ padding: 40 }}>{lists}</div>;
 }
--- packages/@mantinex/colors-generator/src/ColorsGenerator/ColorsList/ColorsList.tsx
+++ packages/@mantinex/colors-generator/src/ColorsGenerator/ColorsList/ColorsList.tsx
@@ -1,15 +1,67 @@
-import { ColorSwatch, Group } from '@mantine/core';
+import chroma from 'chroma-js';
+import { ColorSwatch, Group, Text } from '@mantine/core';
 
 export interface ColorsListProps {
   colors: string[];
+  withSample?: boolean;
 }
 
-export function ColorsList({ colors }: ColorsListProps) {
-  const swatches = colors.map((color, index) => (
+export function ColorsList({ colors, withSample }: ColorsListProps) {
+  const swatches = colors.map((color, index) => {
+    const contrast = chroma.contrast(color, '#fff');
+    return (
+      <div key={index}>
+        <ColorSwatch key={index} radius={0} withShadow={false} size={100} color={color} />
+        <Text fz="sm" ff="monospace">
+          {index}: {color}
+        </Text>
+
+        <Text>
+          <Text span inherit fw="bold" c={contrast < 4.5 ? 'red' : 'green'}>
+            {contrast.toFixed(2)}
+          </Text>
+        </Text>
+      </div>
+    );
+  });
+
+  return (
     <div>
-      <ColorSwatch key={index} radius={0} withShadow={false} size={100} color={color} />
-      {index}:{color}
+      <Group gap={1} my="md">
+        {swatches}
+      </Group>
+
+      {withSample && (
+        <Group>
+          <div
+            style={{
+              width: 200,
+              height: 60,
+              textAlign: 'center',
+              fontWeight: 'bold',
+              color: 'white',
+              background: colors[6],
+              lineHeight: '60px',
+            }}
+          >
+            SAMPLE LIGHT
+          </div>
+
+          <div
+            style={{
+              width: 200,
+              height: 60,
+              textAlign: 'center',
+              fontWeight: 'bold',
+              color: 'white',
+              background: colors[8],
+              lineHeight: '60px',
+            }}
+          >
+            SAMPLE DARK
+          </div>
+        </Group>
+      )}
     </div>
-  ));
-  return <Group gap={1}>{swatches}</Group>;
+  );
 }
