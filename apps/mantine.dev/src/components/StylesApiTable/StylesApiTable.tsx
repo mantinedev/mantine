4acb41d76acad6a818ab32d49b45c7eddbfafcae
Vitaly Rtishchev <rtivital@gmail.com>
2025-07-18 14:38:14 +0300

    [mantine.dev] Add more concise styles api tables

--- apps/mantine.dev/src/components/StylesApiTable/StylesApiTable.tsx
+++ apps/mantine.dev/src/components/StylesApiTable/StylesApiTable.tsx
@@ -1,4 +1,5 @@
-import { Title } from '@mantine/core';
+import { useState } from 'react';
+import { Tabs, Title } from '@mantine/core';
 import * as stylesData from '@docs/styles-api';
 import type { Modifier } from '@docs/styles-api';
 import { getComponentName } from '@/components/PropsTable';
@@ -22,39 +23,51 @@ export interface StylesApiTableProps {
 }
 
 export function StylesApiTable({ component, componentPrefix }: StylesApiTableProps) {
+  const [value, setValue] = useState<string | null>('selectors');
   const data = STYLES_API_DATA[`${component}StylesApi`];
   if (!data) {
     return <TableError errorOf="Styles API" />;
   }
 
   const componentName = getComponentName({ component, componentPrefix });
+  const hasVariables = Object.keys(data.vars).length > 0;
+  const hasModifiers = Array.isArray(data.modifiers) && data.modifiers.length > 0;
 
   return (
     <>
-      <div className={classes.section}>
-        <Title order={2} className={classes.title}>
-          {componentName} selectors
-        </Title>
-        <SelectorsTable component={component} data={data} />
-      </div>
+      <Title order={3} className={classes.title}>
+        {componentName} Styles API
+      </Title>
 
-      {Object.keys(data.vars).length > 0 && (
-        <div className={classes.section}>
-          <Title order={2} className={classes.title}>
-            {componentName} CSS variables
-          </Title>
-          <VariablesTable data={data} />
-        </div>
-      )}
+      <Tabs
+        value={value}
+        onChange={setValue}
+        classNames={{ panel: classes.panel, tab: classes.tab }}
+        radius="md"
+        variant="pills"
+      >
+        <Tabs.List data-with-radius>
+          <Tabs.Tab value="selectors">Selectors</Tabs.Tab>
+          {hasVariables && <Tabs.Tab value="variables">CSS variables</Tabs.Tab>}
+          {hasModifiers && <Tabs.Tab value="modifiers">Data attributes</Tabs.Tab>}
+        </Tabs.List>
 
-      {Array.isArray(data.modifiers) && data.modifiers.length > 0 && (
-        <div className={classes.section}>
-          <Title order={2} className={classes.title}>
-            {componentName} data attributes
-          </Title>
-          <ModifiersTable data={data} />
-        </div>
-      )}
+        <Tabs.Panel value="selectors">
+          <SelectorsTable component={component} data={data} />
+        </Tabs.Panel>
+
+        {hasVariables && (
+          <Tabs.Panel value="variables">
+            <VariablesTable data={data} />
+          </Tabs.Panel>
+        )}
+
+        {hasModifiers && (
+          <Tabs.Panel value="modifiers">
+            <ModifiersTable data={data} />
+          </Tabs.Panel>
+        )}
+      </Tabs>
     </>
   );
 }
