c3f9a919ce3d930871e75ea6540fed70de83ebbd
Vitaly Rtishchev <rtivital@gmail.com>
2025-07-21 13:26:56 +0300

    [mantine.dev] Add AngleSlider usage with uncontrolled forms

--- packages/@mantine/core/src/components/AngleSlider/AngleSlider.story.tsx
+++ packages/@mantine/core/src/components/AngleSlider/AngleSlider.story.tsx
@@ -10,46 +10,19 @@ export function Usage() {
   );
 }
 
-export function WithMarks() {
+export function WithFormData() {
   return (
     <div style={{ padding: 40 }}>
-      <AngleSlider
-        size={100}
-        formatLabel={(value) => `${value}°`}
-        marks={[
-          { value: 0 },
-          { value: 45 },
-          { value: 90 },
-          { value: 135 },
-          { value: 180 },
-          { value: 225 },
-          { value: 270 },
-          { value: 315 },
-        ]}
-        restrictToMarks
-      />
-    </div>
-  );
-}
-
-export function RestrictToMarks() {
-  return (
-    <div style={{ padding: 40 }}>
-      <AngleSlider
-        size={100}
-        formatLabel={(value) => `${value}°`}
-        marks={[
-          { value: 0, label: '0°' },
-          { value: 45, label: '45°' },
-          { value: 90, label: '90°' },
-          { value: 135, label: '135°' },
-          { value: 180, label: '180°' },
-          { value: 225, label: '225°' },
-          { value: 270, label: '270°' },
-          { value: 315, label: '315°' },
-        ]}
-        restrictToMarks
-      />
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const formData = new FormData(event.currentTarget);
+          console.log('Checkbox group value:', formData.get('angle'));
+        }}
+      >
+        <AngleSlider name="angle" defaultValue={120} />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
