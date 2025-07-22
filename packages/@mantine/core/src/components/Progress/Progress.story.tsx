96c536a0d4137a3a851c683d1783f3fba1f89fc2
Vitaly Rtishchev <rtivital@gmail.com>
2025-07-22 16:50:15 +0300

    [@mantine/core] Progress: Add vertical orientation support

--- packages/@mantine/core/src/components/Progress/Progress.story.tsx
+++ packages/@mantine/core/src/components/Progress/Progress.story.tsx
@@ -5,7 +5,15 @@ export default { title: 'Progress' };
 export function Usage() {
   return (
     <div style={{ padding: 40 }}>
-      <Progress value={56} />
+      <Progress value={20} />
+    </div>
+  );
+}
+
+export function Vertical() {
+  return (
+    <div style={{ padding: 40 }}>
+      <Progress value={20} orientation="vertical" h={400} animated />
     </div>
   );
 }
@@ -21,7 +29,7 @@ export function Striped() {
 export function Unstyled() {
   return (
     <div style={{ padding: 40 }}>
-      <Progress value={56} size="xl" unstyled />
+      <Progress value={56} size="xl" unstyled h="400" />
     </div>
   );
 }
@@ -29,15 +37,15 @@ export function Unstyled() {
 export function Compound() {
   return (
     <div style={{ padding: 40 }}>
-      <Progress.Root size="xl" autoContrast>
-        <Progress.Section value={56} color="lime.4">
+      <Progress.Root size="xl" autoContrast orientation="vertical" h={200}>
+        <Progress.Section value={40} color="lime.4">
           <Progress.Label>Documents</Progress.Label>
         </Progress.Section>
         <Progress.Section value={20} color="yellow.4">
           <Progress.Label>Apps</Progress.Label>
         </Progress.Section>
-        <Progress.Section value={15} color="cyan.7">
-          <Progress.Label>Others</Progress.Label>
+        <Progress.Section value={20} color="cyan.7">
+          <Progress.Label>Other</Progress.Label>
         </Progress.Section>
       </Progress.Root>
     </div>
