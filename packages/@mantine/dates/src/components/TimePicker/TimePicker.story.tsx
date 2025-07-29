f71a80f323e9f4a3e2be410b077be45a1957e218
Zach Townsend <idealconceptz@gmail.com>
2025-07-29 15:01:20 +0100

    [@mantine/dates] TimePicker: Fix min/max values not being enforced for 12h time format (#8107)
    The time is clamped between min and max when the TimePicker element loses focus.

--- packages/@mantine/dates/src/components/TimePicker/TimePicker.story.tsx
+++ packages/@mantine/dates/src/components/TimePicker/TimePicker.story.tsx
@@ -158,7 +158,23 @@ export function MinMax() {
 
   return (
     <div style={{ padding: 40 }}>
-      <TimePicker min="10:30" max="18:30" format="12h" value={value} onChange={setValue} />
+      <TimePicker
+        min="10:30"
+        max="18:30"
+        format="12h"
+        value={value}
+        onChange={setValue}
+        label="12h format"
+      />
+      <div>{value}</div>
+      <TimePicker
+        min="10:30"
+        max="18:30"
+        format="24h"
+        value={value}
+        onChange={setValue}
+        label="24h format"
+      />
       <div>{value}</div>
     </div>
   );
