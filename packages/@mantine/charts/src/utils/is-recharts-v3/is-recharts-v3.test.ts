ccb896722980854e778289d7752d9d99153dd50b
Daniel Jacobs <danielhunterjacobs@gmail.com>
2025-08-15 15:26:13 -0400

    [@mantine/charts] Mock modules inside sandbox registry

--- packages/@mantine/charts/src/utils/is-recharts-v3/is-recharts-v3.test.ts
+++ packages/@mantine/charts/src/utils/is-recharts-v3/is-recharts-v3.test.ts
@@ -5,24 +5,22 @@ describe('@mantine/charts/utils/is-recharts-v3', () => {
   });
 
   it('returns false when useActiveTooltipLabel is not present (Recharts v2)', async () => {
-    jest.doMock('recharts', () => ({}), { virtual: true });
-
     await jest.isolateModulesAsync(async () => {
+      jest.doMock('recharts', () => ({}), { virtual: true });
       const { isRechartsV3 } = await import('./is-recharts-v3');
       expect(isRechartsV3()).toBe(false);
     });
   });
 
   it('returns true when useActiveTooltipLabel is present (Recharts v3)', async () => {
-    jest.doMock(
-      'recharts',
-      () => ({
-        useActiveTooltipLabel: () => 'mocked',
-      }),
-      { virtual: true }
-    );
-
     await jest.isolateModulesAsync(async () => {
+      jest.doMock(
+        'recharts',
+        () => ({
+          useActiveTooltipLabel: () => 'mocked',
+        }),
+        { virtual: true }
+      );
       const { isRechartsV3 } = await import('./is-recharts-v3');
       expect(isRechartsV3()).toBe(true);
     });
