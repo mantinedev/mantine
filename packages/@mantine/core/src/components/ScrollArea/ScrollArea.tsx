f30edff8e4609f1a7fb9f7dd27ac7c56fc9fc4d8
Leah Grones <45991112+pgrones@users.noreply.github.com>
2025-08-16 15:46:34 +0200

    [@mantine/core] ScrollArea: Fix ScrollArea.Autosize not resizing when width of children changes (#8160)
    Co-authored-by: Vitaly Rtishchev <rtivital@gmail.com>

--- packages/@mantine/core/src/components/ScrollArea/ScrollArea.tsx
+++ packages/@mantine/core/src/components/ScrollArea/ScrollArea.tsx
@@ -330,22 +330,13 @@ export const ScrollAreaAutosize = factory<ScrollAreaAutosizeFactory>((props, ref
   }, [onOverflowChange, overflowing]);
 
   return (
-    <Box
-      {...others}
-      ref={ref}
-      style={[
-        {
-          display: 'flex',
-          overflow: 'hidden',
-        },
-        style,
-      ]}
-    >
+    <Box {...others} ref={ref} style={[{ display: 'flex', overflow: 'hidden' }, style]}>
       <Box
         style={{
           display: 'flex',
           flexDirection: 'column',
           flex: 1,
+          overflow: 'hidden',
           ...(scrollbars === 'y' && { minWidth: 0 }),
           ...(scrollbars === 'x' && { minHeight: 0 }),
           ...(scrollbars === 'xy' && { minWidth: 0, minHeight: 0 }),
@@ -369,6 +360,7 @@ export const ScrollAreaAutosize = factory<ScrollAreaAutosizeFactory>((props, ref
           scrollbars={scrollbars}
           onBottomReached={onBottomReached}
           onTopReached={onTopReached}
+          data-autosize="true"
         >
           {children}
         </ScrollArea>
