3fe5a0dc57f17ec6788d9d0df14d95b22a622522
Jan Vollmer <jan@vllmr.dev>
2025-08-24 13:30:52 +0200

    keep Record<string, any> as default value for useForm
    Signed-off-by: Jan Vollmer <jan@vllmr.dev>

--- packages/@mantine/form/src/use-form.ts
+++ packages/@mantine/form/src/use-form.ts
@@ -28,7 +28,7 @@ import {
 import { shouldValidateOnChange, validateFieldValue, validateValues } from './validate';
 
 export function useForm<
-  Values extends Record<PropertyKey, any> = Record<PropertyKey, any>,
+  Values extends Record<PropertyKey, any> = Record<string, any>,
   TransformValues extends _TransformValues<Values> = (values: Values) => Values,
 >({
   name,
