232afd31ce473062d546747f1f28e9c9b48f1a53
Daniel Jacobs <danielhunterjacobs@gmail.com>
2025-09-08 10:15:34 -0400

    [@mantine/charts] Reset modules per test

--- packages/@mantine/charts/src/utils/is-recharts-v3/is-recharts-v3.test.ts
+++ packages/@mantine/charts/src/utils/is-recharts-v3/is-recharts-v3.test.ts
@@ -1,11 +1,11 @@
 describe('@mantine/charts/utils/is-recharts-v3', () => {
   afterEach(() => {
     jest.clearAllMocks();
-    jest.resetModules();
   });
 
   it('returns false when useActiveTooltipLabel is not present (Recharts v2)', async () => {
     await jest.isolateModulesAsync(async () => {
+      jest.resetModules();
       jest.doMock('recharts', () => ({}), { virtual: true });
       const { isRechartsV3 } = await import('./is-recharts-v3');
       expect(isRechartsV3()).toBe(false);
@@ -14,13 +14,8 @@ describe('@mantine/charts/utils/is-recharts-v3', () => {
 
   it('returns true when useActiveTooltipLabel is present (Recharts v3)', async () => {
     await jest.isolateModulesAsync(async () => {
-      jest.doMock(
-        'recharts',
-        () => ({
-          useActiveTooltipLabel: () => 'mocked',
-        }),
-        { virtual: true }
-      );
+      jest.resetModules();
+      jest.doMock('recharts', () => ({ useActiveTooltipLabel: () => 'mocked' }), { virtual: true });
       const { isRechartsV3 } = await import('./is-recharts-v3');
       expect(isRechartsV3()).toBe(true);
     });
