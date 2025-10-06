cc1a2c112c899b4d3c45b8c910d99b5e93edc6f4
Vitaly Rtishchev <rtivital@gmail.com>
2025-10-06 12:41:27 +0300

    [@mantine/core] OverflowList: Add tests

---
+++ packages/@mantine/core/src/components/OverflowList/OverflowList.test.tsx
@@ -0,0 +1,23 @@
+import { tests } from '@mantine-tests/core';
+import { OverflowList, OverflowListProps, OverflowListStylesNames } from './OverflowList';
+
+const defaultProps: OverflowListProps<any> = {
+  data: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+  renderItem: (item) => <div>{item}</div>,
+  renderOverflow: (items) => <div>+{items.length}</div>,
+};
+
+describe('@mantine/core/OverflowList', () => {
+  tests.itSupportsSystemProps<OverflowListProps<any>, OverflowListStylesNames>({
+    component: OverflowList,
+    props: defaultProps,
+    styleProps: true,
+    extend: true,
+    variant: true,
+    size: true,
+    classes: true,
+    refType: HTMLDivElement,
+    displayName: '@mantine/core/OverflowList',
+    stylesApiSelectors: ['root'],
+  });
+});
