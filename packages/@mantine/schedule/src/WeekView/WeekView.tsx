6caee0982d44bf3a064c93c9d158fbdab49e9fc1
Vitaly Rtishchev <rtivital@gmail.com>
2025-11-14 18:10:38 +0300

    [@mantine/schedule] WeekView: Add basic props for configuration

--- packages/@mantine/schedule/src/WeekView/WeekView.tsx
+++ packages/@mantine/schedule/src/WeekView/WeekView.tsx
@@ -9,6 +9,8 @@ import {
   useProps,
   useStyles,
 } from '@mantine/core';
+import { DayOfWeek } from '@mantine/dates-utils';
+import { DateLabelFormat } from '../../../dates-utils/lib/types';
 import classes from './WeekView.module.css';
 
 export type WeekViewStylesNames = 'weekView';
@@ -22,6 +24,24 @@ export interface WeekViewProps
     ElementProps<'div'> {
   /** Week to display, Date object or date string in `YYYY-MM-DD` format */
   week: Date | string;
+
+  /** Number 0-6, where 0 – Sunday and 6 – Saturday. @default `1` – Monday */
+  firstDayOfWeek?: DayOfWeek;
+
+  /** `dayjs` format for weekdays names. @default `'ddd'` */
+  weekdayFormat?: DateLabelFormat;
+
+  /** Indices of weekend days, 0-6, where 0 is Sunday and 6 is Saturday. The default value is defined by `DatesProvider`. */
+  weekendDays?: DayOfWeek[];
+
+  /** If set to false, weekend days will not be displayed @default `true` */
+  withWeekendDays?: boolean;
+
+  /** `weekday` – highlights today in the weekday row, `column` – highlights today in the entire column @default `'column'` */
+  highlightToday?: 'weekday' | 'column' | false;
+
+  /** If set, displays a line indicating the current time @default `true` */
+  withCurrentTimeLine?: boolean;
 }
 
 export type WeekViewFactory = Factory<{
@@ -31,7 +51,11 @@ export type WeekViewFactory = Factory<{
   vars: WeekViewCssVariables;
 }>;
 
-const defaultProps = {} satisfies Partial<WeekViewProps>;
+const defaultProps = {
+  withWeekendDays: true,
+  highlightToday: 'column',
+  withCurrentTimeLine: true,
+} satisfies Partial<WeekViewProps>;
 
 const varsResolver = createVarsResolver<WeekViewFactory>(() => ({
   weekView: {
