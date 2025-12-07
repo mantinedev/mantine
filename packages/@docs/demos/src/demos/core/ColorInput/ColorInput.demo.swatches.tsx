69efed6b1ad8a2a80444dd230b8239e32798cdcb
Vitaly Rtishchev <rtivital@gmail.com>
2025-12-07 19:08:05 +0300

    [mantine.dev] Improve ColorInput swatches demos

--- packages/@docs/demos/src/demos/core/ColorInput/ColorInput.demo.swatches.tsx
+++ packages/@docs/demos/src/demos/core/ColorInput/ColorInput.demo.swatches.tsx
@@ -1,4 +1,4 @@
-import { ColorInput, DEFAULT_THEME } from '@mantine/core';
+import { ColorInput } from '@mantine/core';
 import { MantineDemo } from '@mantinex/demo';
 
 const code = `
@@ -8,9 +8,7 @@ function Demo() {
   return (
     <ColorInput
       format="hex"
-      swatches={[${Object.keys(DEFAULT_THEME.colors)
-        .map((color) => `'${DEFAULT_THEME.colors[color][6]}'`)
-        .join(', ')}]}
+      swatches={['#2e2e2e', '#868e96', '#fa5252', '#e64980', '#be4bdb', '#7950f2', '#4c6ef5', '#228be6', '#15aabf', '#12b886', '#40c057', '#82c91e', '#fab005', '#fd7e14']}
     />
   );
 }
@@ -23,7 +21,22 @@ function Demo() {
       mx="auto"
       placeholder="Pick color"
       label="Your favorite color"
-      swatches={Object.keys(DEFAULT_THEME.colors).map((color) => DEFAULT_THEME.colors[color][6])}
+      swatches={[
+        '#2e2e2e',
+        '#868e96',
+        '#fa5252',
+        '#e64980',
+        '#be4bdb',
+        '#7950f2',
+        '#4c6ef5',
+        '#228be6',
+        '#15aabf',
+        '#12b886',
+        '#40c057',
+        '#82c91e',
+        '#fab005',
+        '#fd7e14',
+      ]}
     />
   );
 }
