62a99c0b7b36ff0530335d37eacf8536a8df410e
sungho <84187086+Hosung99@users.noreply.github.com>
2026-01-09 22:49:20 +0900

    [@mantine/core] ScrollArea: Fix incorrect overscroll behavior when `scrollbar` explicitly set on one side (#8591)
    Co-authored-by: 손성호 <sonseongho@sonseonghoui-MacBookAir.local>

--- packages/@mantine/core/src/components/ScrollArea/ScrollArea.tsx
+++ packages/@mantine/core/src/components/ScrollArea/ScrollArea.tsx
@@ -105,12 +105,24 @@ const defaultProps = {
 } satisfies Partial<ScrollAreaProps>;
 
 const varsResolver = createVarsResolver<ScrollAreaFactory>(
-  (_, { scrollbarSize, overscrollBehavior }) => ({
-    root: {
-      '--scrollarea-scrollbar-size': rem(scrollbarSize),
-      '--scrollarea-over-scroll-behavior': overscrollBehavior,
-    },
-  })
+  (_, { scrollbarSize, overscrollBehavior, scrollbars }) => {
+    let overrideOverscrollBehavior = overscrollBehavior;
+
+    if (overscrollBehavior && scrollbars) {
+      if (scrollbars === 'x') {
+        overrideOverscrollBehavior = `${overscrollBehavior} auto`;
+      } else if (scrollbars === 'y') {
+        overrideOverscrollBehavior = `auto ${overscrollBehavior}`;
+      }
+    }
+
+    return {
+      root: {
+        '--scrollarea-scrollbar-size': rem(scrollbarSize),
+        '--scrollarea-over-scroll-behavior': overrideOverscrollBehavior,
+      },
+    };
+  }
 );
 
 export const ScrollArea = factory<ScrollAreaFactory>((_props, ref) => {
