8cc2bcb725c49ce4896da7b4f567117500b0718f
Vitaly Rtishchev <rtivital@gmail.com>
2026-01-09 16:42:55 +0300

    [@mantine/core] AppShell: Add static mode support for nested app shells

--- apps/mantine.dev/src/app-shell-examples/examples/index.ts
+++ apps/mantine.dev/src/app-shell-examples/examples/index.ts
@@ -15,10 +15,14 @@ import MobileNavbarCode from './MobileNavbar/code.json';
 import { MobileNavbar } from './MobileNavbar/MobileNavbar';
 import NavbarSectionCode from './NavbarSection/code.json';
 import { NavbarSection } from './NavbarSection/NavbarSection';
+import { NestedAppShell } from './NestedAppShell/NestedAppShell';
+import NestedAppShellCode from './NestedAppShell/code.json';
 import NoTransitionsCode from './NoTransitions/code.json';
 import { NoTransitions } from './NoTransitions/NoTransitions';
 import ResponsiveSizesCode from './ResponsiveSizes/code.json';
 import { ResponsiveSizes } from './ResponsiveSizes/ResponsiveSizes';
+import { StaticMode } from './StaticMode/StaticMode';
+import StaticModeCode from './StaticMode/code.json';
 
 interface AppShellExampleComponent {
   component: () => React.JSX.Element;
@@ -69,6 +73,14 @@ export const APP_SHELL_EXAMPLES_COMPONENTS: Record<string, AppShellExampleCompon
   NavbarSection: {
     component: NavbarSection,
     code: NavbarSectionCode as AppShellExampleComponent['code'],
+  },
+  StaticMode: {
+    component: StaticMode,
+    code: StaticModeCode as AppShellExampleComponent['code'],
+  },
+  NestedAppShell: {
+    component: NestedAppShell,
+    code: NestedAppShellCode as AppShellExampleComponent['code'],
   },
 };
 
