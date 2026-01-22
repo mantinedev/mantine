79eb08ff870974c1da86f21c7ef4ca59c09229bf
Vitaly Rtishchev <rtivital@gmail.com>
2026-01-22 14:19:02 +0300

    [mantine.dev] Add lists support to component props documentation

--- apps/mantine.dev/src/components/HtmlText/HtmlText.tsx
+++ apps/mantine.dev/src/components/HtmlText/HtmlText.tsx
@@ -10,7 +10,11 @@ function replaceMarkdown(str: string): string {
     .replace(/`([^`]+)`/g, '<code>$1</code>')
     .replace(/!important!/g, '<b>Important</b>')
     .replace(/@deprecated/g, '<i>Deprecated:</i>')
-    .replace(/\[([^\]]+)\]\((.*?)\)/g, '<a href="$2" target="_blank" ref="noreferrer">$1</a>');
+    .replace(/\[([^\]]+)\]\((.*?)\)/g, '<a href="$2" target="_blank" ref="noreferrer">$1</a>')
+    .replace(/(?:^|\n)((?:- .+(?:\n|$))+)/g, (_, list) => {
+      const items = list.replace(/(?:^|\n)- (.+)/g, '<li>$1</li>');
+      return `<ul>${items}</ul>`;
+    });
 }
 
 export function HtmlText({ children, ...others }: HtmlTextProps) {
