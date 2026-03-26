8679b63be1f020587969f192147a8b8ba1d396d4
dkpark10 <dkpark10@naver.com>
2026-03-26 12:23:07 +0900

    [@mantine/hooks] use-session-storage: fix defaultValue type inference (#8769)

--- packages/@mantine/hooks/src/use-session-storage/use-session-storage.ts
+++ packages/@mantine/hooks/src/use-session-storage/use-session-storage.ts
@@ -1,4 +1,17 @@
-import { createStorage, readValue, UseStorageOptions } from '../use-local-storage/create-storage';
+import {
+  createStorage,
+  readValue,
+  UseStorageOptions,
+  UseStorageReturnValue,
+} from '../use-local-storage/create-storage';
+
+export function useSessionStorage<T = string>(
+  props: UseStorageOptions<T> & { defaultValue: T }
+): UseStorageReturnValue<T>;
+
+export function useSessionStorage<T = string>(
+  props: Omit<UseStorageOptions<T>, 'defaultValue'> & { defaultValue?: undefined }
+): UseStorageReturnValue<T | undefined>;
 
 export function useSessionStorage<T = string>(props: UseStorageOptions<T>) {
   return createStorage<T>('sessionStorage', 'use-session-storage')(props);
