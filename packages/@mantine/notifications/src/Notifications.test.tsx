efc2a3bf7d1854014d3ca5bd6c2d2fc5cac03bcc
dkpark10 <43857226+dkpark10@users.noreply.github.com>
2026-04-06 20:05:30 +0900

    [@mantine/notifications] Fix `onOpen` prop being spread to DOM (#8807)
    Co-authored-by: Vitaly Rtishchev <rtivital@gmail.com>

--- packages/@mantine/notifications/src/Notifications.test.tsx
+++ packages/@mantine/notifications/src/Notifications.test.tsx
@@ -1,5 +1,7 @@
-import { tests } from '@mantine-tests/core';
+import { act } from '@testing-library/react';
+import { render, screen, tests } from '@mantine-tests/core';
 import { Notifications, NotificationsProps, NotificationsStylesNames } from './Notifications';
+import { notifications } from './notifications.store';
 
 const defaultProps: NotificationsProps = {
   withinPortal: false,
@@ -12,5 +14,44 @@ describe('@mantine/core/Notifications', () => {
     varsResolver: true,
     displayName: '@mantine/notifications/Notifications',
     stylesApiSelectors: ['root'],
+  });
+
+  it('calls onOpen when notification is mounted', async () => {
+    const onOpen = jest.fn();
+    const consoleError = jest.spyOn(console, 'error');
+
+    render(<Notifications />);
+    act(() => {
+      notifications.show({
+        message: 'open test',
+        onOpen,
+      });
+    });
+
+    expect(onOpen).toHaveBeenCalledTimes(1);
+    expect(consoleError).not.toHaveBeenCalledWith(
+      expect.stringContaining('Unknown event handler property `onOpen`'),
+      expect.anything()
+    );
+    consoleError.mockRestore();
+  });
+
+  it('calls onClose when notification is hidden', async () => {
+    const onClose = jest.fn();
+    render(<Notifications />);
+
+    let id: string;
+    act(() => {
+      id = notifications.show({
+        message: 'close test',
+        onClose,
+      });
+    });
+
+    expect(screen.getByText('close test')).toBeInTheDocument();
+    act(() => {
+      notifications.hide(id);
+    });
+    expect(onClose).toHaveBeenCalledTimes(1);
   });
 });
