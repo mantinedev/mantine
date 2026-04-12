70b4988801683f2e796a3926449b87bcce800715
Vitaly Rtishchev <rtivital@gmail.com>
2026-04-12 14:48:47 +0300

    [@mantine/hooks] use-move: Fix incorrect events removal handling

--- packages/@mantine/hooks/src/use-move/use-move.ts
+++ packages/@mantine/hooks/src/use-move/use-move.ts
@@ -31,10 +31,14 @@ export function useMove<T extends HTMLElement = any>(
   const mounted = useRef<boolean>(false);
   const isSliding = useRef(false);
   const frame = useRef(0);
+  const cleanupRef = useRef<(() => void) | null>(null);
   const [active, setActive] = useState(false);
 
   useEffect(() => {
     mounted.current = true;
+    return () => {
+      cleanupRef.current?.();
+    };
   }, []);
 
   const refCallback: React.RefCallback<T | null> = useCallback(
@@ -120,13 +124,16 @@ export function useMove<T extends HTMLElement = any>(
       node?.addEventListener('mousedown', onMouseDown);
       node?.addEventListener('touchstart', onTouchStart, { passive: false });
 
+      cleanupRef.current = () => {
+        unbindEvents();
+        cancelAnimationFrame(frame.current);
+      };
+
       return () => {
         if (node) {
           node.removeEventListener('mousedown', onMouseDown);
           node.removeEventListener('touchstart', onTouchStart);
         }
-        unbindEvents();
-        cancelAnimationFrame(frame.current);
       };
     },
     [dir, onChange]
