99abbd798ac47e3593a21a61a1ba402751ec809e
Ahmad Saad <64619851+saadpocalypse@users.noreply.github.com>
2026-07-14 16:53:31 -0500

    [@mantine/core] ColorInput: Pass width="target" to Popover when fullWidth is set

--- packages/@mantine/core/src/components/ColorInput/ColorInput.tsx
+++ packages/@mantine/core/src/components/ColorInput/ColorInput.tsx
@@ -238,6 +238,7 @@ export const ColorInput = factory<ColorInputFactory>((_props) => {
         position="bottom-start"
         offset={5}
         opened={dropdownOpened}
+        width={fullWidth ? 'target' : undefined}
         {...popoverProps}
         classNames={resolvedClassNames}
         styles={resolvedStyles}
