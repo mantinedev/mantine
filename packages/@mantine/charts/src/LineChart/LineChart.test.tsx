47998e25711eff62aed1ca35bc2a5b470402b5d0
Venus <coldsmirk@163.com>
2026-09-06 20:33:26 -0700

    [@mantine/charts] LineChart, CompositeChart, BubbleChart: Fix `textColor` and `gridColor` props being passed as attributes to the DOM node

--- packages/@mantine/charts/src/LineChart/LineChart.test.tsx
+++ packages/@mantine/charts/src/LineChart/LineChart.test.tsx
@@ -1,4 +1,4 @@
-import { autoPatchWarn, tests } from '@mantine-tests/core';
+import { autoPatchWarn, render, tests } from '@mantine-tests/core';
 import { LineChart, LineChartProps, LineChartStylesNames } from './LineChart';
 
 const defaultProps: LineChartProps = {
@@ -20,5 +20,11 @@ describe('@mantine/charts/LineChart', () => {
     varsResolver: true,
     displayName: '@mantine/charts/LineChart',
     stylesApiSelectors: ['root'],
+  });
+
+  it('does not pass textColor and gridColor props to the root element', () => {
+    const { container } = render(<LineChart {...defaultProps} textColor="red" gridColor="blue" />);
+    expect(container.querySelector('[textcolor]')).not.toBeInTheDocument();
+    expect(container.querySelector('[gridcolor]')).not.toBeInTheDocument();
   });
 });
