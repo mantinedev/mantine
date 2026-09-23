0433f9905fe3deda5e7042083633200f03251eb5
Vitaly Rtishchev <rtivital@gmail.com>
2026-09-23 17:48:44 +0300

    Revert "[@mantine/core] Box: Keep STYlE_PROPS_DATA as deprecated alias"
    This reverts commit e19a1e9fb01ad926e1f147d51216307860d5328a.

--- packages/@mantine/core/src/core/Box/style-props/style-props-data.ts
+++ packages/@mantine/core/src/core/Box/style-props/style-props-data.ts
@@ -69,6 +69,3 @@ export const STYLE_PROPS_DATA: Record<keyof MantineStyleProps, SystemPropData> =
   display: { type: 'identity', property: 'display' },
   flex: { type: 'identity', property: 'flex' },
 };
-
-/** @deprecated Use `STYLE_PROPS_DATA` instead */
-export const STYlE_PROPS_DATA = STYLE_PROPS_DATA;
